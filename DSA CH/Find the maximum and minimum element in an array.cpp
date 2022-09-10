269bcd6d3f404fe864674a432b0f2697e2897781
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-10 19:00:54 +0530

    Update and rename New Text Document.txt to Find the maximum and minimum element in an array.cpp

---
+++ DSA CH/Find the maximum and minimum element in an array.cpp
@@ -0,0 +1,70 @@
+
+// https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1
+
+// APPROACH 1
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+pair<long long, long long> getMinMax(long long a[], int n) {
+    ll ma = a[0];
+    ll mi = a[0];
+    // pair<ll,ll> p;
+    
+    for(int i=0; i<n; i++){
+        if(a[i] < mi)
+            mi = a[i];
+    }
+    
+    for(int i=0; i<n; i++){
+        if(a[i] > ma) 
+            ma = a[i];
+    }
+    return {mi,ma} ;
+}
+
+
+// APPROACH 2
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    ll ma = a[0];
+    ll mi = a[0];
+    pair<ll,ll> p;
+    
+    for(int i=0; i<n; i++){
+        ma = max(ma, a[i]);
+        mi = min(mi, a[i]);
+    }
+    
+    p.first = mi, p.second = ma;
+    return p;
+    
+    // return {mi,ma} ;
+}
+
+// APPROACH 3
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    sort(a, a+n);
+    
+    return {a[0], a[n-1]};
+}
