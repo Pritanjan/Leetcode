f34a32455cdb3d4c5c8d8fb70e9256a951d62f46
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-11 00:04:15 +0530

    Update and rename 350. Intersection of Two Arrays II ARRAY HASH_TABLE  POINTER BINARY_SEARCH SORTING.cpp to 350. Intersection of Two Arrays II.cpp

--- 0301 - 0400/350. Intersection of Two Arrays II.cpp
+++ 0301 - 0400/350. Intersection of Two Arrays II.cpp
@@ -4,24 +4,20 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> s;
-        
         for(int i : nums1)
             s[i]++;
         
 //        for(int i = 0; i < nums1.size(); i++)
 //           map[nums1[i]]++;
         
-        
         vector<int> ans;
 		
-//		for(int i = 0; i < nums2.size(); i++)
-//        {  
+//	for(int i = 0; i < nums2.size(); i++){  
 //            if(map[nums2[i]] >= 1) {
 //                ans.push_back(nums2[i]);
 //                map[nums2[i]]--;   
 //            }
 //        }
-//        
 		
         for(int i : nums2){ 
             if(s[i]-- > 0)
@@ -30,3 +26,20 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        nums1.erase(set_intersection( nums1.begin(),nums1.end(), nums2.begin(), nums2.end(),nums1.begin()), nums1.end() );
+        
+        return nums1;
+    }
+};
+
+
+
+
