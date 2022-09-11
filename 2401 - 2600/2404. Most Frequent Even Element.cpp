33a4d13d8f48da5f8aa282f5d4660eb47065359b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-11 10:49:51 +0530

    Update and rename New Text Document.txt to 2404. Most Frequent Even Element.cpp

---
+++ 2401 - 2600/2404. Most Frequent Even Element.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/contest/weekly-contest-310/problems/most-frequent-even-element/
+
+class Solution {
+public:
+    int mostFrequentEven(vector<int>& nums) {
+        vector<int> v;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] % 2 == 0)
+                v.push_back(nums[i]);
+        }
+        
+        sort(v.begin(), v.end());
+        
+        // for(int i=0; i<nums.size(); i++){
+        //     cout<<v[i]<<" ";
+        // }
+        
+        if(v.size() == 0)
+            return -1;
+        
+        int max_freq = 1;
+        int ans = v[0];
+        int cnt = 1;
+        
+        for(int i=1; i<v.size(); i++){
+            if(v[i] == v[i-1])
+                cnt++;
+            else{
+                if (cnt > max_freq) { 
+                    max_freq = cnt; 
+                    ans = v[i - 1]; 
+                } 
+                cnt = 1;
+            }
+        }    
+        if(cnt > max_freq){
+            max_freq = cnt;
+            ans = v[v.size() - 1];
+        }
+        return ans;
+    }
+};
+
