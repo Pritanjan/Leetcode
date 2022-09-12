794275e56aa1d0692dc95d1d2d952cae388da759
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-12 17:15:47 +0530

    Update 45. Jump Game I & II ARRAY DP GREEDY.cpp

--- 0001 - 0100/45. Jump Game I & II ARRAY DP GREEDY.cpp
+++ 0001 - 0100/45. Jump Game I & II ARRAY DP GREEDY.cpp
@@ -1,6 +1,57 @@
 // https://leetcode.com/problems/jump-game/
 // https://leetcode.com/problems/jump-game-ii/
 
+// APPROACH 1
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int mx = 0;
+        int jump = 0;
+        int currentMax = 0;
+        int n = nums.size();
+        for(int i=0; i<n-1; i++){
+            mx = max(mx, nums[i]+i);
+            if(i == currentMax){
+                jump++;
+                currentMax = mx;
+            }
+            if(i >= mx)
+                return -1;
+        }
+        return jump;
+    }
+};
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jump = 0;
+        int L = 0;
+        int R = 0;
+        int n = nums.size() ;
+        while(R < n - 1){
+            jump++;
+            int mx = R + 1;
+            for(int i=L; i<=R; i++){
+                if(i + nums[i] >= n){
+                    return jump;
+                }
+                mx = max(mx,i + nums[i]);
+            }
+            L = R + 1;
+            R = mx;
+        }
+        return jump;
+    }
+};
+
+
+// APPROACH 3
+
 class Solution {
 public:
     int jump(vector<int>& nums) {
@@ -19,6 +70,8 @@ public:
             currentMax = nextMax;
         }
         return 0;
-        
     }
 };
+
+
+
