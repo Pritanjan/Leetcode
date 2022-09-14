d730701baf37d4ce6f45974e1ae2b82ba6150c3f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-14 23:05:35 +0530

    Update 48. Rotate Image ARRAY MATH MATRIX.cpp

--- 0001 - 0100/48. Rotate Image ARRAY MATH MATRIX.cpp
+++ 0001 - 0100/48. Rotate Image ARRAY MATH MATRIX.cpp
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/rotate-image/
 // company tag microsoft amazon
 
+// https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/
+
 // ROTATING 90 DEGREE CLOCKWISE
 
 
