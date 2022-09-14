eeb6854eab5f8d596bd719afbb8e6d5fcd506cd6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-14 12:47:18 +0530

    Update 74. Search a 2D Matrix ARRAY BINARY_SEARCH MATRIX.cpp

--- 0001 - 0100/74. Search a 2D Matrix ARRAY BINARY_SEARCH MATRIX.cpp
+++ 0001 - 0100/74. Search a 2D Matrix ARRAY BINARY_SEARCH MATRIX.cpp
@@ -44,6 +44,7 @@ public:
 
 
 
+// APPROAH Using Bool
 
 class Solution {
 public:
@@ -68,4 +69,28 @@ public:
     }
 };
 
+
+
+
+// APPROAH without Using Bool
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = m ? matrix[0].size() : 0;
+        int i = 0;
+        int j = n - 1;
+        
+        while( i < m && j >=0){
+            if (target == matrix[i][j])
+                return true;
+            else if (target > matrix[i][j] )
+                i++;
+            else
+                j--;        
+        }
+        return false;
+    }
+};
 
