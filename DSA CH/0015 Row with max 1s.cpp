ba7ef7b4f1bc8170bd78cf6169527eeb3130faa5
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-14 19:22:25 +0530

    Update 0015 Row with max 1s.cpp

--- DSA CH/0015 Row with max 1s.cpp
+++ DSA CH/0015 Row with max 1s.cpp
@@ -1,5 +1,15 @@
 // https://practice.geeksforgeeks.org/problems/row-with-max-1s0023/1
 
+// SEE EDITORIAL
+
+APPROACH 1
+Explanation
+	1. A simple method is to do a row-wise traversal of the matrix, 
+	2. count the number of 1s in each row, and compare the count with max.
+	3. Finally, return the index of the row with maximum 1s. 
+	4. The time complexity of this method is O(m*n) and space complexity O(1)
+	 where m is the number of rows and n is the number of columns in the matrix.
+
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
@@ -58,3 +68,80 @@ int main() {
 // } Driver Code Ends
 
 
+
+
+APPROACH 2
+Explanation
+    1. Since each row is sorted, we can use Binary Search to count of 1s in each row.
+    2. We find the index of first instance of 1 in each row. 
+    3. The count of 1s will be equal to total number of columns minus the index of first 1.
+
+	
+// Function to find the index of first instance of 1 in a boolean array arr[] 
+int first(bool arr[], int low, int high) 
+{ 
+    if(high >= low) 
+    { 
+        // Get the middle index 
+        int mid = low + (high - low)/2; 
+    
+        // Check if the element at middle index is first 1 
+        if ( ( mid == 0 || arr[mid-1] == 0) && arr[mid] == 1) 
+            return mid; 
+    
+        // If the element is 0, recur for right side 
+        else if (arr[mid] == 0) 
+            return first(arr, (mid + 1), high); 
+        
+        // If element is not first 1, recur for left side 
+        else
+            return first(arr, low, (mid -1)); 
+    } 
+    return -1; 
+} 
+
+// Function that returns index of row with maximum number of 1s. 
+int rowWithMax1s(bool mat[R][C]) 
+{ 
+    // Initialize max values 
+    int max_row_index = 0, max = -1; 
+
+    // Traverse for each row and count number of 1s 
+    // by finding the index of first 1 
+    int i, index; 
+    for (i = 0; i < R; i++) 
+    { 
+        index = first (mat[i], 0, C-1); 
+        if (index != -1 && C-index > max) 
+        { 
+            max = C - index; 
+            max_row_index = i; 
+        } 
+    } 
+
+    return max_row_index; 
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
