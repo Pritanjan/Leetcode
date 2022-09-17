5408e246783bb1689f002cd4a0491b1b584631ea
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-17 18:34:05 +0530

    Update 169. Majority Element ARRAY HASH_TABLE DIVIDE_AND_CONQUER SORTING COUNTING.cpp

--- 0101 - 0200/169. Majority Element ARRAY HASH_TABLE DIVIDE_AND_CONQUER SORTING COUNTING.cpp
+++ 0101 - 0200/169. Majority Element ARRAY HASH_TABLE DIVIDE_AND_CONQUER SORTING COUNTING.cpp
@@ -1,15 +1,10 @@
 // https://leetcode.com/problems/majority-element/
 
+APPROACH 1
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-        // APPROACH 1
-        // int n = nums.size();
-        // sort(nums.begin(), nums.end());
-        // return nums[n/2];
-        
-        // APPROACH 2
         int maj = 0;
         int count = 0;
         
@@ -27,3 +22,18 @@ public:
         return maj;
     }
 };
+
+
+APPROACH 2
+
+    class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        return nums[n/2];
+    }
+};
+
+
+
