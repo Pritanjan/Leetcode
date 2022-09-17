f2b00565681d04768cf907e098f99519f46d6c08
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-17 19:22:33 +0530

    Update 229. Majority Element II  ARRAY HASH_TABLE SORTING COUNTING.cpp

--- 0201 - 0300/229. Majority Element II  ARRAY HASH_TABLE SORTING COUNTING.cpp
+++ 0201 - 0300/229. Majority Element II  ARRAY HASH_TABLE SORTING COUNTING.cpp
@@ -1,4 +1,32 @@
 // https://leetcode.com/problems/majority-element-ii/
+// https://leetcode.com/problems/majority-element/
+// Similar question LC 169
+// https://github.com/Pritanjan/Leetcode/edit/main/0101%20-%200200/169.%20Majority%20Element%20ARRAY%20HASH_TABLE%20DIVIDE_AND_CONQUER%20SORTING%20COUNTING.cpp
+
+// APPROACH 1
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        // sort(nums.begin(), nums.end());
+        
+        vector<int> v;
+        map<int, int> um;
+        
+        for(auto i : nums)
+            um[i]++;
+        
+        for(auto it : um)
+            if(it.second > n/3){
+                v.push_back(it.first);
+            }
+        return v;
+        
+        
+    }
+};
+
+// APPROACH 2
 
 class Solution {
 public:
@@ -8,20 +36,18 @@ public:
         
         vector<int> v;
         unordered_map<int, int> um;
-//        map<int, int> um;  //BOTH WORKS
+//         map<int, int> um;  //BOTH WORKS
         
         for(auto i : nums)
             um[i]++;
         
 //        for(int i=0; i<n; i++)
-//        	um[nums[i]]++:			// same as line 13 & 14
+//        	um[nums[i]]++:			// same as line 41 & 42
         
         for(auto it : um)
             if(it.second > n/3){
                 v.push_back(it.first);
             }
         return v;
-        
-        
     }
 };
