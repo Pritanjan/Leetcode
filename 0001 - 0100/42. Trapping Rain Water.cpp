2b2c5e1b42d032125b58699398cb7fd543b8b13e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-18 20:16:19 +0530

    Create 42. Trapping Rain Water.cpp

---
+++ 0001 - 0100/42. Trapping Rain Water.cpp
@@ -0,0 +1,69 @@
+// https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int L = 0;
+        int R = h.size()-1;
+        int ans = 0;
+        int left_max = 0;
+        int right_max = 0;
+        
+        while(L < R){
+            if(h[L] < h[R]){
+                if(h[L] >= left_max)
+                    left_max = h[L];
+                else
+                    ans += left_max - h[L];
+            L++;
+            }
+            else{
+                if(h[R] >= right_max)
+                    right_max = h[R];
+                else
+                    ans += right_max - h[R];
+            R--;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int L = 0;
+        int R = h.size()-1;
+        int ans = 0;
+        int left_max = 0;
+        int right_max = 0;
+        
+        while(L < R){
+            left_max = max(left_max, h[L]);
+            right_max = max(right_max, h[R]);
+            
+            if(left_max < right_max){
+                ans += left_max - h[L];
+                L++;
+            }
+            else{
+                ans += right_max - h[R];
+                R--;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
