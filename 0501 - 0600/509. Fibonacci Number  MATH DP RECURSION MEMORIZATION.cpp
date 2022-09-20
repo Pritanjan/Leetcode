3cefc48c6c398b29267be0e1dd8c9474ffc91a15
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-20 22:51:42 +0530

    Update 509. Fibonacci Number  MATH DP RECURSION MEMORIZATION.cpp

--- 0501 - 0600/509. Fibonacci Number  MATH DP RECURSION MEMORIZATION.cpp
+++ 0501 - 0600/509. Fibonacci Number  MATH DP RECURSION MEMORIZATION.cpp
@@ -1,2 +1,83 @@
-//https://leetcode.com/problems/fibonacci-number/discuss/2243893/6-c-2-solution-easy-solution-6
+// https://leetcode.com/problems/fibonacci-number/discuss/2243893/6-c-2-solution-easy-solution-6
+
+Fibonacci Number -> It is a series in which a number is sum of 2 previous number.
+ 0,1,1,2,3,5,8,13......
+ 
+[509. Fibonacci Number](https://leetcode.com/problems/fibonacci-number/)
+	
+**METHOD 1 -> RESURSIVE**  
+Using mathmatical recursive implementation .
+
+	class Solution {
+	public:
+		int fib(int n) {
+			if(n==0)        // base case
+				return 0;
+			else if(n==1)   // base case
+				return 1;
+			else{
+				return fib(n-1) + fib(n-2); 
+			}
+			// return fib(n)
+		}
+	};
+// T.C. --> exponential (as every function calls 2 other functions)
+If the original recursion tree were 
+
+							  fib(5)   
+						 /                \
+				   fib(4)                fib(3)   
+				 /        \              /       \ 
+			 fib(3)      fib(2)         fib(2)   fib(1)
+			/    \       /    \        /      \
+	  fib(2)   fib(1)  fib(1) fib(0) fib(1) fib(0)
+	  /     \
+	fib(1) fib(0)
+
+<hr>
+<br>
+
+
+**METHOD 2 -> DP**
+Avoiding the repeated work done in approach 1 by storing the Fibonacci number we calculated so far.
+
+	class Solution {
+	public:
+		int fib(int n) {
+		    // Declare an array to store Fibonacci numbers.
+			int F[n+1];         
+			F[0] = 0;
+			F[1] = 1;
+        
+			for(int i=2; i<=n; i++){
+				// Add the previous to no to get the next number.
+				F[i] = F[i-1] + F[i-2];
+			}
+			return F[n];
+		}
+	};
+	
+<br>
+
+**METHOD 3 (Space Optimized Method 2)**
+We can optimize the space used in method 2 by storing the previous two numbers only because that is all we need to get the next Fibonacci number in series.
+
+	class Solution {
+	public:
+		int fib(int n) {
+			int i, a = 0, b = 1, c = i;
+			if(n==0)
+				return 0;
+        
+			for(i=2; i<=n; i++){
+				c = a + b;
+				a = b;
+				b = c;
+			}
+			return b;
+		}
+	};
+
+T.C. -> O(n)
+S.C. -> O(1)
 
