702083adf89c2d9af791bd7fc6bf43b0b4bacf53
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-23 22:36:49 +0530

    Update 387. First Unique Character in a String STRING HASH_TABLE QUEUE COUNTING.cpp

--- 0301 - 0400/387. First Unique Character in a String STRING HASH_TABLE QUEUE COUNTING.cpp
+++ 0301 - 0400/387. First Unique Character in a String STRING HASH_TABLE QUEUE COUNTING.cpp
@@ -55,3 +55,17 @@ public:
 		return -1;
     }
 };
+
+
+// Solution 4
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        for(int i = 0;i<s.size();i++){
+            if(s.find(s[i], s.find(s[i])+1) == string::npos){
+                return i;
+            }
+        }
+        return -1;
+    }  
+};
