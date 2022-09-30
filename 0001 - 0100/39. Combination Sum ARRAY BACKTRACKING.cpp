1d871efbb8ff86722215a7cedfc64c9822a1817f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-09-30 16:46:24 +0530

    Update 39. Combination Sum ARRAY BACKTRACKING.cpp

--- 0001 - 0100/39. Combination Sum ARRAY BACKTRACKING.cpp
+++ 0001 - 0100/39. Combination Sum ARRAY BACKTRACKING.cpp
@@ -18,10 +18,52 @@ public:
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
+        vector<vector<int>> ans;   // it will store stroes all the combiination which we are going to return ultimately
+        vector<int> ds;         // this ds is used in oiur recursion
         findcombination(0, target, candidates, ans, ds);
         return ans;
     }
 }; 
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void findcombination(int i, vector<int>& candidates, int target, vector<int> ds){
+        if(target == 0){
+            ans.push_back(ds);
+            return;
+        }
+        
+        if(target <= 0)
+            return;
+        
+        if(i == candidates.size())
+            return;
+        
+        findcombination(i+1, candidates, target, ds);
+        
+        ds.push_back(candidates[i]);
+        findcombination(i, candidates, target-candidates[i], ds);
+        
+        ds.pop_back();
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> ds;
+        findcombination(0, candidates, target, ds);
+        return ans;
+    }
+};
+
+
+
+
+
+
 
