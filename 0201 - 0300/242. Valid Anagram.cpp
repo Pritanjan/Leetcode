252e5b1bbff3d4a74fd9fd2f462ddef0f911de1b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-11 02:56:23 +0530

    Update and rename 242. Valid Anagram 1 ARRAY HASH_TABLE SORTING.cpp to 242. Valid Anagram.cpp

--- 0201 - 0300/242. Valid Anagram.cpp
+++ 0201 - 0300/242. Valid Anagram.cpp
@@ -45,3 +45,25 @@ public:
 };
 
 
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s1, string s2) {
+        if(s1.size() != s2.size())
+            return false;
+        
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+        
+        for(int i=0; i<s2.size(); i++){
+            if(s1[i] !=s2[i])
+                return false;
+        }
+        return true;
+    }
+};
+
+
+
