9f8a4d5034a7f77257e241aac3c00dbff7eceece
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-11 03:05:21 +0530

    Update 242. Valid Anagram.cpp

--- 0201 - 0300/242. Valid Anagram.cpp
+++ 0201 - 0300/242. Valid Anagram.cpp
@@ -1,52 +1,6 @@
 // https://leetcode.com/problems/valid-anagram/
 // COMPANY TAG BLOOMBERG MICROSOFT FB
 
-// unordered map
-//This idea uses a hash table to record the times of appearances of each 
-//letter in the two strings s and t. For each letter in s, it increases
-//the counter by 1 while for each letter in t, it decreases the counter 
-//by 1. Finally, all the counters will be 0 if they two are anagrams of
-//each other.
-
-// 2nd approach
-//problem statement says that "the string contains only lowercase 
-//alphabets", we can simply use an array to simulate the unordered_map
-//and speed up the code
-
-class Solution {
-public:
-    bool isAnagram(string s1, string s2) {
-        if(s1.size() != s2.size())
-            return false;
-        
-        // unordered_map <char, int> um;
-        // for(int i=0; i<s1.size(); i++){
-        //     um[s1[i]]++;
-        //     um[s2[i]]--;
-        // }
-        
-        int um[26] = {0};
-        for(int i=0; i<s1.size(); i++){
-            um[s1[i] - 'a']++;
-            um[s2[i] - 'a']--;
-        }
-        
-//         for(auto i : um){
-//             if(i.second)
-//                 return false;
-//         }
-        
-        for(int i=0; i<26; i++){
-            if(um[i])
-                return false;
-        }
-        return true;
-    }
-};
-
-
-
-
 
 class Solution {
 public:
@@ -65,5 +19,71 @@ public:
     }
 };
 
+
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        return s == t;
+    }
+};
+
+
+// unordered map
+//This idea uses a hash table to record the times of appearances of each 
+//letter in the two strings s and t. For each letter in s, it increases
+//the counter by 1 while for each letter in t, it decreases the counter 
+//by 1. Finally, all the counters will be 0 if they two are anagrams of
+//each other.
+
+//problem statement says that "the string contains only lowercase 
+//alphabets", we can simply use an array to simulate the unordered_map
+//and speed up the code
+
+
+class Solution {
+public:
+    bool isAnagram(string s1, string s2) {
+        if(s1.size() != s2.size())
+            return false;
+        
+        unordered_map <char, int> um;
+        for(int i=0; i<s1.size(); i++){
+            um[s1[i]]++;
+            um[s2[i]]--;
+        }
+        
+        for(auto i : um){
+            if(i.second)
+                return false;
+        }
+        return true;
+    }
+};
+
+
+class Solution {
+public:
+    bool isAnagram(string s1, string s2) {
+        if(s1.size() != s2.size())
+            return false;
+        
+        int um[26] = {0};
+        for(int i=0; i<s1.size(); i++){
+            um[s1[i] - 'a']++;
+            um[s2[i] - 'a']--;
+        }
+        
+        for(int i=0; i<26; i++){
+            if(um[i])
+                return false;
+        }
+        return true;
+    }
+};
 
 
