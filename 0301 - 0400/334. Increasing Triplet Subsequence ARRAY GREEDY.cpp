488a5bbeb1606a53f39d96c31e23ef4e3af21772
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-11 06:35:19 +0530

    Update 334. Increasing Triplet Subsequence ARRAY GREEDY.cpp

--- 0301 - 0400/334. Increasing Triplet Subsequence ARRAY GREEDY.cpp
+++ 0301 - 0400/334. Increasing Triplet Subsequence ARRAY GREEDY.cpp
@@ -8,12 +8,17 @@ public:
         
         for(auto i : nums){
             if(i <= c1)
-                c1 = i ;
-            else if(i <= c2)
-                c2 = i;
-            else 
-                return true;
+                c1 = i ;        // c1 is min seen so far (it's a candidate for 1st element)
+            else if(i <= c2)	// here when i > c1, i.e. i might be either c2 or c3
+                c2 = i;			// i is better than the current c2, store it
+            else 				// here when we have/had c1 < c2 already and i > c2
+                return true;	// the increasing subsequence of 3 elements exists
         }
         return false;
     }
 };
+
+
+
+
+
