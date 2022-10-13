32aae9c23cad5705557969d27eaf5b4bf55d829d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-13 12:17:03 +0530

    Update 1679. Max Number of K-Sum Pairs 1 ARRAY HASH_TABLE SORTING 2POINTER.cpp

--- 1601 - 1800/1679. Max Number of K-Sum Pairs 1 ARRAY HASH_TABLE SORTING 2POINTER.cpp
+++ 1601 - 1800/1679. Max Number of K-Sum Pairs 1 ARRAY HASH_TABLE SORTING 2POINTER.cpp
@@ -1,5 +1,36 @@
 //https://leetcode.com/problems/max-number-of-k-sum-pairs/
 
+// Approach 1: Brute force approach Time complexty is o(logn) space complexty is O(1)
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int L = 0;
+        int R = nums.size() - 1;
+        int count = 0;
+        
+        while(L < R){
+            if(nums[L] + nums[R] == k){
+                count++;
+                L++;
+                R--;
+            }
+            if(nums[L] + nums[R] > k){
+                R--;
+            }
+            if(nums[L] + nums[R] < k){
+                L++;
+            }
+                
+        }
+        return count;
+    }  
+};
+
+
+// Approach 2:Optimize approach Time complexty is O(n) space complexty is O(n) because we are using map
+
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
@@ -9,10 +40,10 @@ public:
         for(int i=0; i<nums.size(); i++){
             if(um[k - nums[i]] > 0){
                 um[k - nums[i]]--;    // as we have used one pair so we are deleting it
-                count++;            //if element found in hashmap increase count 
+                count++;              // if element found in hashmap increase count 
             }
             else
-                um[nums[i]]++;  //if element is not present in map then add the current element
+                um[nums[i]]++;        // if element is not present in map then add the current element
         }
         return count;
     }
