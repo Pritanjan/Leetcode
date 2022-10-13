25ae7cb9e2618b2c070a736782af9c604ed90773
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-13 11:54:27 +0530

    Update and rename 203. Remove Linked List Elements 1 LINKED_LIST RECURSION.cpp to 203. Remove Linked List Elements.cpp

--- 0201 - 0300/203. Remove Linked List Elements.cpp
+++ 0201 - 0300/203. Remove Linked List Elements.cpp
@@ -19,3 +19,22 @@ public:
         return head;
     }
 };
+
+
+
+
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL)
+            return NULL;
+        
+        head->next = removeElements(head->next, val);
+        return head->val == val ? head->next : head;
+    }
+};
+
+
+
+
