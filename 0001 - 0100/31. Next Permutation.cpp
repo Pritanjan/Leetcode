b6386068ce3a2aa6e6bd358925c016a98d996d89
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-14 20:05:42 +0530

    Update and rename 31. Next Permutation array 2 pointer.cpp to 31. Next Permutation.cpp

--- 0001 - 0100/31. Next Permutation.cpp
+++ 0001 - 0100/31. Next Permutation.cpp
@@ -39,3 +39,31 @@ public:
 };
 
 
+// 2ND QUESTION PRINTING NEXT PERMUTATION OF A GIVEN NUMBER
+// https://www.codingninjas.com/codestudio/problems/bob-homework_2656187?topList=amazon-sde-challenge
+
+#include<bits/stdc++.h>
+
+void nextPermutation(string &nums){
+    int i;
+    for(i=nums.size()-2;i>=0;i--){
+        if(nums[i]<nums[i+1])
+            break;
+    }
+       int j=nums.size()-1;
+       if(i>=0){
+       while(nums[j]<=nums[i])j--;
+       
+         swap(nums[j],nums[i]);
+       }
+    
+    reverse(nums.begin()+i+1,nums.end());
+}
+long long int bobsHomework(int n)
+{
+    // Write your code here. 
+    string num=to_string(n);
+    nextPermutation(num);
+    size_t ans=stoll(num);
+    return (ans<=n)?-1:ans;
+}
