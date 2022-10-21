f2b91056101ce1cc3117637d027177465e975108
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-21 19:06:27 +0530

    Update 1299. Replace Elements with Greatest Element on Right Side.cpp

--- 1201 - 1400/1299. Replace Elements with Greatest Element on Right Side.cpp
+++ 1201 - 1400/1299. Replace Elements with Greatest Element on Right Side.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& a) {
         int n = a.size();
-          vector<int> ans;
         int mx = a[n-1];
+        
+        vector<int> ans;
         ans.push_back(-1);
+        
         for(int i=n-2;i>=0;i--){
           ans.push_back(mx);
-            if(a[i]>=mx){
-               
+            if(a[i]>=mx){       
                 mx = a[i];
             }
-
-            
         }
+        
         reverse(ans.begin(),ans.end());
         return ans;
     }
