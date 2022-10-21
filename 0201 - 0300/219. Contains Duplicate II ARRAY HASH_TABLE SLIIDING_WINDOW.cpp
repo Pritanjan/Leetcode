871f2c20b1d72c0c5a25ffb0aacd6e58a63c8759
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-21 16:07:39 +0530

    Update 219. Contains Duplicate II ARRAY HASH_TABLE SLIIDING_WINDOW.cpp

--- 0201 - 0300/219. Contains Duplicate II ARRAY HASH_TABLE SLIIDING_WINDOW.cpp
+++ 0201 - 0300/219. Contains Duplicate II ARRAY HASH_TABLE SLIIDING_WINDOW.cpp
@@ -22,3 +22,21 @@ public:
 
 // The basic idea is to maintain a set s which contain unique values from nums[i - k] to nums[i - 1],
 // if nums[i] is in set s then return true else update the set.
+
+
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map <int, int> ump;
+        for(int i=0; i<nums.size(); i++){
+            if(ump.count(nums[i]) == 0)
+                ump[nums[i]] = i;
+            else if (i - ump[nums[i]] <= k)
+                return true;
+            else
+                ump[nums[i]] = i;
+        }
+        return false;
+    }
+};
