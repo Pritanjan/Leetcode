99ccabb7846458801d53be277eb3dab59e9f386f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-24 22:03:50 +0530

    Create 78. Subsets.cpp

---
+++ 0001 - 0100/78. Subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    void f(int idx, vector<int>& arr, vector<int>& ds, vector<vector<int>> &ans){
+        ans.push_back(ds);
+        for(int i=idx; i<arr.size(); i++){
+            ds.push_back(arr[i]);
+            f(i+1, arr, ds, ans);            
+            ds.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        f(0, arr, ds, ans);
+        return ans;
+    }
+};
+
+
