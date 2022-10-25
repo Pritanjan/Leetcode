c86ef304fee2477aded3b4b002ae8a6064b1405e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-25 19:39:03 +0530

    Update 90. Subsets II.cpp

--- 0001 - 0100/90. Subsets II.cpp
+++ 0001 - 0100/90. Subsets II.cpp
@@ -1,21 +1,23 @@
 class Solution {
 private:
     void f(int idx, vector<int>& arr, vector<int>& ds, vector<vector<int>> &ans){
-        ans.push_back(ds);
+        ans.push_back(ds);      // to print empty set
         for(int i=idx; i<arr.size(); i++){
-            if(i != idx and arr[i] == arr[i-1])
+            if(i != idx and arr[i] == arr[i-1])   // if duplicate element if found then ignore it 
                 continue;
-            ds.push_back(arr[i]);
-            f(i+1, arr, ds, ans);            
-            ds.pop_back();
+            ds.push_back(arr[i]);   // else push the element into the ds 
+            f(i+1, arr, ds, ans);   // recursive call the function
+            ds.pop_back();          // remove the first element form the top of the array
         }
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& arr) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        sort(arr.begin(), arr.end());
-        f(0, arr, ds, ans);
+        vector<vector<int>> ans;    // to store all the required pair 
+        vector<int> ds;             // creating the pair
+        
+        sort(arr.begin(), arr.end());   // sorting the array as we can ignore the element the duplicate the pair
+        f(0, arr, ds, ans);         
+        
         return ans;
     }
 };
