1f27a9b7f9660bd74569d8c3b08a9195f923d9eb
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-10-25 19:47:49 +0530

    Update 90. Subsets II.cpp

--- 0001 - 0100/90. Subsets II.cpp
+++ 0001 - 0100/90. Subsets II.cpp
@@ -1,3 +1,5 @@
+// APPROACH 1 take u forward
+
 class Solution {
 private:
     void f(int idx, vector<int>& arr, vector<int>& ds, vector<vector<int>> &ans){
@@ -10,6 +12,47 @@ private:
             ds.pop_back();          // remove the first element form the top of the array
         }
     }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& arr) {
+        vector<vector<int>> ans;    // to store all the required pair 
+        vector<int> ds;             // creating the pair
+        
+        sort(arr.begin(), arr.end());   // sorting the array as we can ignore the element the duplicate the pair
+        f(0, arr, ds, ans);         
+        
+        return ans;
+    }
+};
+
+
+
+
+
+// APPROACH 2
+
+// take/not_take approach.
+
+// If take nums[i],                     then go to index = i+1
+// If not take nums[i],                 then go to index = upper_bound(nums[i])
+                    
+                    
+class Solution {
+private:
+    void f(int idx, vector<int>& arr, vector<int>& ds, vector<vector<int>> &ans){
+        if (idx == arr.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        ds.push_back(arr[idx]);   
+        f(idx+1, arr, ds, ans);   
+        ds.pop_back();          
+        
+        int nextIdx = upper_bound(arr.begin(), arr.end(), arr[idx]) - arr.begin();
+        
+        f(nextIdx, arr, ds, ans);
+    }
+    
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& arr) {
         vector<vector<int>> ans;    // to store all the required pair 
