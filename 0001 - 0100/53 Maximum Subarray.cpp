47287c4bc10deee5b0467e86d8a430261d3db19a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-02 19:31:29 +0530

    Update 53 Maximum Subarray.cpp

--- 0001 - 0100/53 Maximum Subarray.cpp
+++ 0001 - 0100/53 Maximum Subarray.cpp
@@ -4,22 +4,17 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
        int sum=0, ma = nums[0] ;
-	   for(int i=0;i<nums.size();i++){
+	   for(int i=0; i<nums.size(); i++){
             // step 1 add new elements
-			sum+=nums[i];
+			sum += nums[i];
            
             // step 1 add new elements
-			ma=max(ma,sum);
+			ma = max(ma, sum);
 			
             // step 3 check sum 
-			if(sum<0)
-				sum=0;
-				
+			if(sum < 0)
+				sum = 0;
 	   }
 	   return ma;
-	    
     }
 };
-
-
-
