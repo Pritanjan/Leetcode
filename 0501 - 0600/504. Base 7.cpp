f83c48e90db05a9e25e105972b39116422687047
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-04 19:12:19 +0530

    Update 504. Base 7.cpp

--- 0501 - 0600/504. Base 7.cpp
+++ 0501 - 0600/504. Base 7.cpp
@@ -12,4 +12,9 @@ public:
 
 
 
-
+class Solution {
+public:
+    string convertToBase7(int num) {
+        return num/7 == 0 ? to_string(num) : convertToBase7(num/7) + to_string(abs(num)%7);
+    }
+};
