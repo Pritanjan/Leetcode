19549510d04908cc5d01ad57aa4a1ca7a4610b7a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-04 09:51:10 +0530

    Update and rename 345. Reverse Vowels of a String1  2POINTER STRING.cpp to 345. Reverse Vowels of a String.cpp

--- 0301 - 0400/345. Reverse Vowels of a String.cpp
+++ 0301 - 0400/345. Reverse Vowels of a String.cpp
@@ -27,3 +27,27 @@ public:
         return s;
     }
 };
+
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<int> vowel;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' || s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' )
+            vowel.push_back(i);
+        }
+        
+        int R = vowel.size()-1;
+        int L = 0;
+        
+        while(L < R ){
+            // if(s[])
+            swap(s[vowel[L]] , s[vowel[R]]);
+                L++;
+                R--;
+        }
+        return s;
+        
+    }
+};
