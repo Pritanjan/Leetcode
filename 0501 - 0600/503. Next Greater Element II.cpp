86f2c1e8698b360d87d98677ed225c5a50b290cc
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-07 19:38:04 +0530

    Update 503. Next Greater Element II.cpp

--- 0501 - 0600/503. Next Greater Element II.cpp
+++ 0501 - 0600/503. Next Greater Element II.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& arr){
-        int n = arr.size();
-        stack<int> st;
-        st.push(-1);
-        vector<int> ans(n);
-    
-        for(int i=n-1; i>=0; i--){
-            while(st.top() != -1 and st.top() <= arr[i]){
-                st.pop();
+    vector<int> nextGreaterElements(vector<int>& A) {
+        int n = A.size();
+        vector<int> v(n);
+        for(int i=0; i<n; i++){
+            v[i]  = -1;
+            for(int j=1; j<n; j++){
+                if(A[i] < A[(i+j) % n]){
+                    v[i] = A[(i+j) % n];
+                    // v.push_back(A[j]);
+                    break;
+                }                  
             }
-            ans[i] = st.top();
-            st.push(arr[i]);
         }
-        return ans;
+        return v;
     }
 };
 
