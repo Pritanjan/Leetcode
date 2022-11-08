d7293c4de37e30a5f9df0d88f07696dee97b1910
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-08 17:24:46 +0530

    Create 1544. Make The String Great.cpp

---
+++ 1401 - 1600/1544. Make The String Great.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int n = s.size();
+        
+        if(s.empty())
+            return s;
+        
+        for(int i=0; i<n-1; i++){
+            if(abs(s[i] - s[i+1]) == 32){
+                return makeGood(s.substr(0, i) + s.substr(i+2));                
+            }
+        }
+        return s;
+    }
+};
+
+
+
+
