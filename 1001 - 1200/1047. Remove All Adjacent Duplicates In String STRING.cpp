441c81eda970c0be1bf17f8930b32a0d28ddce52
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-10 08:57:01 +0530

    Update 1047. Remove All Adjacent Duplicates In String STRING.cpp

--- 1001 - 1200/1047. Remove All Adjacent Duplicates In String STRING.cpp
+++ 1001 - 1200/1047. Remove All Adjacent Duplicates In String STRING.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string ans;  // store ans
-        for(char i : s){	// traverse each element in the string
+        string ans;                     // store ans
+        for(char i : s){	            // traverse each element in the string
             if(ans.size() == 0)			// if string is empty then just push element simply
                 ans.push_back(i);	
             else if(i == ans.back())	//or if the current element is equal to the last element of the string then it's the adjacent one, so delete it.
                 ans.pop_back();
             else	
-                ans.push_back(i);	 //else just push the element simply
+                ans.push_back(i);	    //else just push the element simply
         }	
-        return ans;	 	//return the string
+        return ans;	 	                //return the string
     }
 };
 
@@ -30,3 +30,6 @@ public:
         return s.substr(0,i+1);
     }
 };
+
+
+
