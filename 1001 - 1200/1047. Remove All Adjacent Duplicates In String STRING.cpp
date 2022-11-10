aee3cab4a3b12343f206bbd2aa0b46f8c6ea0ef2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-10 07:10:44 +0530

    Update and rename 1047. Remove All Adjacent Duplicates In String STRING 1 STACK.cpp to 1047. Remove All Adjacent Duplicates In String STRING.cpp

--- 1001 - 1200/1047. Remove All Adjacent Duplicates In String STRING.cpp
+++ 1001 - 1200/1047. Remove All Adjacent Duplicates In String STRING.cpp
@@ -13,3 +13,20 @@ public:
         return ans;	 	//return the string
     }
 };
+
+
+
+
+
+//  use string as a stack.
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i = 0 ;
+        for(int j=1; j<s.length(); j++){
+            i < 0 || s[i] != s[j] ?  s[++i] = s[j] : --i;
+        }
+        return s.substr(0,i+1);
+    }
+};
