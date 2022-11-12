efb51471c2cf38dbb318adef3e1f5334627bd971
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-12 19:41:18 +0530

    Update 234. Palindrome Linked List.cpp

--- 0201 - 0300/234. Palindrome Linked List.cpp
+++ 0201 - 0300/234. Palindrome Linked List.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/palindrome-linked-list/
+// https://practice.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
 
 /**
  * Definition for singly-linked list.
@@ -10,6 +10,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// APPROACH 1
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
@@ -34,5 +37,102 @@ public:
     }
 };
 
+// T.C. --> O(N) As we iterate over whole LL
+// S.C. --> O(N) As we create an array of size N
 
+
+// APPROACH 2
+
+#include<bits/stdc++.h>
+class Solution{
+  private:
+    bool checkPali(vector<int> &v) {
+        int n = v.size();
+        int L = 0;
+        int R = n-1;
+        
+        while(L <= R){
+            if(v[L] != v[R])
+                return 0;
+            
+            L++;
+            R--;
+        }
+        return L;
+    }
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        vector<int> v;
+        Node* temp = head;
+        while(temp != NULL){
+            v.push_back(temp -> data);
+            temp = temp -> next;
+        }
+        return checkPali(v);
+    }
+};
+
+
+// APPROACH 3
+// FIND MID AND COMPARE BOTH PART OF LL
+
+class Solution {
+public:
+    ListNode* getMid(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+        
+        while(fast != NULL and fast -> next != NULL){
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        return slow;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(curr != NULL){
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL or head -> next == NULL)
+            return true;
+        
+        // s1 find mid
+        ListNode* mid = getMid(head);
+        
+        // s2 reverse ll after middle
+        ListNode* temp = mid -> next;
+        mid -> next = reverse(temp);
+     
+        // compare both half
+        ListNode* head1 = head;
+        ListNode* head2 = mid -> next;
+        
+        while(head2 != NULL){
+            if(head1 -> val != head2 -> val)
+                return false;
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+            
+        }
+        
+        return true;
+    }
+};
+
+// t.c. O(N)
+// s.c. O(1)
 
