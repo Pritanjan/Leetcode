02ca90879546f4c3568beb5d90047cc75b67cd5f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-12 15:40:13 +0530

    Update 21. Merge Two Sorted Lists LINKED_LIST RECURSION.cpp

--- 0001 - 0100/21. Merge Two Sorted Lists LINKED_LIST RECURSION.cpp
+++ 0001 - 0100/21. Merge Two Sorted Lists LINKED_LIST RECURSION.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/merge-two-sorted-lists/
+// https://www.codingninjas.com/codestudio/problems/merge-two-sorted-linked-lists_800332?leftPanelTab=0
 // COMPANY TAG FB MICROSOFT AMAZON UBER BLOOMBERG
 
 class Solution {
@@ -26,5 +26,65 @@ public:
         return 0;    
     }
 };
+
+
+
+
+// APPROACH 2
+
+
+class Solution {
+public:
+        ListNode* solve(ListNode* first, ListNode* second) {
+        // if one element is present in the first
+        if(first -> next == NULL){
+            first -> next = second;
+            return first;
+        }
+    
+        ListNode* curr1 = first; 
+        ListNode* next1 = curr1 -> next;
+        ListNode* curr2 = second;
+        ListNode* next2 = curr2 -> next;
+    
+        while(next1 != NULL and curr2 != NULL){
+            if((curr2 -> val >= curr1 -> val) and (curr2 -> val <= next1 -> val)) {
+                // adding node in b/w the first list
+                curr1 -> next = curr2;
+                next2 = curr2 -> next;
+                curr2 -> next = next1; 
+            
+                // update pointer
+                curr1 = curr2;
+                curr2 = next2;
+            }
+            else{
+                curr1 = next1;
+                next1 = next1 -> next;
+            
+                if(next1 == NULL){
+                    curr1 -> next = curr2;
+                    return first;
+                }
+            }   
+        }
+        return first;
+    }
+
+    
+    ListNode* mergeTwoLists(ListNode* first, ListNode* second) {
+        if(first == NULL)
+            return second;
+        if(second == NULL)
+            return first;
+    
+        if(first -> val <= second -> val)
+            return solve(first, second);
+        else
+            return solve(second, first);
+    }
+    
+};
+
 
 
