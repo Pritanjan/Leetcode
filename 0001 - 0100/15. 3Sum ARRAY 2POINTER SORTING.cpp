3752d562991645342f0e1ac2db9a02adc7be7806
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-13 18:04:45 +0530

    Update 15. 3Sum ARRAY 2POINTER SORTING.cpp

--- 0001 - 0100/15. 3Sum ARRAY 2POINTER SORTING.cpp
+++ 0001 - 0100/15. 3Sum ARRAY 2POINTER SORTING.cpp
@@ -1,4 +1,48 @@
-// APPROACH 1
+// https://www.codingninjas.com/codestudio/problems/triplets-with-given-sum_893028?leftPanelTab=2
+
+// APPROACH 1 
+// BRUTE FORCE
+
+#include<set>
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K)  {
+    // Set to keep the track of visited triplets.
+    set<vector<int>>visited;
+    vector<vector<int>>ans;
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                // If we find a valid triplet.
+                if (arr[i] + arr[j] + arr[k] == K) {
+                    vector<int> triplet;
+                    triplet.push_back(arr[i]);
+                    triplet.push_back(arr[j]);
+                    triplet.push_back(arr[k]);
+                    // Sorting the triplet track distinct triplets.
+                    sort(triplet.begin(), triplet.end());
+                    if (visited.find(triplet) == visited.end()) {
+                        ans.push_back(triplet);
+                        visited.insert(triplet);
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+
+// Time Complexity: O(N^3)
+// Space Complexity: O(1)
+
+
+
+
+
+
+
+// APPROACH 2 
+
+// 2 POINTER
 
 class Solution {
 public:
@@ -36,6 +80,9 @@ public:
     }
 };
 
+
+// Time Complexity: O(N^2)
+// Space Complexity: O(1)
 
 
 
@@ -86,5 +133,8 @@ public:
         return answer ;
     }
 };
+
+
+
 
 
