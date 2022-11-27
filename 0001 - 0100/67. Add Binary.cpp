73013e0aa219b477ee4b24fb6481c2d4cf3e1d7e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-27 19:03:26 +0530

    Update 67. Add Binary.cpp

--- 0001 - 0100/67. Add Binary.cpp
+++ 0001 - 0100/67. Add Binary.cpp
@@ -1,3 +1,7 @@
+// https://github.com/Pritanjan/Question/blob/main/POTD/gfg/0020%20271122%20Add%20Binary%20Strings.cpp
+
+//  APPROACH 1
+
 class Solution {
 public:
     string addBinary(string a, string b) {
@@ -24,6 +28,8 @@ public:
 };
 
 
+// OR 
+
 class Solution {
 public:
     string addBinary(string a, string b) {
@@ -47,4 +53,78 @@ public:
         return s;
     }
 };
+
+
+
+// APPROACH 2
+
+class Solution{
+public:	
+	string addBinary(string A, string B) {
+	    if(A.size() > B.size())
+	        return addBinary(B, A);
+	        
+	    int size = B.size() - A.size();
+	    
+	    string s;
+	    for(int i=0; i<size; i++){
+	        s.push_back('0');
+	    }
+	    
+	    A = s + A;
+	    string ans;
+	    char carry = '0';
+	    
+	    for(int i=A.size()-1; i>=0; i--){
+	        if(A[i] == '0' and B[i] == '0'){
+	            if(carry == '1'){
+	                ans.push_back('1');
+	                carry = '0';
+	            }
+	            else{
+	                ans.push_back('0');
+	                carry = '0';
+	            }
+	        }
+	        else if(A[i] == '1' and B[i] == '1'){
+	            if(carry == '1'){
+	                ans.push_back('1');
+	                carry = '1';
+	            }
+	            else{
+	                ans.push_back('0');
+	                carry = '1';
+	            }
+	        }
+	        else if(A[i] != B[i]){
+	            if(carry == '1'){
+	                ans.push_back('0');
+	                carry = '1';
+	            }
+	            else{
+	                ans.push_back('1');
+	                carry = '0';
+	            }
+	        }
+
+	    }
+        	        
+	   if(carry == '1')
+            ans.push_back(carry);
+	            
+	   reverse(ans.begin(), ans.end());
+	   int idx = 0;
+	       
+	   while(idx + 1 < ans.size() and ans[idx] == '0'){
+	       idx++;
+	   }
+	   
+	   return (ans.substr(idx));
+	}
+};
+
+
+
+
+
 
