3e37b3a074348cdf00773e381dc25527ccce318a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-11-30 16:54:09 +0530

    Update 143. Reorder List.cpp

--- 0101 - 0200/143. Reorder List.cpp
+++ 0101 - 0200/143. Reorder List.cpp
@@ -1,13 +1,8 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// https://practice.geeksforgeeks.org/problems/reorder-list/1
+
+// https://www.geeksforgeeks.org/rearrange-a-given-linked-list-in-place/
+
+// APPROACH 1
 class Solution {
 public:
     void reorderList(ListNode* head) {
@@ -41,8 +36,73 @@ public:
 
 
 
+// APPROACH 2
+
+// 1. Split the list into 2 halves. Next reverse the second half.
+// 2. Now, merge the 2 halves while picking elements alternatively from each half.
+
+class Solution {
+public:    
+    void reorderList(struct Node* head) {
+        int r = 0, c;
+        struct Node* temp, *temp1, *temp2, *p;
+        temp = head;
+
+        // if list contains 2 or lesser nodes, no change needed
+        if (head == NULL || head->next == NULL || head->next->next == NULL) return;
+
+        while (temp != NULL) {
+            // finding number of nodes in list
+            r++;
+            temp = temp->next;
+        }
+
+        c = (r + 1) / 2;
+        // c represents mid point
+        
+        temp = head;
+        while (c--) {
+            p = temp;
+            temp = temp->next;
+        }
+
+        p->next = NULL;
+        // dividing the list into 2
+        // temp holds the address to head of second half
+
+        temp1 = NULL;
+        temp2 = temp;
+
+        // reversing the second half
+        while (temp2 != NULL) {
+            p = temp2->next;
+            temp2->next = temp1;
+            temp1 = temp2;
+            temp2 = p;
+        }
+        // second half is now reversed
+
+        p = temp1;
+        // merging the 2 halves by selecting nodes alternatively
+        Node *a, *b;
+        while (p) {
+           a = head->next;
+           b = p->next;
+           
+           head->next=p;
+           p->next=a;
+           
+           head=a;
+           p=b;
+        }
+    }
+};
 
 
+
+// T.C. ->  As we are traversing the list roughly 2N times so the time complexity is O(N),
+//          N is length of the string.
+// S.C. ->  As we are not using any extra space so the space complexity is O(1).
 
 
 
