d4a2806500b502f19d50279f6d758fc86fb66cac
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2022-12-19 12:02:21 +0530

    Update 344. Reverse String.cpp

--- 0301 - 0400/344. Reverse String.cpp
+++ 0301 - 0400/344. Reverse String.cpp
@@ -1,10 +1,39 @@
 // https://leetcode.com/problems/reverse-string/submissions/
 
+// APPROACH 1
 class Solution {
 public:
     string reverseString(string s) {
         reverse(s.begin() , s.end() ); 
         return s;
+    }
+};
+
+
+// APPROACH 2
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left  =  0;
+        int right = s.size() - 1;
+       
+        while(left <= right){
+            swap(s[left], s[right]);
+                left++; right--;
+        }   
+    }
+};
+
+
+
+// APPROACH 3
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i=0;i<s.size()/2;i++){
+            swap(s[i],s[s.size()-1-i]);
+        }
+        // return s;
     }
 };
 
