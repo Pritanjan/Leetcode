f6f131b0fce4e4027e22321db321bb7a5a7f19d6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-01-18 13:20:20 +0530

    Update and rename 142. Linked List Cycle II to 142. Linked List Cycle II.cpp

--- 0101 - 0200/142. Linked List Cycle II.cpp
+++ 0101 - 0200/142. Linked List Cycle II.cpp
@@ -1,3 +1,5 @@
+// APPROACH 1
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -17,6 +19,31 @@ public:
                 return temp;
             ump[temp]++;
             temp = temp -> next;
+        }
+        return NULL;
+    }
+};
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head)
+    {
+        set<ListNode*>s;
+        ListNode* itr = head;
+        s.insert(itr);
+        while(itr != NULL){
+            if(s.find(itr -> next) == s.end()){
+                s.insert(itr -> next);
+            }
+            else{
+                return itr -> next;
+            }
+            itr = itr -> next;
         }
         return NULL;
     }
