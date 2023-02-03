6e30f6e500bbf47405eab2fbf9c208da3f131187
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-03 09:46:37 +0530

    Update 06. Zigzag Conversion STRING.cpp

--- 0001 - 0100/06. Zigzag Conversion STRING.cpp
+++ 0001 - 0100/06. Zigzag Conversion STRING.cpp
@@ -1,3 +1,5 @@
+// APPROACH 1
+
 /*n=numRows
 ?=2n-2    1                           2n-1                         4n-3
 ?=        2                     2n-2  2n                    4n-4   4n-2
@@ -42,6 +44,38 @@ public:
                     ans += s.at(pos);
             }
         }
+        return ans;
+    }
+};
+
+
+
+
+
+
+// APPROACH 1
+
+class Solution {
+public:
+    string convert(string s, int numRow) {
+        
+        if(numRow == 1)
+            return s;
+        
+        vector<string> v(numRow);
+        int row = 0;
+        int inc = -1;
+        
+        for(int i=0; i<s.size(); i++){
+            if(i % (numRow - 1) == 0)
+                inc *= -1;
+            v[row].push_back(s[i]);
+            row += inc;
+        }
+        string ans ;
+        for(const auto& str:v )
+            ans += str;
+        
         return ans;
     }
 };
