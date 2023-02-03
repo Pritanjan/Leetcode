57c27a0ee26a0f1e89a99126683ebf2bcb178b0c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-03 10:40:25 +0530

    Update 06. Zigzag Conversion.cpp

--- 0001 - 0100/06. Zigzag Conversion.cpp
+++ 0001 - 0100/06. Zigzag Conversion.cpp
@@ -51,9 +51,7 @@ public:
 
 
 
-
-
-// APPROACH 1
+// APPROACH 2
 
 class Solution {
 public:
@@ -79,3 +77,44 @@ public:
         return ans;
     }
 };
+
+
+// APPROACH 3
+
+// simulate and add each character to the corresponding row
+// go down -> reach bottom -> go up -> reach top -> go down ...
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        // edge case
+        if(numRows == 1)
+            return s;
+        vector<string> rows(numRows);
+
+        // j is the index to track which rows a character should be added to
+        // d is the direction: -1 means go up, 1 means go down
+        int j = 0, d = 1;
+        for (int i = 0; i < s.size(); i++) {
+            // add the current character to corresponding row
+            rows[j] += s[i];
+            // if it reaches to the last row, we need to go up
+            if(j == numRows - 1) d = -1;
+            // if it reaches to the first row, we need to go down
+            else if(j == 0) d = 1;
+            // move j pointer
+            j += d;
+        }
+
+        // rows would look like below in the first example
+        // ['PAHN', 'APLSIIG', 'YIR']
+        // we use join to build the final answer
+        string res = "";
+        for(int i=0; i<numRows; i++){
+            res += rows[i];
+        }
+
+        return res;
+    }
+};
+
