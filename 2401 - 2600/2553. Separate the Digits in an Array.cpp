1270962b99ba9476e0d89fd0d52aa1c4bb08d61b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-04 22:03:55 +0530

    Create 2553. Separate the Digits in an Array.cpp

---
+++ 2401 - 2600/2553. Separate the Digits in an Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums){
+        vector<int> res;
+        for(int num : nums) {
+            string s = to_string(num);
+            for(char c : s) {
+                res.push_back(c - '0');
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
