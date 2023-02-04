6c952193aa572ad87b37515558a18d191bbf5a37
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-04 18:37:42 +0530

    Create 567. Permutation in String.cpp

---
+++ 0501 - 0600/567. Permutation in String.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool checkEqual(int a[26], int b[26]){
+        for(int i=0; i<26; i++)
+            if(a[i] != b[i]){
+                return 0;
+            }
+            return 1;
+    }
+        
+    bool checkInclusion(string s1, string s2) {
+        
+        // count charracter array
+        int count1[26] = {0};
+        for(int i=0; i<s1.length(); i++){
+            int index = s1[i] - 'a';
+            count1[index]++;
+        }
+        
+        // traverse s2 string string in window of size s1 and compare
+        int i=0;
+        int window = s1.length();
+        int count2[26] = {0};
+        
+        //running for first window
+        while(i<window && i < s2.length()){
+            int index = s2[i] - 'a';
+            count2[index]++;
+            i++;
+        }
+        
+        if(checkEqual(count1, count2))
+            return 1;
+        
+        // shift window proceess
+        while(i < s2.length()){
+            char newchar = s2[i];
+            int index = newchar - 'a';
+            count2[index]++; 
+            
+            char oldchar = s2[i - window];
+            index = oldchar - 'a';
+            count2[index]--;
+            
+            i++;
+            
+            if(checkEqual(count1, count2))
+            return 1;
+        }
+        return 0;
+    }
+};
+
+
+
+
+
+
+
+
