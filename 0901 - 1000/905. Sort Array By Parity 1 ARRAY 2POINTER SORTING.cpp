0ed41606450ce90b102a1c30d9973c950b1cfc39
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-07 21:21:42 +0530

    Update 905. Sort Array By Parity 1 ARRAY 2POINTER SORTING.cpp

--- 0901 - 1000/905. Sort Array By Parity 1 ARRAY 2POINTER SORTING.cpp
+++ 0901 - 1000/905. Sort Array By Parity 1 ARRAY 2POINTER SORTING.cpp
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/sort-array-by-parity/
+// APPROACH 1
 
 class Solution {
 public:
@@ -17,16 +17,35 @@ public:
     }
 };
 
+
 // APPROACH 2 
+
+class Solution {
+public:
+   vector<int> sortArrayByParity(vector<int>& nums) {
+       for (int i = 0, j = 0; j < nums.size(); j++)
+           if (nums[j] % 2 == 0) swap(nums[i++], nums[j]);
+       return nums;
+   }
+};
+
 // TC = O(N)
 
-//class Solution {
-//public:
-//    vector<int> sortArrayByParity(vector<int>& nums) {
-//        for (int i = 0, j = 0; j < nums.size(); j++)
-//            if (nums[j] % 2 == 0) swap(nums[i++], nums[j]);
-//        return nums;
-//    }
-//};
 
+// APPROACH 3
 
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]%2 == 0)
+                ans.push_back(nums[i]);
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]%2 != 0)
+                ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
