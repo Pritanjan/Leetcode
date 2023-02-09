208a2167228ce1493e7d529c95f2dcb3e7a3c338
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-09 16:20:56 +0530

    Update 2306. Naming a Company.cpp

--- 2201 - 2400/2306. Naming a Company.cpp
+++ 2201 - 2400/2306. Naming a Company.cpp
@@ -1,3 +1,6 @@
+// APPROACH 1
+
+
 class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
@@ -29,6 +32,7 @@ public:
                 res += 2 * (ump[i].size() - temp) * (ump[j].size() - temp) ; 
             }
         }
+        
         return res;
     }
 };
@@ -38,3 +42,29 @@ public:
 // With constants, time complexity would be (26 * 25 / 2 * m * n)
 // SPACE COMPLEXITY: O(m * n), where m is the average length of a string and n is the number of strings.
 
+
+
+
+// APPROACH 2
+
+
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_set<string> ump(ideas.begin(), ideas.end());
+        vector<vector<int>> cur(128, vector<int>(128,0));
+        
+        long long ans = 0;
+        for (string& s : ideas) {
+            for (char c='a'; c<='z'; c++){
+                string word = s;
+                word[0] = c;
+                if (ump.find(word) == ump.end()){
+                    ans += cur[s[0]][c];
+                    cur[c][s[0]]++;
+                }
+            }
+        }
+        return ans * 2;
+    }
+};
