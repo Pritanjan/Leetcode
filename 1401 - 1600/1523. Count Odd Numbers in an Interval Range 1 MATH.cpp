1a02d03e5e874221500daf27eb97c355ce6279c4
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-13 07:40:19 +0530

    Update 1523. Count Odd Numbers in an Interval Range 1 MATH.cpp

--- 1401 - 1600/1523. Count Odd Numbers in an Interval Range 1 MATH.cpp
+++ 1401 - 1600/1523. Count Odd Numbers in an Interval Range 1 MATH.cpp
@@ -14,3 +14,18 @@ public:
         return (R+1)/2 - L/2;
     }
 };
+
+
+
+
+class Solution {
+public:
+    int countOdds(int L, int R) {
+        int ans = (R-L)/2;
+        if(L%2!=0 || R%2!=0)
+            ans++;
+        
+        return ans;
+        
+    }
+};
