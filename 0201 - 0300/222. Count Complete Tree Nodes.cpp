7e6c3c3a124d403c8e0c544316496e6c89f6bfc8
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-16 16:16:08 +0530

    Update 222. Count Complete Tree Nodes.cpp

--- 0201 - 0300/222. Count Complete Tree Nodes.cpp
+++ 0201 - 0300/222. Count Complete Tree Nodes.cpp
@@ -1,3 +1,5 @@
+// APPROACH 1
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -37,4 +39,57 @@ public:
 
 
 
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+        
+        int left_height = 0;
+        int right_height = 0;
+
+        TreeNode* left_node = root;
+        TreeNode* right_node = root;
+        
+        while (left_node) {
+            left_height++;
+            left_node = left_node->left;
+        }
+        
+        while (right_node) {
+            right_height++;
+            right_node = right_node->right;
+        }
+        
+        if (left_height == right_height) {
+            return pow(2, left_height) - 1;
+        }
+
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
+
+
+// This solution also uses recursion, but it first determines the height of the left
+// and then right subtrees of the root node. 
+// If the height of both subtrees is equal, the binary tree is a complete binary tree, 
+// and we can use a formula to calculate the number of nodes in the tree.
+// Otherwise, we recursively count the number of nodes in the left and right subtrees,
+// and add 1 for the root node.
+
+// Time complexity -->  O(log^2(n)), where n is the number of nodes in the binary tree.
+// Space complexity --> O(log(n)), because the maximum depth of the recursion is the
+// height of the binary tree.
+
+// Note that this solution assumes that the binary tree is a perfect binary tree, 
+// which means that all internal nodes have two children and
+// all leaves are at the same level. 
+
+// If the binary tree is not a perfect binary tree, this solution may still work, 
+// but it will not be as efficient.
 
