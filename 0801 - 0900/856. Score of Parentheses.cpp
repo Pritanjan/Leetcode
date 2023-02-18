3d11fd85e72dbc4d77b33cfc29a1d33a1f3c478b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-18 16:19:56 +0530

    Update 856. Score of Parentheses.cpp

--- 0801 - 0900/856. Score of Parentheses.cpp
+++ 0801 - 0900/856. Score of Parentheses.cpp
@@ -68,3 +68,33 @@ public:
 
 
 
+// APPROACH 2
+
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        stack<int> stack;
+        stack.push(0); 
+
+        for(char c: S) {
+            if(c == '(')
+                stack.push(0);
+            else{
+                int v = stack.top();
+                stack.pop();
+                int w = stack.top();
+                stack.pop();
+                stack.push(w + max(2 * v, 1));
+            }
+        }
+        return stack.top();
+    }
+};
+
+
+
+
+
+
+
+
