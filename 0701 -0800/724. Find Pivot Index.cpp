5b0a30fe83747542a6c39c17403536f568cacc50
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-18 10:05:54 +0530

    Update 724. Find Pivot Index.cpp

--- 0701 -0800/724. Find Pivot Index.cpp
+++ 0701 -0800/724. Find Pivot Index.cpp
@@ -3,19 +3,82 @@ public:
     int pivotIndex(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(),0);
         
-        int current = 0;
+        int ans = 0;
         for(int i=0; i<nums.size(); i++){
-            if(current == sum - current - nums[i] )
-                return i;   
-            current += nums[i];
+            if(ans == sum - ans - nums[i] )
+                return i;
+            
+            ans += nums[i];
         }
         return -1;
         
     }
 };
+ 
 
 //T.C. O(n)
 
 
 //https://leetcode.com/problems/find-pivot-index/discuss/2425048/c-2-solution-with-explanation
 
+
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int ans = 0;
+        
+        for(auto i : nums){
+            ans += i;
+        }
+        
+        int sum = 0;
+        for(int i=0; i<nums.size(); sum +=nums[i++] ){
+            if(sum * 2 == ans - nums[i] )
+                return i;
+        }
+        return -1;
+        
+    }
+};
+
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int ans = 0;
+        // for(int i=0; i<nums.size(); i++){
+            // ans += nums[i];
+        // }
+        
+        for(auto i : nums){
+            ans += i;
+        }
+        
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i] ;
+            if(sum * 2 == ans + nums[i] )
+                return i;
+        }
+        return -1;
+        
+    }
+};
+
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int i=0;
+        while(i<nums.size()){
+            int Lsum = accumulate(nums.begin(), nums.begin()+i, 0);
+            int Rsum = accumulate(nums.begin()+i+1, nums.end(),0);
+    
+            if(Lsum == Rsum)
+                return i;
+            i++;
+        }
+        return -1;
+    }
+};
