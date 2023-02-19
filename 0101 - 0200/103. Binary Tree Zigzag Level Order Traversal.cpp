a8731eeedd3710b453c794035229f8f84549476f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-02-19 14:14:58 +0530

    Update 103. Binary Tree Zigzag Level Order Traversal.cpp

--- 0101 - 0200/103. Binary Tree Zigzag Level Order Traversal.cpp
+++ 0101 - 0200/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -32,8 +32,38 @@ public:
 
 
 
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) { 
+         vector<vector<int>>res;
+        if(root == NULL) return res;
 
+        queue<TreeNode*>q;
+        q.push(root);
+        
+        int cnt = -1;
+        while(!q.empty()) {
+            int n = q.size();
+            vector<int>l;
+            
+            for(int i=0; i<n; ++i){
+                auto temp = q.front();
+                q.pop();
+                
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
 
+                l.push_back(temp->val);
+            }
+            cnt++;
+         
+            if(cnt % 2 != 0) reverse(l.begin(),l.end());
+
+            res.push_back(l);
+        }
+        return res;
+    }
+};
 
 
 
