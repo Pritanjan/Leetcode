5563cf140c615817d4118de7c2baf598a583dd15
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-06 17:18:49 +0530

    Update 1539. Kth Missing Positive Number ARRAY BINARY_SEARCH.cpp

--- 1401 - 1600/1539. Kth Missing Positive Number ARRAY BINARY_SEARCH.cpp
+++ 1401 - 1600/1539. Kth Missing Positive Number ARRAY BINARY_SEARCH.cpp
@@ -20,3 +20,51 @@ public:
 
     } 
 }; 
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        if (k <= arr[0] - 1) return k;
+        
+        k -= arr[0] - 1;
+        for(int i = 0; i < n - 1; i++) {
+            int currMissing = arr[i + 1] - arr[i] - 1;
+            if(k <= currMissing) 
+                return arr[i] + k;
+            
+            k -= currMissing;
+        }
+
+        return arr[n - 1] + k;
+    }
+};
+
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        vector<int> mis;
+        
+        int n = *max_element(arr.begin(), arr.end());
+        set<int> st(arr.begin(), arr.end());
+        for (int i = 1; i <= n; i++) {
+            if (st.find(i) == st.end()) {
+                mis.push_back(i);
+            }
+        }
+        
+        if (mis.empty()) {
+            return arr[arr.size() - 1] + k;
+        }
+        
+        if (k > mis.size()) {
+            return arr[arr.size() - 1] + (k - mis.size());
+        }
+        
+        return mis[k - 1];
+    }
+};
+
