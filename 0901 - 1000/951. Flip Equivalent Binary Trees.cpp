0c2e4eb688256910493e3ee074ad812314e27c9a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-06 21:16:39 +0530

    Update 951. Flip Equivalent Binary Trees.cpp

--- 0901 - 1000/951. Flip Equivalent Binary Trees.cpp
+++ 0901 - 1000/951. Flip Equivalent Binary Trees.cpp
@@ -13,4 +13,30 @@ public:
 
 
 
+// First, it checks if root1 and root2 are equal using a pointer comparison, and if so, returns true,
+// since any node is flip equivalent to itself.
+
+// Then, it checks if either root1 or root2 is null or if the values at root1 and root2 are not equal,
+// then returns false, since the trees cannot be flip equivalent.
+
+// If neither of those cases applies, it recursively checks whether the left and right subtrees
+// of root1 and root2 are flip equivalent using the flipEquiv function itself. 
+
+// There are two cases to consider:
+// 1. The subtrees are in the same orientation in both trees.
+//    In this case, the function recursively calls itself with the left subtrees
+//    of root1 and root2, and then with the right subtrees of root1 and root2.
+// 2. The subtrees are flipped in one of the trees.
+//    In this case, the function recursively calls itself with the left subtree of
+//    root1 and the right subtree of root2, and then with the right subtree of root1 
+//    and the left subtree of root2.
+
+// If either of these cases is true, the function returns true, indicating that 
+// the trees are flip equivalent. Otherwise, it returns false.
+
+
+
+
+
 // APPROACH 2
+
