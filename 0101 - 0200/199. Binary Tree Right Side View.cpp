3d62c9d992d33ade931cf31595ba61023d841d97
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-08 14:58:12 +0530

    Update 199. Binary Tree Right Side View.cpp

--- 0101 - 0200/199. Binary Tree Right Side View.cpp
+++ 0101 - 0200/199. Binary Tree Right Side View.cpp
@@ -1,3 +1,27 @@
+// Right View of Binary Tree
+
+class Solution {
+public:
+    void RT(TreeNode* root, int level, int& mxLevel, vector<int>& res){
+        if(root == nullptr) return ;
+    
+        if(level > mxLevel) {
+            mxLevel = level;
+            res.push_back(root -> val);
+        }
+    
+        RT(root->right, level + 1, mxLevel, res);
+        RT(root->left, level + 1, mxLevel, res);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        int mxLevel = -1;
+        RT(root, 1, mxLevel, res);
+        return res;
+    }
+};
+
 
 // Left View of Binary Tree
 // https://www.geeksforgeeks.org/print-left-view-binary-tree/
