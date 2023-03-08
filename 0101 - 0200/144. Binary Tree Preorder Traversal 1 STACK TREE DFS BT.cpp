75dfe64bc9cf3e15f77366e3b69ff94fc0b42143
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-08 12:26:40 +0530

    Update 144. Binary Tree Preorder Traversal 1 STACK TREE DFS BT.cpp

--- 0101 - 0200/144. Binary Tree Preorder Traversal 1 STACK TREE DFS BT.cpp
+++ 0101 - 0200/144. Binary Tree Preorder Traversal 1 STACK TREE DFS BT.cpp
@@ -1,9 +1,4 @@
-//https://leetcode.com/problems/binary-tree-preorder-traversal/
-
-//In interview recursive & iterative and then we can present the third 
-//one if they ask specifically for optimizing the space complexity.
-
-//Recursive solution :-O(n) time and O(n) space (function call stack);
+// APPROACH 1 STACK Recursive solution 
 
 class Solution {
 public:
@@ -21,3 +16,35 @@ public:
             return nodes;
     }
 };
+
+// T.C. --> O(N)
+// S.C. --> O(N)
+
+
+
+// APPROACH 2 USING 1 STACK (Iterative Solution)   
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> v;
+        stack <TreeNode*> st;
+        while (root || !st.empty()){
+            if(root){
+                v.push_back(root->val);
+                if(root->right)
+                    st.push(root->right);
+                root = root->left;
+            }
+            else{
+                root = st.top();
+                st.pop();
+            }
+        }
+        return v;
+    }
+};
+
+// T.C. --> O(N)
+// S.C. --> O(N)
+
