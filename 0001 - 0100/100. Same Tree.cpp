a4123f9eb8546461c8b42cc987ecd585d458061c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-11 21:13:00 +0530

    Update 100. Same Tree.cpp

--- 0001 - 0100/100. Same Tree.cpp
+++ 0001 - 0100/100. Same Tree.cpp
@@ -1,25 +1,17 @@
 // https://github.com/Pritanjan/Question/blob/main/POTD/gfg/0013%20191122%20Check%20if%20all%20levels%20of%20two%20trees%20are%20anagrams%20or%20not.cpp
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// APPROACH 1
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == NULL and q == NULL)
+        if(p == NULL and q == NULL)  // both trees are empty, so they are the same
             return true;
-        if(p == NULL or q == NULL)
+        if(p == NULL or q == NULL)  // one of the trees is empty, so they are different
             return false;
-        if(p -> val != q -> val)
+        if(p -> val != q -> val)   // the node values are different, so the trees are different
             return false;
+        // recursively check the left and right subtrees
         return isSameTree(p -> left, q -> left) and isSameTree(p -> right, q -> right);
     }
 };
