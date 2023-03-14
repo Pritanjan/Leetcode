fbaff74a4690813bdeadab024019f5229dfcea17
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-14 13:47:51 +0530

    Update 129. Sum Root to Leaf Numbers.cpp

--- 0101 - 0200/129. Sum Root to Leaf Numbers.cpp
+++ 0101 - 0200/129. Sum Root to Leaf Numbers.cpp
@@ -23,3 +23,28 @@ public:
 
 
 
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        int totalSum = 0;
+        
+        if (root == NULL) return totalSum;
+        
+        stack<pair<TreeNode*, int>> stk;
+        stk.push({root, root->val});
+        
+        while(!stk.empty()) {
+            auto [node, currentSum] = stk.top();
+            stk.pop();
+            
+            if(node->left == NULL && node->right == NULL) totalSum += currentSum;
+            if(node->left != NULL) stk.push({node->left, currentSum * 10 + node->left->val});
+            if(node->right != NULL) stk.push({node->right, currentSum * 10 + node->right->val});
+        }
+        return totalSum;
+    }
+};
+
+
+
