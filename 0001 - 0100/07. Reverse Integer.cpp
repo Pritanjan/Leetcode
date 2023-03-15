11bcd5cab089e3e9e52f429a935599eafedc18c0
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-15 11:06:07 +0530

    Update 07. Reverse Integer.cpp

--- 0001 - 0100/07. Reverse Integer.cpp
+++ 0001 - 0100/07. Reverse Integer.cpp
@@ -1,21 +1,40 @@
-// https://leetcode.com/problems/reverse-integer/
+//  ADOBE GOOGLE AMAZON MICROSOFT BLOOMBERG
+
 
 class Solution {
 public:
     int reverse(int x) {
         int r= 0;
         while(x){
-        	if(r>INT_MAX/10 || r<INT_MIN/10){
-        		return 0;
-			}
-        	else{
+	    if(r>INT_MAX/10 || r<INT_MIN/10){
+        	  return  0;
+	    }
+            else{
             	r = r*10 + x%10;
             	x=x/10;
             	
-        	}
+           }
         }
         return r;
     }
 };
 
-//  ADOBE GOOGLE AMAZON MICROSOFT BLOOMBERG
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        long n = 0;
+        while(x != 0) {
+            n = n*10 + x%10;
+            x = x/10;
+        }
+        return (int)n==n? (int)n:0;
+    
+    }
+};
+
+
+
+
+
