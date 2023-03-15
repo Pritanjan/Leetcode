9bd132b95b2f783e4292726d64d7b42876db9f00
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-15 14:06:21 +0530

    Update 1588. Sum of All Odd Length Subarrays ARRAY MATH PRFFIX_SUM.cpp

--- 1401 - 1600/1588. Sum of All Odd Length Subarrays ARRAY MATH PRFFIX_SUM.cpp
+++ 1401 - 1600/1588. Sum of All Odd Length Subarrays ARRAY MATH PRFFIX_SUM.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+// APPROACH 1
 
 class Solution {
 public:
@@ -14,13 +14,47 @@ public:
 };
 
 
-
-
-
-
-
 // (i+1)*(n-i) --> toatal no. of subarray 
 // (i+1)*(n-i) / 2  no. of odd subarray 
 
-// T.C. = O(n)
+// T.C. --> O(n)
+
+
+
+
+
+// APPROACH 2
+
+// The idea is to iterate over all possible odd lengths of subarrays (i), 
+// then for each length, iterate over all possible subarrays of that length (j), 
+// and finally sum up the elements of each subarray (k). 
+// The total sum of all odd length subarrays is returned at the end.
+
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int n = arr.size();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i += 2) { // iterate over odd lengths
+            for (int j = 0; j <= n - i; j++) { // iterate over all subarrays of length i
+                for (int k = j; k < j + i; k++) {
+                    sum += arr[k];
+                }
+            }
+        }
+
+        return sum;
+    }
+};
+
+
+// T.C. --> O(n^3), where n is the length of the input array arr. 
+// This is because we have three nested loops, each iterating up to n times.
+
+// S.C. --> O(1), as we are only using a constant amount of extra space 
+// to store the variables n and sum.
+// The input array arr is modified in place and not stored in any additional data structures.
+
+
 
