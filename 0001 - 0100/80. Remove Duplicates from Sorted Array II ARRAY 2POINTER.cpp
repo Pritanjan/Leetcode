6a1666453a76403f4d200847b47381dac8e6f92f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-16 12:03:34 +0530

    Update 80. Remove Duplicates from Sorted Array II ARRAY 2POINTER.cpp

--- 0001 - 0100/80. Remove Duplicates from Sorted Array II ARRAY 2POINTER.cpp
+++ 0001 - 0100/80. Remove Duplicates from Sorted Array II ARRAY 2POINTER.cpp
@@ -43,3 +43,28 @@ public:
     }
 };
 
+
+
+
+
+
+// APPROACH 3
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = 1,num = nums[0];
+        for(int i=1;  i<nums.size();){
+           if(num == nums[i] && n < 2){
+                n++;
+                i++;
+            } else if(num == nums[i] && n == 2){
+                vector<int>::iterator it = nums.erase(nums.begin() + i);
+            } else if(num != nums[i]){
+                num = nums[i];
+                n = 1;
+                i++;
+            }
+        }
+        return nums.size();
+    }
+};
