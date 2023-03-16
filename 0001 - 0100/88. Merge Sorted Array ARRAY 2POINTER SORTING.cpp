45bff2a6eb4b2d96fe010bf53c23b31fd1a8448d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-16 15:06:42 +0530

    Update 88. Merge Sorted Array ARRAY 2POINTER SORTING.cpp

--- 0001 - 0100/88. Merge Sorted Array ARRAY 2POINTER SORTING.cpp
+++ 0001 - 0100/88. Merge Sorted Array ARRAY 2POINTER SORTING.cpp
@@ -38,7 +38,86 @@ public:
             k--;
             j--;
         }
-            
-        
     }
 };
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        merge1(nums1, m, nums2, n);
+    }
+
+    vector<int> merge1(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> v(m+n);
+
+        for(int i=0; i<m; i++) v[i] = nums1[i];
+        for(int i=0; i<n; i++) v[m+i] = nums2[i];
+    
+        sort(v.begin(), v.end());
+
+        // Copy the sorted values back to nums1
+        for(int i=0; i<m+n; i++) nums1[i] = v[i];
+
+        return nums1;
+    }
+};
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int sorted[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 - 1] = cur;
+        }
+        for (int i = 0; i != m + n; ++i) {
+            nums1[i] = sorted[i];
+        }
+    }
+};
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // nums1.merge(nums2)
+        //     return nums;
+        
+//         vector<int> :: iterator it;
+        
+//         vector<int> nums(m+n);
+//         it = set_union(nums1.begin(), nums1.end(), nums2.end(),nums2.end(),nums.begin());
+//         sort
+//         for(int st = nums.begin(); st!=it; ++st)
+//             return st;
+        
+        int j=0;
+        for(int i=m;i<nums1.size();i++){
+            if(j<n){
+                nums1[i]=nums2[j] ;
+            }
+            j++;
+        }
+        sort(nums1.begin(),nums1.end());
+        
+        
+        
+    }
+//     return -1;
+    
+};
+
