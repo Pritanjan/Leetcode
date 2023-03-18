8e7bb0e8455ec9bc67c125741f78799c1ca9ae51
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-18 11:25:34 +0530

    Update 700. Search in a Binary Search Tree 1 TREE BST BT.cpp

--- 0601 - 0700/700. Search in a Binary Search Tree 1 TREE BST BT.cpp
+++ 0601 - 0700/700. Search in a Binary Search Tree 1 TREE BST BT.cpp
@@ -1,6 +1,8 @@
-//https://leetcode.com/problems/search-in-a-binary-search-tree/submissions
+// https://leetcode.com/problems/search-in-a-binary-search-tree/
+// HERE WE R PRINTING THE SUBTREE OF VAL
 
-//iterative solution
+
+// APPROACH 1 iterative solution
 
 class Solution {
 public:
@@ -13,3 +15,51 @@ public:
         return root;
     }
 };
+
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    struct TreeNode* searchBST(TreeNode* root, int val) {
+        
+        // if node is empty return NULL
+        if(root == NULL)
+            return NULL ;
+        // if we find the node that we are looking for the return directly
+        else if(root->val == val)
+            return root;
+        
+        else if(root -> val < val)
+            return searchBST(root->right, val);
+        // if the val is smaller then the val of current node, then look for left child of the current node, else  look for right chind of the current node.
+        else
+            return searchBST(root->left, val );
+        
+    }
+};
+
+
+
+
+
+
+
+// HERE WE R CHECKING WHTHER THE GIVEN VAL IS PRESENT IN THE TREE OR NOT
+
+bool searchInBST(BinaryTreeNode<int> *root, int val) {
+       if(root == NULL)
+            return NULL ;
+        else if(root->data == val)
+            return root;
+        else if(root -> data < val)
+            return searchInBST(root->right, val);
+        else
+            return searchInBST(root->left, val );
+}
+
+
+
