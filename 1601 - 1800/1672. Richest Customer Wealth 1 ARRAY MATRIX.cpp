063eb744d6fb9b25806cda27851d1ed2023cf916
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-18 09:34:52 +0530

    Update 1672. Richest Customer Wealth 1 ARRAY MATRIX.cpp

--- 1601 - 1800/1672. Richest Customer Wealth 1 ARRAY MATRIX.cpp
+++ 1601 - 1800/1672. Richest Customer Wealth 1 ARRAY MATRIX.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/richest-customer-wealth/
+// APPROACH 1
 
 class Solution {
 public:
@@ -18,4 +18,23 @@ public:
 };
 
 
+
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int ans = 0;
+        int sum = 0;
+        for(int i=0; i<accounts.size(); i++){
+            sum = accumulate(accounts[i].begin(), accounts[i].end(), 0);
+            ans = max(ans,sum);
+        }
+        return ans;
+    }
+};
 
