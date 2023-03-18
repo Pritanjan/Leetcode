219ea99440825a2b8ad2abaf86d00dbc98e9d12c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-18 17:38:56 +0530

    Update 230. Kth Smallest Element in a BST TREE DFS BST BT.cpp

--- 0201 - 0300/230. Kth Smallest Element in a BST TREE DFS BST BT.cpp
+++ 0201 - 0300/230. Kth Smallest Element in a BST TREE DFS BST BT.cpp
@@ -1,10 +1,14 @@
-// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+// APPROACH 1 inorder traversal
 
-// inorder traversal
+// Go inorder and decrease k at each node. Stop the whole search as soon as k is 
+// zero, and then the k-th element is immediately returned all the way to the 
+// recursion top and to the original caller.
 
-// Go inorder and decrease k at each node. Stop the whole search as soon as k is zero, and then the k-th element is immediately returned all the way to the recursion top and to the original caller.
+// Try the left subtree first. If that made k zero, then its answer is the overall answer
+// and we return it right away. Otherwise, decrease k for the current node, and
+// if that made k zero, then we return the current node's value right away.
+// Otherwise try the right subtree and return whatever comes back from there.
 
-// Try the left subtree first. If that made k zero, then its answer is the overall answer and we return it right away. Otherwise, decrease k for the current node, and if that made k zero, then we return the current node's value right away. Otherwise try the right subtree and return whatever comes back from there.
 
 class Solution {
 public:
@@ -29,9 +33,19 @@ public:
     
 };
 
-//int kthSmallest(TreeNode* root, int& k) {
-//    if (root) {
-//        int x = kthSmallest(root->left, k);
-//        return !k ? x : !--k ? root->val : kthSmallest(root->right, k);
-//    }
-//}
+
+
+
+
+
+// APPROACH 2
+
+int kthSmallest(TreeNode* root, int& k) {
+   if (root) {
+       int x = kthSmallest(root->left, k);
+       return !k ? x : !--k ? root->val : kthSmallest(root->right, k);
+   }
+}
+
+
+
