c178537211467a7a1cfcf9bdae135c3c0e9b1eb8
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-18 10:01:21 +0530

    Update 412. Fizz Buzz MATH STRING SIMULATION.cpp

--- 0401 - 0500/412. Fizz Buzz MATH STRING SIMULATION.cpp
+++ 0401 - 0500/412. Fizz Buzz MATH STRING SIMULATION.cpp
@@ -44,6 +44,20 @@ public:
 
 
 
+// 1. The function fizzBuzz takes an integer n as its argument and returns a vector of strings.
+// 2. The vector ans is initialized to be empty.
+// 3. A for loop is used to iterate from 1 to n.
+// 4. For each number in the loop, a string s is initialized to be empty.
+// 5. If the number is divisible by 3, the string s is appended with "Fizz".
+// 6. If the number is divisible by 5, the string s is appended with "Buzz".
+// 7. If the string s is still empty, meaning the number is not divisible by either 3 or 5,
+//    then the number itself is converted to a string using the to_string function and
+//    pushed into the ans vector.
+// 8. If the string s is not empty, then it is pushed into the ans vector.
+// 9. Once the loop finishes, the ans vector is returned.
+    
+
+
 // T.C. --> O(N)
 // S.C. --> O(N)
 
