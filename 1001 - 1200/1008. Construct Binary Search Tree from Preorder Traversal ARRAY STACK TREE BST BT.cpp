315a4016e1502c60707d401f4cccb62ed5a56aeb
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-19 17:31:21 +0530

    Update 1008. Construct Binary Search Tree from Preorder Traversal ARRAY STACK TREE BST BT.cpp

--- 1001 - 1200/1008. Construct Binary Search Tree from Preorder Traversal ARRAY STACK TREE BST BT.cpp
+++ 1001 - 1200/1008. Construct Binary Search Tree from Preorder Traversal ARRAY STACK TREE BST BT.cpp
@@ -1,3 +1,5 @@
+// APPROACH 1
+
 // Give the function ma the maximum number it will handle.
 // The left recursion will take the elements smaller than node->val
 // The right recursion will take the remaining elements smaller than bound
@@ -28,3 +30,31 @@ public:
 
 
 
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    TreeNode* f(vector<int> &preorder, int mi, int mx, int &i){
+        if(i >= preorder.size()) return NULL;
+
+        // out of rsnge
+        if(preorder[i] < mi or preorder[i] > mx) return NULL;
+
+        TreeNode* root = new TreeNode (preorder[i++]);
+        root -> left = f(preorder, mi, root -> val, i);
+        root -> right = f(preorder, root -> val, mx, i);
+
+        return root;
+    }
+
+    TreeNode* bstFromPreorder(vector<int> &preorder) {
+        int mi = INT_MIN;
+        int mx = INT_MAX;
+        int i = 0;
+        
+        return f(preorder, mi, mx, i);
+    }
+};
