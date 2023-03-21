feaf1d42c4309a5fd1ff98569cada0442de0c1c6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-21 15:39:36 +0530

    Update 75. Sort Colors ARRAY 2 POINTER SORTING.cpp

--- 0001 - 0100/75. Sort Colors ARRAY 2 POINTER SORTING.cpp
+++ 0001 - 0100/75. Sort Colors ARRAY 2 POINTER SORTING.cpp
@@ -1,24 +1,22 @@
-//https://leetcode.com/problems/sort-colors/
-
-APPROACH 1
+// APPROACH 1
     
-DUTCH NATIONAL FLAG (DNF)
-Explanation : -
-This programming problem was proposed by Edsger Dijkstra. The flag of the Netherlands consists 
-of three colors: white, red, and blue. The task is to randomly arrange balls of red, white, and 
-blue in such a way that balls of the same color are placed together. For DNF (Dutch National Flag),
-we sort an array of 0, 1, and 2's in linear time that does not consume any extra space. We have 
-to keep in mind that this algorithm can be implemented only on an array that has three unique 
-elements.
+// DUTCH NATIONAL FLAG (DNF)
+// Explanation : -
+// This programming problem was proposed by Edsger Dijkstra. The flag of the Netherlands consists 
+// of three colors: white, red, and blue. The task is to randomly arrange balls of red, white, and 
+// blue in such a way that balls of the same color are placed together. For DNF (Dutch National Flag),
+// we sort an array of 0, 1, and 2's in linear time that does not consume any extra space. We have 
+// to keep in mind that this algorithm can be implemented only on an array that has three unique 
+// elements.
 
-ALGORITHM : -
-Take three-pointers, namely - L , mid , high.
-We use L and mid pointers at the start, and the R pointer will point at the end of the given array.
+// ALGORITHM : -
+// Take three-pointers, namely - L , mid , high.
+// We use L and mid pointers at the start, and the R pointer will point at the end of the given array.
 
-CASES : -
-If array [mid] =0 , then swap array [mid] with array [L] and increment both pointers once.
-If array [mid] = 1 , then no swapping is required. Increment mid pointer once.
-If array [mid] = 2 , then we swap array [mid] with array [R] and decrement the high pointer once.
+// CASES : -
+// If array [mid] =0 , then swap array [mid] with array [L] and increment both pointers once.
+// If array [mid] = 1 , then no swapping is required. Increment mid pointer once.
+// If array [mid] = 2 , then we swap array [mid] with array [R] and decrement the high pointer once.
 
     
 class Solution {
@@ -52,6 +50,9 @@ public:
 };
 
 
+
+
+
 // APPROACH 2 STL
 
 class Solution {
@@ -60,6 +61,11 @@ public:
         sort(nums.begin(),nums.end());
     }
 };
+
+
+
+
+
 
 
 // APPROACH 3
@@ -98,4 +104,32 @@ public:
 
 
     
+
+
+
+
+// APPROACH 4
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int len = nums.size();
+        int i;
+        int j;
+        int t;
+        for(i = 0 ; i < len -1;i++)
+            for(j = 0;j< len -1;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    t = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = t;
+                }
+            }
+        
+    }
+};
+
+
 
