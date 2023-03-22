7841c4dcb78ed9812b3b27e5d1ae90f862779ad2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-22 16:33:10 +0530

    Update 88. Merge Sorted Array ARRAY 2POINTER SORTING.cpp

--- 0001 - 0100/88. Merge Sorted Array ARRAY 2POINTER SORTING.cpp
+++ 0001 - 0100/88. Merge Sorted Array ARRAY 2POINTER SORTING.cpp
@@ -148,3 +148,23 @@ public:
 
 
 
+// APPROACH 5
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0, j = 0, k = 0;
+        int temp[m+n];
+
+        while(i < m && j < n) temp[k++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+        while(i<m) temp[k++] = nums1[i++];        
+        while(j<n) temp[k++] = nums2[j++];
+        while(k--) nums1[k] = temp[k];
+    }
+};
+
+
+
+
+
+
