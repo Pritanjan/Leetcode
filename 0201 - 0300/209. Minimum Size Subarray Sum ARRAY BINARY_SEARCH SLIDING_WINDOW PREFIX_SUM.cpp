5d36f372035c0ce247a1f2a19978c9e2ab56efe0
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-24 18:08:10 +0530

    Update 209. Minimum Size Subarray Sum ARRAY BINARY_SEARCH SLIDING_WINDOW PREFIX_SUM.cpp

--- 0201 - 0300/209. Minimum Size Subarray Sum ARRAY BINARY_SEARCH SLIDING_WINDOW PREFIX_SUM.cpp
+++ 0201 - 0300/209. Minimum Size Subarray Sum ARRAY BINARY_SEARCH SLIDING_WINDOW PREFIX_SUM.cpp
@@ -1,5 +1,4 @@
 // APPROACH 1
-// APPROACH 1
 
 // 1. Initialize 2 pointer, L to 0 and sum to 0
 // 2. Iterate over the nums and add nums[i] to sum
@@ -34,13 +33,74 @@ public:
 };
 
 
-
-
-
 // The O(n) solution is to use two pointers: l and r. First we move r until we get a sum >= s,
 // then we move l to the right until sum < s. In this process, store the minimum length 
 // between l and r. Since each element in nums will be visited by l and r for at
 // most once. 
+
+
+
+
+
+
+// OR
+
+
+
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size();
+        if (n < 1 || accumulate(nums.begin(), nums.end(), 0) < s) return 0;
+        
+        // Maintain a sliding window nums[i,j], nums[i...j] < s
+        int i = 0;
+        int j = -1;
+        int total = 0;
+        int res = n + 1;
+
+        while(i <= n-1) {
+            if(j + 1 < n && total < s) {
+                j += 1;
+                total += nums[j];
+            }else {
+                total -= nums[i];
+                i += 1;
+            }
+            
+            if(total >= s) {
+                res = min(res, j-i+1);
+            }
+        }
+
+        if(res == n+1) return 0;
+
+        return res;
+    }
+};
+
+
+// The code uses a two-pointer sliding window approach to iterate through the input
+// array nums and maintain a subarray whose sum is greater than or equal to the
+// target value s. The algorithm initializes two pointers i and j to the first and
+// the last element of the subarray, respectively. It then advances the pointer j to
+// the right as long as the sum of the subarray is less than s. 
+// If the sum of the subarray becomes greater than or equal to s, it updates the 
+// minimal length of the subarray seen so far and advances the pointer i to the
+// right until the sum of the subarray is less than s again. 
+// The algorithm keeps repeating this process until the entire array has been processed. 
+// If no subarray is found whose sum is greater than or equal to s, the algorithm returns 0.
+
+
+// Time complexity: O(n)
+// The code iterates through the input array nums exactly once with the two pointers i and j.
+// Therefore, the overall time complexity of the algorithm is O(n).
+
+// Space complexity: O(1)
+// The code uses a constant amount of extra space to store variables i, j, total, res, 
+// and the input vector nums.
+// Therefore, the space complexity of the algorithm is O(1).
 
 
 
