384a417b1c3ea13d7fa0128563507fcdc41e9d95
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-27 16:20:42 +0530

    Update 64. Minimum Path Sum.cpp

--- 0001 - 0100/64. Minimum Path Sum.cpp
+++ 0001 - 0100/64. Minimum Path Sum.cpp
@@ -1,16 +1,21 @@
 // APPROACH 1
 
+
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int width = grid[0].size(), high = grid.size();
         if (high == 0 || width == 0) return 0;
+
         // initialization
-        for (int i = 1; i < high; i++) grid[i][0] += grid[i - 1][0];
-        for (int i = 1; i < width; i++) grid[0][i] += grid[0][i - 1];
-        for (int i = 1; i < high; i++)
-            for (int j = 1; j < width; j++)
+        for(int i=1; i<high; i++)    grid[i][0] += grid[i - 1][0];
+        for(int i=1; i<width; i++)   grid[0][i] += grid[0][i - 1];
+
+        for(int i=1; i<high; i++) {
+            for (int j = 1; j < width; j++) {
                 grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }            
         return grid[high - 1][width - 1];
     }
 };
@@ -24,9 +29,7 @@ public:
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
-        }
+        if (grid.empty() || grid[0].empty()) return 0;
 
         int row = grid.size();
         int col = grid[row - 1].size();
@@ -35,13 +38,8 @@ public:
 
         dp[0][0] = grid[0][0];
 
-        for (int i = 1; i < row; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
-
-        for (int i = 1; i < col; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
-        }
+        for(int i=1; i<row; i++)  dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for(int i=1; i<col; i++)  dp[0][i] = dp[0][i - 1] + grid[0][i];
 
         for (int i = 1; i < row; i++) {
             for (int j = 1; j < col; j++) {
@@ -52,6 +50,8 @@ public:
         return dp[row - 1][col - 1];
     }
 };
+
+
 
 
 
