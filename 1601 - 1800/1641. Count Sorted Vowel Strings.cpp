68bccc04816a6601000348d4b6d0ac6f1401c163
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-28 22:15:08 +0530

    Update 1641. Count Sorted Vowel Strings.cpp

--- 1601 - 1800/1641. Count Sorted Vowel Strings.cpp
+++ 1601 - 1800/1641. Count Sorted Vowel Strings.cpp
@@ -24,14 +24,21 @@ public:
 // Then, we can compute dp[i][j] as the sum of all dp[i-1][k] for k >= j. 
 // Becs we can append any vowel that comes after j to a string of length i-1 that ends with j.
 
-// Since we only care about the number of strings of length n, the SC can be reduce by using 
-// a 1D dp table dp[j] to store the number of strings of length i that end with the vowel j. We can initialize the first row of the dp table as all ones, since there is only one string of length 1 that ends with each vowel.
+// Since we only care about the number of strings of length n, therefor SC can be reduce by using 
+// a 1D dp table dp[j] to store the number of strings of length i that end with the vowel j.
+// We can initialize the first row of the dp table as all ones, 
+// since there is only one string of length 1 that ends with each vowel.
 
-// Then, we can fill in the rest of the dp table using the recurrence relation dp[j] = dp[j] + dp[j+1]. This is because the number of strings of length i that end with the vowel j is equal to the number of strings of length i-1 that end with the vowel j plus the number of strings of length i-1 that end with any vowel that comes after j.
+// Then, we can fill in the rest of the dp table using the recurrence relation 
+// dp[j] = dp[j] + dp[j+1]. 
+// as no of strings of length i that end with the vowel j == no. of strings of length i-1 
+// that end with the vowel j + the number of strings of length i-1 that end with any
+// vowel that comes after j.
 
-// Finally, we can return the sum of the dp table, which is the total number of strings of length n that consist only of vowels and are lexicographically sorted.
 
 
+
+// APPROACH 2
 
 
 class Solution {
@@ -54,4 +61,5 @@ public:
         return dp[n + 1][0];
     }
 };
+
 
