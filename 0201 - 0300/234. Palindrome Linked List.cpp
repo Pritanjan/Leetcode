069dd1318c1664504e523939349c6193ad0e86a7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-29 23:25:47 +0530

    Update 234. Palindrome Linked List.cpp

--- 0201 - 0300/234. Palindrome Linked List.cpp
+++ 0201 - 0300/234. Palindrome Linked List.cpp
@@ -128,3 +128,53 @@ public:
 
 
 
+// APPROACH 4
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode* ptr = endOfFirstHalf(head);                // firstHalfEnd
+        ListNode* ptr1 = reverseList(ptr->next);             // secondHalfStart
+
+        // Check whether or not there is a palindrome.
+        ListNode* p1 = head;
+        ListNode* p2 = ptr1;
+        bool result = true;
+        while (result && p2 != nullptr) {
+            if (p1->val != p2->val) result = false;
+            p1 = p1->next;
+            p2 = p2->next;
+        }        
+
+        // Restore the list and return the result.
+        ptr->next = reverseList(ptr1);
+        return result;
+    }
+
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    ListNode* endOfFirstHalf(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
+
+
