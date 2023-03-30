a72adb6358ead3c6c89d67c67cda2bd352fac51d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-30 11:54:33 +0530

    Update 1637. Widest Vertical Area Between Two Points Containing No Points.cpp

--- 1601 - 1800/1637. Widest Vertical Area Between Two Points Containing No Points.cpp
+++ 1601 - 1800/1637. Widest Vertical Area Between Two Points Containing No Points.cpp
@@ -5,18 +5,18 @@ public:
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
         int n = points.size();
         int max_width = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (points[i][0] != points[j][0]) {
+        for(int i=0; i<n; ++i) {
+            for(int j=i+1; j<n; ++j) {
+                if(points[i][0] != points[j][0]) {
                     int width = abs(points[i][0] - points[j][0]);
                     bool contains_points = false;
-                    for (int k = 0; k < n; ++k) {
-                        if (k != i && k != j && points[k][0] > min(points[i][0], points[j][0]) &&
-                            points[k][0] < max(points[i][0], points[j][0])) {
+                    for(int k=0; k<n; ++k) {
+                        if(k != i && k != j && points[k][0] > min(points[i][0], points[j][0]) && points[k][0] < max(points[i][0], points[j][0])) {
                             contains_points = true;
                             break;
                         }
                     }
+                    
                     if (!contains_points) {
                         max_width = max(max_width, width);
                     }
@@ -26,6 +26,7 @@ public:
         return max_width;
     }
 };
+
 
 
 
@@ -76,5 +77,54 @@ public:
         return max_width;
     }
 };
+
+
+
+
+
+
+// APPROACH 4
+
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(points.begin(), points.end()); // sort the points by x-coordinate
+        int max_width = 0;
+        
+        for (int i=1; i<points.size(); ++i) {
+            int width = points[i][0] - points[i-1][0]; // calculate the width between adjacent points
+            max_width = max(max_width, width);         // update max_width if necessary
+        }
+        return max_width;
+    }
+};
+
+// T.C. --> O(N log N) , for  sort() function
+// S.C. --> O(N),        for store the sorted points.
+
+
+
+
+// APPROACH 4 [BY USING LAMBDA FUNCTION ]
+
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](const auto& a, const auto& b) {
+            return a[0] < b[0];
+        });
+        
+        int max_width = 0;
+        for (int i = 0; i < points.size() - 1; ++i) {
+            max_width = max(max_width, points[i+1][0] - points[i][0]);
+        }
+        return max_width;
+    }
+};
+
+
+// T.C. --> O(N log N) 
+// S.C. --> O(N)
+
 
 
