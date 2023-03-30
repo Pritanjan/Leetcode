76dceff3368e17f62993bb61b130b2fcb008a7a0
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-03-31 00:12:53 +0530

    Update 61. Rotate List.cpp

--- 0001 - 0100/61. Rotate List.cpp
+++ 0001 - 0100/61. Rotate List.cpp
@@ -1,13 +1,5 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// APPROACH 1
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
@@ -47,4 +39,37 @@ public:
 
 // If the input offset is always going to be valid, a fast and slow pointer way is a little better.
 // But in the Leetcode version, k could be larger than the length, so we need the length anyway.
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next) {
+            return head;
+        }
+        // Length of the linked list
+        int n = 1;
+        ListNode* curr = head;
+        while (curr->next) {
+            n++;
+            curr = curr->next;
+        }
+        // Connect the tail node to the head node to form a cycle
+        curr->next = head;
+        // Index of the new head node of the rotated linked list
+        // Index of the new tail node of the rotated linked list
+        for (int i = 0; i < n - k % n - 1; i++) {
+            head = head->next;
+        }
+        ListNode* newHead = head->next;
+        // Break the cycle
+        head->next = nullptr;
+        return newHead;
+    }
+};
+
 
