6798df96a28815e3464c4e3341fc73932df080a1
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-02 17:41:46 +0530

    Update 138. Copy List with Random Pointer.cpp

--- 0101 - 0200/138. Copy List with Random Pointer.cpp
+++ 0101 - 0200/138. Copy List with Random Pointer.cpp
@@ -43,3 +43,38 @@ public:
 
 
 
+
+
+
+// APPROACH 3
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) return nullptr;
+        // Connect the original linked list and its corresponding "mirror linked list" in series
+        for (Node* node = head; node != nullptr; node = node->next->next) {
+            Node* nodeNew = new Node(node->val);
+            nodeNew->next = node->next;
+            node->next = nodeNew;
+        }
+        
+        // According to the random pointer of each node of the original linked list, through the connection between the original linked list and the new linked list, set the random pointer of each node of the new linked list
+        for (Node* node = head; node != nullptr; node = node->next->next) {
+            Node* nodeNew = node->next;
+            nodeNew->random = (node->random != nullptr) ? node->random->next : nullptr;
+        }
+
+        // Concatenate the new linked list
+        Node* headNew = head->next;
+        for (Node* node = head; node != nullptr; node = node->next) {
+            Node* nodeNew = node->next;
+            node->next = node->next->next;
+            nodeNew->next = (nodeNew->next != nullptr) ? nodeNew->next->next : nullptr;
+        }
+        return headNew;
+    }
+};
+
+
+
