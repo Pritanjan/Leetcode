e63caabf5fef2e48fbf23fc0510b00afeac04a5b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-02 21:18:16 +0530

    Update 485. Max Consecutive Ones.cpp

--- 0401 - 0500/485. Max Consecutive Ones.cpp
+++ 0401 - 0500/485. Max Consecutive Ones.cpp
@@ -1,19 +1,105 @@
-https://leetcode.com/problems/max-consecutive-ones/
+// APPROACH 1
 
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int count = 0;
+        int cnt = 0;
         int ans = 0;
         
         for(int i=0; i<nums.size(); i++){
             if(nums[i] == 1){
-                count++;
-                ans = max(ans, count);
+                cnt++;
+                ans = max(ans, cnt);
             }
-            else 
-                count = 0;
+            else cnt = 0;
         }
         return ans;
     }
 };
+
+
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        string s = "";
+        for (int i=0; i<nums.size(); i++) {
+            s += to_string(nums[i]);
+        }
+
+        vector<string> substrings;
+        string temp = "";
+        for (int i=0; i<s.size(); i++) {
+            if (s[i] == '0') {
+                substrings.push_back(temp);
+                temp = "";
+            } else {
+                temp += s[i];
+            }
+        }
+        substrings.push_back(temp);
+        
+        int max_len = 0;
+        for (int i=0; i<substrings.size(); i++) {
+            if (substrings[i].size() > max_len) {
+                max_len = substrings[i].size();
+            }
+        }
+        return max_len;
+    }
+};
+
+
+
+
+
+
+
+// APPROACH 3
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0; i<1024; ++i){
+            if(__builtin_popcount(i) == 3 && (i & (i >> 1)) == 0) ++ans;
+            
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+// APPROACH 4
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cur = 0;
+        int maximum = 0;
+        for (int num : nums) {
+            cur = (cur + num) * num;
+            if (cur > maximum) {
+                maximum = cur;
+            }
+        }
+        return maximum;
+    }
+};
+
+
+
+
+
+
+
