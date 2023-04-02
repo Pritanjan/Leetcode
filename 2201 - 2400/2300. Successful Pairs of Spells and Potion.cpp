cd5d7be4898a3970676f0f71e184dd591d28435d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-02 11:20:02 +0530

    Update 2300. Successful Pairs of Spells and Potion.cpp

--- 2201 - 2400/2300. Successful Pairs of Spells and Potion.cpp
+++ 2201 - 2400/2300. Successful Pairs of Spells and Potion.cpp
@@ -1,26 +1,45 @@
-// APPROACH 1
+// 🙏🙏 JAI HANUMAN 🙏🙏
+
+// APPROACH 1 [ BRUTE FORCE -- TLE ]
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success) {
+        int n = spells.size();
+        int m = potions.size();
+        
+        vector<int> res(n, 0);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                // if(spells[i] * potions[j] >= success) {  --> RTE
+                if((long long)spells[i] * (long long)potions[j] >= success) {
+                    res[i]++;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+// APPROACH 2
 
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) 
-    {
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         sort(potions.begin(),potions.end());
         vector<int> v(spells.size());
         
-        for(int i=0; i<spells.size(); i++)
-        {
+        for(int i=0; i<spells.size(); i++) {
             int L = 0;
             int R = potions.size()-1;
             int mid;
             
-            while(L <= R)
-            {
+            while(L <= R) {
                 mid = L + (R - L)/2;
-                if((long long int)spells[i] * (long long int)potions[mid] >= success)
-                    R = mid - 1;
-                else
-                    L = mid + 1;
-                
+                if((long long int)spells[i] * (long long int)potions[mid] >= success) R = mid - 1;
+                else L = mid + 1;
             }
             v[i] = potions.size()-R-1;
         }
@@ -30,9 +49,31 @@ public:
 
 
 
+// OR
+
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success) {
+        int n = spells.size();
+        int m = potions.size();
+        
+        sort(potions.begin(), potions.end());
+        vector<int> res(n, 0);
+        int j = m - 1;
+        for(int i=0; i<n; i++) {
+            while(j >= 0 && (long long)spells[i] * (long long)potions[j] < success) {
+                j--;
+            }
+            res[i] = j + 1;
+        }
+        return res;
+    }
+};
 
 
-// APPROACH 2
+
+
+// APPROACH 3
 
 class Solution {
 public:
@@ -43,7 +84,6 @@ public:
         return spells;
     }
 };
-
 
 
 
