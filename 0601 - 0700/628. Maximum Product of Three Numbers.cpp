63a3643e4988a7ac1774209425412d902fd2df30
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-03 15:58:46 +0530

    Update 628. Maximum Product of Three Numbers.cpp

--- 0601 - 0700/628. Maximum Product of Three Numbers.cpp
+++ 0601 - 0700/628. Maximum Product of Three Numbers.cpp
@@ -25,22 +25,22 @@ public:
     int maximumProduct(vector<int>& nums) {
         int min1 = INT_MAX, min2 = INT_MAX;
         int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
-        for (int n : nums) {
-            if (n <= min1) {
+        for (int i : nums) {
+            if (i <= min1) {
                 min2 = min1;
-                min1 = n;
-            } else if (n <= min2) {
-                min2 = n;
+                min1 = i;
+            } else if (i <= min2) {
+                min2 = i;
             }
-            if (n >= max1) {
+            if (i >= max1) {
                 max3 = max2;
                 max2 = max1;
-                max1 = n;
-            } else if (n >= max2) {
+                max1 = i;
+            } else if (i >= max2) {
                 max3 = max2;
-                max2 = n;
-            } else if (n >= max3) {
-                max3 = n;
+                max2 = i;
+            } else if (i >= max3) {
+                max3 = i;
             }
         }
         return max(min1 * min2 * max1, max1 * max2 * max3);
