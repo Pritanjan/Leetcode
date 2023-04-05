0b0ba0bf78c4254376130c79643bba77722c13ea
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-05 16:07:04 +0530

    Update 448. Find All Numbers Disappeared in an Array ARRAY HASH_TABLE.cpp

--- 0401 - 0500/448. Find All Numbers Disappeared in an Array ARRAY HASH_TABLE.cpp
+++ 0401 - 0500/448. Find All Numbers Disappeared in an Array ARRAY HASH_TABLE.cpp
@@ -1,9 +1,8 @@
-// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+// APPROACH 1
 
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        
         vector<int> ans;
         int n = nums.size();
         
@@ -13,10 +12,39 @@ public:
         }
         
         for(int i=0; i<n; i++){
-            if(nums[i] >0 )
-                ans.push_back(i+1);
+            if(nums[i] >0 ) ans.push_back(i+1);
         }
-        
         return ans;
     }
 };
+
+
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        
+        for(int i=0;i<n; i++){
+            while(nums[nums[i] - 1] != nums[i] ){
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            if(nums[i] != i+1) ans.push_back(i+1);
+        }        
+        return ans;
+    }
+};
+
+
+
+
+
