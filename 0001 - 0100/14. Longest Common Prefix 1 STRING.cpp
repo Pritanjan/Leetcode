766596c928cf78c209aa7340487b76deda3826f7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-06 23:40:31 +0530

    Update 14. Longest Common Prefix 1 STRING.cpp

--- 0001 - 0100/14. Longest Common Prefix 1 STRING.cpp
+++ 0001 - 0100/14. Longest Common Prefix 1 STRING.cpp
@@ -1,12 +1,10 @@
-//https://leetcode.com/problems/longest-common-prefix/
-//Horizontal Scanning
+// APPROACH 1 [ Horizontal Scanning ]
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& s) {
         string s1 = s[0];
-        if(s.empty())
-            return "";
+        if(s.empty()) return "";
         
         for(int i=0; i<s.size(); i++){
             while(s[i].find(s1) != 0){
@@ -20,17 +18,33 @@ public:
 };
 
 
+
+
+
+
+// APPROACH 2 [ Vertical Scanning ]
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& s) {
-        if(s.empty())
-            return "";
+        if(s.empty()) return "";
+        
         for(int i=0; i<s[0].size(); i++){
             for(int j=1; j<s.size(); j++){
-                if(i == s[j].size() || s[0][i] != s[j][i])
+                // If(s[0][i] != s[j][i])
+                if(i == s[j].size() || s[0][i] != s[j][i]) 
                     return s[0].substr(0,i);
             }
         }
         return s[0];
     }
 };
+
+
+// T.C. --> O(S), where S is the sum of all characters in all strings.
+// S.C. --> O(1), constant extra space IS use
+
+
+
+
+
