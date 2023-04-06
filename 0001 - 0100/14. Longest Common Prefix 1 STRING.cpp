e369673c484ef10b787dfbdfeab33e45cc267128
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-06 23:47:11 +0530

    Update 14. Longest Common Prefix 1 STRING.cpp

--- 0001 - 0100/14. Longest Common Prefix 1 STRING.cpp
+++ 0001 - 0100/14. Longest Common Prefix 1 STRING.cpp
@@ -47,4 +47,25 @@ public:
 
 
 
+// OR
 
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int ans=0;
+        for(int i=0;i<strs[0].size();i++){
+            bool flag=true;
+            for(int j=1;j<strs.size();j++){
+                if(strs[j][i]!=strs[0][i]){
+                    flag=false;
+                    break;
+                    
+                }
+            }
+            if(flag) ans++;
+            else break;
+        }
+        return strs[0].substr(0,ans);
+    }
+};
