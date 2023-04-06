8816b673f7a7b2a03f5a04064111b54cdb5890d6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-06 17:33:43 +0530

    Update 274. H-Index.cpp

--- 0201 - 0300/274. H-Index.cpp
+++ 0201 - 0300/274. H-Index.cpp
@@ -29,4 +29,37 @@ public:
 
 // APPROACH 2
 
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end()); // Sort the citations in ascending order
+        int n = citations.size();
+        int h = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (citations[i] > h) {
+                h++; // Increment h if the citation count is greater than h
+            } else {
+                break; // Exit the loop if the citation count is less than or equal to h
+            }
+        }
+        return h;
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int h;
+        priority_queue<int> pq(begin(citations), end(citations));
+        
+        for (h = 0; !pq.empty() && h < pq.top(); ++h)
+            pq.pop();  
+
+        return h;
+    }
+};
 
