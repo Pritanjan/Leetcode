afdcf291af81498a36879b5bc0630cb2a49d4831
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-06 22:07:02 +0530

    Create 1040. Moving Stones Until Consecutive II.cpp

---
+++ 1001 - 1200/1040. Moving Stones Until Consecutive II.cpp
@@ -0,0 +1,53 @@
+
+
+class Solution {
+public:
+    vector<int> numMovesStonesII(vector<int>& stones) {
+        sort(stones.begin(), stones.end());
+        
+        int n = stones.size();
+        // 最大值
+        int maxd = max(stones[n - 1] - stones[1], stones[n-2] - stones[0]) - n + 2;
+        int mind = 10001;
+        
+        // 最小值
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j + 1 < n && stones[j + 1] - stones[i] < n) {
+                j++;
+            }
+            
+            int cost = n - (j - i + 1);
+            if (j - i + 1 == n - 1 && stones[j] - stones[i] + 1 == n - 1) {
+                cost = 2;
+            }
+            
+            mind = min(mind, cost);
+        }
+        
+        return {mind, maxd};
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    vector<int> numMovesStonesII(vector<int>& stones) {
+        sort(stones.begin(), stones.end());
+        int n = stones.size();
+        int maxstep = (stones[n-1]-stones[0]+1-n)-min(stones[n-1]-stones[n-2]-1,stones[1]-stones[0]-1);
+        int minstep = maxstep,i = 0,j = 0,step = 0;
+        for(;i < n;i++){
+            while(j + 1 < n && stones[j + 1] - stones[i] + 1 <= n)
+                ++j;
+            step = n - (j - i + 1);
+            if(j - i + 1 == n - 1 && stones[j] - stones[i] + 1 == n - 1)
+                step = 2;
+            minstep = min(minstep,step);
+        }
+        return {minstep,maxstep};
+    }
+};
