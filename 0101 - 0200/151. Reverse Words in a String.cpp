f4d3c09c294b7d70bb41b9ac0eb3e2730a905c8c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-07 22:38:12 +0530

    Update 151. Reverse Words in a String.cpp

--- 0101 - 0200/151. Reverse Words in a String.cpp
+++ 0101 - 0200/151. Reverse Words in a String.cpp
@@ -17,7 +17,31 @@ public:
 };
 
 
+// OR
 
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        // Split the input string into words
+        istringstream iss(s);
+        vector<string> words;
+        string word;
+
+        while (iss >> word)  words.push_back(word);
+
+        // Reverse the vector of words
+        reverse(words.begin(), words.end());
+
+        // Join the words into a string with spaces
+        string reversed = "";
+        for(int i=0; i<words.size(); i++) {
+            if(i > 0) reversed += " ";
+            reversed += words[i];
+        }        
+        return reversed;
+    }
+};
 
 
 // APPROACH 2 
@@ -132,4 +156,62 @@ public:
 // S.C. --> O(1)
 
 
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    //Flip, interval writing: left closed right closed []
+    void reverse(string& s, int start, int end){  
+        for (int i = start, j = end; i < j; i++, j--) {
+            swap(s[i], s[j]);
+        }
+    }
+
+    //Remove all spaces and add spaces between adjacent words, fast and slow pointers.
+    void removeExtraSpaces(string& s) { 
+        int slow = 0;  
+        for (int i = 0; i < s.size(); ++i) { //
+            //Process when non-space is encountered, that is, delete all spaces.
+            if (s[i] != ' ') { 
+                // manually controls whitespace, adding spaces between words. slow != 0 indicates that it is not the first word, and a space needs to be added before the word.
+                if (slow != 0) s[slow++] = ' '; 
+                
+                //Fill the word, if a space is encountered, it means the end of the word.
+                while (i < s.size() && s[i] != ' ') { 
+                    s[slow++] = s[i++];
+                }
+            }
+        }
+
+        //The size of slow is the size after removing extra spaces.
+        s.resize(slow); 
+    }
+
+    string reverseWords(string s) {
+        //Remove redundant spaces, ensure that there is only one space between words, and there are no spaces at the beginning and end of the string
+        removeExtraSpaces(s); 
+        reverse(s, 0, s.size() - 1);
+        
+        // After removeExtraSpaces, ensure that the start subscript of the first word must be 0.
+        int start = 0; 
+        for (int i = 0; i <= s.size(); ++i) {
+            // Reaching a space or the end of a string indicates the end of a word. to flip.
+            if (i == s.size() || s[i] == ' ') { 
+                //Flip, pay attention to the flip of left-closed-right-closed [].
+                reverse(s, start, i - 1); 
+                
+                // Update the start subscript start of the next word 
+                start = i + 1; 
+            }
+        }
+        return s;
+    }
+};
 
