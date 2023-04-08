9584dad77b9b9360125a7cd73c858287582e8eb3
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-08 18:18:36 +0530

    Update 344. Reverse String.cpp

--- 0301 - 0400/344. Reverse String.cpp
+++ 0301 - 0400/344. Reverse String.cpp
@@ -1,16 +1,5 @@
-// https://leetcode.com/problems/reverse-string/submissions/
+// APPROACH 1 [ 2 POINTER ]
 
-// APPROACH 1
-class Solution {
-public:
-    string reverseString(string s) {
-        reverse(s.begin() , s.end() ); 
-        return s;
-    }
-};
-
-
-// APPROACH 2
 class Solution {
 public:
     void reverseString(vector<char>& s) {
@@ -28,12 +17,41 @@ public:
 // S.C. O(1)
 
 
+
+
+
+
+
+// APPROACH 2 [ STL ]
+
+// This approach uses the reverse function from the STL algorithm library to 
+// reverse the vector in place.
+
+
+class Solution {
+public:
+    string reverseString(string s) {
+        reverse(s.begin() , s.end() ); 
+        return s;
+    }
+};
+
+
+
+
+
+
 // APPROACH 3
+
+
+// This approach is to iterates over the first half of the vector and swaps each character
+// with its corresponding character from the end of the vector using the swap function.
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
         for(int i=0;i<s.size()/2;i++){
-            swap(s[i],s[s.size()-1-i]);
+            swap(s[i], s[s.size()-1-i]);
         }
         // return s;
     }
@@ -44,32 +62,99 @@ public:
 
 
 
+// APPROACH 4
+
+// This approach uses the XOR swap algorithm to swap each pair of characters in the vector. 
+// The XOR swap algorithm is a bitwise operation that swaps two values without using a
+// temporary variable. It modifies the input vector in place and has O(1) space complexity.
 
 
-// REVDRSE A WORD 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            s[left] ^= s[right];
+            s[right] ^= s[left];
+            s[left] ^= s[right];
+            left++;
+            right--;
+        }
+    }
+};
 
 
-#include<bits/stdc++.h>
-using namespace std;
 
-int main(){
-    string s = "poi";
-    stack<char> st;
 
-    for(int i=0; i<s.size(); i++){
-        st.push(s[i]);
+
+
+// APPROACH 5
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        for(int i=n-1; i>=0; i--){
+            s.push_back(s[i]);
+        }
+        s.erase(s.begin(), s.begin() + n);
+        return ;        
+    }
+};
+
+
+
+
+
+
+// APPROACH 6 [ Using recursion ]
+
+// This approach uses a recursive function to swap the first and last characters of the string,
+// then recursively swaps the remaining characters until the entire string is reversed. 
+// It takes 3 arguments: the vector s, and the left and right indices of the current substring 
+// being reversed. The reverseString function that's called externally just passes in the 
+// entire vector to start the recursive process. 
+
+class Solution {
+public:
+    void reverseString(vector<char>& s, int left, int right) {
+        if(left >= right)  return ;
+        
+        swap(s[left], s[right]);
+        reverseString(s, left+1, right-1);
     }
 
-    string res = "";
-
-    while(!st.empty()){
-        res.push_back(st.top());
-
-        st.pop();
+    void reverseString(vector<char>& s) {
+        reverseString(s, 0, s.size()-1);
     }
+};
 
-    cout << res;
-}
+
+
+
+
+
+// APPROACH 7 [ Using a stack ]
+
+// This approach uses a stack to reverse the order of characters in the vector.
+// It pushes each character onto the stack and then pops them off one by one,
+// assigning them back into the vector in reverse order.
+
+class Solution {
+public:       
+    void reverseString(vector<char>& s) {
+        stack<char> st;
+        for (char c : s) {
+            st.push(c);
+        }
+        for (int i = 0; i < s.size(); i++) {
+            s[i] = st.top();
+            st.pop();
+        }
+    }
+};
+
+
 
 // T.C. O(N)
 // S.C. O(N)  -> due to stack 
