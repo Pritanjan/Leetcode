5687505e629629f78f67c6636494be069f34ac5a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-09 22:40:45 +0530

    Update 503. Next Greater Element II.cpp

--- 0501 - 0600/503. Next Greater Element II.cpp
+++ 0501 - 0600/503. Next Greater Element II.cpp
@@ -1,5 +1,8 @@
+// Next smaller element
 // https://github.com/Pritanjan/time-pass/blob/main/Stack/7%20Next%20Smaller%20Element.cpp
-// https://github.com/Pritanjan/Leetcode/blob/main/0401%20-%200500/496.%20Next%20Greater%20Element%20I%20ARRAY%20HASH_TABLE%20STACK.cpp
+
+
+// APPROACH 1
 
 class Solution {
 public:
@@ -21,6 +24,11 @@ public:
 };
 
 
+
+
+
+
+// APPROACH 2
 
 // https://practice.geeksforgeeks.org/problems/214734e358208c1c6811d9b237b518f6b3c3c094/1
 // IF THE aRRaY IS NOT CIRCULaR
@@ -44,6 +52,38 @@ public:
     }
 };
 
+
+
+
+
+
+
+// APPROACH 3
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1); // Initialize ans array with -1
+        stack<int> st; // Stack to store the indices of the elements
+
+        // Traverse the array twice to consider the circular nature of the array
+        for(int i=0; i<n*2; i++) {
+            // Compute the index in the range [0, n-1]
+            int idx = i % n; 
+            while (!st.empty() && nums[st.top()] < nums[idx]) {
+                // Assign the next greater element to the index at the top of the stack
+                ans[st.top()] = nums[idx]; 
+                 // Pop the index from the stack
+                st.pop();
+            }            
+            // Push the index into the stack only during the first traversal
+            if(i < n) st.push(idx);
+        }
+        return ans;
+    }
+};
 
 
 
