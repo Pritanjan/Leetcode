ba51804ebb49b16d052ad213c07bf22f7d33aac4
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-10 21:43:56 +0530

    Update 557. Reverse Words in a String III.cpp

--- 0501 - 0600/557. Reverse Words in a String III.cpp
+++ 0501 - 0600/557. Reverse Words in a String III.cpp
@@ -39,6 +39,26 @@ public:
 
 
 
+// OR
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i = 0;
+        int n = s.length();
+        while (i < n) {
+            int j = i;
+            while (j < n && s[j] != ' ') j++;
+            reverse(s.begin() + i, s.begin() + j);
+            i = j + 1;
+        }
+        return s;
+    }
+};
+
+
+
 
 
 
