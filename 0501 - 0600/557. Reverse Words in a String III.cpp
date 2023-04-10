cfa7225f1ff872acf2d41f63b011cbbc00efd496
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-10 18:37:42 +0530

    Update and rename 557. Reverse Words in a String III 2PONTER STRING.cpp to 557. Reverse Words in a String III.cpp

---
+++ 0501 - 0600/557. Reverse Words in a String III.cpp
@@ -0,0 +1,130 @@
+// APPROACH 1
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int j = -1;
+        int n = s.size();
+        for(int i=0; i<=n; i++){
+            if(i == n or s[i] == ' '){
+                int L = j + 1;
+                int R = i - 1;
+                
+                while(L < R){
+                    char ch = s[L];
+                    s[L] = s[R];
+                    s[R] = ch;
+                    L++;
+                    R--;
+                }
+                j = i;
+            }
+        }
+        return s;
+    }
+};
+
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    string reverseWords(string s)  {
+        stringstream ss(s);
+        string ans;
+        string word;
+        
+        while(ss >> word) {
+            reverse(word.begin(), word.end());   //reversing and adding
+            ans+=word+' ';
+        }
+        ans.pop_back();    //for the last extra space
+        return ans;
+    }
+};
+
+
+
+
+
+// APPROACH 3
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans="";
+        istringstream str(s);
+        for(string s; str>>s; ){
+            int first = 0, last = s.size()-1;
+            while(first < last) swap(s[first++], s[last--]);
+            ans += ans == "" ? s : " " + s;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+// APPROACH 5
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int a = 0;
+        for(int i=0; i<=s.length(); ++i){
+            if(s[i] == ' '){
+                reverse(&s[a], &s[i]);
+                a = i + 1;
+            }
+        }
+        reverse(s.begin()+a, s.end());
+        return s;
+    }
+};
+
+
+
+
+
+// OR 
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        size_t a = 0;
+        for(int i=0; i<=s.length(); ++i){
+            if(i == s.length() || s[i] == ' '){
+                reverse(&s[a], &s[i]);
+                a = i + 1;
+            }
+        }        
+        return s;
+    }
+};
+
+// function of '&' at reverse()?
+// Its to pass the value as address so that the code does not 
+// take extra time to make a copy and makes changes in it.
+// Here the changes are directly reflected on the original string
+
+// why use size_t?
+// It is a variable to assign size
+// Since we are using reference in reverse function
+// So we declare front as size_t
+
+
+
+
+
+
+
+
+
+
