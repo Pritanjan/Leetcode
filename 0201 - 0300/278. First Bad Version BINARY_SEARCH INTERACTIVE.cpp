df32121523ebfd179521b262b1627bf2904063d2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-11 08:22:37 +0530

    Update 278. First Bad Version BINARY_SEARCH INTERACTIVE.cpp

--- 0201 - 0300/278. First Bad Version BINARY_SEARCH INTERACTIVE.cpp
+++ 0201 - 0300/278. First Bad Version BINARY_SEARCH INTERACTIVE.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/first-bad-version/
+// APPROACH 1
 
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
@@ -18,13 +18,33 @@ public:
                 ans = mid;
                 R = mid - 1;
             }
-            else
-                L = mid + 1;
-                
-            
+            else L = mid + 1;            
         }
         // return -1;
         return ans;
-        
     }
 };
+
+
+
+
+
+// OR
+
+
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int L = 1, R = n;
+        while(L <= R) {
+            int mid = L + (R - L) / 2;
+            if(isBadVersion(mid)) R = mid - 1;
+            else L = mid + 1;
+        }
+        return L;
+    }
+};
+
+
+
