43440f0ef5d3c3cd40e65a43c45067d742d51bef
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-11 05:54:57 +0530

    Update 2390. Removing Stars From a String.cpp

--- 2201 - 2400/2390. Removing Stars From a String.cpp
+++ 2201 - 2400/2390. Removing Stars From a String.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/removing-stars-from-a-string/
+// APPROACH 1 [ STRINGS ]
 
 class Solution {
 public:
@@ -14,6 +14,87 @@ public:
     }
 };
 
+
+// T.C. --> O(N)
+// S.C. --> O(N)
+
+
+
+
+
+
+
+// APPROACH 2 [ USING STACK ]
+
+
 // Use a stack to store the characters.
 // Pop one character off the stack at each star. 
 // Otherwise, we push the character onto the stack.
+
+
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> stk;
+        for(auto i : s){
+            if(i == '*') {
+                if (!stk.empty()) {
+                    stk.pop();
+                }
+            } else {
+                stk.push(i);
+            }
+        }
+
+        string res;
+        while(!stk.empty()){
+            res.push_back(stk.top());
+            stk.pop();
+        }
+        
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+
+// T.C. --> O(N)
+// S.C. --> O(N)
+
+
+
+
+
+
+
+// APPROACH 3 [ 2 POINTER ]
+
+
+class Solution {
+public:
+    string removeStars(string s) {
+        vector<char> ch(s.size());
+        int j = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '*') {
+                j--;
+            } else {
+                ch[j++] = s[i];
+            }
+        }
+
+        string answer = "";
+        for (int i = 0; i < j; i++) {
+            answer.push_back(ch[i]);
+        }
+
+        return answer;
+    }
+};
+
+
+// T.C. --> O(N)
+// S.C. --> O(N)
+
+
