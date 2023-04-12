ec217c5343700fc3d291f491316625aade1caece
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-12 22:38:43 +0530

    Update 189. Rotate Array ARRAY MATH 2POINTER.cpp

--- 0101 - 0200/189. Rotate Array ARRAY MATH 2POINTER.cpp
+++ 0101 - 0200/189. Rotate Array ARRAY MATH 2POINTER.cpp
@@ -1,9 +1,4 @@
-// https://leetcode.com/problems/rotate-array/
-
-// Reveral Algorithm 
-// https://www.geeksforgeeks.org/array-rotation/
-// https://www.geeksforgeeks.org/program-for-array-rotation-continued-reversal-algorithm/
-
+// APPROACH 1
 
 class Solution {
 public:
@@ -26,16 +21,50 @@ public:
 };
 
 
+
+
+
+
+// APPROACH 2
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         k = k % nums.size();
         							// k = 3
         reverse(nums.begin(),nums.end());		         // [1,2,3,4,5]
-        reverse(nums.begin(),nums.begin()+k);		        // [3,2,1,4,5]
-        reverse(nums.begin()+k,nums.end());			// [1,2,3,5,4] 
+        reverse(nums.begin(),nums.begin()+k);		     // [3,2,1,4,5]
+        reverse(nums.begin()+k,nums.end());			     // [1,2,3,5,4] 
     }
 };
+
+
+
+
+
+
+// APPROACH 3
+
+class Solution {
+public:        
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        int L = 0;
+        int R = n - 1;
+        while(L < R) swap(nums[L++], nums[R--]);
+        
+        L = 0;
+        R = k - 1;
+        while(L < R) swap(nums[L++], nums[R--]);
+
+        L = k;
+        R = n - 1;
+        while(L < R) swap(nums[L++], nums[R--]);
+    }
+};
+
 
 
 
