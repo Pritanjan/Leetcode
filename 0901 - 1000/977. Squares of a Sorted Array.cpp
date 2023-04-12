c2f71d3a57f712e447aa25df8c55bd1ec87444c3
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-12 22:17:34 +0530

    Update 977. Squares of a Sorted Array.cpp

--- 0901 - 1000/977. Squares of a Sorted Array.cpp
+++ 0901 - 1000/977. Squares of a Sorted Array.cpp
@@ -16,9 +16,6 @@ public:
 
 
 
-
-
-
 // APPROACH 2
 
 class Solution {
@@ -81,3 +78,85 @@ public:
 
 
 // APPROACH 3
+
+
+// It uses transform function to apply the square operation to each element of the input vector A.
+// The sort function is then used to sort the resulting vector in non-descending order.
+// Now, sorted vector is returned.
+
+// Lambda function used in transform to square each element of the input vector 
+// is defined using the [] notation.
+
+
+class Solution {
+public:            
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int> B(A.size());
+        transform(A.begin(), A.end(), B.begin(), [](int i) { return i * i; });
+        sort(B.begin(), B.end());
+        return B;
+    }
+};
+
+
+
+
+// APPROACH 4 [ Direct insertion sorting ] 
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        int n = A.size();
+        vector<int> result(n);
+        int j;
+        for(int i = 0; i < n; i++){
+            A[i] = A[i] * A[i];
+        }
+        for(int i = 1; i < n; i++){
+            int temp = A[i];
+            for(j=i- 1; j>=0; j--){
+                if(temp < A[j]) A[j + 1] = A[j];
+                else break;
+            }
+            A[j + 1] = temp;
+        }
+        return A;
+    }
+};
+
+
+
+
+// APPROACH 5
+
+
+
+
+
+
+
+
+
+
+
+// APPROACH 1
+// APPROACH 1
+// APPROACH 1
+// APPROACH 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
