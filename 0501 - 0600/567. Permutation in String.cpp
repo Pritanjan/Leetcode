e887b1ff5e3b7a239961d719d9b0d8c41802b588
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-16 14:38:29 +0530

    Update 567. Permutation in String.cpp

--- 0501 - 0600/567. Permutation in String.cpp
+++ 0501 - 0600/567. Permutation in String.cpp
@@ -1,3 +1,6 @@
+// APPROACH 1
+
+
 class Solution {
 public:
     bool checkEqual(int a[26], int b[26]){
@@ -56,6 +59,9 @@ public:
 
 
 
+
+
+// APPROACH 2
 
 
 // Here's how the code works:
@@ -142,4 +148,46 @@ public:
 // 8. Return False if True is not encountered.
 
 
+
+
+
+// OR
+
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        
+        if(n > m) return false;
+        
+        vector<int> cnt1(26, 0), cnt2(26, 0);
+        
+        for(int i=0; i<n; i++) {
+            cnt1[s1[i] - 'a']++;
+            cnt2[s2[i] - 'a']++;
+        }
+
+        if(cnt1 == cnt2) return true;
+        
+        for(int i=n; i<m; i++) {
+            cnt2[s2[i] - 'a']++;
+            cnt2[s2[i - n] - 'a']--;
+
+            if(cnt1 == cnt2) return true;
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+
+
+
+// APPROACH 3
 
