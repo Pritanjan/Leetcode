e7ef488d3fc342d0d6815e657742441c789b69b3
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-16 20:31:14 +0530

    Update 145. Binary Tree Postorder Traversal STACK TREE DFS BT.cpp

--- 0101 - 0200/145. Binary Tree Postorder Traversal STACK TREE DFS BT.cpp
+++ 0101 - 0200/145. Binary Tree Postorder Traversal STACK TREE DFS BT.cpp
@@ -1,5 +1,5 @@
-// APPROACH 1 RECURSION
-// fb
+// APPROACH 1 [ RECURSION ]
+
 
 class Solution {
 public:
@@ -21,9 +21,9 @@ public:
 
 
 
+// OR
 
 
-// APPROACH 1 RECURSION
 
 class Solution {
 public:
@@ -42,7 +42,11 @@ public:
 
 
 
-// APPROACH 2 USING 2 STACK
+
+
+
+// APPROACH 2 [ USING 2 STACK ]
+
 
 class Solution {
 public:
@@ -72,13 +76,21 @@ public:
     }
 };
 
+
 // Time complexity: O(n) where n is no of nodes in a binary tree
 // Auxiliary space: O(n) because using stack stk1 and stk2
 
 
 
 
-// APPROACH USING 1 STACK
+
+
+
+
+
+
+// APPROACH 3 [  USING 1 STACK ]
+
 
 class Solution {
 public:
@@ -108,7 +120,11 @@ public:
 
 
 
-// APPROACH USING 1 STACK
+
+// OR
+
+
+
 
 class Solution {
 public:
@@ -140,6 +156,120 @@ public:
 
         reverse(ans.begin(),ans.end());
         return ans;
+    }
+};
+
+
+
+
+
+
+
+// OR
+
+
+
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(root == nullptr) return res;
+        
+        stack<TreeNode*> stk;
+        stk.push(root);
+        
+        while(!stk.empty()) {
+            root = stk.top();
+            stk.pop();
+            
+            if(root != nullptr) {
+                stk.push(root);
+                stk.push(nullptr);
+                if (root->right != nullptr)  stk.push(root->right);
+                if (root->left != nullptr) stk.push(root->left);
+                
+            } 
+            else {
+                res.push_back(stk.top()->val);
+                stk.pop();
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+// APPROACH 4 [ Morris Traversal ]
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
+        TreeNode* dummy = new TreeNode(-1);
+        dummy->left = root;
+        TreeNode* cur = dummy;
+        while (cur != nullptr) {
+            if (cur->left == nullptr) {
+                cur = cur->right;
+            } else {
+                TreeNode* pre = cur->left;
+                while (pre->right != nullptr && pre->right != cur) {
+                    pre = pre->right;
+                }
+                if (pre->right == nullptr) {
+                    pre->right = cur;
+                    cur = cur->left;
+                } else {
+                    reverseAddNodes(cur->left, pre, res);
+                    pre->right = nullptr;
+                    cur = cur->right;
+                }
+            }
+        }
+        delete dummy;
+        return res;
+    }
+    
+    void reverseAddNodes(TreeNode* from, TreeNode* to, vector<int>& res) {
+        reverse(from, to);
+        TreeNode* node = to;
+        while (true) {
+            res.push_back(node->val);
+            if (node == from) {
+                break;
+            }
+            node = node->right;
+        }
+        reverse(to, from);
+    }
+    
+    void reverse(TreeNode* from, TreeNode* to) {
+        if (from == to) {
+            return;
+        }
+        TreeNode* x = from;
+        TreeNode* y = from->right;
+        while (true) {
+            TreeNode* temp = y->right;
+            y->right = x;
+            x = y;
+            y = temp;
+            if (x == to) {
+                break;
+            }
+        }
     }
 };
 
