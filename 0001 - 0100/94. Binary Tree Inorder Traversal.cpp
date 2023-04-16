ed41bf8300263027ec3e04239f942a50f629fa07
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-16 22:09:52 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- 0001 - 0100/94. Binary Tree Inorder Traversal.cpp
+++ 0001 - 0100/94. Binary Tree Inorder Traversal.cpp
@@ -1,22 +1,21 @@
 // APPROACH 1 [ RECURSIVE ]
 
-
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& nodes) {
-        if(!root)
-            return ;
-        inorder(root->left,  nodes);
-        nodes.push_back(root->val);
-        inorder(root->right, nodes);
-    }
-    
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> nodes;
-        inorder(root, nodes);
-            return nodes;
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+private:
+    void inorder(TreeNode* root, vector<int>& res) {
+        if (!root) return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
     }
 };
+
 
 //Time complexity: O(n)
 //The time complexity is O(n) because the recursive function is T(n)=2·T(n/2)+1.
@@ -31,30 +30,27 @@ public:
 
 
 
-
-
-
 // APPROACH 2 [ Iterative method using Stack ]
 
 
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        stack<TreeNode*> stack;
-        while(root || !stack.empty()) {
-            while(root) {
-                stack.push(root);
+        vector<int> res;
+        stack<TreeNode*> s;
+        while (root || !s.empty()) {
+            while (root) {
+                s.push(root);
                 root = root->left;
             }
-            root=stack.top(), stack.pop();
-            ans.push_back(root->val);
-            root=root->right;
+            root = s.top();
+            s.pop();
+            res.push_back(root->val);
+            root = root->right;
         }
-        return ans;
+        return res;
     }
 };
-
 
 
 // Time complexity: O(n)
@@ -68,11 +64,72 @@ public:
 
 
 
+
 // APPROACH 3 [ Morris Traversal ]
 
 
-
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        while(root) {
+            if(root->left) {
+                TreeNode* pre = root->left;
+                while(pre->right && pre->right != root) pre = pre->right;
+                
+                if(!pre -> right) {
+                    pre -> right = root;
+                    root = root -> left;
+                } 
+                else {
+                    pre -> right = nullptr;
+                    res.push_back(root -> val);
+                    root = root -> right;
+                }
+            } 
+            else {
+                res.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return res;
+    }
+};
 
 
 // Time complexity: O(n)
 // Space complexity: O(1)
+
+
+
+
+
+
+// APPROACH 4 [ Iterative approach using a stack and flag ]
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<pair<TreeNode*, bool>> s;
+        s.push(make_pair(root, false));
+        
+        while(!s.empty()) {
+            auto cur = s.top();
+            s.pop();
+            
+            if(!cur.first) continue;
+            if(cur.second) res.push_back(cur.first->val);
+            else {
+                s.push(make_pair(cur.first->right, false));
+                s.push(make_pair(cur.first, true));
+                s.push(make_pair(cur.first->left, false));
+            }
+        }
+        return res;
+    }
+};
+
+
+
