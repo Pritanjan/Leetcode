538704539ef3b63200d049c6416c47e8b39d3e71
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-17 15:45:25 +0530

    Update 102. Binary Tree Level Order Traversal.cpp

--- 0101 - 0200/102. Binary Tree Level Order Traversal.cpp
+++ 0101 - 0200/102. Binary Tree Level Order Traversal.cpp
@@ -46,8 +46,7 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        if(root == NULL)
-            return ans;
+        if(root == NULL) return ans;
     
         queue<TreeNode*> que;
         que.push(root);
@@ -61,10 +60,8 @@ public:
                 que.pop();
 				          
                 v.emplace_back(root->val);
-                if(root->left)
-                    que.push(root->left);
-                if(root->right)
-                    que.push(root->right);
+                if(root->left)  que.push(root->left);
+                if(root->right) que.push(root->right);
             }
             ans.emplace_back(v);
         }
@@ -88,7 +85,41 @@ public:
 // APPROACH 3
 
 
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root) return res;
+        
+        queue<TreeNode*> currLevel, nextLevel;
+        currLevel.push(root);
 
+        while(!currLevel.empty()) {
+            vector<int> level;
+            while(!currLevel.empty()) {
+                TreeNode* node = currLevel.front();
+                currLevel.pop();
+                level.push_back(node -> val);
+
+                if(node -> left)  nextLevel.push(node -> left);
+                if(node -> right) nextLevel.push(node -> right);
+            }
+
+            res.push_back(level);
+            swap(currLevel, nextLevel);
+        }
+        return res;
+    }
+};
+
+
+
+// Time Complexity: O(N), where N is the number of nodes in the binary tree. 
+// We visit each node exactly once.
+
+// Space Complexity: O(W), where W is the maximum width of the binary tree. 
+// In the worst case, the width of the binary tree (i.e., the maximum number of nodes 
+// at any given level) is equal to the space required for the two queues.
 
 
 
