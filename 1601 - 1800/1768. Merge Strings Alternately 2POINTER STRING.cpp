80aa43e4a3cb7e4ccb308a06854aebbaf2954b9c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-18 10:06:24 +0530

    Update 1768. Merge Strings Alternately 2POINTER STRING.cpp

--- 1601 - 1800/1768. Merge Strings Alternately 2POINTER STRING.cpp
+++ 1601 - 1800/1768. Merge Strings Alternately 2POINTER STRING.cpp
@@ -1,4 +1,5 @@
-//https://leetcode.com/problems/merge-strings-alternately/
+// APPROACH 1 [ Two Pointers ]
+
 
 class Solution {
 public:
@@ -8,19 +9,82 @@ public:
         string ans;
         
         while((L1 < word1.size()) || (L2 < word2.size())  ){
-            if(L1 < word1.size()){
-                ans += word1[L1++];
-                // L1++;
-            }
-            if(L2 < word2.size()){
-                ans += word2[L2++];
-                // L2++;
-            }
+            if(L1 < word1.size()) ans += word1[L1++];
+            if(L2 < word2.size()) ans += word2[L2++];
         }
         return ans  ;
     }
 };
 
 
+
+// T.C. --> O(s1.size() + s2.size() )
+// S.C. --> O(1)
+
+
+
+
+
+
+
+// OR
+
+
+
+
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int L1 = 0;
+        int L2 = 0;
+        string ans;
+        
+        while(L1 < word1.size() && L2 < word2.size() ){
+            ans += string() + word1[L1++] + word2[L2++];
+        }
+        return ans + word1.substr(L1) + word2.substr(L2);
+    }
+};
+
+
+
+
+
+
+
+
+
+// APPROACH 2 [ One Pointers ]
+
+
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int L1 = 0;
+        string ans;
+        
+        while( L1 < max(word1.size(), word2.size())) {
+            if(L1 < word1.size()) ans += word1[L1];
+            if(L1 < word2.size()) ans += word2[L1];
+
+            L1++;
+        }
+        return ans  ;
+    }
+};
+
+
+
+
+
 //T.C. --> O(s1.size() + s2.size() )
 //S.C. --> O(1)
+
+
+
+
+
+
+
