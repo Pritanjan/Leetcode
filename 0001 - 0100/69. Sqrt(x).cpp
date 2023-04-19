dacb2fa750718dd3733707065f06fc15ab105c7e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-19 18:20:42 +0530

    Update 69. Sqrt(x).cpp

--- 0001 - 0100/69. Sqrt(x).cpp
+++ 0001 - 0100/69. Sqrt(x).cpp
@@ -1,21 +1,27 @@
-// https://leetcode.com/problems/sqrtx/
+// APPROACH 1 
+
 
 class Solution {
 public:
     int mySqrt(int x) {
-        
         long long int i=0;
         // for(int i=0;i<INT_MAX;i++){
-        
-        while(i*i<=x)
-            i++;
-        
+        while(i*i<=x) i++;
         return i-1;
-    // }
-        
+        // }
     }
 };
 
+
+
+
+
+
+
+
+
+
+// APPROACH 2 
 
 
 class Solution {
@@ -26,16 +32,59 @@ public:
         long long int ans=0;
         
         while(a<=ma){
-            long long q=a+(ma-a)/2;
-            
-            if(q*q<=x){
-               ans=q;
+            long long q = a + (ma - a) / 2;
+            if(q*q <= x) {
+               ans = q;
                 a=q+1;
             }
-            else
-                ma=q-1;
+            else ma=q-1;
         }
         return ans;
         
     }
 };
+
+
+
+
+
+
+
+
+// APPROACH 3 [ BINARY SEARCH ]
+
+
+class Solution {
+public:
+    int binarysearch(int n){
+        int l = 0;
+        int r = n;
+        int mid ;
+        long long  int ans = -1;
+        
+        while(l <= r){
+            long long int mid = l + (r-l)/2;
+            long long int sq = mid * mid ;
+            
+            if(sq == n)
+                return mid;
+            if(sq < n){
+                ans = mid;
+                l = mid + 1;
+            }
+            else 
+                r = mid - 1;
+        }
+        return ans;
+    }
+    
+    int mySqrt(int x) {
+        return binarysearch(x) ;
+    }
+};
+
+
+
+
+
+
