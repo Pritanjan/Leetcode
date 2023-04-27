1ee96d3d5949f28dbfcf5ac766df10a254b85da7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-04-27 16:27:18 +0530

    Update 319. Bulb Switcher.cpp

--- 0301 - 0400/319. Bulb Switcher.cpp
+++ 0301 - 0400/319. Bulb Switcher.cpp
@@ -13,3 +13,6 @@ public:
 
 
 
+Since we only care about the bulbs that are on after n rounds, we can simply count the number of perfect squares less than or equal to n. This is because a bulb will be toggled once for every factor it has. For example, the first bulb will be toggled on the first round, but then it will only be toggled by itself (the first factor) on the second round, so it will end up off. However, the fourth bulb will be toggled on the first, second, and fourth rounds, so it will end up on.
+
+Therefore, the number of bulbs that will be on after n rounds is simply the number of perfect squares less than or equal to n. This can be calculated by iterating from 1 to the square root of n and counting each iteration.
