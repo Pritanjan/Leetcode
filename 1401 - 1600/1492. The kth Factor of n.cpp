2c48449cc3a8314c28f3091cdbe426b216dd6b02
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-05-02 13:30:01 +0530

    Update 1492. The kth Factor of n.cpp

--- 1401 - 1600/1492. The kth Factor of n.cpp
+++ 1401 - 1600/1492. The kth Factor of n.cpp
@@ -1,4 +1,51 @@
-// APPROACH 1
+// APPROACH 1 [ Brute Force ]
+
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> factors;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                factors.push_back(i);
+            }
+        }
+        return k <= factors.size() ? factors[k - 1] : -1;
+    }
+};
+
+
+
+
+
+
+ 
+// APPROACH 2 [ Optimized Brute Force ]
+
+
+class Solution {
+public:       
+    int kthFactor(int n, int k) {
+        // iterate from 1 to n and count the factors
+        int cnt = 0;
+        for(int i=1; i<=n; i++) {
+            if(n % i == 0) {
+                cnt++;
+                if(cnt == k) {
+                    return i;
+                }
+            }
+        }
+        return -1; // if there are less than k factors, return -1
+    }
+};
+
+
+
+
+// OR
+
+
 
 
 class Solution {
@@ -25,36 +72,59 @@ public:
 
 
 
-
-
-// APPROACH 2
+// APPROACH 3 [ Square Root Optimization ]
 
 
 class Solution {
-public:       
+public:
     int kthFactor(int n, int k) {
-        // iterate from 1 to n and count the factors
-        int cnt = 0;
-        for(int i=1; i<=n; i++) {
-            if(n % i == 0) {
-                cnt++;
-                if(cnt == k) {
-                    return i;
+        vector<int> factors;
+        int sqrt_n = sqrt(n);
+        for (int i = 1; i <= sqrt_n; i++) {
+            if (n % i == 0) {
+                factors.push_back(i);
+                if (n / i != i) {
+                    factors.push_back(n / i);
                 }
             }
         }
-        return -1; // if there are less than k factors, return -1
+        sort(factors.begin(), factors.end());
+        return k <= factors.size() ? factors[k - 1] : -1;
     }
 };
 
 
 
 
+// APPROACH 4 [ Square Root Optimization with Early Return ]
 
 
-
-
-// APPROACH 3
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int sqrt_n = sqrt(n);
+        for (int i = 1; i <= sqrt_n; i++) {
+            if (n % i == 0) {
+                k--;
+                if (k == 0) {
+                    return i;
+                }
+            }
+        }
+        if (sqrt_n * sqrt_n == n) {
+            k++;
+        }
+        for (int i = sqrt_n; i >= 1; i--) {
+            if (n % i == 0) {
+                k--;
+                if (k == 0) {
+                    return n / i;
+                }
+            }
+        }
+        return -1;
+    }
+};
 
 
 
