22509eed07080afbed3d211ca78a372fa9349180
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-05-08 11:57:10 +0530

    1572. Matrix Diagonal Sum.cpp

--- 1401 - 1600/1572. Matrix Diagonal Sum ARRAY MATRIX.cpp
+++ 1401 - 1600/1572. Matrix Diagonal Sum ARRAY MATRIX.cpp
@@ -1,4 +1,5 @@
-//https://leetcode.com/problems/matrix-diagonal-sum/
+// APPROACH 1 
+
 
 class Solution {
 public:
@@ -15,3 +16,65 @@ public:
         return sum;
     }
 };
+
+
+
+
+
+
+
+
+// APPROACH 2
+
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int ans = 0;
+
+        for(int i=0; i<n; i++) {
+            // Add elements from primary diagonal.
+            ans += mat[i][i];
+            // Addd elements from secondary diagonal.
+            ans += mat[n - 1 - i][i];
+        }
+        
+        // If n is odd, subtract the middle element as its added twice.
+        if(n % 2 != 0) ans -= mat[n / 2][n / 2];
+
+        return ans;
+    }
+};
+
+
+// T.C. --> O(n^2) 
+// S.C. --> O(1)
+
+
+
+
+
+
+
+// APPROACH 3
+
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+        for(int i=0; i<n; i++) {
+            sum += mat[i][i]; // Add primary diagonal element
+            if (i != n - i - 1) { // Check if element is not on primary diagonal
+                sum += mat[i][n - i - 1]; // Add non-primary element of secondary diagonal
+            }
+        }
+        return sum;
+    }
+};
+
+
+
+
