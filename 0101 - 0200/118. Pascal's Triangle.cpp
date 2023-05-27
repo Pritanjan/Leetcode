078558663e1adda844feaaf11e679b3f389429dd
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-05-27 09:58:03 +0530

    Update 118. Pascal's Triangle.cpp

--- 0101 - 0200/118. Pascal's Triangle.cpp
+++ 0101 - 0200/118. Pascal's Triangle.cpp
@@ -43,3 +43,21 @@ public:
 };
 
 
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret;
+        if(numRows == 0) return ret;
+        ret.push_back(vector<int> (1,1));
+
+        for(int i=1; i<numRows; i++){
+            vector<int> a(i+1);
+            for(int j=0; j<=i; j++){
+                if(j == 0 || j == i) a[j] = 1;
+                else a[j] = ret[i-1][j-1] + ret[i-1][j];
+            }
+            ret.push_back(a);
+        }
+        return ret;
+    }
+};
