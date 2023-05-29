a40001af490798fecc610b75bbf57ec719891940
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-05-29 09:23:29 +0530

    Update 31. Next Permutation.cpp

--- 0001 - 0100/31. Next Permutation.cpp
+++ 0001 - 0100/31. Next Permutation.cpp
@@ -1,36 +1,31 @@
-// https://leetcode.com/problems/next-permutation/
-
-
 // APPROACH 1
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int i,j;
         for(i=n-2; i>=0; i--){
-            if(nums[i+1] > nums[i])
-                break;
+            if(nums[i+1] > nums[i]) break;
         }
         
-        if(i < 0){
-            reverse(nums.begin(), nums.end() );
-        }
-        
-        else{
+        if(i < 0) reverse(nums.begin(), nums.end() );       
+        else {
             for(j=n-1; j>=0; j--){
-                if(nums[j] > nums[i])
-                    break;
+                if(nums[j] > nums[i]) break;
             }
             swap(nums[i], nums[j]);
             reverse(nums.begin()+i+1, nums.end());
-        }
-            
-       
+        }       
     }
 };
 
 
+
+
+
 // APPROACH 2
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
@@ -39,31 +34,37 @@ public:
 };
 
 
+
+
+
+// APPROACH 3
+
 // 2ND QUESTION PRINTING NEXT PERMUTATION OF A GIVEN NUMBER
 // https://www.codingninjas.com/codestudio/problems/bob-homework_2656187?topList=amazon-sde-challenge
 
 #include<bits/stdc++.h>
-
 void nextPermutation(string &nums){
     int i;
     for(i=nums.size()-2;i>=0;i--){
         if(nums[i]<nums[i+1])
             break;
     }
-       int j=nums.size()-1;
-       if(i>=0){
+   int j=nums.size()-1;
+   if(i>=0){
        while(nums[j]<=nums[i])j--;
-       
-         swap(nums[j],nums[i]);
-       }
-    
-    reverse(nums.begin()+i+1,nums.end());
+       swap(nums[j],nums[i]);
+   }
+   reverse(nums.begin()+i+1,nums.end());
 }
-long long int bobsHomework(int n)
-{
+
+long long int bobsHomework(int n) {
     // Write your code here. 
     string num=to_string(n);
     nextPermutation(num);
     size_t ans=stoll(num);
     return (ans<=n)?-1:ans;
 }
+
+
+
+
