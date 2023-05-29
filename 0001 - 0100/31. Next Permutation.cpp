3c8ea5a3e0b8be4ad16e129bd316c92ad2ef1eb5
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-05-29 09:46:40 +0530

    Update 31. Next Permutation.cpp

--- 0001 - 0100/31. Next Permutation.cpp
+++ 0001 - 0100/31. Next Permutation.cpp
@@ -23,6 +23,33 @@ public:
 
 
 
+// OR
+
+class Solution {
+public:        
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-2;
+
+        // Find the first decreasing element from the right
+        while(i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+        if(i >= 0) {
+            // Find the next greater element than nums[i]
+            int j = n-1;
+            while(j > i && nums[j] <= nums[i]) j--;
+            
+            // Swap nums[i] and nums[j]
+            swap(nums[i], nums[j]);
+        }
+        // Reverse the suffix starting from i+1
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
+
+
+
+
 
 // APPROACH 2
 
