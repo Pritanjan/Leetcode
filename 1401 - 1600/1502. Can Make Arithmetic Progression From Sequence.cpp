6f9f276ca59bac34ef9724061f53e44edc2213e8
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-06-06 11:01:33 +0530

    Update 1502. Can Make Arithmetic Progression From Sequence.cpp

--- 1401 - 1600/1502. Can Make Arithmetic Progression From Sequence.cpp
+++ 1401 - 1600/1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -23,5 +23,23 @@ public:
 
 
 
-// APPROACH 2
+// OR
+
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int diff = arr[1] - arr[0];
+        
+        for (int i = 2; i < arr.size(); ++i) {
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
 
