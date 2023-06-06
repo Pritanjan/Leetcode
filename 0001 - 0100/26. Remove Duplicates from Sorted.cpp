ae30c931163afab71194caf50e9114faec39741d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-06-06 19:33:32 +0530

    Update 26. Remove Duplicates from Sorted.cpp

--- 0001 - 0100/26. Remove Duplicates from Sorted.cpp
+++ 0001 - 0100/26. Remove Duplicates from Sorted.cpp
@@ -1,7 +1,5 @@
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// APPROACH 1 [ STL ]
 
-// APPROACH 1
-// STL
 class Solution {
 public:
     int removeDuplicates(vector<int>& arr) {
@@ -11,6 +9,9 @@ public:
 };
 
 
+
+
+
 // APPROACH 2
 
 class Solution {
@@ -18,43 +19,28 @@ public:
     int removeDuplicates(vector<int>& arr) {
         int count = 0;
         for(int i=1; i<arr.size(); i++){
-            if(arr[i] == arr[i-1])
-                count++;
-            else
-                arr[i-count] = arr[i];
+            if(arr[i] == arr[i-1]) count++;
+            else arr[i-count] = arr[i];
         }
-        return arr.size() - count;
-    }
-};
-
-
-// APPROACH 3
-
-class Solution {
-public:
-    int removeDuplicates(vector<int>& arr) {
-        int count = 0;
-        for(int i=0; i<arr.size() - 1; i++){
-            if(arr[i] == arr[i+1])
-                count++;
-            else
-                arr[i+1 - count] = arr[i+1];
-        }
+        // return arr.size() - count;
         arr.erase(arr.end() - count, arr.end());
         return arr.size();
     }
 };
 
 
-// APPROACH 4
-// 2 POINTER
+
+
+
+
+
+// APPROACH 3 [ 2 POINTER ]
 
 class Solution {
 public:
     int removeDuplicates(vector<int>& A) {
         int L = 0;
         int R = 1;
-        
         while(R < A.size()){
             if(A[R] > A[L]){
                 L++;
@@ -68,8 +54,31 @@ public:
 
 
 
-// https://practice.geeksforgeeks.org/problems/6e63df6d2ebdf6408a9b364128bb1123b5b13450/1
 
+
+
+
+// APPROACH 4
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int cnt = 1;
+        for(int i=1; i<nums.size(); ++i){
+            if(nums[i] != nums[i-1]) nums[cnt++] = nums[i];
+        }
+
+        return cnt;
+    }
+};
+
+
+
+
+
+
+
+// https://practice.geeksforgeeks.org/problems/6e63df6d2ebdf6408a9b364128bb1123b5b13450/1
 // Make array elements unique
 
 class Solution {
@@ -86,25 +95,6 @@ class Solution {
         return cnt;
     }
 };
-
-
-
-
-
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        int cnt = 1;
-        for(int i=1; i<nums.size(); ++i){
-            if(nums[i] != nums[i-1]) nums[cnt++] = nums[i];
-        }
-
-        return cnt;
-    }
-};
-
-
-
 
 
 
