db5a42b2939bd1f27bebbf382eae6306aa759efe
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-06-08 15:45:56 +0530

    Update 1351. Count Negative Numbers in a Sorted Matrix.cpp

--- 1201 - 1400/1351. Count Negative Numbers in a Sorted Matrix.cpp
+++ 1201 - 1400/1351. Count Negative Numbers in a Sorted Matrix.cpp
@@ -1,4 +1,29 @@
-// APPROACH 1
+// APPROACH 1 [ BRUTE FOCE ]
+
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int count = 0;
+        // Iterate on all elements of the matrix one by one.
+        for(auto& row : grid) {
+            for(auto& element : row) {
+                // If the current element is negative, we count it.
+                if(element < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+// T.C. --> O(m*n)
+// S.C. --> O(1)
+
+
+
+// APPROACH 2
 
 class Solution {
 public:
@@ -28,3 +53,73 @@ public:
 
 
 
+
+
+
+// APPROACH 2 [ BINARY SEARCH USING STL] 
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int count = 0;
+        int n = grid[0].size();
+        // Iterate on all rows of the matrix one by one.
+        for(auto& row : grid) {
+            // Using binary search find the index of the first negative element.
+            int index = upper_bound(row.begin(), row.end(), 0, greater<int>()) - row.begin();
+            // 'index' points to the first negative element,
+            // which means 'n - index' is the number of all negative elements.
+            count += (n - index);
+        }
+        return count;
+    }
+};
+
+
+
+// T.C. --> O(m log n)
+// For each row of the matrix we perform a binary search which will take O(log⁡n) time.
+// Thus, for mmm rows, overall we will take O(m log n) time.
+// S.C. --> O(1)
+
+
+
+
+
+
+
+// APPROACH 2 [ BINARY SEARCH wirthout USING STL] 
+
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int count = 0;
+        int n = grid[0].size();
+
+        // Iterate on all rows of the matrix one by one.
+        for(auto& row : grid) {
+            // Using binary search find the index of the first negative element.
+            int index = binarySearch(row, 0, n - 1);
+
+            // 'index' points to the first negative element,
+            // which means 'n - index' is the number of all negative elements.
+            count += (n - index);
+        }
+        return count;
+    }
+private:
+    int binarySearch(vector<int>& row, int low, int high) {
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // If the middle element is negative, search in the left half.
+            if(row[mid] < 0) high = mid - 1;
+            // If the middle element is non-negative, search in the right half.
+            else low = mid + 1;
+            
+        }
+        // Return the index of the first negative element.
+        return low;
+    }
+};
