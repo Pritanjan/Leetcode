3aa0e3cb403e34ef8c14770f2bdf2d49f9560a1f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-08 23:04:01 +0530

    Update and rename 15. 3Sum ARRAY 2POINTER SORTING.cpp to 15. 3Sum.cpp

--- 0001 - 0100/15. 3Sum.cpp
+++ 0001 - 0100/15. 3Sum.cpp
@@ -1,7 +1,4 @@
-// https://www.codingninjas.com/codestudio/problems/triplets-with-given-sum_893028?leftPanelTab=2
-
-// APPROACH 1 
-// BRUTE FORCE
+// APPROACH 1 [ BRUTE FORCE ]
 
 #include<set>
 vector<vector<int>> findTriplets(vector<int>arr, int n, int K)  {
@@ -40,46 +37,42 @@ vector<vector<int>> findTriplets(vector<int>arr, int n, int K)  {
 
 
 
-// APPROACH 2 
-
-// 2 POINTER
+// APPROACH 2 [ 2 POINTER ]
 
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res; // Container for the result
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
         
-        vector<vector<int>> answer;
-        
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0; i<nums.size(); i++){
-                
-                int sum  = 0 - nums[i];
-                int left = i+1;
-                int right = nums.size()-1;
-                
-                while(left < right){
-                    if(nums[left] + nums[right] == sum ){
-                        
-                        vector <int> triplet = {nums[i], nums[left], nums[right]};
-                        answer.push_back(triplet);
-                        
-                        while(left < right && nums[left]  == triplet[1]) left++;
-                        while(left < right && nums[right] == triplet[2]) right--;
-                        
-                    }
-                    else if(nums[left] + nums[right] < sum) left++;
-                    else right--;
-                    
-                }
-                while (i + 1 < nums.size() && nums[i + 1] == nums[i]) 
-                i++;
+        for(int i=0; i<n-1; i++) {
+            if(nums[i] > 0) break;
+            int L = i + 1;
+            int R = n - 1;
             
+            while(L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                
+                if(sum == 0) {
+                    res.push_back({nums[i], nums[L], nums[R]});
+                    L++;
+                    R--;
+                    
+                    // Skip duplicate values
+                    while(L < R && nums[L] == nums[L-1]) L++;
+                    while(L < R && nums[R] == nums[R+1]) R--;
+                } 
+                else if(sum > 0) R--;
+                else L++;
             }
-        return answer;
+            
+            // Skip duplicate values
+            while(i < n-1 && nums[i] == nums[i+1]) i++;
+        }        
+        return res;
     }
 };
-
 
 // Time Complexity: O(N^2)
 // Space Complexity: O(1)
