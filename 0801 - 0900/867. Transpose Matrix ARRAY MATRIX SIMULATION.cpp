d206c196e368d35f854abf99967462ab0c23cfb5
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-10 19:38:06 +0530

    Update 867. Transpose Matrix.cpp

--- 0801 - 0900/867. Transpose Matrix ARRAY MATRIX SIMULATION.cpp
+++ 0801 - 0900/867. Transpose Matrix ARRAY MATRIX SIMULATION.cpp
@@ -1,4 +1,6 @@
-//https://leetcode.com/problems/transpose-matrix/
+// inplace works fpr only squuaare matrix only 
+// Use 2 loops and swap       
+
 
 class Solution {
 public:
@@ -12,6 +14,27 @@ public:
             for(int j=0;j<m; j++){
                 mat1[i][j] = matrix[j][i];
             }
+        }
+        return mat1;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {     
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> mat1;
+        
+        for(int i=0; i<n; i++){
+            vector <int> v;
+            for(int j=0;j<m; j++){
+                v.push_back(matrix[j][i]);
+            }
+            mat1.push_back(v);
         }
         return mat1;
     }
