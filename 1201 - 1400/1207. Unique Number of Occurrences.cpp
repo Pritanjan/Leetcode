2bf271e67ff6c8ed17b0aaba868acb405f620f2c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-13 18:45:11 +0530

    Update 1207. Unique Number of Occurrences.cpp

--- 1201 - 1400/1207. Unique Number of Occurrences.cpp
+++ 1201 - 1400/1207. Unique Number of Occurrences.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:  
     bool uniqueOccurrences(vector<int>& arr) {
         map<int, int> mp;
-        for (int i=0; i<arr.size(); i++)
-            mp[arr[i]]++;
+        // Count the occurrences of each value in the array
+        for(int i=0; i<arr.size(); i++) mp[arr[i]]++;
+
+        // set<int> st;
+        // n=0;
+        // for(auto &m : mp){
+        //     n++;
+        //     st.insert(m.second);
+        // }
+        // return n == st.size();
         
         set<int> unq;
         for(auto [first, second] : mp){
             unq.insert(second);
-        }
-            
+        }            
         return unq.size() == mp.size();
     }
 };
