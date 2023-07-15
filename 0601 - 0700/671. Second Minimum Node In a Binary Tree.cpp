4675f33190e3eff58bfd96fb9324f8f332114c16
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-15 16:42:13 +0530

    Create 671. Second Minimum Node In a Binary Tree.cpp

---
+++ 0601 - 0700/671. Second Minimum Node In a Binary Tree.cpp
@@ -0,0 +1,75 @@
+// APPROACH 1 BFS
+
+class Solution {
+public:    
+    int findSecondMinimumValue(TreeNode* root) {
+        if(root -> left == nullptr) return -1;
+
+        queue<TreeNode*> que;
+        que.push(root);
+
+        int m = root->val;
+        int t = pow(2, 31)-1;
+        int f = 0;   
+
+        while(!que.empty()){
+            root = que.front();
+            que.pop();
+            if(root -> left){
+                if(root -> left -> val != root -> right -> val){
+                    if(root -> left -> val > m and root -> left -> val <= t) {
+                        t = root -> left -> val;
+                        f = 1;
+                    }
+                    if(root -> right -> val > m and root -> right -> val <= t){
+                        t = root -> right -> val;
+                        f = 1;
+                    }
+                }      
+                que.push(root->left);
+                que.push(root->right);  
+            } 
+        }
+
+        if(f == 0) return -1;
+        return t;
+    }
+};
+
+
+
+
+
+
+
+
+// APPROACH 2 BFS
+
+class Solution {
+public:
+     void pre(set<int> &v, TreeNode* root){
+         if(root == NULL) return ;
+        //  v.push_back(root -> val);
+         v.insert(root -> val);
+         pre(v, root -> left);
+         pre(v, root -> right);
+     }
+    
+    int findSecondMinimumValue(TreeNode* root) {
+        set<int> v;
+        pre(v, root);
+
+        if(v.size() < 2) return -1;
+
+        auto it = v.begin();
+        it++;
+
+        return *it;
+    }
+}
+
+
+;
+
+
+
