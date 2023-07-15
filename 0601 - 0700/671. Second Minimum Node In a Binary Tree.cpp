c0a04d744da7da0b07ecefea49d2ddc532071ceb
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-15 19:14:06 +0530

    Update 671. Second Minimum Node In a Binary Tree.cpp

--- 0601 - 0700/671. Second Minimum Node In a Binary Tree.cpp
+++ 0601 - 0700/671. Second Minimum Node In a Binary Tree.cpp
@@ -43,7 +43,7 @@ public:
 
 
 
-// APPROACH 2 BFS
+// APPROACH 2 DFS
 
 class Solution {
 public:
@@ -60,16 +60,41 @@ public:
         pre(v, root);
 
         if(v.size() < 2) return -1;
-
-        auto it = v.begin();
+        auto it = v.begin(); 
         it++;
 
         return *it;
     }
-}
-
-
-;
+};
 
 
 
+
+
+
+// LEETCODE EDITORIAL 2    
+
+class Solution {
+private:
+    int min1;
+    long long ans = LLONG_MAX;
+
+    void dfs(TreeNode* root) {
+        if(root != nullptr) {
+            if(min1 < root -> val && root -> val < ans) {
+                ans = root -> val;
+            } 
+            else if(min1 == root -> val) {
+                dfs(root -> left);
+                dfs(root -> right);
+            }
+        }
+    }
+
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        min1 = root -> val;
+        dfs(root);
+        return ans < LLONG_MAX ? static_cast<int>(ans) : -1;
+    }
+};
