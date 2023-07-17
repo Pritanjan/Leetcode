1608d1b2731a5554ed2b2a6bb61ad22f2ca62967
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-17 20:12:11 +0530

    Update 415. Add Strings.cpp

--- 0401 - 0500/415. Add Strings.cpp
+++ 0401 - 0500/415. Add Strings.cpp
@@ -24,7 +24,45 @@ public:
 };
 
 
-Time: O(M+N), where M <= 10^4 is length of num1, N <= 10^4 is length of num2.
-Extra Space (without counting output as space): O(1)
+// Time: O(M+N), where M <= 10^4 is length of num1, N <= 10^4 is length of num2.
+// Extra Space (without counting output as space): O(1)
+
+
+
+// OR
+
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int n = num1.size();
+        int m = num2.size();
+        int carry = 0;
+        string res = "";
+        
+        // Convert strings to character arrays
+        const char* str1 = num1.c_str();
+        const char* str2 = num2.c_str();
+        
+        // Pointers to the last character of each string
+        int i = n - 1;
+        int j = m - 1;
+        
+        // Iterate over the strings from right to left
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            
+            if(i >= 0) sum += str1[i--] - '0';
+            if(j >= 0) sum += str2[j--] - '0';
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            res = char(sum + '0') + res;
+        }
+        return res;
+    }
+};
+
+
 
 
