d50f1cfe4a71c7dc6da52c5c6aaf1febf8144699
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-20 14:05:01 +0530

    Update 735. Asteroid Collision.cpp

--- 0701 -0800/735. Asteroid Collision.cpp
+++ 0701 -0800/735. Asteroid Collision.cpp
@@ -21,44 +21,29 @@
 
 // 8. Finally, the result vector is returned.
 
-
-
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& a) {
         stack <int> s;
         int n = a.size();
-        for(int i = 0;i<n;i++){
-            if(s.empty())
-            {
-                s.push(a[i]);
-            }
+        for(int i=0; i<n; i++){
+            if(s.empty()) s.push(a[i]);
             else{
-                while( !s.empty() and s.top()>0 and a[i]<0){
-                    if(abs(s.top())<abs(a[i])){
-                        s.pop();
-                    }
-                    else {
-                        break;
-                    }
-
-                 
+                while(!s.empty() and s.top() > 0 and a[i] < 0){
+                    if(abs(s.top())<abs(a[i])) s.pop();
+                    else break;                
                 }
-                if(!s.empty() and s.top() >0 and a[i]<0){
-                    if(abs(s.top())==abs(a[i])){
+                if(!s.empty() and s.top() > 0 and a[i] < 0) {
+                    if(abs(s.top())==abs(a[i])) {
                         s.pop();
                         continue;
                     }
-                    else {
-                        continue;
-                    }
+                    else continue;
                 }
-                else{
-                    s.push(a[i]);
-                }                
+                else s.push(a[i]);            
             }
-
         }
+        
         vector<int> ans;
         while(!s.empty()){
             ans.push_back(s.top());
@@ -66,7 +51,6 @@ public:
         }
         reverse(ans.begin() ,ans.end());
         return ans;
-       
     }
 };
 
@@ -79,24 +63,23 @@ public:
 // asteroids vector.
 
 
+
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& A) {
         vector<int> ans;
         for(int i : A) {
-            while(!ans.empty() and ans.back() > 0 and ans.back() < -i){
-                ans.pop_back();
-            }
-            if(ans.empty() or ans.back() < 0 or i > 0){
-                ans.push_back(i);
-            }
-            else if(i < 0 and ans.back() == -i){
-                ans.pop_back();
-            }
+            while(!ans.empty() and ans.back() > 0 and ans.back() < -i) ans.pop_back();
+            
+            if(ans.empty() or ans.back() < 0 or i > 0) ans.push_back(i);
+            else if(i < 0 and ans.back() == -i) ans.pop_back();
         }
         return ans;
     }
 };
+
+
 
 
 
@@ -106,22 +89,13 @@ public:
         stack<int> st;
 
         for(auto it: asteroids){
-            if(it>=0) st.push(it);
-            else{
-                while(st.size() && st.top()<abs(it) && st.top()>=0){
-                    st.pop();
-                }
-
-                if(st.size()==0){
-                    st.push(it);
-                }
-                else{
-                    if(st.top()==abs(it)){
-                        st.pop();
-                    }
-                    else if(st.top()<0){
-                        st.push(it);
-                    }
+            if(it >= 0) st.push(it);
+            else {
+                while(st.size() && st.top() < abs(it) && st.top() >= 0) st.pop();
+                if(st.size()==0) st.push(it);
+                else {
+                    if(st.top() == abs(it)) st.pop();
+                    else if(st.top() < 0) st.push(it);
                 }
             }
         }
@@ -133,8 +107,9 @@ public:
         }
 
         reverse(ans.begin(),ans.end());
-
         return ans;
     }
 };
+
+
 
