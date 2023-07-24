0e4fd38a426088ceaabef276bf702e86e063b9e2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-24 10:11:00 +0530

    Update 50. Pow(x, n).cpp

--- 0001 - 0100/50. Pow(x, n).cpp
+++ 0001 - 0100/50. Pow(x, n).cpp
@@ -38,3 +38,57 @@ public:
 };
 
 
+
+// OR
+
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    double myPow(double x, long long n) {
+        // Handle negative exponent separately to avoid overflow with -INT_MIN
+        if(n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        return f(x, n);
+    }
+
+    double f(double base, long long exponent) {
+        double res = 1;
+        while(exponent > 0) {
+            // If exponent is odd, multiply the result with base
+            if(exponent % 2 == 1) res *= base;
+
+            // Reduce the power of base and double the result
+            base *= base; 
+            exponent /= 2;
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1.0;
+        int sign = (n>0)?1:-1;
+        long long exp =( long long)n * sign;//avoid of overflow for n=INT_MIN change sign 
+        bitset<32> bexp(exp);
+        double a = 1;
+        x = (sgn == 1) ? x : 1.0 / x;
+        for(int b=31; b>=0; b--){
+            a = a*a;
+            if(bexp[b]) a *= x;
+        }
+        return a;
+    }
+};
+
+
+
+
