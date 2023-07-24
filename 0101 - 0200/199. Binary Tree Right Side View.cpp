7f13c5bd1daa61496dc94ef66a5b5d2c97dac801
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-24 18:05:07 +0530

    Update 199. Binary Tree Right Side View.cpp

--- 0101 - 0200/199. Binary Tree Right Side View.cpp
+++ 0101 - 0200/199. Binary Tree Right Side View.cpp
@@ -1,6 +1,6 @@
 // Right View of Binary Tree
 
-// APPROACH 2 DFS 
+// APPROACH 1 DFS 
 
 class Solution {
 public:
@@ -59,30 +59,6 @@ public:
 
 // T.C. O(N)
 // S.C. O(N)
-
-// Left View of Binary Tree
-// https://www.geeksforgeeks.org/print-left-view-binary-tree/
-// https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
-
-void LEFT(Node* root, int level, int& mxLevel, vector<int>& res){
-    if(root == nullptr) return ;
-    
-    if(level > mxLevel) {
-        mxLevel = level;
-        res.push_back(root -> data);
-    }
-    
-    
-    LEFT(root->left, level + 1, mxLevel, res);
-    LEFT(root->right, level + 1, mxLevel, res);
-}
-
-vector<int> leftView(Node *root){
-    vector<int> res;
-    int mxLevel = -1;
-    LEFT(root, 1, mxLevel, res);
-    return res;
-}
 
 
 
