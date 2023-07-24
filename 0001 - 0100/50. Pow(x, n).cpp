063aa7e405e25ddf2a7ceeb26eea59e116af4ed9
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-24 09:54:32 +0530

    Update 50. Pow(x, n).cpp

--- 0001 - 0100/50. Pow(x, n).cpp
+++ 0001 - 0100/50. Pow(x, n).cpp
@@ -1,25 +1,40 @@
-// https://leetcode.com/problems/powx-n/
-
 class Solution {
 public:
     double myPow(double x, int n) {
-		if(n==0)
-			return 1;
-		// else if(n>0)
-		// 	return x*myPow(x,n-1);
-		// else
-		// 	return (1/x)*myPow(x,n+1);
-        
-        
-        if(n<0){
-        //  n=-n;
-        //  x=(1/x);
-        	return 1/x * myPow(1/x, -(n+1));
-        }
-        return (n%2==0) ? myPow(x*x, n/2 ) : x*myPow(x*x, n/2);
-        
+	if(n == 0) return 1;    
+        if(n <  0) return 1/x * myPow(1/x, -(n+1));        
+        return (n % 2 == 0) ? myPow(x*x, n/2 ) : x*myPow(x*x, n/2);
     }
 };
 
+
+
+
+// OR
+
+
+
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    double myPow(double x, long long n) {
+        if(n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        return f(x, n);
+    }
+
+    double f(double base, long long exponent) {
+        double res = 1;
+        while(exponent > 0) {
+            if(exponent % 2 == 1)
+                res = fmod(res * base, mod);
+            base = fmod(base * base, mod);
+            exponent = exponent / 2;
+        }
+        return res;
+    }
+};
 
 
