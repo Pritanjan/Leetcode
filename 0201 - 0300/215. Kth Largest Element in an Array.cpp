7bdbf1414352aeaa1d99f315f34141dd287d54ec
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-27 18:18:36 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- 0201 - 0300/215. Kth Largest Element in an Array.cpp
+++ 0201 - 0300/215. Kth Largest Element in an Array.cpp
@@ -4,9 +4,7 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         priority_queue<int> pq(nums.begin(), nums.end() );
-        for(int i=0; i<k-1; i++){
-                pq.pop();
-        }
+        for(int i=0; i<k-1; i++) pq.pop();
         return pq.top();
     }
 };
@@ -83,64 +81,30 @@ public:
 
 class Solution {
 public:
-    int quick_sort(vector<int>& nums, int l, int r, int k) {
-        if(l == r) return nums[l];
-        int i = l - 1, j = r + 1, x = nums[ (i + j) >> 1];
+    int quick_sort(vector<int>& nums, int L, int R, int k) {
+        if(L == R) return nums[L];
+        int i = L-1;
+        int j = R+1;
+        int x = nums[(i+j) >> 1];
+        
         while(i < j){
             do i++; while(nums[i] > x);
             do j--; while(nums[j] < x);
             if(i < j) swap(nums[i],nums[j]);
         }
-        int ll = j - l + 1;
-        if(ll >= k) return quick_sort(nums,l,j,k);
-        return quick_sort(nums,j + 1, r, k - ll);
+
+        int ll = j - L + 1;
+        if(ll >= k) return quick_sort(nums, L, j, k);
+        return quick_sort(nums, j+1, R, k-ll);
     }
+    
     int findKthLargest(vector<int>& nums, int k) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-        return quick_sort(nums,0,nums.size() - 1,k);
+        return quick_sort(nums, 0, nums.size()-1, k);
     }
 };
 
 
 
-
-
-
-
-
-
-
-
-
-
-// Kth smallest element
-// https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1
-
-class Solution{
-    public:
-    // arr : given array
-    // l : starting index of the array i.e 0
-    // r : ending index of the array i.e size-1
-    // k : find kth smallest element and return using this function
-    int kthSmallest(int p[], int l, int r, int k) {
-        priority_queue<int> pq;
-        
-        // step 1 
-        for(int i=0; i<k; i++) pq.push(p[i]);
-        
-        // step 2
-        for(int i=k; i<=r; i++){
-            if(p[i] < pq.top()){
-                pq.pop();
-                pq.push(p[i]);
-            }
-        }
-        
-        int res = pq.top();
-        return res;
-    }
-};
 
 
 
