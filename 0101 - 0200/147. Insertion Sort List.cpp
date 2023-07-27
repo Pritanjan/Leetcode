473dde2391bc892b29dfd0b8fde768ba5fbafcc9
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-27 22:56:53 +0530

    Update 147. Insertion Sort List.cpp

--- 0101 - 0200/147. Insertion Sort List.cpp
+++ 0101 - 0200/147. Insertion Sort List.cpp
@@ -1,16 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-
-
 // APPROACH 1 INSERTION SORT 
 
 class Solution {
@@ -21,61 +8,49 @@ public:
 
         while(curr){
             ListNode* prev = res;
-            while(prev -> next != NULL && prev -> next -> val <= curr -> val){
-                prev = prev -> next;
-            }
+            while(prev -> next != NULL && prev -> next -> val <= curr -> val) prev = prev -> next;
 
             ListNode* next = curr -> next;
             curr -> next = prev -> next;
             prev -> next = curr;
-
+    
             curr = next;
         }
         return res -> next;
     }
 };
 
-// N be the number of elements in the input list.
-// Time Complexity: O(N^2)
-// First of all, we run an iteration over the input list.
-// At each iteration, we insert an element into the resulting list. In the worst case where the
-// position to insert is the tail of the list, we have to walk through the entire resulting list.
-// As a result, the total steps that we need to walk in the worst case would be N(N+1)/2  .
 
+// T.C. --> O(N^2)  // N be the number of elements 
+// First iterate over the input list.
+// At each iteration, insert an element into the resulting list. 
+// In the worst case where the position to insert is the tail of the list, 
+// we have to move through the entire resulting list.
+// As a result, the total steps that we need to walk in the worst case would be N(N+1)/2  .
 // To sum up, the overall time complexity of the algorithm is O(N^2).
 
-
 // Space Complexity: O(1)
-// We used some pointers within the algorithm. However, their memory consumption is constant 
-// regardless of the input.
-// Note, we did not create new nodes to hold the values of input list, but simply reorder the 
-// existing nodes.
+// some pointers within the algorithm are used. Therefore, memory is constant.
+// we did not create new nodes to hold the values of input list, but reorder the existing nodes.
 
 
 
 // APPROACH 2 RECURSIVE 
 
-
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        if(head == NULL || head -> next == NULL) {
-            return head;
-        }
+        if(head == NULL || head -> next == NULL) return head;
         head -> next = insertionSortList(head -> next);
-        
         return insert(head);
     }
 
     ListNode* insert(ListNode *head){
-        if(head -> next == NULL || head -> next -> val > head -> val) {
-            return head;
-        }
+        if(head -> next == NULL || head -> next -> val > head -> val) return head;
         ListNode* temp = head -> next;
         head -> next = temp -> next;
         temp -> next = insert(head);
         return temp;
-
     }
 };
 
