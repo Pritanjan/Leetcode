baa85529096fd080b93f885754e66619395d1b3c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-07-28 21:20:26 +0530

    Update 164. Maximum Gap.cpp

--- 0101 - 0200/164. Maximum Gap.cpp
+++ 0101 - 0200/164. Maximum Gap.cpp
@@ -1,17 +1,86 @@
-// https://leetcode.com/problems/maximum-gap/
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        int diff = 0;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n-1; i++){
+            diff = max(diff, nums[i+1] - nums[i]);
+        }        
+        return diff;
+    }
+};
+
 
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        // if(nums.size() < 2)
-            // return 0;
+        int n = nums.size();
+        // If the array contains less than two elements, return 0.
+        if(n < 2) return 0;
         
-        int dif=0;
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-1; i++){
-            dif = max(dif, nums[i+1] - nums[i]);
+        // Find the maximum element in the array.
+        int mx = *max_element(nums.begin(), nums.end());
+        
+        // Perform Radix Sort.
+        int exp = 1;
+        vector<int> temp(n), count(10, 0);
+        
+        while(mx / exp > 0) {
+            // Count the occurrences of each digit at the current place value.
+            for(int i=0; i<n; i++) count[(nums[i] / exp) % 10]++;
+            
+            // Update the count array to hold the cumulative count of elements.
+            for(int i=1; i<10; i++) count[i] += count[i - 1];
+            
+            // Rearrange elements into the temporary array based on their place value.
+            for(int i=n-1; i>=0; i--) temp[--count[(nums[i] / exp) % 10]] = nums[i];
+            
+            // Copy the elements back to the original array.
+            for(int i=0; i<n; i++) nums[i] = temp[i];
+            
+            // Move to the next place value.
+            exp *= 10;
+            
+            // Reset the count array.
+            fill(count.begin(), count.end(), 0);
         }
         
-        return dif;
+        // Calculate the maximum difference between two successive elements.
+        int maxDiff = 0;
+        for(int i=1; i<n; i++) maxDiff = max(maxDiff, nums[i] - nums[i - 1]);
+        return maxDiff;
     }
 };
+
+
+// OR
+
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 2) return 0;
+        
+        int mx = *max_element(nums.begin(), nums.end());
+        
+        int exp = 1;
+        vector<int> temp(n), count(10, 0);
+        
+        while(mx / exp > 0) {
+            for(int i=0; i<n; i++) count[(nums[i] / exp) % 10]++;
+            for(int i=1; i<10; i++) count[i] += count[i - 1];
+            for(int i=n-1; i>=0; i--) temp[--count[(nums[i] / exp) % 10]] = nums[i];
+            for(int i=0; i<n; i++) nums[i] = temp[i];            
+            exp *= 10;
+            fill(count.begin(), count.end(), 0);
+        }
+        
+        int maxDiff = 0;
+        for(int i=1; i<n; i++) maxDiff = max(maxDiff, nums[i] - nums[i - 1]);
+        return maxDiff;
+    }
+};
+
+
