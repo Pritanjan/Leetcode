2288b7fc8b7be5d8be9901d2bff7a95c1988af3e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-02 18:40:03 +0530

    Update 46. Permutations.cpp

--- 0001 - 0100/46. Permutations.cpp
+++ 0001 - 0100/46. Permutations.cpp
@@ -25,7 +25,9 @@ public:
 
 
 
-// APPROACH 2  Recursive
+// APPROACH 2  BACKTRACKING
+
+// Swap elements to generate permutations and then swapping them back.
 
 class Solution {
 public:
@@ -52,31 +54,18 @@ public:
 
 
 
-
-
-
-// APPROACH 3
+// Approach 3 STL
 
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void permute(int idx, vector<int>& nums){
-        int n = nums.size();
-        if(idx = n){
-            ans.push_back(nums);
-        }
-        else{
-            for(int i=idx; i<n; i++){
-                swap(nums[idx], nums[i]);
-                permute(idx+1, nums);
-            }
-        }
-        
-    }
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        permute(ans, nums, 0);
-        return ans;
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        do{
+            res.push_back(nums);
+        }while(next_permutation(nums.begin(), nums.end()));
+
+        return res;
     }
 };
 
@@ -85,12 +74,93 @@ public:
 
 
 
-```javascript []
-console.log('Hello world!')
-```
-```C++ []
-print('Hello world!')
-```
-```ruby []
-puts 'Hello world!'
-```
+
+// Approach 4 More Efficient than backtracking (Heap's Algorithm)
+
+class Solution {
+public:
+    void f(vector<int>& nums, int n, vector<vector<int>>& res) {
+        if(n == 1) {
+            res.push_back(nums);
+            return ;
+        }
+
+        for(int i=0; i<n; ++i) {
+            f(nums, n-1, res);
+            if(n%2 == 0) swap(nums[i], nums[n-1]);
+            else swap(nums[0], nums[n-1]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        f(nums, nums.size(), res);
+        return res;
+    }
+};
+
+
+
+
+
+
+// APPROOACH 5 Bitmaskng Approach
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        int totalPermutations = 1 << n;
+        vector<vector<int>> result;
+
+        for (int i = 0; i < totalPermutations; ++i) {
+            vector<int> permutation;
+
+            for (int j = 0; j < n; ++j) {
+                if (i & (1 << j)) {
+                    permutation.push_back(nums[j]);
+                }
+            }
+
+            result.push_back(permutation);
+        }
+
+        return result;
+    }
+};
+
+
+
+
+
+
+// APPROACH  6 uSING count array approach
+
+class Solution {
+public:        
+    void f(vector<int>& nums, vector<int>& cnt, int level, vector<int>& per, vector<vector<int>>& res) {
+        if(level == nums.size()) {
+            res.push_back(per);
+            return ;
+        }
+
+        for(int i=0; i<nums.size(); ++i) {
+            if(cnt[i] == 0) continue;
+            
+            cnt[i]--;
+            per[level] = nums[i];
+            f(nums, cnt, level+1, per, res);
+            cnt[i]++;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> cnt(nums.size(), 1); // Count array to track usage of elements
+        vector<int> per(nums.size());
+        f(nums, cnt, 0, per, res);
+        return res;
+    }
+};
+
+
