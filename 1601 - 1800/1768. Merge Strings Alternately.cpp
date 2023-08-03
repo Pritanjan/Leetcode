e898815a421d248204204f081a3f788bf8e114ce
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-03 16:39:48 +0530

    Update 1768. Merge Strings Alternately.cpp

--- 1601 - 1800/1768. Merge Strings Alternately.cpp
+++ 1601 - 1800/1768. Merge Strings Alternately.cpp
@@ -36,6 +36,30 @@ public:
 };
 
 
+// OR Using Iterator
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res;
+        auto it1 = word1.begin();
+        auto it2 = word2.begin();
+        
+        while(it1 != word1.end() || it2 != word2.end()) {
+            if(it1 != word1.end()) {
+                res += *it1;
+                ++it1;
+            }
+            if(it2 != word2.end()) {
+                res += *it2;
+                ++it2;
+            }
+        }        
+        return res;
+    }
+};
+
+
 
 
 // APPROACH 2 [ One Pointers ]
@@ -80,4 +104,27 @@ public:
 
 
 
+
+
+
+// APPROACH 4 
+
+class Solution {
+public:
+    string mergeAlternately(string s1, string s2) {
+        string res = "";
+        int n = min(s1.length(), s2.length());
+        
+        for(int i=0; i<n; i++) {
+            res += s1[i];
+            res += s2[i];
+        }
+        
+        // Add any remaining characters from the longer string
+        if(s1.length() > n) res += s1.substr(n);
+        else if(s2.length() > n) res += s2.substr(n);
+                
+        return res;
+    }
+};
 
