3371adf66c127ae167df040962b1504d313399db
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-06 20:31:51 +0530

    Update 13. Roman to Integer.cpp

--- 0001 - 0100/13. Roman to Integer.cpp
+++ 0001 - 0100/13. Roman to Integer.cpp
@@ -65,8 +65,7 @@ public:
 
 
 
-
-// APPROACH 3
+// APPROACH 3 [ Use Switch }
 
 class Solution {
 public:
@@ -120,3 +119,36 @@ public:
     }
 };
 
+
+// OR
+
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        for(int i=0; i<s.length(); ++i) {
+            if(s[i] == 'I' && (s[i+1] == 'V' || s[i+1] == 'X')) res -= 1;
+            else if(s[i] == 'X' && (s[i+1] == 'L' || s[i+1] == 'C')) res -= 10;
+            else if(s[i] == 'C' && (s[i+1] == 'D' || s[i+1] == 'M')) res -= 100;
+            else {
+                switch(s[i]) {
+                    case 'I': res += 1; break;
+                    case 'V': res += 5; break;
+                    case 'X': res += 10; break;
+                    case 'L': res += 50; break;
+                    case 'C': res += 100; break;
+                    case 'D': res += 500; break;
+                    case 'M': res += 1000; break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+// APPROACH 4 
