776f3a4a8e104a24a027a8ad8ca160f278f76627
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-09 12:19:54 +0530

    Update 1822. Sign of the Product of an Array.cpp

--- 1801 - 2000/1822. Sign of the Product of an Array.cpp
+++ 1801 - 2000/1822. Sign of the Product of an Array.cpp
@@ -1,43 +1,34 @@
-class Solution {
-public:
-    int arraySign(vector<int>& nums) {
-        int count =  0;
-        for(int i : nums ){
-            if(i==0)
-                return 0;
-            if(i<0)
-                count++;
-        }
-        
-        return count%2==0 ? 1 : -1;
-    }
-};
-
-
-
-
+// APPROACH 1 
 
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int  p = 1;
-        int count = 0;
-        
+        int cnt = 0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] == 0){
-                return 0;
-                // break;
-            }
-        
-            if(nums[i] < 0){
-                count++;
-            }    
+            if(nums[i] == 0) return 0;        
+            if(nums[i] < 0) cnt++;
         }
         
-        if(count % 2 == 0)
-            return 1;
-        else 
-            return  -1;
-        
+        if(cnt % 2 == 0) return 1;
+        else return  -1;
     }
 };
+
+
+
+
+
+// APPROACH 2
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int cnt =  0;
+        for(int i : nums ){
+            if(i == 0) return 0;
+            if(i < 0) cnt++;
+        }
+        return cnt%2 == 0 ? 1 : -1;
+    }
+};
+
