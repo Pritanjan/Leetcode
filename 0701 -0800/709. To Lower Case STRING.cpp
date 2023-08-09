3cbb51d8c9ac5589b6ef7e5b7c2978ef5a360893
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-09 17:12:50 +0530

    Update 709. To Lower Case STRING.cpp

--- 0701 -0800/709. To Lower Case STRING.cpp
+++ 0701 -0800/709. To Lower Case STRING.cpp
@@ -1,9 +1,9 @@
-//https://leetcode.com/problems/to-lower-case/
+// APPROACH 1
 
 class Solution {
 public:
     string toLowerCase(string s) {
-        for(auto& ch : s){
+        for(auto& ch : s) {
             ch = tolower(ch);
         }
         return s;
@@ -11,16 +11,49 @@ public:
 };
 
 
-// APPROACH 2
+// OR
 
 class Solution {
 public:
     string toLowerCase(string s) {
-        for(auto& ch : s){
-            // ch = tolower(ch);
-            if(ch >= 'A' && ch <= 'Z')
-                ch += 'a' - 'A';
+        string res = s;
+        for(size_t i=0; i<res.size(); ++i) {
+            if(isupper(res[i])) res[i] = tolower(res[i]);
+        }
+        return res;
+    }
+};
+
+
+// OR
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string res = s;
+        transform(res.begin(), res.end(), res.begin(), [](char c) {
+            return tolower(c);
+        });
+        return res;
+    }
+};
+
+
+
+
+
+
+// APPROACH 2 [  Index Manipulation ]
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(auto& ch : s) {
+            if(ch >= 'A' && ch <= 'Z') ch += 'a' - 'A';
         }
         return s;
     }
 };
+
+
+
