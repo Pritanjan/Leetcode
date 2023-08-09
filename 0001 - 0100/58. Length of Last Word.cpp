c9cef0f21e2a5df1d5e5242ccceb53c443f5ef8f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-09 15:54:10 +0530

    Update 58. Length of Last Word.cpp

--- 0001 - 0100/58. Length of Last Word.cpp
+++ 0001 - 0100/58. Length of Last Word.cpp
@@ -1,32 +1,34 @@
-//https://leetcode.com/problems/length-of-last-word/
+// APPROACH 1
 
 class Solution {
 public:
     int lengthOfLastWord(string s) {
         stringstream s1(s);
         string s2;
-        
         while(s1 >> s2){}
-        
         return s2.size();
     }
 };
 
 
 
+
+
+// APPROACH 2
+
 class Solution {
 public:
     int lengthOfLastWord(string s) {
         int size = 0;
         for(int i=s.size()-1; i>=0; i--){
-            if(s[i] != ' ')
-                size++;
+            if(s[i] != ' ') size++;
             // and size > 0  is added because if space is given 
             // in the last 
             // s = "   fly me   to   the moon  "
-            if(s[i] == ' ' and size > 0)
-                return size;
+            if(s[i] == ' ' and size > 0) return size;
         }
         return size;
     }
 };
+
+
