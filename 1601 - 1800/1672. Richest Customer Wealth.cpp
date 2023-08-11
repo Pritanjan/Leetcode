719793f0cee34c39ed5f4557311fabb98b647d8c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-11 19:39:12 +0530

    Update and rename 1672. Richest Customer Wealth 1 ARRAY MATRIX.cpp to 1672. Richest Customer Wealth.cpp

--- 1601 - 1800/1672. Richest Customer Wealth.cpp
+++ 1601 - 1800/1672. Richest Customer Wealth.cpp
@@ -1,23 +1,18 @@
-// APPROACH 1
+// APPROACH 1 Brute Force
 
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int ans = INT_MIN;
-        int sum ;
-        
+        int sum;
         for(auto i : accounts){
             sum = 0;
-            for(auto j : i){
-                sum += j;
-            }
+            for(auto j : i) sum += j;
             ans = max(sum, ans);
         }
         return ans;
     }
 };
-
-
 
 
 
