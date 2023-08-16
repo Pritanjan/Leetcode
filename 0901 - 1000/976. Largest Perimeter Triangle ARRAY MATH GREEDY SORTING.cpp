7c4ef4bd37d92ec1e5bc8e36c0e720b3ba6daf10
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-16 19:20:17 +0530

    Update 976. Largest Perimeter Triangle ARRAY MATH GREEDY SORTING.cpp

--- 0901 - 1000/976. Largest Perimeter Triangle ARRAY MATH GREEDY SORTING.cpp
+++ 0901 - 1000/976. Largest Perimeter Triangle ARRAY MATH GREEDY SORTING.cpp
@@ -1,4 +1,34 @@
-// https://leetcode.com/problems/largest-perimeter-triangle/
+// APPOACH 2 BRUTE FORECE TLE
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        int maxPerimeter = 0;
+        
+        for(int i=0; i<n-2; ++i) {
+            for(int j=i+1; j<n-1; ++j) {
+                for(int k=j+1; k<n; ++k) {
+                    if (nums[i] + nums[j] > nums[k] &&
+                        nums[j] + nums[k] > nums[i] &&
+                        nums[k] + nums[i] > nums[j]) {
+                        maxPerimeter = max(maxPerimeter, nums[i] + nums[j] + nums[k]);
+                    }
+                }
+            }
+        }        
+        return maxPerimeter;
+    }
+};
+
+
+
+
+
+
+// APPROACH 1 GREEEDY
+
+// sort the array in descending order and then iterate through it to find the largest valid triangle perimeter.
 
 class Solution {
 public:
@@ -12,3 +42,8 @@ public:
         return 0;
     }
 };
+
+
+
+
+
