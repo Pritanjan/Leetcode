e5baebf6791cad74a481315d4ef366f4f006a65b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-16 15:54:05 +0530

    Update 2164. Sort Even and Odd Indices Independently.cpp

--- 2000 - 2100/2164. Sort Even and Odd Indices Independently.cpp
+++ 2000 - 2100/2164. Sort Even and Odd Indices Independently.cpp
@@ -1,7 +1,5 @@
-
-
-
 // APPROACH 1
+
 class Solution {
 public:
     vector<int> sortEvenOdd(vector<int>& nums) {
@@ -9,27 +7,22 @@ public:
         vector<int> even, odd;
         
         for(int i=0; i<n; i++){
-            if(i%2 == 0)
-                even.push_back(nums[i]);   
-            else
-                odd.push_back(nums[i]);    
-        }
-        
-        sort(even.begin(), even.end());
-        sort(odd.rbegin(), odd.rend());
-                
-        for(int i=0,j=0; i<n; i+=2){
-            nums[i] = even[j++];
-        }
-        for(int i=1,j=0; i<n; i+=2){
-            nums[i] = odd[j++];    
+            if(i%2 == 0) even.push_back(nums[i]);   
+            else odd.push_back(nums[i]);    
         }        
+        sort(even.begin(), even.end());
+        sort(odd.rbegin(), odd.rend());        
+        for(int i=0,j=0; i<n; i+=2) nums[i] = even[j++];
+        for(int i=1,j=0; i<n; i+=2) nums[i] = odd[j++];    
         return nums;
     }
 };
 
-TC : O(NlogN) , SC : O(N)
+// TC : O(NlogN) , SC : O(N)
 
 
 
+
+
+// APPROACH 2 
 
