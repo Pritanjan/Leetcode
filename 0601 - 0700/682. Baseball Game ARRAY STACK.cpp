a8184d52389dc650dc61007e7ab2ed7986b71583
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-16 21:17:25 +0530

    Update 682. Baseball Game ARRAY STACK.cpp

--- 0601 - 0700/682. Baseball Game ARRAY STACK.cpp
+++ 0601 - 0700/682. Baseball Game ARRAY STACK.cpp
@@ -1,4 +1,4 @@
-// APPROACH 1
+// APPROACH 1 use stack
 
 class Solution {
 public:
@@ -27,9 +27,38 @@ public:
     }
 };
 
+ 
+
+
+
+// APPROACH 2 use array
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> v;    
+        for(string& op : operations) {
+            int n = v.size();
+            if(op == "+") v.push_back(v[n-1] + v[n-2]);
+            else if(op == "D") v.push_back(2 * v[n-1]);
+            else if(op == "C") v.pop_back();
+            else v.push_back(stoi(op));
+        }
+        
+        int res = 0;
+        for(int i : v) {
+            res += i;
+        }        
+        return res;
+    }
+};
 
 
 
 
-// APPROACH 2
+
+
+// APPROACH 3
+
+
 
