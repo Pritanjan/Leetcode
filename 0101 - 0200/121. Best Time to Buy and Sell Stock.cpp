5b36663979104681d639ea1d5f0acf0ac2570551
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-17 19:59:16 +0530

    Update 121. Best Time to Buy and Sell Stock.cpp

--- 0101 - 0200/121. Best Time to Buy and Sell Stock.cpp
+++ 0101 - 0200/121. Best Time to Buy and Sell Stock.cpp
@@ -93,3 +93,26 @@ public:
 
 
 
+// APPROACH 4 Kadane's Algorithm
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxCur = 0;
+        int maxSoFar = 0;
+        int n = prices.size();
+        
+        for(int i=1; i<n; ++i) {
+            maxCur = max(0, maxCur + prices[i] - prices[i-1]);
+            maxSoFar = max(maxSoFar, maxCur);
+        }        
+        return maxSoFar;
+    }
+};
+
+
+
+
+
+// APPROACH 5 
+
