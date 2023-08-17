193430137e3af190c52a5783b8c7374b7418a137
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-17 10:59:35 +0530

    Update 1572. Matrix Diagonal Sum.cpp

--- 1401 - 1600/1572. Matrix Diagonal Sum.cpp
+++ 1401 - 1600/1572. Matrix Diagonal Sum.cpp
@@ -63,6 +63,17 @@ public:
 };
 
 
+// or
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+        for(int i=0; i<n; i++) sum += mat[i][i] + mat[i][n-1-i];
+        return sum - (n % 2 == 1 ? mat[n/2][n/2] : 0);
+    }
+};
 
 
 // APPROACH 4
