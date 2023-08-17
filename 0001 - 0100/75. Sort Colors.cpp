0f4136b74b2cbe2ac15fb5270598f6d846f583a6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-17 18:40:06 +0530

    Update 75. Sort Colors.cpp

--- 0001 - 0100/75. Sort Colors.cpp
+++ 0001 - 0100/75. Sort Colors.cpp
@@ -23,16 +23,13 @@ class Solution {
 public:
     void sortColors(vector<int>& a) {
         int L = 0;
-        int n = a.size();
-        int R = n-1;
+        int R = a.size() - 1;
         int mid = 0;
         
-        while(mid <= R ){
+        while (mid <= R){
             switch(a[mid]){
-                case 0 : {
-                    swap(a[L], a[mid]);
-                    L++;
-                    mid++;
+                case 0 :{
+                    swap(a[L++], a[mid++]);
                     break;
                 }
                 case 1 : {
@@ -40,14 +37,16 @@ public:
                     break;
                 }
                 case 2 : {
-                    swap(a[mid], a[R]);
-                    R--;
+                    swap(a[mid], a[R--]);
                     break;
-                }   
+                }
             }
         }
     }
 };
+
+// https://leetcode.com/problems/sort-colors/discuss/1981035/c-solution
+
 
 
 
