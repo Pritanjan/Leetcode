8ef4d6196d0c53b87e74b1b61b81b77ce5280f86
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-18 09:37:18 +0530

    Update and rename 48. Rotate Image ARRAY MATH MATRIX.cpp to 48. Rotate Image.cpp

--- 0001 - 0100/48. Rotate Image.cpp
+++ 0001 - 0100/48. Rotate Image.cpp
@@ -123,12 +123,33 @@ public:
                 k--;
             }
         }
-        
-//         // reverse the matrix
-//         for(int i=0; i<n; i++)
-//             reverse(matrix[i].begin(), matrix[i].end());
     }
 };
+
+
+// OR
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // transpose the matrix
+        int n = matrix.size();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+                cout<<i<<" "<<j<<endl;
+            }
+        }
+        // reverse the matrix
+        for(int i=0; i<n; i++)
+            reverse(matrix[i].begin(), matrix[i].end());
+    }
+};
+
+
+
+
 
 
 
