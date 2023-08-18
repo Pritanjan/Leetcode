9435bcf772c0141161b99126f21f722124274393
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-18 13:34:19 +0530

    Update 88. Merge Sorted Array.cpp

--- 0001 - 0100/88. Merge Sorted Array.cpp
+++ 0001 - 0100/88. Merge Sorted Array.cpp
@@ -15,7 +15,8 @@ public:
 };
 
 
-// SAME AS APPROACH 1
+// OR
+
 
 class Solution {
 public:
@@ -100,19 +101,15 @@ public:
         int p1 = 0, p2 = 0;
         int sorted[m + n];
         int cur;
-        while (p1 < m || p2 < n) {
-            if (p1 == m) {
-                cur = nums2[p2++];
-            } else if (p2 == n) {
-                cur = nums1[p1++];
-            } else if (nums1[p1] < nums2[p2]) {
-                cur = nums1[p1++];
-            } else {
-                cur = nums2[p2++];
-            }
+        while(p1 < m || p2 < n) {
+            if(p1 == m) cur = nums2[p2++];
+            else if(p2 == n) cur = nums1[p1++];
+            else if(nums1[p1] < nums2[p2]) cur = nums1[p1++];
+            else cur = nums2[p2++];
             sorted[p1 + p2 - 1] = cur;
         }
-        for (int i = 0; i != m + n; ++i) {
+
+        for(int i=0; i!=m+n; ++i) {
             nums1[i] = sorted[i];
         }
     }
