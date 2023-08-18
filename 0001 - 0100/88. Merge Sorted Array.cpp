89031b113e5b1194a16cb59f0a97d78c08a6699e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-18 14:41:54 +0530

    Update 88. Merge Sorted Array.cpp

--- 0001 - 0100/88. Merge Sorted Array.cpp
+++ 0001 - 0100/88. Merge Sorted Array.cpp
@@ -65,6 +65,49 @@ public:
 
 
 
+// OR
+// In-place with Two Pointers (Forward)
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        vector<int> temp(nums1);
+
+        while(i < m && j < n) {
+            if(temp[i] <= nums2[j]) {
+                nums1[k] = temp[i];
+                i++;
+            } 
+            else {
+                nums1[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < m) {
+            nums1[k] = temp[i];
+            i++;
+            k++;
+        }
+
+        while(j < n) {
+            nums1[k] = nums2[j];
+            j++;
+            k++;
+        }
+    }
+};
+
+
+
+
 
 
 
