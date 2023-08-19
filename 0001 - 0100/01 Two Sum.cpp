e8f91e23c9bfb8a03e1d800a056a9ebaf7438712
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-19 12:32:42 +0530

    Update 01 Two Sum.cpp

--- 0001 - 0100/01 Two Sum.cpp
+++ 0001 - 0100/01 Two Sum.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/two-sum/
+// APPROACH 1
 
 class Solution {
 public:
@@ -6,15 +6,14 @@ public:
         vector <int> diff;
         for(int i=0;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
-                if( nums[i]+nums[j] == target ){
-			diff.push_back(i);
-                    	diff.push_back(j);
-                    	// diff.push_back(min(nums[i], nums[j]));
-                    	// diff.push_back(max(nums[i], nums[j]));  // to get element  
-			break;
-		}
-		else 
-			continue;
+                if(nums[i]+nums[j] == target ){
+		     diff.push_back(i);
+                     diff.push_back(j);
+                     // diff.push_back(min(nums[i], nums[j]));
+                     // diff.push_back(max(nums[i], nums[j]));  // to get element  
+		     break;
+	        }
+		else continue;
             }
         }
         return diff;
@@ -25,10 +24,11 @@ public:
 
 
 
+// APPROACH 2
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int>& nums, int target) {        
         unordered_map<int,int> mp;
         for(int i=0; i<nums.size(); i++){
             if(mp.find(target - nums[i]) != mp.end() )
@@ -38,6 +38,7 @@ public:
         return {};
     }
 };
+
 
 
 
