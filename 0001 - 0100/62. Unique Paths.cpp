bef216151fcd2790c1291e84c2437a8605a3e225
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-19 10:12:20 +0530

    Update 62. Unique Paths.cpp

--- 0001 - 0100/62. Unique Paths.cpp
+++ 0001 - 0100/62. Unique Paths.cpp
@@ -1,5 +1,7 @@
 // https://github.com/Pritanjan/Question/blob/main/POTD/gfg/0060%20230323%20Unique%20Paths%20in%20a%20Grid.cpp
 
+// APPROACH 1
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
@@ -21,24 +23,35 @@ public:
 };
 
 
+
+
+
+
+// APPROACH 2 
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int dp[m][n];
-        for(int i=0;i<m;i++)
-        {
-           for(int j=0;j<n;j++)
-           {
-               if(i==0 || j==0)
-                   dp[i][j]=1;
-               else
-                   dp[i][j]=dp[i-1][j]+dp[i][j-1];
-           }
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i=0; i<m; ++i) {
+            for(int j=0; j<n; ++j) {
+                // Initialize the edges to 1 since there's only one way to reach them
+                if(i == 0 || j == 0) dp[i][j] = 1;
+                // Paths from above and left
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
+            }
         }
-        return dp[m-1][n-1];
+        // Number of unique paths to bottom-right corner
+        return dp[m-1][n-1]; 
     }
 };
 
+
+
+
+
+
+// APPROACH 3
 
 class Solution {
 public:
@@ -53,6 +66,11 @@ public:
     }
 };
 
+
+
+
+
+// APPROACH 4
 
 class Solution {
 public:
