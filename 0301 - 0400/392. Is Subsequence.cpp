68a014666a1973ff1b0684f8e6f9fbb84b6cea2f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-20 12:21:38 +0530

    Update 392. Is Subsequence.cpp

--- 0301 - 0400/392. Is Subsequence.cpp
+++ 0301 - 0400/392. Is Subsequence.cpp
@@ -1,5 +1,46 @@
-//https://leetcode.com/problems/is-subsequence/submissions/
+// APPROACH 1 [ 2 Pointer ] 
 
-//https://leetcode.com/problems/is-subsequence/discuss/?currentPage=1&orderBy=newest_to_oldest&query=
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while(i < s.length() && j < t.length()){
+            if(s.at(i) == t.at(j)){
+                i++;
+            }
+            j++;
+        }
+        return i == s.length());  // If i reaches the end of s, then s is a subsequence of 
+    }
+};
 
+
+
+
+
+// APPROACH 2 USE FIND FUNCTION 
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        size_t pos = 0; // Position to search for character in t
+        for(char c : s) {
+            pos = t.find(c, pos); // Search for the character in t starting from pos
+            if(pos == string::npos) {
+                return false; // Character not found in t
+            }
+            pos++; // Move to the next position in t
+        }
+        
+        return true;
+    }
+};
+
+
+
+
+
+
+// APPROACH 3 
 
