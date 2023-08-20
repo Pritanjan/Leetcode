b2b3e3605b2833560dfa1cd1157be52730d80dda
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-20 13:37:50 +0530

    Update 2825. Make String a Subsequence Using Cyclic Increments.cpp

--- 2801 - 3000/2825. Make String a Subsequence Using Cyclic Increments.cpp
+++ 2801 - 3000/2825. Make String a Subsequence Using Cyclic Increments.cpp
@@ -1,4 +1,35 @@
-// APPROACH 1 
+// APPROACH 0 Brute Force
+
+// try all possible sets of indices and check if the resulting modified string str1 contains str2
+// as a subsequence. 
+// This approach has an exponential time complexity and is not efficient for larger inputs,
+// but it's a simple way to start.
+
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        for(int j=0; j<(1 << str1.size()); ++j) {
+            string tmp = str1;
+            for(int i=0; i<str1.size(); ++i) {
+                if(j & (1 << i)) tmp[i] = (tmp[i] - 'a' + 1) % 26 + 'a'; // Increment cyclically
+            }
+            int i = 0, k = 0;
+            while(i < tmp.size() && k < str2.size()) {
+                if(tmp[i] == str2[k]) ++k;
+                ++i;
+            }
+            if(k == str2.size()) return true;
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+// APPROACH 1
 
 class Solution {
 public:
@@ -13,4 +44,28 @@ public:
     }
 };
 
+
+
+
+
+
+// APPROACH 2 
+
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int P = 0;
+        for(auto it : str1){
+            if(it == str2[P]) P++;
+            else {
+                auto it2 = it;
+                if(it2 == 'z') it2 = 'a';
+                else it2++;
+                if(it2 == str2[P]) P++;
+            }
+            if(P == str2.size()) return true;
+        }
+        return false;
+    }
+};
 
