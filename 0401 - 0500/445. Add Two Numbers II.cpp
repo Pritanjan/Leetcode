d61afd671572913fa62f8d0591661c1deec1a13a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-21 14:43:18 +0530

    Update 445. Add Two Numbers II.cpp

--- 0401 - 0500/445. Add Two Numbers II.cpp
+++ 0401 - 0500/445. Add Two Numbers II.cpp
@@ -1,5 +1,6 @@
 // SAME AS LC Q 2 Add Two Numbers II
 
+// APPROACH 1
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -16,35 +17,78 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* p = reverseList(l1);
         ListNode* l = reverseList(l2);
-
         int sum = 0;
         ListNode* temp = new ListNode(0) ;
-        
         ListNode* ans = temp;
         
         // ListNode* rtn = new ListNode((l1->val + l2->val) % 10);
-        
-        while(p or l){
-            if(p){
+        while(p or l) {
+            if(p) {
                 sum += p -> val;
                 p = p -> next;
             }
-            if(l){
+            if(l) {
                 sum += l -> val;
                 l = l -> next;
             }
             temp -> next = new ListNode(sum % 10);
             temp = temp -> next;
             
-            if(sum > 9)
-                sum = 1;
-            else 
-                sum = 0;
+            if(sum > 9) sum = 1;
+            else sum = 0;
         }
-        
-        if(sum)
-            temp -> next = new ListNode(sum);
-        
+        if(sum) temp -> next = new ListNode(sum);
         return reverseList(ans -> next);
     }
 };
+
+
+
+
+
+
+// APPROACH 2 
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> stk1, stk2;
+        while(l1 != nullptr) {
+            stk1.push(l1 -> val);
+            l1 = l1 -> next;
+        }
+        while(l2 != nullptr) {
+            stk2.push(l2 -> val);
+            l2 = l2 -> next;
+        }
+
+        int totalSum = 0, carry = 0;
+        ListNode* ans = new ListNode();
+        while(!stk1.empty() || !stk2.empty()) {
+            if(!stk1.empty()) {
+                totalSum += stk1.top();
+                stk1.pop();
+            }
+            if(!stk2.empty()) {
+                totalSum += stk2.top();
+                stk2.pop();
+            }
+
+            ans->val = totalSum % 10;
+            carry = totalSum / 10;
+            ListNode* newNode = new ListNode(carry);
+            newNode->next = ans;
+            ans = newNode;
+            totalSum = carry;
+        }
+        return carry == 0 ? ans->next : ans;
+    }
+};  
+
+
+
+
+
+
+// APPROACH 3 
+
