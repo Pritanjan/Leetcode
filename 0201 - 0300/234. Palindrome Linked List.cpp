a668c624937b74f8b08146c9e02c619d97b434f8
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-22 14:10:02 +0530

    Update 234. Palindrome Linked List.cpp

--- 0201 - 0300/234. Palindrome Linked List.cpp
+++ 0201 - 0300/234. Palindrome Linked List.cpp
@@ -25,6 +25,27 @@ public:
 // S.C. --> O(N) As we create an array of size N
 
 
+// OR
+
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> arr;
+        while(head != NULL) {
+            arr.push_back(head -> val);
+            head = head -> next;
+        }
+
+        for(int i=0; i<arr.size()/2; i++) {
+            if(arr[i] != arr[arr.size()-i-1]) return false;
+        }            
+        return true;
+    }
+};
+
+
+
 
 
 
