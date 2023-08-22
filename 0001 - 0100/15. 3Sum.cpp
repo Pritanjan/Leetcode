36c946cc25a490d0a94dce04e129dbc3eabe8c39
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-22 19:35:47 +0530

    Update 15. 3Sum.cpp

--- 0001 - 0100/15. 3Sum.cpp
+++ 0001 - 0100/15. 3Sum.cpp
@@ -1,4 +1,7 @@
-// APPROACH 1 [ BRUTE FORCE ]
+// APPROACH 1 [ BRUTE FORCE ] TLE
+
+// Use 3 nested loops to generate all possible combinations of triplets and then check if their sum is 0
+// T.C. = O(n^3), 
 
 class Solution {
 public:
@@ -23,14 +26,17 @@ public:
     }        
 };
 
-// Time Complexity: O(N^3)
 // Space Complexity: O(1)
 
 
 
 
 
+
 // APPROACH 2 [ 2 POINTER ]
+
+// Use 2 pointers while iterating through the array after sorting it. 
+// T.C. =  of O(n^2).
 
 class Solution {
 public:
@@ -67,7 +73,9 @@ public:
 // Time Complexity: O(N^2)
 // Space Complexity: O(1)
 
+
 // OR
+
 
 class Solution {
 public:
@@ -103,6 +111,9 @@ public:
 
 // APPROACH 3 USE HASH SET
 
+// It involves using a hash set to keep track of seen elements while iterating through the array.
+// T.C. =  of O(n^2).
+    
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -132,4 +143,36 @@ public:
 
 
 
-// APPRAOCH 4 
+// APPRAOCH 4 [ Using Sorting and Binary Search ]
+
+// It involves sorting the array and then using binary search to find the third element that
+// complements the sum of the first two elements to zero. 
+// T.C. =  O(n^2 * log n).
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> res;
+        for(int i=0; i<n-2; ++i) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;  // Skip duplicates
+            for(int j=i+1; j<n-1; ++j) {
+                if(j > i+1 && nums[j] == nums[j-1]) continue;  // Skip duplicates
+                int target = -nums[i] - nums[j];
+                if(binary_search(nums.begin() + j+1, nums.end(), target))
+                    res.push_back({nums[i], nums[j], target});
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
