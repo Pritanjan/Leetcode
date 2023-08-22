c15b822a0fc720e47e76381b88fbcb23c3388ee3
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-22 10:10:36 +0530

    Update 168. Excel Sheet Column Title.cpp

--- 0101 - 0200/168. Excel Sheet Column Title.cpp
+++ 0101 - 0200/168. Excel Sheet Column Title.cpp
@@ -14,3 +14,27 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+// APPROACH 2 [ Using Recursion ]
+// WE can solve this problem recursively by repeatedly dividing the column number by 26 and 
+// converting the remainder into the corresponding character. Here's how you can do it:
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        if(columnNumber <= 0) return "";
+        int rem = (columnNumber - 1) % 26;
+        return convertToTitle((columnNumber - 1) / 26) + char('A' + rem);
+    }
+};
+
+
+
+
+
+// APPROACH 3 
+
