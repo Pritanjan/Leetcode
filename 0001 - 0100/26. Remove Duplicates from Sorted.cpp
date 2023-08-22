22b21fee1d44484163e3069e12031cf2f8271328
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-22 22:20:29 +0530

    Update 26. Remove Duplicates from Sorted.cpp

--- 0001 - 0100/26. Remove Duplicates from Sorted.cpp
+++ 0001 - 0100/26. Remove Duplicates from Sorted.cpp
@@ -55,6 +55,26 @@ public:
 // Space Complexity: O(1)
 
 
+// OR
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int k = 1; // Pointer to the last position of unique elements
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] != nums[k-1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }   
+        return k;
+    }
+};
+
+
+
 
 
 
