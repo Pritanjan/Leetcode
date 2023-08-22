b7acd0f7df03436a0c44a7a3417f8ea536c09a54
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-23 00:12:46 +0530

    Update 42. Trapping Rain Water.cpp

--- 0001 - 0100/42. Trapping Rain Water.cpp
+++ 0001 - 0100/42. Trapping Rain Water.cpp
@@ -1,4 +1,9 @@
-// A1 
+// A1 - Using Two Pointers
+
+// It involves using two pointers, one starting from the left and another starting from the right. 
+// Now traverse the array while maintaining the maximum height encountered from the left and the
+// maximum height encountered from the right. 
+// Calculate the amount of water that can be trapped at each index and add it to the total.
 
 class Solution {
 public:
@@ -21,6 +26,76 @@ public:
         return water;
     }
 };
+
+
+
+
+
+
+// A2 - DP
+
+// Here, we precompute the maximum height on the left and right of each index. 
+// Then, for each index, calculate the trapped water based on the minimum of the 
+// maximum height on the left and right sides.
+
+class Solution {
+public:
+    int trap(vector<int>& height) { 
+        int n = height.size();
+        if(n <= 2) return 0;
+
+        vector<int> leftMax(n), rightMax(n);
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+
+        for(int i=1; i<n; i++) leftMax[i] = max(leftMax[i-1], height[i]);
+        for(int i=n-2; i>=0; i--) rightMax[i] = max(rightMax[i+1], height[i]);
+
+        int water = 0;
+        for(int i=0; i<n; i++) water += min(leftMax[i], rightMax[i]) - height[i];
+
+        return water;
+    }
+};
+
+
+
+
+
+
+// A3 -  Using a Stack
+
+// It uses a stack to keep track of the indices of the bars in a decreasing order of height.
+// As we traverse the array, calculate the trapped water by popping elements from the stack and
+// updating the trapped water accordingly.
+
+class Solution {
+public:        
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n <= 2) return 0;
+
+        stack<int> st;
+        int water = 0;
+
+        for(int i=0; i<n; i++) {
+            while(!st.empty() && height[i] > height[st.top()]) {
+                int top = st.top();
+                st.pop();
+                if(st.empty()) break;
+                int distance = i - 1 - st.top() ;
+                int boundedHeight = min(height[i], height[st.top()]) - height[top];
+                water += distance * boundedHeight;
+            }
+            st.push(i);
+        }
+        return water;
+    }
+};
+
+
+
+
 
 
 
