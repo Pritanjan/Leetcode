358a66c02d208515370180ffc8f285da278b8fe7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-23 20:11:25 +0530

    Update 60. Permutation Sequence.cpp

--- 0001 - 0100/60. Permutation Sequence.cpp
+++ 0001 - 0100/60. Permutation Sequence.cpp
@@ -32,4 +32,27 @@ public:
 
 // APPROACH 2 
 
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string res;
+        for(int i=1; i<=n; ++i) {
+            res += to_string(i);
+        }
+
+        for(int i=1; i<k; ++i) {
+            next_permutation(res.begin(), res.end());
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// APPROACH 3 
+
+
 
