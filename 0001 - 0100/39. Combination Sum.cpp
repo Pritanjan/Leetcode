122c626ae059189277751a80fecc961d0b15c6a6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-23 15:17:41 +0530

    Update 39. Combination Sum.cpp

--- 0001 - 0100/39. Combination Sum.cpp
+++ 0001 - 0100/39. Combination Sum.cpp
@@ -56,8 +56,39 @@ public:
 };
 
 
+// OR
+
+
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& res, vector<int>& combination, vector<int>& candidates, int target, int start) {
+        if(target == 0) {
+            res.push_back(combination);
+            return ;
+        }
+
+        for(int i=start; i<candidates.size(); ++i) {
+            if(candidates[i] <= target) {
+                combination.push_back(candidates[i]);
+                backtrack(res, combination, candidates, target - candidates[i], i);
+                combination.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> combination;
+        backtrack(res, combination, candidates, target, 0);
+        return res;
+    }
+};
 
 
 
+
+
+
+// A3
 
 
