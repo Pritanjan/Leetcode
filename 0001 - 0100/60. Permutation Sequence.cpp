6f2679942b0f4c7602a1ac4b89213235db861cbd
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-23 20:08:55 +0530

    Update and rename 60. Permutation Sequence MATH RECURSION.cpp to 60. Permutation Sequence.cpp

--- 0001 - 0100/60. Permutation Sequence.cpp
+++ 0001 - 0100/60. Permutation Sequence.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/permutation-sequence/
+// APPROACH 1 
 
 class Solution {
 public:
@@ -13,14 +13,23 @@ public:
         nums.push_back(n);
         string ans = "";
         k--;
+
         while(1){
-            ans = ans+to_string(nums[k/fact]);
-            nums.erase(nums.begin() + k/fact);
-            if(nums.size() == 0)
-                break;
+            ans = ans + to_string(nums[k / fact]);
+            nums.erase(nums.begin() + k / fact);
+            if(nums.size() == 0) break;
             k %= fact;
             fact /= nums.size();
         }
         return ans;
     }
 };
+
+
+
+
+
+
+// APPROACH 2 
+
+
