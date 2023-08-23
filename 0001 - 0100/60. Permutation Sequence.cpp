ee188a6e6065e1c9848868ca3913abf33ee7833b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-23 21:10:57 +0530

    Update 60. Permutation Sequence.cpp

--- 0001 - 0100/60. Permutation Sequence.cpp
+++ 0001 - 0100/60. Permutation Sequence.cpp
@@ -30,7 +30,11 @@ public:
 
 
 
-// APPROACH 2 
+// APPROACH 2 - Generating Permutations
+
+// Generate all permutations of the sequence [1, 2, ..., n] and then return the kth permutation. 
+// We can use the next_permutation function to generate permutations. 
+// But, this approach might be inefficient for large values of n and k.
 
 class Solution {
 public:
@@ -52,7 +56,41 @@ public:
 
 
 
-// APPROACH 3 
+// APPROACH 3 - Using Factorials
+// It takes advantage of the factorial property. 
+// Given n distinct elements, there are (n-1)! permutations that start with each of the n elements. 
+// We can calculate the digits of the kth permutation using this property.
 
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string res;
+        vector<int> nums;
+        for(int i=1; i<=n; ++i) {
+            nums.push_back(i);
+        }
+
+        k--;  // Convert k to 0-based index
+        int fact = 1;
+        for(int i=2; i<n; ++i) {
+            fact *= i;
+        }
+
+        for(int i=n-1; i>0; k %= fact, fact /= i, --i) {
+            int idx = k / fact;
+            res += to_string(nums[idx]);
+            nums.erase(nums.begin() + idx);
+        }
+        res += to_string(nums[0]);
+        return res;
+    }
+};
+
+
+
+
+
+
+// APPROACH 4 
 
 
