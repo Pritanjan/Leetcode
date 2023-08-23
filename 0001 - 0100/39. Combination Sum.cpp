f4c12dfb791562480e00c58022cdfea3ca9d90f2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-23 15:09:56 +0530

    Update 39. Combination Sum.cpp

--- 0001 - 0100/39. Combination Sum.cpp
+++ 0001 - 0100/39. Combination Sum.cpp
@@ -1,11 +1,10 @@
-//https://leetcode.com/problems/combination-sum/
+// APPRAOCH 1 
 
 class Solution {
 public:
     void findcombination(int i, int target, vector<int> &nums, vector<vector<int>> &ans, vector<int> &ds){
         if(i == nums.size()){
-            if(target == 0)
-                ans.push_back(ds);
+            if(target == 0) ans.push_back(ds);
             return ;
         }
         if(nums[i] <= target){
@@ -13,13 +12,12 @@ public:
             findcombination(i, target-nums[i], nums, ans, ds);
             ds.pop_back();
         }
-        
         findcombination(i+1, target, nums, ans, ds);
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;   // it will store stroes all the combiination which we are going to return ultimately
-        vector<int> ds;         // this ds is used in oiur recursion
+        vector<int> ds;            // this ds is used in oiur recursion
         findcombination(0, target, candidates, ans, ds);
         return ans;
     }
@@ -29,25 +27,21 @@ public:
 
 
 
+// APPROACH 2 
 
 class Solution {
 public:
     vector<vector<int>> ans;
-    
     void findcombination(int i, vector<int>& candidates, int target, vector<int> ds){
-        if(target == 0){
+        if(target == 0) {
             ans.push_back(ds);
             return;
         }
         
-        if(target <= 0)
-            return;
-        
-        if(i == candidates.size())
-            return;
+        if(target <= 0) return;
+        if(i == candidates.size()) return;
         
         findcombination(i+1, candidates, target, ds);
-        
         ds.push_back(candidates[i]);
         findcombination(i, candidates, target-candidates[i], ds);
         
