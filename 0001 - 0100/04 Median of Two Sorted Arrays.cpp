bc61be1d20c3de8460ea2bda77cbd4cf31137e3f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-24 19:22:52 +0530

    Update 04 Median of Two Sorted Arrays.cpp

--- 0001 - 0100/04 Median of Two Sorted Arrays.cpp
+++ 0001 - 0100/04 Median of Two Sorted Arrays.cpp
@@ -144,3 +144,43 @@ public:
 };
 
 // explain -  post
+
+
+
+
+
+
+// A5
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<int> merged(m + n);
+        
+        int i = 0, j = 0, k = 0;
+        while(i < m && j < n) {
+            if(nums1[i] < nums2[j]) merged[k++] = nums1[i++];
+            else merged[k++] = nums2[j++];
+        }
+        
+        while(i < m) merged[k++] = nums1[i++];
+        while(j < n) merged[k++] = nums2[j++];
+        
+        int size = m + n;
+        if(size % 2 == 0) {
+            int mid1 = merged[(size - 1) / 2];
+            int mid2 = merged[size / 2];
+            return (double)(mid1 + mid2) / 2;
+        } 
+        else return (double)merged[size / 2];
+    }
+};
+
+
+
+
+
+
+// A6
