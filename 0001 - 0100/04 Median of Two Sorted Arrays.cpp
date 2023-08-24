a49592c3000efb0383015b7598dabfcb9f22cda9
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-24 18:12:47 +0530

    Update 04 Median of Two Sorted Arrays.cpp

--- 0001 - 0100/04 Median of Two Sorted Arrays.cpp
+++ 0001 - 0100/04 Median of Two Sorted Arrays.cpp
@@ -107,3 +107,40 @@ public:
 };
 
 
+
+
+
+
+// A4 
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();        
+        
+        if(m > n) return findMedianSortedArrays(nums2, nums1);        
+        int L = 0, R = m;
+        
+        while(L <= R) {
+            int partitionX = (L + R) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+
+            int maxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int maxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+
+            int minX = (partitionX == m) ? INT_MAX : nums1[partitionX];
+            int minY = (partitionY == n) ? INT_MAX : nums2[partitionY];
+
+            if(maxX <= minY && maxY <= minX) {
+                if((m + n) % 2 == 0) return (double)(max(maxX, maxY) + min(minX, minY)) / 2.0;
+                else return (double)max(maxX, maxY);
+            }
+            else if(maxX > minY) R = partitionX - 1;
+            else L = partitionX + 1;
+        }
+        throw invalid_argument("Input arrays are not sorted.");
+    }
+};
+
+// explain -  post
