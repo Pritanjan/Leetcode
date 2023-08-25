8c5a0fec8e6eb14120123355d7a6e6f5d3d24c19
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-25 20:31:10 +0530

    Update 14. Longest Common Prefix.cpp

--- 0001 - 0100/14. Longest Common Prefix.cpp
+++ 0001 - 0100/14. Longest Common Prefix.cpp
@@ -1,3 +1,55 @@
+// A0 Brute Force 
+
+// It involves comparing characters of all strings at each index and checking if they are the same. 
+// The loop runs until the characters mismatch or the end of any string is reached.
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int ans = 0;
+        for(int i=0; i<strs[0].size(); i++){
+            bool flag = true;
+            for(int j=1; j<strs.size(); j++){
+                if(strs[j][i] != strs[0][i]){
+                    flag = false;
+                    break;                    
+                }
+            }
+            if(flag) ans++;
+            else break;
+        }
+        return strs[0].substr(0, ans);
+    }
+};
+
+
+// OR
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())  return "";      
+        int mi = INT_MAX;
+        for(string& str : strs) {
+            mi = min(mi, static_cast<int>(str.length()));
+        }
+        
+        for(int i=0; i<mi; ++i) {
+            char curr = strs[0][i];
+            for(string& str : strs) {
+                if(str[i] != curr) return str.substr(0, i);
+            }
+        }        
+        return strs[0].substr(0, mi);
+    }
+};
+
+
+
+
+
+
 // APPROACH 1 [ Horizontal Scanning ]
 
 class Solution {
@@ -37,30 +89,6 @@ public:
 
 // T.C. --> O(S), where S is the sum of all characters in all strings.
 // S.C. --> O(1), constant extra space IS use
-
-
-// OR
-
-
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int ans = 0;
-        for(int i=0; i<strs[0].size(); i++){
-            bool flag = true;
-            for(int j=1; j<strs.size(); j++){
-                if(strs[j][i] != strs[0][i]){
-                    flag = false;
-                    break;                    
-                }
-            }
-            if(flag) ans++;
-            else break;
-        }
-        return strs[0].substr(0, ans);
-    }
-};
-
 
 
 
