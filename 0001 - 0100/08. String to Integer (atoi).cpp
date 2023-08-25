e7db356f25179f6e5d8b3f73cbeb603fed7090ce
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-25 16:56:14 +0530

    Update and rename 08. String to Integer (atoi) STRING.cpp to 08. String to Integer (atoi).cpp

--- 0001 - 0100/08. String to Integer (atoi).cpp
+++ 0001 - 0100/08. String to Integer (atoi).cpp
@@ -1,32 +1,38 @@
-//https://leetcode.com/problems/string-to-integer-atoi/
+// A1
 
 class Solution {
 public:
-    int myAtoi(string s) {
-        
+    int myAtoi(string s) {        
         string s1 = "";
         int i = 0;
         
         // Making a valid string of numbers which is going to be converted into number
         while(i < s.size()){
-            if(s[i] == ' ' && !s1.empty() ) // if string != empty + next character is " " means this is string termination.
-                break;
-            if(s[i] != ' ') // skipping the "spaces"
-                s1.push_back(s[i]);
-            
+            // if string != empty + next character is " " means this is string termination.
+            if(s[i] == ' ' && !s1.empty()) break;
+            // skipping the "spaces"
+            if(s[i] != ' ')  s1.push_back(s[i]);
             i++;
         }
         
         // if our string is empty means there is absence of number in string so we simply return 0
         // if(s1.size() == 0)
-        if(s1 == "")
-            return 0;
+        if(s1 == "") return 0;
         
         // otherwise
         stringstream ss(s1);
         int n;
-        ss>>n;
+        ss >> n;
         
         return n;
     }
 };
+
+
+
+
+
+
+// A2
+
+
