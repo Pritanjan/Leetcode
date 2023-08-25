aeacab0a00c1522cb5b5a8c41d45e31d1c6bac8c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-25 18:34:36 +0530

    Update 08. String to Integer (atoi).cpp

--- 0001 - 0100/08. String to Integer (atoi).cpp
+++ 0001 - 0100/08. String to Integer (atoi).cpp
@@ -84,3 +84,70 @@ public:
 };
 
 
+
+
+
+
+// A 4 - Using Finite State Machine - Chat gpt
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int state = 0; // 0: Initial, 1: Sign, 2: Digits
+        int sign = 1;
+        long long res = 0;
+
+        for(char c : s) {
+            switch(state) {
+                case 0: // Initial state
+                    if(isspace(c)) {
+                        continue;
+                    } 
+                    else if(c == '+' || c == '-') {
+                        sign = (c == '-') ? -1 : 1;
+                        state = 1; // Move to Sign state
+                    } 
+                    else if(isdigit(c)) {
+                        res = c - '0';
+                        state = 2; // Move to Digits state
+                    } 
+                    else {
+                        return 0; // Invalid character
+                    }
+                    break;
+
+                case 1: // Sign state
+                    if(isdigit(c)) {
+                        res = c - '0';
+                        state = 2; // Move to Digits state
+                    } 
+                    else {
+                        return 0; // Invalid character
+                    }
+                    break;
+
+                case 2: // Digits state
+                    if(isdigit(c)) {
+                        res = res * 10 + (c - '0');
+
+                        // Check for overflow
+                        if(sign == 1 && res > INT_MAX) return INT_MAX;
+                        else if(sign == -1 && -res < INT_MIN) return INT_MIN;
+                    } 
+                    else {
+                        return static_cast<int>(res * sign);
+                    }
+                    break;
+            }
+        }
+        return static_cast<int>(res * sign);
+    }
+};
+
+
+
+
+
+
+// A 5  -  Manual Parsing - Chat gpt
+
