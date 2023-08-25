7c67a1a2a1ffaf9c7198411acfd8812e4abb16c1
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-25 14:21:55 +0530

    Update and rename 12. Integer to Roman HASH TABLE MATH STRING.cpp to 12. Integer to Roman.cpp

--- 0001 - 0100/12. Integer to Roman.cpp
+++ 0001 - 0100/12. Integer to Roman.cpp
@@ -1,17 +1,13 @@
-// https://leetcode.com/problems/integer-to-roman/
+// A1 
 
 class Solution {
 public:
     string intToRoman(int num) {
         string ret;
         string str[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int ans[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        
-        
-        for(int i=0;num!=0; i++)
-        {
-            while(num>= ans[i])    
-            {
+        int ans[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};        
+        for(int i=0; num != 0; i++) {
+            while(num >= ans[i]) {
                 num = num - ans[i];
                 ret = ret + str[i];
             }
@@ -19,3 +15,10 @@ public:
         return ret;
     }
 };
+
+
+
+
+
+
+// A 2 
