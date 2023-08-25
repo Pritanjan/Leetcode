da42c2b68cb10e6191c9220f536a221ce1691e52
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-25 14:28:11 +0530

    Update 12. Integer to Roman.cpp

--- 0001 - 0100/12. Integer to Roman.cpp
+++ 0001 - 0100/12. Integer to Roman.cpp
@@ -1,4 +1,4 @@
-// A1 
+// A1 - Using Arrays
 
 class Solution {
 public:
@@ -21,4 +21,33 @@ public:
 
 
 
-// A 2 
+// A2 - Using a Map
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        map<int, string, greater<int>> mp;
+        mp[1000] = "M";
+        mp[900] = "CM";
+        mp[500] = "D";
+        mp[400] = "CD";
+        mp[100] = "C";
+        mp[90] = "XC";
+        mp[50] = "L";
+        mp[40] = "XL";
+        mp[10] = "X";
+        mp[9] = "IX";
+        mp[5] = "V";
+        mp[4] = "IV";
+        mp[1] = "I";
+
+        string res = "";
+        for(auto& i : mp) {
+            while(num >= i.first) {
+                num -= i.first;
+                res += i.second;
+            }
+        }
+        return res;
+    }
+};
