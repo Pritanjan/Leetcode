d5453377bb3c37f6764aa2766315055f6cba1f0b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-25 21:51:53 +0530

    Update 14. Longest Common Prefix.cpp

--- 0001 - 0100/14. Longest Common Prefix.cpp
+++ 0001 - 0100/14. Longest Common Prefix.cpp
@@ -128,4 +128,33 @@ public:
 
 
 
-// A 4 
+// A 4 - Binary Search - Leetcode
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0) return "";
+        int mi = Integer.MAX_VALUE;
+        for(String str : strs) mi = Math.min(mi, str.length());
+        int low = 1;
+        int high = mi;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(isCommonPrefix(strs, mid)) low = mid + 1;
+            else high = mid - 1;
+        }
+        return strs[0].substring(0, (low + high) / 2);
+    }
+
+    private boolean isCommonPrefix(String[] strs, int len){
+        String str1 = strs[0].substring(0, len);
+        for(int i=1; i<strs.length; i++) {
+            if(!strs[i].startsWith(str1)) return false;
+        }
+        return true;
+    }
+}
+
+
+
+
