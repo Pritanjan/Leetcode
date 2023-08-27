e576a89eb2c7d6444ccbe68581d18935242ac147
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-27 15:22:03 +0530

    Update 236. Lowest Common Ancestor of a Binary Tree.cpp

--- 0201 - 0300/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ 0201 - 0300/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -1,30 +1,44 @@
-//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// A 1
 
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL)
-            return root;
-        if(root == p or root == q)
-            return root;
+        if(root == NULL) return root;
+        if(root == p or root == q) return root;
         
         TreeNode* L = lowestCommonAncestor(root->left, p,q);
         TreeNode* R = lowestCommonAncestor(root->right,p,q);
+        if(L != NULL and R != NULL) return root;
+        else if(L == NULL and R != NULL) return R;
+        else if(L != NULL and R == NULL) return L;
+        else return L;
+    }
+};
+
+// T.C. --> O(N)
+// S.C. --> O(h)
+
+
+// OR
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root == p || root == q) return root;
+
+        TreeNode* L = lowestCommonAncestor(root -> left,  p, q);
+        TreeNode* R = lowestCommonAncestor(root -> right, p, q);
         
-        if(L != NULL and R != NULL)
-            return root;
-        else if(L == NULL and R != NULL)
-            return R;
-        else if(L != NULL and R == NULL)
-            return L;
-        else
-            return L;
+        if(L && R) return root; // LCA found       
+        return L ? L : R;
     }
 };
 
 
 
-// T.C. --> O(N)
-// S.C. --> O(h)
 
+
+
+// A 2
 
