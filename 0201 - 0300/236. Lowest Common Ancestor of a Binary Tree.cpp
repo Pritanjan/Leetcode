f12e5b0d1eb402df05b1e786066151c35215662e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-27 15:27:52 +0530

    Update 236. Lowest Common Ancestor of a Binary Tree.cpp

--- 0201 - 0300/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ 0201 - 0300/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -42,3 +42,38 @@ public:
 
 // A 2
 
+class Solution {
+public:
+    bool findPath(TreeNode* root, TreeNode* target, vector<TreeNode*>& path) {
+        if(!root) return false;       
+        path.push_back(root);
+        if(root == target) return true;       
+        
+        if((root -> left && findPath(root -> left,  target, path)) ||
+          (root -> right && findPath(root -> right, target, path))) return true;
+        
+        path.pop_back();
+        return false;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> pathP, pathQ;
+        
+        if(!findPath(root, p, pathP) || !findPath(root, q, pathQ)) return nullptr;
+                
+        TreeNode* lca = nullptr;
+        for(int i=0; i<min(pathP.size(), pathQ.size()); ++i) {
+            if(pathP[i] == pathQ[i]) lca = pathP[i];
+            else break;
+        }        
+        return lca;
+    }
+};
+
+
+
+
+
+
+// A 3
+
