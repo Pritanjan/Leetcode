02bebfd02ca5cd0087ecd3d0bbf2495cb3dddd9f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-28 12:20:15 +0530

    Update 103. Binary Tree Zigzag Level Order Traversal.cpp

--- 0101 - 0200/103. Binary Tree Zigzag Level Order Traversal.cpp
+++ 0101 - 0200/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -1,16 +1,15 @@
+// A 1 a
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        if (root == nullptr) return ans;
-        
+        if (root == nullptr) return ans;         
         queue<TreeNode*> q;
         q.push(root);
         
         bool reverse = false;
-        
         while (!q.empty()) {
-        
             int n = q.size();
             vector<int> level(n);
             
@@ -24,12 +23,9 @@ public:
                 
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
-                
-            }
-            
+            }            
             // change direction 
-            reverse = !reverse;
-            
+            reverse = !reverse;            
             ans.push_back(level);
         }
         return ans;
@@ -37,34 +33,31 @@ public:
 };
 
 
+// OR
+
 
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) { 
-         vector<vector<int>>res;
+        vector<vector<int>>res;
         if(root == NULL) return res;
 
         queue<TreeNode*>q;
         q.push(root);
-        
         int cnt = -1;
         while(!q.empty()) {
             int n = q.size();
-            vector<int>l;
-            
+            vector<int> l;
             for(int i=0; i<n; ++i){
                 auto temp = q.front();
                 q.pop();
                 
                 if(temp->left!=NULL) q.push(temp->left);
                 if(temp->right!=NULL) q.push(temp->right);
-
                 l.push_back(temp->val);
             }
-            cnt++;
-         
+            cnt++;         
             if(cnt % 2 != 0) reverse(l.begin(),l.end());
-
             res.push_back(l);
         }
         return res;
@@ -74,12 +67,15 @@ public:
 
 
 
+
+
+// A 2
+
 class Solution {
 public:
     vector<vector<int>> ret;
     void DFS(TreeNode* root, int depth) {
         if(root == nullptr) return;
-
         if(depth >= ret.size()) ret.push_back(vector<int>());
         
         // add this value to the list
@@ -87,11 +83,10 @@ public:
         else ret[depth].insert(ret[depth].begin(), root -> val);
         
         depth++;
-
         DFS(root -> left, depth);
         DFS(root -> right, depth);
     }
-
+    
     // Clown ass order traversal
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         DFS(root, 0);
