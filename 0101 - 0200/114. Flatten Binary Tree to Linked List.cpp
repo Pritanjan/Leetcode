765e47bf1fac351a135d3266f5f8506428612885
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-29 16:37:43 +0530

    Update 114. Flatten Binary Tree to Linked List.cpp

--- 0101 - 0200/114. Flatten Binary Tree to Linked List.cpp
+++ 0101 - 0200/114. Flatten Binary Tree to Linked List.cpp
@@ -114,6 +114,32 @@ public:
 };
 
 
+// OR
+
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) return;          
+        
+        stack<TreeNode*> stk;
+        stk.push(root);
+        TreeNode* prev = NULL;
+        while(!stk.empty()) {
+            TreeNode* curr = stk.top();
+            stk.pop();
+            
+            if(prev) prev -> right = curr;
+            if(curr -> right) stk.push(curr -> right);            
+            if(curr -> left) {
+                stk.push(curr -> left);
+                curr -> left = NULL; // Set left child to NULL
+            }            
+            prev = curr;
+        }
+    }
+};
+
 
 
 
