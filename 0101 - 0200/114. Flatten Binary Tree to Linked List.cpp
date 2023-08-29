d58701aa896bc3d4cda4f44feebcfe471cb0338c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-29 15:42:37 +0530

    Update 114. Flatten Binary Tree to Linked List.cpp

--- 0101 - 0200/114. Flatten Binary Tree to Linked List.cpp
+++ 0101 - 0200/114. Flatten Binary Tree to Linked List.cpp
@@ -8,13 +8,29 @@
 // check copy pg 28
 
 
-// MORRIS TRAVERSAL CODE
-// https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion-and-without-stack/
+// A 1 
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr){
+            if(curr -> left){
+                TreeNode* pred = curr -> left;
+                while(pred -> right != NULL) pred = pred -> right;
+                pred -> right = curr -> right;
+                curr -> right = curr -> left;
+                curr -> left = NULL;
+            }
+            curr = curr -> right;
+        }
+    }
+};
 
 
+// OR
 
 
-// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
 class Solution{
 public:
     void flatten(TreeNode *root) {
@@ -22,13 +38,11 @@ public:
         while(curr != NULL){
             if(curr -> left) {
                 TreeNode* pred = curr -> left;
-                while(pred -> right){
-                    pred = pred -> right;
-                }
+                while(pred -> right) pred = pred -> right;
                 pred -> right = curr -> right;
                 curr -> right = curr -> left;
                 // curr -> left = NULL
-                // line 112 is equivalent to 118 to 124
+                // line 44 is equivalent to 50 to 55
             }
             curr = curr -> right;
         }
@@ -54,47 +68,6 @@ public:
 
 
 
-
-
-
-
-// https://www.codingninjas.com/codestudio/problems/flatten-bst-to-a-sorted-list_1169459?leftPanelTab=0
-
-
-#include <bits/stdc++.h> 
-void inorder(TreeNode<int>* root, vector<int>& A){
-    if(root == NULL) return ;
-    
-    inorder(root -> left, A);
-    A.push_back(root -> data);
-    inorder(root -> right, A);
-}
-
-TreeNode<int>* flatten(TreeNode<int>* root) {
-    vector<int> v;
-    inorder(root, v);
-    int n = v.size();
-
-    TreeNode<int>* newroot = new TreeNode<int>(v[0]);
-
-    TreeNode<int>* curr = newroot;
-
-    // 2nd step
-    for(int i=1; i<n; i++){
-
-        TreeNode<int>* temp = new TreeNode<int> (v[i]);
-
-        curr -> left = NULL;
-        curr -> right = temp;
-        curr = temp;
-    }
-
-    // 3rd step
-    curr -> left = NULL;
-    curr -> right = NULL;
-
-    return newroot;
-}
 
 
 
