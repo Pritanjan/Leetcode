36ce8d2f8b7e72ae4d6c8603f6bd7c7a63a4f9c1
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-29 22:34:42 +0530

    Update and rename 700. Search in a Binary Search Tree 1 TREE BST BT.cpp to 700. Search in a Binary Search Tree.cpp

--- 0601 - 0700/700. Search in a Binary Search Tree.cpp
+++ 0601 - 0700/700. Search in a Binary Search Tree.cpp
@@ -1,4 +1,4 @@
-// A 1 iterative solution
+// A 1 - iterative solution
 
 class Solution {
 public:
@@ -17,7 +17,9 @@ public:
 
 
 
-// A 2
+// A 2 - Recursive DFS
+// It utilizes the properties of a BST to traverse the tree in a depth-first manner. 
+// It starts at the root node and recursively searches for the node with the given value.
 
 class Solution {
 public:
@@ -40,6 +42,25 @@ public:
 
 
 
-// A 3
+// A 3 - Iterative BFS
+// It uses an iterative BFS to traverse the tree level by level. 
+// It searches for the node with the given value and returns the corresponding subtree.
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        queue<TreeNode*> que;
+        que.push(root);     
+        while(!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            
+            if(node -> val == val) return node;            
+            if(node -> val > val && node -> left) que.push(node -> left);
+            else if(node -> val < val && node -> right) que.push(node -> right);
+        }        
+        return nullptr;
+    }
+};
 
 
