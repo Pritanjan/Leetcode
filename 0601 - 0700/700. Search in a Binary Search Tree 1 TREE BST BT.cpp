b0ba1af912ab13092c7299a5c301b683611c27df
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-29 22:26:19 +0530

    Update 700. Search in a Binary Search Tree 1 TREE BST BT.cpp

--- 0601 - 0700/700. Search in a Binary Search Tree 1 TREE BST BT.cpp
+++ 0601 - 0700/700. Search in a Binary Search Tree 1 TREE BST BT.cpp
@@ -1,8 +1,4 @@
-// https://leetcode.com/problems/search-in-a-binary-search-tree/
-// HERE WE R PRINTING THE SUBTREE OF VAL
-
-
-// APPROACH 1 iterative solution
+// A 1 iterative solution
 
 class Solution {
 public:
@@ -20,25 +16,19 @@ public:
 
 
 
-// APPROACH 2
+
+// A 2
 
 class Solution {
 public:
-    struct TreeNode* searchBST(TreeNode* root, int val) {
-        
+    struct TreeNode* searchBST(TreeNode* root, int val) {        
         // if node is empty return NULL
-        if(root == NULL)
-            return NULL ;
+        if(root == NULL) return NULL;
         // if we find the node that we are looking for the return directly
-        else if(root->val == val)
-            return root;
-        
-        else if(root -> val < val)
-            return searchBST(root->right, val);
+        else if(root->val == val) return root;
+        else if(root -> val < val) return searchBST(root->right, val);
         // if the val is smaller then the val of current node, then look for left child of the current node, else  look for right chind of the current node.
-        else
-            return searchBST(root->left, val );
-        
+        else return searchBST(root->left, val );
     }
 };
 
@@ -50,41 +40,6 @@ public:
 
 
 
-
-
-
-// https://www.codingninjas.com/codestudio/problems/search-in-bst_1402878?leftPanelTab=1
-// HERE WE R CHECKING WHTHER THE GIVEN VAL IS PRESENT IN THE TREE OR NOT
-
-
-// APPROACH 1
-
-bool searchInBST(BinaryTreeNode<int> *root, int val) {
-       if(root == NULL)
-            return NULL ;
-        else if(root->data == val)
-            return root;
-        else if(root -> data < val)
-            return searchInBST(root->right, val);
-        else
-            return searchInBST(root->left, val );
-}
-
-
-
-
-// APPROACH 2
-
-bool searchInBST(BinaryTreeNode<int> *root, int val) {
-    BinaryTreeNode<int> *temp = root;
-
-    while(temp != NULL){
-        if(temp -> data == val) return true;
-        if(temp -> data >  val) temp = temp -> left;
-        else temp = temp -> right;
-
-    }
-    return false;
-}
+// A 3
 
 
