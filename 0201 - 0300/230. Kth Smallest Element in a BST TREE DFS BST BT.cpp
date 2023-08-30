677903ae4a2ebdb917df4792819d497853d6ce83
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-31 01:43:16 +0530

    Update 230. Kth Smallest Element in a BST TREE DFS BST BT.cpp

--- 0201 - 0300/230. Kth Smallest Element in a BST TREE DFS BST BT.cpp
+++ 0201 - 0300/230. Kth Smallest Element in a BST TREE DFS BST BT.cpp
@@ -1,44 +1,31 @@
-// SOLUTION
+// A 1 - Inorder traversal
 
-// https://www.codingninjas.com/codestudio/problems/find-k-th-smallest-element-in-bst_1069333?leftPanelTab=2
+// Go inorder and decrease k at each node. 
+// Stop the whole search as soon as k is 0,
+// and then the k-th element is immediately returned all 
+// the way to the recursion top and to the original caller.
 
-
-// APPROACH 1 inorder traversal
-
-// Go inorder and decrease k at each node. Stop the whole search as soon as k is 
-// zero, and then the k-th element is immediately returned all the way to the 
-// recursion top and to the original caller.
-
-// Try the left subtree first. If that made k zero, then its answer is the overall answer
-// and we return it right away. Otherwise, decrease k for the current node, and
+// Try the left subtree first. 
+// If that made k zero, then its answer is the overall answer and we return it right away. 
+// Otherwise, decrease k for the current node, and 
 // if that made k zero, then we return the current node's value right away.
 // Otherwise try the right subtree and return whatever comes back from there.
-
 
 class Solution {
 public:
     int inorder(TreeNode* root, int &k){
-        if(!root )
-            return 0;
-        
-        int min = inorder(root -> left, k);
-        
-        if(k==0)
-            return min;
-            // return inorder(root -> left, k);
-        if(--k == 0)
-            return root -> val;
-        else
-            return inorder(root -> right, k);
+        if(!root) return 0;
+        int mi = inorder(root -> left, k);
+        if(k == 0) return mi;
+        // return inorder(root -> left, k);
+        if(--k == 0) return root -> val;
+        else return inorder(root -> right, k);
     }
     
     int kthSmallest(TreeNode* root, int k) {
         return inorder(root, k);
-    }
-    
+    }    
 };
-
-
 
 
 // OR
@@ -50,50 +37,71 @@ public:
         int i = 0;
         int res = f(root, i, k);
         return res;
-    }
-        
+    }        
         
     int f(TreeNode *root, int &i, int k){
         // base case
         if(root == NULL) return -1;
 
-        // L
-        int L = f(root -> left, i, k);
+        int L = f(root -> left, i, k);   // Left
         if(L != -1) return L;
 
-        // N
         i++;
-        if(i == k) return root -> val;
+        if(i == k) return root -> val;   // Node
 
-        // R 
-        return f(root -> right, i, k);
-
+        return f(root -> right, i, k);   // Right
     }
 };
 
 
 
 
-// APPROACH 2
 
+
+// A 2
 
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int& k) {
-        if (root) {
+        if(root) {
             int x = kthSmallest(root->left, k);
             // return !k ? x : !--k ? root->val : kthSmallest(root->right, k);
 
-            // LINE 7 IS SAME AS LINE 10 TO 13
+            // LINE 68 IS SAME AS LINE 71 TO 73
             if (!k) return x;
             else if (!--k) return root->val;
             else return kthSmallest(root->right, k);
-            
         }
         return -1;
     }
 };
 
+
+
+
+
+
+// A 3 
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> stk;
+        TreeNode* curr = root;
+
+        while(curr || !stk.empty()) {
+            while(curr) {
+                stk.push(curr);
+                curr = curr -> left;
+            }
+            curr = stk.top();
+            stk.pop();
+            if(--k == 0) return curr -> val;
+            curr = curr -> right;
+        }
+        return -1; // Not found
+    }
+};
 
 
 
