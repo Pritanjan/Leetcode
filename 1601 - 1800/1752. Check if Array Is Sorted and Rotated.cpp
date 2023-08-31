31b08d1b430273eb32bb6aaee94f6690917a33e2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-08-31 22:08:00 +0530

    Create 1752. Check if Array Is Sorted and Rotated.cpp

---
+++ 1601 - 1800/1752. Check if Array Is Sorted and Rotated.cpp
@@ -0,0 +1,44 @@
+// A 1
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i-1] > nums[i])
+                cnt++;
+        }
+        if(nums[n-1] > nums[0])
+            cnt++;
+        
+        return cnt <= 1;
+    }
+};
+
+
+// OR
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0; // Count of decreasing segments        
+        for(int i=0; i<n; ++i) {
+            if(nums[i] > nums[(i+1) % n]) {
+                cnt++;
+                if(cnt > 1) return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+// A 2
+
