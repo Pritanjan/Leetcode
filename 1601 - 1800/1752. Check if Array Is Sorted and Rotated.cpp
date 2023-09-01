d0cab796714ec04d207ddfa76422e70e6e24bc68
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-01 11:06:48 +0530

    Update 1752. Check if Array Is Sorted and Rotated.cpp

--- 1601 - 1800/1752. Check if Array Is Sorted and Rotated.cpp
+++ 1601 - 1800/1752. Check if Array Is Sorted and Rotated.cpp
@@ -42,3 +42,27 @@ public:
 
 // A 2
 
+class Solution {
+public:
+    bool check(vector<int>& nums) {   
+        int n = nums.size();
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+
+        for (int i = 0; i < n; ++i) {
+            if (nums == sorted) {
+                return true;
+            }
+            rotate(nums.begin(), nums.begin() + 1, nums.end());
+        }
+
+        return false;
+    }
+};
+
+
+
+
+
+
+// A 3
