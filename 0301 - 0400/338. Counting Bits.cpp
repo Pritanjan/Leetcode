ffa0b03a666500e7bb35417b991eafa77ac6a147
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-01 09:51:55 +0530

    Update 338. Counting Bits.cpp

--- 0301 - 0400/338. Counting Bits.cpp
+++ 0301 - 0400/338. Counting Bits.cpp
@@ -21,9 +21,52 @@ public:
 };
 
 
+// OR
+
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1);
+        for(int i=0; i<=n; i++) {
+            int cnt = 0;
+            int temp = i;
+            while(temp > 0) {
+                if(temp & 1) cnt++;
+                temp >>= 1;
+            }
+            ans[i] = cnt;
+        }
+        return ans;
+    }
+};
+
+
 
 
 
 
 // A 2 
+
+class Solution {
+public:
+    int countOne(int n) {
+        return __builtin_popcount(n);
+    }
+
+    vector<int> countBits(int n) {
+        vector<int> res;
+        for(int i=0; i<=n; i++) {
+            res.push_back(countOne(i));
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 3
 
