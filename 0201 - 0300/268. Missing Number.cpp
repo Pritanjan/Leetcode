f15dbd45fc062730bdc919cfac58ca98f972edd9
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-02 10:42:39 +0530

    Update 268. Missing Number.cpp

--- 0201 - 0300/268. Missing Number.cpp
+++ 0201 - 0300/268. Missing Number.cpp
@@ -1,3 +1,25 @@
+// A 0  - Sorting and Linear Search:
+// Sort the array and then iterate through it to find the missing number. 
+// If the numbers are distinct and in the range [0, n], the missing number will be the first index
+// where the value does not match the index
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+            if(nums[i] != i) return i;
+        }
+        return n; // If all numbers from 0 to n-1 are present, n is the missing number.
+    }
+};
+
+
+
+
+
+
 // A 1 - Sum of First n Natural Numbers 
 
 // We can use the mathematical formula for the sum of the first n natural numbers
@@ -74,3 +96,90 @@ public:
         return -1; // This line should never be reached if there is a missing number
     }
 };
+
+
+
+
+
+
+// A 4 - Hash Map with Index:
+// Use a hash map to store the elements from the input array along with their indices. 
+// Then, iterate through numbers from 0 to n and check if each number exists in the hash map. 
+// If it doesn't, return the missing number.
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int, int> ump;
+        for(auto i=0; i<nums.size(); i++) {
+            ump[nums[i]] = i;
+        }
+        
+        int n = nums.size();
+        for(int i=0; i<=n; i++) {
+            if(ump.find(i) == ump.end()) return i;
+        }
+        return -1; // This line should never be reached if there is a missing number
+    }
+};
+
+
+
+
+
+
+// A 5  - Binary Search:
+// Use binary search to find the missing number efficiently. 
+// sort the input array. 
+// Then, perform a binary search to find the point where the element value does not match its index.
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int L = 0;
+        int R = nums.size();
+        while(L < R) {
+            int mid = L + (R - L) / 2;
+            if(nums[mid] > mid) R = mid;
+            else L = mid + 1;
+        }
+        return L;
+    }
+};
+
+
+
+
+
+
+// A 6  - Counting Array
+// We can use an auxiliary array to count the occurrences of numbers from 0 to n. 
+// Then, iterate through the counting array to find the number with a count of 0, 
+// which is the missing number.
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> v(n+1, 0);
+        
+        // Count the occurrences
+        for(int num : nums) {
+            v[num]++;
+        }
+        
+        // Find the missing number
+        for(int i=0; i<=n; i++) {
+            if(v[i] == 0) return i;
+        }        
+        return -1; // This line should never be reached if there is a missing number
+    }
+};
+
+
+
+
+
+
+// A 7
