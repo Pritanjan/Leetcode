07e35f382ba62ff07577f11ccaf7bf58cdeabfca
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-03 07:29:00 +0530

    Update 75. Sort Colors.cpp

--- 0001 - 0100/75. Sort Colors.cpp
+++ 0001 - 0100/75. Sort Colors.cpp
@@ -146,3 +146,24 @@ public:
 
 
 // A 5
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();   
+        for(int i=0; i<n-1; i++) {
+            int minIdx = i;
+            for(int j=i+1; j<n; j++) {
+                if(nums[j] < nums[minIdx]) minIdx = j;
+            }
+            swap(nums[i], nums[minIdx]);
+        }
+    }
+};
+
+
+
+
+
+
+// A 6
