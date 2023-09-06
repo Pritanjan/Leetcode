f10074917d1bcfe5ab0a3f7b35359aff66f8d7dd
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-06 15:50:39 +0530

    Update 33. Search in Rotated Sorted Array.cpp

--- 0001 - 0100/33. Search in Rotated Sorted Array.cpp
+++ 0001 - 0100/33. Search in Rotated Sorted Array.cpp
@@ -218,4 +218,21 @@ public:
 
 
 
-// A 6
+// A 6  -  STL
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it = find(nums.begin(), nums.end(), target);
+        if(it != nums.end()) return distance(nums.begin(), it);
+        return -1;
+    }
+};
+
+
+
+
+
+
+// A 7
+
