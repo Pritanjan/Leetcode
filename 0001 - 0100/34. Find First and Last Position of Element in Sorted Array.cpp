eab9dbf7bfeea01fdace2a9f8ae5945965242131
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-06 12:31:58 +0530

    Update and rename 34. Find First and Last Position of Element in Sorted Array  ARRAY BINARY_SEARCH.cpp to 34. Find First and Last Position of Element in Sorted Array.cpp

--- 0001 - 0100/34. Find First and Last Position of Element in Sorted Array.cpp
+++ 0001 - 0100/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,7 +1,4 @@
-// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-
-
-// APPROACH 1
+// A 1
 
 class Solution {
 public:
@@ -17,7 +14,6 @@ public:
                 break;
             }
         }
-    
         for(int j=n-1; j>=0; j--){
             if(arr[j] == x){
                 last = j;
@@ -28,13 +24,16 @@ public:
         // return {first, last};
         v.push_back(first) ;
         v.push_back(last) ;
-    
         return v;
     }
 };
 
 
-// APPROACH 2
+
+
+
+
+// A 2
 
 class Solution {
 public:
@@ -42,10 +41,8 @@ public:
         int l = 0, r = nums.size() - 1; 
         while(l<=r){
             int mid = (l+r)/2;
-            if(nums[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
+            if(nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
         }
         return l;
     }
@@ -54,16 +51,17 @@ public:
         int idx1 = lower_bound(nums, target);
         int idx2 = lower_bound(nums, target+1)-1;
         
-        if(idx1 < nums.size() && nums[idx1] == target)
-            return {idx1, idx2};
-        else
-            return {-1,-1};
+        if(idx1 < nums.size() && nums[idx1] == target) return {idx1, idx2};
+        else return {-1,-1};
     }
 };
 
 
 
-// APPROACH 3
+
+
+
+// A 3
 
 class Solution {
 public:
@@ -80,18 +78,13 @@ public:
         while(left <= right){
             mid = (left + right)/2;
             
-            if(nums[mid] == num){
-                answer = mid;
-                
-                if(find == "FIRST")
-                    right = mid - 1;
-                else
-                    left  = mid + 1; 
+            if(nums[mid] == num) {
+                answer = mid;                
+                if(find == "FIRST") right = mid - 1;
+                else left  = mid + 1; 
             }
-            else if(nums[mid] > num)
-                right = mid - 1;
-            else
-                left = mid + 1;
+            else if(nums[mid] > num) right = mid - 1;
+            else left = mid + 1;
         }
         return answer;
     }
@@ -99,21 +92,20 @@ public:
 
 
 
-// APPROACH 4
+
+
+
+// A 4
 
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         bool search = binary_search(nums.begin(), nums.end(), target);
-        if(search == false)
-            return {-1, -1};
+        if(search == false) return {-1, -1};
         
         int First = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
         int Last  = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
-        
         return {First, Last};
-        
-        
     }
 };
 
@@ -122,3 +114,55 @@ public:
 
 
 
+// A 5 -  Binary Search for Both Start and End Positions seperatelky
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int L = binarySearchLeft(nums, target);
+        int R = binarySearchRight(nums, target);
+
+        if(L <= R) return {L, R};
+        else return {-1, -1};
+    }
+
+    int binarySearchLeft(vector<int>& nums, int target) {
+        int L = 0;
+        int R = nums.size() - 1;
+        int res = -1;
+        while(L <= R) {
+            int mid = L + (R - L) / 2;
+            if(nums[mid] == target) {
+                res = mid;
+                R = mid - 1; // Continue searching on the left half
+            } 
+            else if(nums[mid] < target) L = mid + 1;
+            else R = mid - 1;
+        }
+        return res;
+    }
+
+    int binarySearchRight(const std::vector<int>& nums, int target) {
+        int L = 0;
+        int R = nums.size() - 1;
+        int res = -1;
+
+        while(L <= R) {
+            int mid = L + (R - L) / 2;
+            if(nums[mid] == target) {
+                res = mid;
+                L = mid + 1; // Continue searching on the right half
+            } 
+            else if(nums[mid] < target) L = mid + 1;
+            else R = mid - 1;
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 7
