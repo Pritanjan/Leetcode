ade62ef1316c8fa23a7f8e1e1acecb80997a296c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-07 09:21:47 +0530

    Update 92. Reverse Linked List II.cpp

--- 0001 - 0100/92. Reverse Linked List II.cpp
+++ 0001 - 0100/92. Reverse Linked List II.cpp
@@ -1,16 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-
-// Approach
+// A 1
 // Find the addresses of the nodes at the left and right position by traversing the given list.
 // Reverse the list from left to right separately.
 // After the reversal, reattach the list in the original list at the same position.
@@ -30,10 +18,10 @@ public:
     }
 
     ListNode* reverseBetween(ListNode* head, int L, int R) {
-        ListNode* CN = head;
-        ListNode* SN;
-        ListNode* LN;
-        int idx = 1;
+        ListNode* CN = head;   // current node
+        ListNode* SN;          // strt node
+        ListNode* LN;          // last node
+        int idx = 1;           // index
 
         while(CN != NULL){
             if(idx > R) break;
@@ -53,15 +41,39 @@ public:
             LN = SN -> next;
             SN -> next = reverse(SN -> next);
         }
-
         LN -> next = CN;
         return head;
     }
 };
 
+// 1. It initializes CN (current node), SN (start node), and LN (end node) to head, NULL, and NULL, 
+//    and idx to 1.
+// 2. It iterates through the list using a while loop until it reaches the Rth node or the end of the list. 
+// 3. If idx is less than L, it updates SN to point to the current node (CN) as 
+//    it represents the node just before the left position.
+// 4. If idx is equal to R, it updates LN to point to the current node (CN) as 
+//    it represents the node at the right position.
+// 5. It then updates CN and increments idx.
+// 6. After the loop, it sets the next pointer of the LN node to NULL, 
+//    effectively splitting the list into two parts: the one to be reversed and the rest.
+// 7. If L is 1, it means that the reversal starts from the beginning of the list, 
+//    so it reverses the entire list using the reverse function and updates the head of the list.
+// 8. If L is greater than 1, it means the reversal starts from a node other than the head. 
+//    In this case, it reverses the sublist between SN and LN using the reverse function
+//    and updates SN->next to point to the new head of the reversed sublist.
+// 9. Finally, it connects the reversed sublist (LN->next) to the remaining part of the list (CN)
+//    and returns the updated head of the list.
 
 // Time Complexity: O(n)
 // Auxiliary Space Complexity: O(1)
+
+
+
+
+
+
+// A 2
+
 
 
 
