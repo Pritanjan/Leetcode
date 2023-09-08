bb01f8f85aaae9f52c0b09a6a276235c741720a6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-08 22:27:14 +0530

    Update and rename 1283. Find the Smallest Divisor Given a Threshold ARRAY BINARY_SEARCH.cpp to 1283. Find the Smallest Divisor Given a Threshold.cpp

---
+++ 1201 - 1400/1283. Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,61 @@
+// A 0 - Brute Force
+// We can solve this problem using a brute force approach by trying all possible divisors and
+// checking if the sum of divisions is less than or equal to the threshold.
+
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int L = 1;
+        int R = 1e6;
+        // int R = *max_element(nums.begin(), nums.end());
+        int res = R;
+        while(L <= R) {
+            int mid = (R - L ) / 2 + L;
+            int sum = 0;
+            for(int i=0; i<nums.size(); i++) sum += (nums[i] + mid - 1) / mid;
+            if(sum > threshold) L = mid + 1;
+            else {
+                R = mid-1;
+                res = min(res, mid);
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 1 - Binary Search
+// if sum > threshold,  the divisor is small do L = mid + 1
+// if sum <= threshold, then the divisor is big, do R = mid
+
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int L = 1;
+        int R = 1e6;
+	// int R = *max_element(nums.begin(), nums.end());
+        while(L < R) {
+            int mid = (R - L ) / 2 + L;
+            int sum = 0;
+            for(int i=0; i<nums.size(); i++) sum += (nums[i] + mid - 1) / mid;
+            if(sum > threshold) L = mid + 1;
+            else R = mid;
+        }
+        return L;
+    }
+};
+
+// T.C.  --> O(n log m) , m = max(nums)
+// S.C.  --> O(1)
+
+
+
+
+
+
+// A 2
+
