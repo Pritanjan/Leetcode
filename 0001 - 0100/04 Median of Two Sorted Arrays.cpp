fb31394a50cbf8ec09fce126aea46e685822f30d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-09 15:58:36 +0530

    Update 04 Median of Two Sorted Arrays.cpp

--- 0001 - 0100/04 Median of Two Sorted Arrays.cpp
+++ 0001 - 0100/04 Median of Two Sorted Arrays.cpp
@@ -188,6 +188,47 @@ public:
 
 // A6
 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        if(m > n) {
+            swap(nums1, nums2);
+            swap(m, n);
+        }
+        
+        int imin = 0, imax =  m;
+        int halfLen = (m+n+1) / 2;
+        
+        while(imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+            
+            // Increase i to make the left part of nums1 bigger.
+            if(i < m && nums2[j-1] > nums1[i]) imin = i + 1;
+            // Decrease i to make the left part of nums1 smaller.
+            else if(i > 0 && nums1[i-1] > nums2[j]) imax = i - 1;
+            else {
+                int maxLeft, minRight;
+                if(i == 0) maxLeft = nums2[j-1];
+                else if(j == 0) maxLeft = nums1[i-1];
+                else maxLeft = max(nums1[i-1], nums2[j-1]);
+                                
+                if((m + n) % 2 == 1) return maxLeft;
+
+                if(i == m) minRight = nums2[j];
+                else if (j == n) minRight = nums1[i];
+                else minRight = min(nums1[i], nums2[j]);
+                                
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return -1;    
+        // This should never be reached if the input arrays are sorted properly.
+    }
+};
+
 
 
 
