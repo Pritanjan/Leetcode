892fefec30858ab9e654ad3f101fe7d7f9c7e3c0
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-11 00:26:28 +0530

    Update 974. Subarray Sums Divisible by K.cpp

--- 0901 - 1000/974. Subarray Sums Divisible by K.cpp
+++ 0901 - 1000/974. Subarray Sums Divisible by K.cpp
@@ -1,16 +1,41 @@
+// A 0  - Brute Force - TLE
+// Generate all possible subarrays.
+// Calculate the sum of each subarray.
+// Check if the sum is divisible by k.
+// Count the valid subarrays.
+    
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int cnt = 0;
+        int n = nums.size();
+        for(int start=0; start<n; ++start) {
+            int sum = 0;
+            for(int end=start; end<n; ++end) {
+                sum += nums[end];
+                if(sum % k == 0) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+
+
+
+// A 1 
 // https://www.youtube.com/watch?v=u9m-hnlcydk
 
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        vector<int> cnt(k,0);
+        vector<int> cnt(k, 0);
         int sum = 0;
-
         for(auto i : nums){
             sum += (i % k + k) % k;
             cnt[sum % k]++;
         }
-
         int res = cnt[0];
         for(auto c : cnt){
             res += (c * (c-1))/2;
@@ -19,12 +44,39 @@ public:
     }
 };
 
-
-
 // +k is use becs if rem is -ve
-//  outer %k is use becs if rem is +ve on adding k it will become double so to avoid it
+// outer %k is use becs if rem is +ve on adding k it will become double so to avoid it
 
 
 
 
+
+
+// A 2
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int cnt = 0;
+        int n = nums.size();
+        vector<int> prefixSum(n+1, 0);
+        unordered_map<int, int> ump;
+        ump[0] = 1;
+
+        int sum = 0;
+        for(int i=0; i<n; ++i) {
+            sum += nums[i];
+            int rem = (sum % k + k) % k;
+            cnt += ump[rem];
+            ump[rem]++;
+        }
+        return cnt;
+    }
+};
+
+
+
+
+
+// A 3
 
