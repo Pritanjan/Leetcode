12c9c40abc0c58a9ea1aa3134872f29366dc8681
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-10 22:28:14 +0530

    Update 451. Sort Characters By Frequency.cpp

--- 0401 - 0500/451. Sort Characters By Frequency.cpp
+++ 0401 - 0500/451. Sort Characters By Frequency.cpp
@@ -50,7 +50,7 @@ public:
 
 
 
-// A 3
+// A 3 
 
 class Solution {
 public:
@@ -66,6 +66,9 @@ public:
         for (char c : s) {
             charFreq[c]++;
         }
+
+        // // Convert the map to a vector of pairs for sorting.
+        // vector<pair<char, int>> freqVector(charFreq.begin(), charFreq.end());
 
         // Convert the map to a vector of pairs for sorting.
         vector<pair<char, int>> freqVector;
@@ -89,5 +92,40 @@ public:
 
 
 
-// A 4
+// A 4 -  Using an Array to Count Frequencies
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        // Count the frequency of each character using an array.
+        int freq[256] = {0};
+        for(char c : s) freq[c]++;
+        
+        // Create a vector of pairs (character, frequency) for bucket sort.
+        vector<pair<char, int>> freqVect;
+        for(int i = 0; i < 256; i++) {
+            if(freq[i] > 0) {
+                freqVect.push_back({(char)(i), freq[i]});
+            }
+        }
+
+        // Sort the vector based on character frequency in descending order.
+        sort(freqVect.begin(), freqVect.end(), [](pair<char, int>& a, pair<char, int>& b) {
+            return a.second > b.second;
+        });
+
+        // Reconstruct the sorted string.
+        string res = "";
+        for(auto& it : freqVect) {
+            res += string(it.second, it.first);
+        }
+        return res;        
+    }
+};
+
+
+
+
+
+// A 5 
 
