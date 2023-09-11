77848101ea7b900ba3b68d8d235822b2bf56b7ee
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-11 10:32:15 +0530

    Update 974. Subarray Sums Divisible by K.cpp

--- 0901 - 1000/974. Subarray Sums Divisible by K.cpp
+++ 0901 - 1000/974. Subarray Sums Divisible by K.cpp
@@ -75,6 +75,30 @@ public:
 };
 
 
+// OR
+
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int cnt = 0;
+        int n = nums.size();
+        unordered_map<int, int> freq;        
+        freq[0] = 1;
+
+        int sum = 0;
+        for(int i=0; i<n; ++i) {
+            sum += nums[i];
+            int rem = (sum % k + k) % k;
+            cnt += freq[rem];
+            freq[rem]++;
+        }        
+        return cnt;        
+    }
+};
+
+
+
 
 
 
