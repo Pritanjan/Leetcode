3edc8c213696de126ccdeb3b3d397a86cb70b120
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-12 17:20:28 +0530

    Update and rename 1647. Minimum Deletions to Make Character Frequencies Unique STRING GREEDY SORTING.cpp to 1647. Minimum Deletions to Make Character Frequencies Unique .cpp

---
+++ 1601 - 1800/1647. Minimum Deletions to Make Character Frequencies Unique .cpp
@@ -0,0 +1,54 @@
+// A 1 USing Hash Map
+
+class Solution {
+public:
+    int minDeletions(string s) {
+        int freq[26] = {};
+        int ans = 0;       
+        unordered_map<int, bool> map;
+        for(char i : s) freq[i - 'a']++;
+        
+        for(int i=0; i<26; i++) {
+            // int it = freq[i];
+            while(freq[i] > 0 && map[freq[i]] > 0) {
+                ans++;
+                map[freq[i]];
+                freq[i]--;
+            }
+            map[freq[i]] = true;
+        }
+        return ans;
+        
+    }
+};
+
+
+
+
+
+
+// A 2  - USing Hash Set
+
+// Greedy works since we can only delete characters (we cannot add or replace characters).
+// So, count each character first. For each 26 characters, check if it's count is already used. 
+// If so, delete characters until you find unused count, or reach zero.
+
+class Solution {
+public:
+    int minDeletions(string s) {
+        int freq[26] = {};
+        int ans = 0;        
+        unordered_set<int> set;
+        for(char i : s) freq[i - 'a']++;        
+        for(int i=0; i<26; i++){
+            for(;freq[i] > 0  && !set.insert(freq[i]).second; freq[i]--)  ans++;
+        }
+        return ans;
+    }
+};
+
+
+// Time: O(n); note that the second loop will not have more than 26 * 26 total operations.
+// Memory: O(1) as we will not store more than 26 different frequencies.
+
+
