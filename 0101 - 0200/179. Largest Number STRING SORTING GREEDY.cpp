db656edf4575ce711a3d70f6adc26502f8cee67a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-13 10:27:54 +0530

    Update 179. Largest Number STRING SORTING GREEDY.cpp

--- 0101 - 0200/179. Largest Number STRING SORTING GREEDY.cpp
+++ 0101 - 0200/179. Largest Number STRING SORTING GREEDY.cpp
@@ -1,24 +1,34 @@
-//https://leetcode.com/problems/largest-number/
+// A 1 - Lambda Function with Sorting:
+// You can use a lambda function in the sorting to achieve the same result.
 
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        
         int n = nums.size();
-        string ans = "";
         vector<string> v(n);
-        
+
+        // Convert integers to strings
         for(int i=0; i<n; i++){
             v[i] = to_string(nums[i]);
         }
-        
+
+        // Sort using a lambda function
         sort(v.begin(), v.end(), [](const string &a, const string &b) {
             return ((a+b) > (b+a));
         });
-        
-            for(auto i : v)
-                ans += i;
-        
+
+        // Concatenate sorted strings to form the largest number
+        string ans = "";
+        for(auto i : v) ans += i;
+
+        // Else handle the case where all numbers are 0
         return ans[0] == '0' ? "0" : ans;
     }
 };
+
+
+
+
+
+
+// A 2
