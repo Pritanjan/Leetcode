f093eb427a29b68421a3b215bd8993c1b6c2a75f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-13 10:55:16 +0530

    Update and rename 179. Largest Number STRING SORTING GREEDY.cpp to 179. Largest Number.cpp

--- 0101 - 0200/179. Largest Number.cpp
+++ 0101 - 0200/179. Largest Number.cpp
@@ -3,6 +3,10 @@
 
 class Solution {
 public:
+    static bool compare (string &a, string &b) {
+        return a+b > b+a;
+    }
+    
     string largestNumber(vector<int>& nums) {
         int n = nums.size();
         vector<string> v(n);
@@ -12,16 +16,20 @@ public:
             v[i] = to_string(nums[i]);
         }
 
+        // sort(v.begin(), v.end(), compare);
+        
         // Sort using a lambda function
         sort(v.begin(), v.end(), [](const string &a, const string &b) {
             return ((a+b) > (b+a));
         });
 
         // Concatenate sorted strings to form the largest number
-        string ans = "";
-        for(auto i : v) ans += i;
+        // string ans = "";
+        // for(auto i : v) ans += i;
 
-        // Else handle the case where all numbers are 0
+        // Concatenate sorted strings to form the largest number using std::accumulate
+        string ans = accumulate(v.begin(), v.end(), string(""));
+    
         return ans[0] == '0' ? "0" : ans;
     }
 };
