d0b1cae57e2e73f2a74c344deb49f14d2ef00531
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-14 00:31:13 +0530

    Update 328. Odd Even Linked List.cpp

--- 0301 - 0400/328. Odd Even Linked List.cpp
+++ 0301 - 0400/328. Odd Even Linked List.cpp
@@ -1,15 +1,11 @@
-// APPROACH 1
-
-
-// 2-pointer
-// The idea is to maintain two pointers, one of which stores the list of nodes at
-// odd positions and the other stores the list of nodes at an even position and then finally merge them.
+// A 1
+// The idea is to maintain two pointers, one of which stores the list of nodes at odd positions 
+// and the other stores the list of nodes at an even position and then finally merge them.
 
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head == NULL)
-            return NULL;
+        if(head == NULL) return NULL;
         ListNode* odd  = head;
         ListNode* even = head -> next;
         ListNode* evenhead = head -> next;
@@ -32,9 +28,38 @@ public:
     }
 };
 
-
 // T.C. --> O(N) There are total n nodes and we visit each node once.
 // S.C. --> O(1). Since we are not using any extra space, the overall space complexity is O(1).
 
 
+// OR
+// Same as above
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL) return NULL;
+        ListNode* oddHead = head;
+        ListNode* evenHead = head -> next;
+        ListNode* odd  = oddHead;
+        ListNode* even = evenHead;        
+        
+        while(even != NULL and even -> next != NULL){
+            odd ->next = even -> next;
+            odd = odd -> next;
+        
+            even -> next = odd -> next;
+            even = even -> next;
+        }
+        odd -> next = evenHead;
+        return oddHead;    
+    }
+};
+
+
+
+
+
+
+// A 2
 
