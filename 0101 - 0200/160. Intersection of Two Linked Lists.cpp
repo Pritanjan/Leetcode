73aa80a24e4dd51469dc22540aa854b1e3929771
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-14 12:24:46 +0530

    Update 160. Intersection of Two Linked Lists.cpp

--- 0101 - 0200/160. Intersection of Two Linked Lists.cpp
+++ 0101 - 0200/160. Intersection of Two Linked Lists.cpp
@@ -1,15 +1,13 @@
-// APPROACH 0 BRUTE FORCE
+// A 0 BRUTE FORCE
 
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         while (headB != NULL) {
             ListNode* temp = headA;
-            while (temp != NULL) {
+            while(temp != NULL) {
                 // If both nodes are the same
-                if (temp == headB) {
-                    return headB;
-                }
+                if(temp == headB) return headB;
                 temp = temp->next;
             }
             headB = headB->next;
@@ -23,7 +21,7 @@ public:
 
 
 
-// APPROACH 1 [Using map]
+// A 1 [Using Hash map]
 
 class Solution {
 public:
@@ -42,7 +40,7 @@ public:
 
 
 
-// APPROACH 2 [Using set]
+// A 2 [Using Hash set]
 
 // First of all store all the nodes of the first linked list in the set, and then iterate through 
 // each node of the second linked list to determine whether there exists in the set, and if so, 
@@ -74,13 +72,10 @@ public:
 };
 
 // Here we've used unordered_set instead of set as it provides faster lookup times in practice.
-// Also, we've used nullptr instead of NULL as it's the preferred way of representing null 
-// pointers in modern C++.
 
-// T.C. -->  O(m+n), where m and n are the lengths of the two linked lists,
+// T.C. -->  O(m + n), where m and n are the lengths of the two linked lists,
 // because we need to traverse both lists to create the set and check for intersection.
-
-// S.C. -->  O(m+n), because we need to store all nodes of one of the lists in the set.
+// S.C. -->  O(m + n), because we need to store all nodes of one of the lists in the set.
 
 // Note that the space complexity can be improved to O(min(m, n)) by storing the nodes
 // of the smaller list in the set instead of the larger one, but the time complexity remains the same.
@@ -88,7 +83,9 @@ public:
 
 
 
-// APPROACH 3 [2 Pointer]
+
+
+// A 3 [2 Pointer]
 
 class Solution {
 public:
@@ -97,12 +94,11 @@ public:
         ListNode *temp2 = headB;
         while(temp1 != temp2){
             if(temp1 == NULL) temp1 = headB;
-            else temp1 = temp1->next;
-            // temp1 = temp1 -> next == NULL ? headB : temp1 -> next;
-            // temp2 = temp2 -> next == NULL ? headA : temp2 -> next;
-            
+            else temp1 = temp1->next;            
             if(temp2 == NULL) temp2 = headA;
             else temp2 = temp2->next;
+            // temp1 = temp1 -> next == NULL ? headB : temp1 -> next;
+            // temp2 = temp2 -> next == NULL ? headA : temp2 -> next;
         }
         return temp1;
     }
@@ -113,7 +109,7 @@ public:
 
 
 
-// APPROACH 4 [Use of Lambda Function] copied from cn
+// A 4 [Use of Lambda Function] copied from cn
 
 // It initializes a lambda function, which sets the synchronization between C++ and C input/output 
 // streams to false and unties the cin stream from the cout stream.
@@ -123,7 +119,6 @@ public:
 // '<value>'\n", where '<value>' is the value of the input line. 
 // If the input line is "0", it writes "No intersection\n" to the output file. 
 // Finally, it flushes the output buffer and exits the program.
-
 
 int init = [] {
     ios_base::sync_with_stdio(false);
@@ -148,41 +143,41 @@ public:
 
 
 
-// APPROACH 5 [ Count the length of the two linked lists first ]
 
-// We can count the length of the two linked lists first, if the length of the two linked lists is not the same,
-// let the linked list go first, until the length of the two linked lists is the same, at this time
-// the two linked lists move back at the same time to see if the nodes are the same, 
+// A 5 [ Count the length of the two linked lists first ]
+
+// We can count the length of the two LL first, if the length of the two linked lists is not same,
+// let the linked list go first, until the length of the two linked lists is the same, 
+// at this time the two linked lists move back at the same time to see if the nodes are the same, 
 // if there are equal, it means that this equal node is the intersection of the two linked lists, 
-// otherwise if you have not found an equal node after walking, it means that they have no intersection, 
+// otherwise if we have not found an equal node after movieng, it means that they have no intersection, 
 // return directly
 
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         int lenA = length(headA), lenB = length(headB);
-        //If the lengths of the nodes are different, the more nodes go first until their lengths are the same
-        while (lenA != lenB) {
-            if (lenA > lenB) {
-                //If the linked list A is long, then the linked list A goes first
+        // If lengths of the nodes are different, the more nodes go first until lengths are the same
+        while(lenA != lenB) {
+            if(lenA > lenB) {
+                // If the linked list A is long, then the linked list A goes first
                 headA = headA->next;
                 lenA--;
             } 
             else {
-                //If the linked list B is long, then the linked list B goes first
+                // If the linked list B is long, then the linked list B goes first
                 headB = headB->next;
                 lenB--;
             }
         }
-
-        //Then start comparing, if they are not equal, keep going down
+        // Then start comparing, if they are not equal, keep going down
         while(headA != headB) {
             headA = headA->next;
             headB = headB->next;
         }
-        //At the end, there will be two possibilities in the end, one is that headA is empty,
-        //That is to say, the two do not intersect. Another possibility is headA
-        //Not empty, that is to say headA is their intersection point
+        // At the end, there will be two possibilities in the end, one is that headA is empty,
+        // That is to say, the two do not intersect. Another possibility is headA
+        // Not empty, that is to say headA is their intersection point
         return headA;
     }
 private:
@@ -201,10 +196,12 @@ private:
 
 
 
-// APPROACH 6  [  Difference in Lengths ]
+
+// A 6  [  Difference in Lengths ]
 
 // Traverse both linked lists to find their lengths and calculate the difference in lengths.
-// Move the pointer of the longer list forward by the difference in lengths, so both pointers are at the same relative position from the end.
+// Move the pointer of the longer list forward by the difference in lengths, 
+// so both pointers are at the same relative position from the end.
 // Now, traverse both linked lists together until the pointers meet. This will be the intersection point.
 
 class Solution {
@@ -248,10 +245,11 @@ public:
 
 
 
-// APPROACH 7 [ Cyclic Approach ]
-
-// Traverse the first linked list and make the last node's next point to the beginning of the second linked list. This forms a cycle.
-// Use Floyd's cycle detection algorithm (tortoise and hare approach) to find the cycle's starting point. This point is the intersection point of the two lists.
+// A 7 [ Cyclic Approach ]
+// Traverse the 1st LL and make the last node's next point to the beginning of the 2nd linked list. 
+// This forms a cycle.
+// Use Floyd's cycle detection algorithm (tortoise and hare approach) to find the cycle's starting point. 
+// This point is the intersection point of the two lists.
 // After finding the intersection point, break the cycle by updating the last node's next to nullptr.
 
 class Solution {
@@ -259,9 +257,7 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         // Traverse the first list to make the last node's next point to headB
         ListNode *currA = headA;
-        while(currA -> next != nullptr) {
-            currA = currA->next;
-        }
+        while(currA -> next != nullptr) currA = currA->next;
         currA -> next = headB;
         
         // Detect the cycle's starting point (intersection point)
@@ -284,8 +280,7 @@ public:
         while(slow != fast) {
             slow = slow -> next;
             fast = fast -> next;
-        }
-        
+        }        
         currA -> next = nullptr; // Restore the original structure
         return slow; // Intersection point
     }
@@ -294,3 +289,53 @@ public:
 
 
 
+
+
+// A 8
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA = 0, lenB = 0;
+        ListNode* ptrA = headA;
+        ListNode* ptrB = headB;
+
+        // Find the lengths of both lists
+        while(ptrA) {
+            lenA++;
+            ptrA = ptrA -> next;
+        }
+        while(ptrB) {
+            lenB++;
+            ptrB = ptrB -> next;
+        }
+        // Reset pointers to the beginning of the lists
+        ptrA = headA;
+        ptrB = headB;
+
+        // Move the longer list's pointer ahead by the length difference
+        while(lenA > lenB) {
+            ptrA = ptrA -> next;
+            lenA--;
+        }
+        while(lenB > lenA) {
+            ptrB = ptrB -> next;
+            lenB--;
+        }
+
+        // Compare nodes of both lists until intersection or end
+        while(ptrA && ptrB) {
+            if(ptrA == ptrB) return ptrA; // Intersection found
+            ptrA = ptrA->next;
+            ptrB = ptrB->next;
+        }
+        return nullptr; // No intersection
+    }
+};
+
+
+
+
+
+
+// A 9 
