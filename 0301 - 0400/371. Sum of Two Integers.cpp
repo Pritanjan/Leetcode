1559e5fdd06c07e097be22c84f18326889d6fb46
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-14 16:26:18 +0530

    Update 371. Sum of Two Integers.cpp

--- 0301 - 0400/371. Sum of Two Integers.cpp
+++ 0301 - 0400/371. Sum of Two Integers.cpp
@@ -1,9 +1,28 @@
+// A 1
+
 class Solution {
 public: 
     int getSum(int x, int y){
-        if (y == 0)
-            return x;
-        else
-            return getSum(x^y, (unsigned) (x & y)<<1);
+        if(y == 0) return x;
+        else return getSum( x^y, (x & y) << 1);
     }
 };
+
+
+
+
+
+// A 2
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b != 0){
+            int c = a & b;
+            a = a ^ b;
+            b = c << 1;
+        }
+        return a;
+    }
+};
+
