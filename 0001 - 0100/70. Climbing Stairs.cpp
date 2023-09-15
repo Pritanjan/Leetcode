1347d85346324854cd6ce49929886b1b31629bae
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-16 01:14:51 +0530

    Update 70. Climbing Stairs.cpp

--- 0001 - 0100/70. Climbing Stairs.cpp
+++ 0001 - 0100/70. Climbing Stairs.cpp
@@ -19,5 +19,27 @@ public:
 
 
 
-// A 2
+// A 2 - DP (Bottom-Up)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; ++i) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }        
+        return dp[n];
+    }
+};
+
+
+
+
+
+
+// A 3
 
