5e73e56a0a7dcbf2c9cfbdc2149259c2bdd8a9b2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-15 12:20:52 +0530

    Update 78. Subsets.cpp

--- 0001 - 0100/78. Subsets.cpp
+++ 0001 - 0100/78. Subsets.cpp
@@ -1,7 +1,12 @@
 // A1 -  Backtracking (Recursive):
-
 // It uses recursive backtracking to generate all possible subsets. 
 // At each step, we decide whether to include the current element in the subset or not.
+
+// 1. Define a function f, inside f, we add ds to the ans vector to store the current subset.
+// 2. We iterate through the nums array starting from the current index idx. 
+//    For each element, we include it in the ds, recursively call backtrack with the next index, 
+//    and then remove the element to backtrack.
+// 3. In the subsets function, we initialize the ans vector and call backtrack to generate all subsets.
 
 class Solution {
 private:
@@ -27,9 +32,16 @@ public:
 
 
 // A2 -  Bit Manipulation (Iterative)
-
 // It uses bit manipulation to represent whether an element is included in the subset or not. 
 // Each bit position corresponds to an element in the array.
+
+// 1. Calculate the total no of subsets for an array of size n using bitwise left shift (1 << n).
+// 2. Iterate from 0 to (1 << n ) - 1, representing each possible subset using inary number from 0 to 
+//    2^n - 1.
+// 3. Inside the loop, we check each bit in the binary representation of i using bitwise AND
+//    (i & (1 << j)) and add the corresponding element from nums to the current subset 
+//    if the bit is set (1).
+// 4. We add each generated subset to the result vector.
 
 class Solution {
 public:
@@ -58,8 +70,7 @@ public:
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res = {{}};
-        
+        vector<vector<int>> res = {{ }};        
         for(int num : nums) {
             int n = res.size();
             for(int i=0; i<n; ++i) {
@@ -100,6 +111,14 @@ public:
 
 
 // A4 - Lexicographic (Binary Sorted) Subsets:
+
+// 1. Sort input array nums to ensure that we get unique subsets & that they are in lexicographic order.
+// 2. Iterate from 0 to (1 << n) - 1, where n is the no of elements in nums. 
+//    This represents all possible combinations of elements using their binary representation.
+// 3. Inside the loop, we check each bit of the binary representation of the current index i. 
+//    If a bit is set (1), we add the corresponding element from nums to the current subset.
+// 4. We add each generated subset to the result vector.
+// It gives subsets that are lexicographically sorted
 
 class Solution {
 public:
