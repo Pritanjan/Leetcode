7a4a271c82b78d3bc36ef13c3b21e92c2183288f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-15 14:11:00 +0530

    Update 39. Combination Sum.cpp

--- 0001 - 0100/39. Combination Sum.cpp
+++ 0001 - 0100/39. Combination Sum.cpp
@@ -1,4 +1,4 @@
-// APPRAOCH 1 
+// A 1 
 
 class Solution {
 public:
@@ -27,7 +27,7 @@ public:
 
 
 
-// APPROACH 2  - Backtracking
+// A 2  - Backtracking
 
 class Solution {
 public:
@@ -37,10 +37,9 @@ public:
             ans.push_back(ds);
             return;
         }
-        
         if(target <= 0) return;
         if(i == candidates.size()) return;
-        
+
         findcombination(i+1, candidates, target, ds);
         ds.push_back(candidates[i]);
         findcombination(i, candidates, target-candidates[i], ds);
@@ -66,7 +65,6 @@ public:
             res.push_back(combination);
             return ;
         }
-
         for(int i=start; i<candidates.size(); ++i) {
             if(candidates[i] <= target) {
                 combination.push_back(candidates[i]);
@@ -75,7 +73,6 @@ public:
             }
         }
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
         vector<int> combination;
@@ -89,36 +86,31 @@ public:
 
 
 
-// A3
+// A 3
 
 class Solution {
 public:
-    // for the sake of convience, not to include again and again in my function call, I declare target here
-    int target;
-    
+    int target;    
     vector<vector<int>> ans; // 2-D vector to store our answer
-    
-    void solve(vector<int>& arr, int i, int sum, vector<int> op)
-    {
+    void solve(vector<int>& arr, int i, int sum, vector<int> op) {
         // if i crosses the array size, we will return saying that no more possibilty is left to choose
+        if(i >= arr.size()) return;
         
-        if(i >= arr.size()) 
-        {
-            return;
-        }
-        
-        // if value at ith index + sum becomes equal to target, then we will store it in our answer array, saying that yes it is a possible combination
-        if(arr[i] + sum == target)
-        {
+        // if value at ith index + sum becomes equal to target, then we will store it in answer array, 
+        // saying that yes it is a possible combination
+        if(arr[i] + sum == target) {
             op.push_back(arr[i]);
             ans.push_back(op);
             return;
         }
         
-        // if value at ith index + sum is less than target, then we have two choices i.e whether to include this value in our possible combiation array or not include that,
-        if(arr[i] + sum < target)
-        {
-            // we make two output vector, one for calling function at same index and anthor for calling function frm next index. Because for every element we have unlimited choices, that it will contribute in making our sum any number of times.
+        // if value at ith index + sum is less than target, then we have two choices i.e
+        // whether to include this value in our possible combiation array or not include that,
+        if(arr[i] + sum < target) {
+            // take 2 output vector, one for calling function at same index and anthor for calling
+            // function frm next index. 
+            // Because for every element we have unlimited choices, that it will contribute in making
+            // our sum any number of times.
             vector<int> op1 = op;
             vector<int> op2 = op;
             
@@ -126,10 +118,7 @@ public:
             solve(arr, i, sum + arr[i], op2);
             solve(arr, i + 1, sum, op1);
         }
-        else
-        {
-            solve(arr, i + 1, sum, op); // call for the next index
-        }
+        else solve(arr, i + 1, sum, op); // call for the next index
     }
     
     vector<vector<int>> combinationSum(vector<int>& arr, int required_target) {
