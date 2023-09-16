e574cf09011aaa0c6cff2623a37fea80ef1df0c3
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-16 16:05:17 +0530

    Update 2220. Minimum Bit Flips to Convert Number.cpp

--- 2201 - 2400/2220. Minimum Bit Flips to Convert Number.cpp
+++ 2201 - 2400/2220. Minimum Bit Flips to Convert Number.cpp
@@ -73,4 +73,39 @@ public:
 
 
 
-// A 4 - 
+// A 4 -  Count the number of bits to be flipped to convert A to B using the AND operator - gfg
+// Start comparing the bits in A and B, starting from the least significant bit and
+// if (A & 1) is not equal to (B & 1) then the current bit needs to be flipped, 
+// as the value of bits is different at this position in both the numbers
+
+// 1. Declare variable flips equal to zero
+// 2. Run a loop, while a is greater than zero and b is also greater than zero
+//    -  Calculate values of (A AND 1) and (B AND 1)
+//    -  If these values are not equal then increase the flip value by 1
+//    - Right shift a and b by 1
+// 3. Return flips
+
+class Solution {
+public:
+    int minBitFlips(int a, int b) {
+        // initially flips is equal to 0
+        int flips = 0;
+        // each bits of a && b with 1 and store them if t1 and t2 
+        // if t1 != t2 then we will flip that bit
+        while(a > 0 || b > 0) {
+            int t1 = (a & 1);
+            int t2 = (b & 1);
+            if(t1 != t2) flips++;
+            // right shifting a and b
+            a >>= 1;
+            b >>= 1;
+        }    
+        return flips;
+    }
+};
+
+
+
+
+
+// A 5
