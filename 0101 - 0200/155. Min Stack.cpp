7bf0fbe875d3136e8568170dc4569c27ecbf1d15
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-17 20:03:01 +0530

    Update 155. Min Stack.cpp

--- 0101 - 0200/155. Min Stack.cpp
+++ 0101 - 0200/155. Min Stack.cpp
@@ -1,24 +1,19 @@
-// https://www.codingninjas.com/codestudio/problems/two-stacks_983634?leftPanelTab=0
+// A 1
 
 class MinStack {
 public:
     stack<int> st1;
     stack<int> st2;
-    
-    MinStack() {
-        
-    }
+    MinStack() {}
     
     void push(int val) {
         st2.push(val);
-	    if(st1.empty() || val <= getMin())  
-            st1.push(val);	  
+    	if(st1.empty() || val <= getMin()) st1.push(val);	  
     }
     
     void pop() {
-	    if(st2.top() == getMin())  
-            st1.pop();
-	    st2.pop();
+	if(st2.top() == getMin()) st1.pop();
+        st2.pop();
     }
     
     int top() {
@@ -30,11 +25,9 @@ public:
     }
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+
+
+
+
+// A 2
+
