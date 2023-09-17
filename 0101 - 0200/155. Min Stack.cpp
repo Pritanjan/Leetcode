6485ffcb8f39b1ddaf3400ca2d149738ceac7a8e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-17 20:24:20 +0530

    Update 155. Min Stack.cpp

--- 0101 - 0200/155. Min Stack.cpp
+++ 0101 - 0200/155. Min Stack.cpp
@@ -31,7 +31,49 @@ public:
 
 // A 2
 
+class MinStack {
+private:
+    stack<pair<int, int>> st;
+public:
+    // Constructor, initialize the stack.
+    MinStack() {}
+
+    void push(int val) {
+        // If the stack is empty, 
+        // push the value as both the element and the minimum.
+        if(st.empty()) st.push({val, val});
+        else {
+            // else, push the new value and 
+            // minimum of the current minimum and the new value.
+            int minVal = min(val, st.top().second);
+            st.push({val, minVal});
+        }
+    }
+
+    void pop() {
+        // Pop the top element from the stack.
+        if(!st.empty()) st.pop();
+    }
+
+    int top() {
+        // Return the top element of the stack.
+        if(!st.empty()) return st.top().first;
+        return -1; // Stack is empty, return an arbitrary value.
+    }
+
+    int getMin() {
+        // Return the minimum element stored at the top of the stack.
+        if(!st.empty()) return st.top().second;
+        return -1; // Stack is empty, return an arbitrary value.
+    }
+};
+
+
+
+
+
 
 // A 3
+
 
 
