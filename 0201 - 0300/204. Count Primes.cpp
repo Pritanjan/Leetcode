6495debe8ded44f85865148adc808e56e57cb73d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-17 13:49:31 +0530

    Update and rename 204. Count Primes ARRAY MATH NUMBER_THEORY.cpp to 204. Count Primes.cpp

--- 0201 - 0300/204. Count Primes.cpp
+++ 0201 - 0300/204. Count Primes.cpp
@@ -1,3 +1,28 @@
+// A 0 - Brute Force - TLE
+
+class Solution {
+public:
+    bool isPrime(int num) {
+        if(num <= 1) return false;
+        for(int i=2; i*i<=num; ++i) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+
+    int countPrimes(int n) {
+        int count = 0;
+        for(int i=2; i<n; ++i) {
+            if(isPrime(i)) ++count;
+        }
+        return count;
+    }
+};
+
+
+
+
+
 // A 1
 
 class Solution {
