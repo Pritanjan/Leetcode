aeac32747c0c705e6f08ce228ea1e205d5ba7f8e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-18 22:14:09 +0530

    Update 735. Asteroid Collision.cpp

--- 0701 -0800/735. Asteroid Collision.cpp
+++ 0701 -0800/735. Asteroid Collision.cpp
@@ -1,24 +1,16 @@
-// \\ APPROACH 1 (USING STACK)
-      
+// A 1 (USING STACK)   
+
 // 1. If the current asteroid is positive, it is pushed onto the stack.
-
-// 2. If the current asteroid is negative, the stack is checked for any positive
-//    asteroids that the current asteroid might collide with.
-
+// 2. If the current asteroid is negative, the stack is checked for any positive asteroids that
+//    the current asteroid might collide with.
 // 3. If the absolute value of the current asteroid is greater than the top of the stack,
 //    the top asteroid is popped from the stack and the current asteroid is checked again.
-
 // 4. If the absolute value of the current asteroid is equal to the top of the stack, 
 //    both asteroids are popped. 
-
 // 5. If the absolute value of the current asteroid is less than the top of the stack, 
 //    the current asteroid is not added to the stack. 
-
 // 6. If the stack is empty, the current asteroid is added to the result vector.
-
-// 7. After processing all asteroids, the remaining asteroids in the stack are added to the 
-//    result vector. 
-
+// 7. After processing all asteroids, the remaining asteroids in stack are added to the result vector. 
 // 8. Finally, the result vector is returned.
 
 class Solution {
@@ -45,11 +37,28 @@ public:
         }
         
         vector<int> ans;
-        while(!s.empty()){
+        while(!s.empty()) {
             ans.push_back(s.top());
             s.pop();
         }
         reverse(ans.begin() ,ans.end());
+        return ans;
+    }
+};
+
+
+// OR
+
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& A) {
+        vector<int> ans;
+        for(int i : A) {
+            while(!ans.empty() and ans.back() > 0 and ans.back() < -i) ans.pop_back();
+            if(ans.empty() or ans.back() < 0 or i > 0) ans.push_back(i);
+            else if(i < 0 and ans.back() == -i) ans.pop_back();
+        }
         return ans;
     }
 };
@@ -59,57 +68,44 @@ public:
 // for each asteroid. 
 
 // Space complexity : O(n)
-// as the maximum number of elements in the stack is equal to the number of elements in the 
-// asteroids vector.
+// as the max np of elements in the stack is equal to the number of elements in the asteroids vector.
 
 
 
 
-class Solution {
-public:
-    vector<int> asteroidCollision(vector<int>& A) {
-        vector<int> ans;
-        for(int i : A) {
-            while(!ans.empty() and ans.back() > 0 and ans.back() < -i) ans.pop_back();
-            
-            if(ans.empty() or ans.back() < 0 or i > 0) ans.push_back(i);
-            else if(i < 0 and ans.back() == -i) ans.pop_back();
-        }
-        return ans;
-    }
-};
 
 
-
-
+// A 2
 
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
-
-        for(auto it: asteroids){
-            if(it >= 0) st.push(it);
+    vector<int> asteroidCollision(vector<int> &asteroids) {
+        int n = asteroids.size();
+        vector<int> v;
+        stack<int> st;        
+        for(int i=0; i<n; i++){
+            if(asteroids[i] > 0) st.push(asteroids[i]);
             else {
-                while(st.size() && st.top() < abs(it) && st.top() >= 0) st.pop();
-                if(st.size()==0) st.push(it);
-                else {
-                    if(st.top() == abs(it)) st.pop();
-                    else if(st.top() < 0) st.push(it);
-                }
-            }
+                while(!st.empty() && (-1 * asteroids[i]) > st.top()) st.pop();
+                if(!st.empty() && (-1 * asteroids[i]) == st.top()) st.pop();
+                else if(st.empty()) v.push_back(asteroids[i]);
+            } 
         }
-
-        vector<int> ans;
-        while(st.size()){
-            ans.emplace_back(st.top());
+        
+        vector<int> p;
+        while(!st.empty()){
+            p.push_back(st.top());
             st.pop();
         }
 
-        reverse(ans.begin(),ans.end());
-        return ans;
+        reverse(p.begin(), p.end());
+        v.insert(v.end(), p.begin(), p.end());
+        return v;
     }
 };
+
+
+
 
 
 
