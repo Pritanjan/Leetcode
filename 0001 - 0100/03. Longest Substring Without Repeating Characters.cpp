407ce39f5bf3f8a07a33aaf7902c31f878bd6c9b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-19 21:19:03 +0530

    Update 03. Longest Substring Without Repeating Characters.cpp

--- 0001 - 0100/03. Longest Substring Without Repeating Characters.cpp
+++ 0001 - 0100/03. Longest Substring Without Repeating Characters.cpp
@@ -1,5 +1,4 @@
-// APPROACH 1 [ Brute force with hash set ]
-
+// A 1 [ Brute force with hash set ]
 // Here, we are considering all possible substrings and check whether they have repeating characters.
 // We will be using a hash set to keep track of the characters in each substring and 
 // check if a character is already in the hash set before adding it. 
@@ -30,15 +29,13 @@ public:
 
 
 
-// APPROACH 2 [  Sliding window with hash set ]
-
-// Using a sliding window approach to solve the problem more efficiently than brute force.
-// We can maintain a hash set to keep track of the characters in the current substring, 
-// and use two pointers, i and j, to define the current substring.
+// A 2 [  Sliding window with hash set ] more efficiently than brute force.
+// Kaintain a hash set to keep track of the characters in the current substring, & use 2 pointers,
+// i & j, to define the current substring.
 // We can move the end pointer to the right and add the new character to the hash set. 
-// If the new character is already in the hash set, we move the start pointer to the right
-// until the repeating character is removed from the hash set. We can update the maximum
-// length of the substring seen so far at each step.
+// If the new character is already in the hash set, we move the start pointer to the right until the
+// repeating character is removed from the hash set. 
+// Update the maximum length of the substring seen so far at each step.
 
 class Solution {
 public:
@@ -59,7 +56,6 @@ public:
 
 // T.C. --> O(N),  N is the length of the input string
 // Since each character is visited at most twice once with the end pointer and once with start pointer.
-
 // S.C. --> O(min(n, m)) where m is the size of the character set. 
 // We need to store the characters in the hash set, which can take up to m space in the worst case.
 
@@ -67,8 +63,8 @@ public:
     
 
 
-// APPROACH 3 [  Sliding window with hash map ]
 
+// A 3 [  Sliding window with hash map ]
 // We can further optimize the sliding window approach by using a hash map to keep track 
 // of the last occurrence of each character in the input string. 
 // Instead of moving the start pointer one character at a time until the repeating character
@@ -84,7 +80,8 @@ public:
         unordered_map<char, int> last_seen;
         int ans = 0, i = 0;
         for (int j = 0; j < n; j++) {
-            if (last_seen.count(s[j])) {
+            // if (last_seen.count(s[j])) {
+            if(last_seen.find(s[j]) != last_seen.end()) {
                 i = max(i, last_seen[s[j]]+1);
             }
             ans = max(ans, j-i+1);
@@ -94,10 +91,8 @@ public:
     }
 };
 
-
 // T.C. --> O(n) where n is the length of the input string.
 // We visit each character at most twice, once with the j pointer and once with the i pointer.
-
 // S.C. --> O(min(n, m)) where m is the size of the character set. 
 // We need to store the last occurrence of each character in the hash map, 
 // which can take up to m space in the worst case.
@@ -107,7 +102,7 @@ public:
 
 
 
-// APPROACH 4
+// A 4
 
 class Solution {
 public:
@@ -117,8 +112,7 @@ public:
         int lensub = 0;
         for(int i=0; i<s.size(); i++){
             if(v[s[i]] > start)  start  = v[s[i]];
-            v[s[i]] = i;
-            
+            v[s[i]] = i;            
             lensub = max(lensub, i-start);
         }
         return lensub;
@@ -131,11 +125,10 @@ public:
 
 
 
-// APPROACH 5 [ SLIDING WINDOW ]
 
+// A 5 [ SLIDING WINDOW ]
 // Here the L and R pointers define the substring, and a vector called v is used to keep track 
 // of the occurrence of each character in the substring.
-
 // The code iterates over the string s until the right pointer R reaches the end of the string.
 // In each iteration, it increments the count of the character at the right pointer in the v vector. 
 // If the occurrence count becomes more than 1, it contracts the present window by moving the
@@ -143,7 +136,6 @@ public:
 
 // At the end of each iteration, the code updates the lensub variable with the maximum length 
 // of the substring found so far. The final value of lensub is returned as the result.
-
 
 class Solution {
 public:
@@ -166,10 +158,8 @@ public:
     }
 };
 
-
 // T.C. --> O(N), N is the length of the input string
 // Since each character is visited at most twice.
-
 // S.C. --> O(1),  
 // Since the size of the v vector is constant at 256.
 
