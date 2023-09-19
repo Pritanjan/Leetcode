e0e30cd354c6f78e7dcf7b96c201939597641b9b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-19 13:59:07 +0530

    Update 402. Remove K Digits.cpp

--- 0401 - 0500/402. Remove K Digits.cpp
+++ 0401 - 0500/402. Remove K Digits.cpp
@@ -1,46 +1,35 @@
-// https://sarthaksehgal.com/leetcode101/lc402/
-
-//  APPROACH 1
+//  A 1 - Using Stack
 
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<char> s;
-
+        stack<char> st;
         // if the curr element is less than prev then remove the prev element else push it into stack
         for(char c : num) {
-            while(k > 0 && !s.empty() && s.top() > c) {
-                s.pop();
+            while(k > 0 && !st.empty() && st.top() > c) {
+                st.pop();
                 k--;
             }
-            s.push(c);
+            st.push(c);
         }
 
-
-        while(k > 0) {
-            // for cases like "456" where every num[i] > num.top()
-            s.pop();
-            k--;
-        }
-        
+        // for cases like "456" where every num[i] > num.top()
+        while(k-- > 0) st.pop();
         string res;
-        while(!s.empty()) {
+        while(!st.empty()) {
             // res = s.top() + res;
-            res.push_back(s.top());     // pushing stack top to string
-            s.pop();    // pop the top element
+            res.push_back(st.top());     // pushing stack top to string
+            st.pop();    // pop the top element
         }
 
-        reverse(res.begin(), res.end()); // reverse the string 
-
-        
-        int i = 0;
-        while (i < res.size() - 1 && res[i] == '0') i++;
-        res = res.substr(i);
+        reverse(res.begin(), res.end()); // reverse the string         
+        int idx = 0;
+        while(idx < res.size() - 1 && res[idx] == '0') idx++;
+        res = res.substr(idx);
         
         return res.length() == 0 ? "0" : res;
     }
 };
-
 
 // T.C. ->  O(N)
 // S.C. -> O(N)
@@ -48,30 +37,26 @@ public:
 
 
 
-//  APPROACH 2
+
+
+// A 2
 
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        string ans  = "";
-        
+        string ans  = "";        
         for(char c : num){
             while(ans.size() && ans.back() > c && k) {
                 ans.pop_back();
                 k--;
-            }
-            
+            }            
             if(ans.size() || c!='0') ans.push_back(c);
         }
         
-        while(ans.size() && k--)
-            ans.pop_back();
-        
+        while(ans.size() && k--) ans.pop_back();
         return ans == "" ? "0" :ans;
     }
 };
-
-
 
 // Time complexity: O(n), where n is the length of the input string "num". 
 // The algorithm iterates through the input string once.
@@ -83,13 +68,13 @@ public:
 
 
 
-//  APPROACH 3
+
+//  A 3
 
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        if(num.size() == k) return "0";
-        
+        if(num.size() == k) return "0";        
         for(int i=0; i<num.size() - 1 && k > 0; i++){
             if(num[i] > num[i+1] && k > 0){ 
                 k--;
@@ -98,14 +83,13 @@ public:
                 if(i >= 0) i--;
             }
         }
+        
         while(k-- > 0 && num.size() > 0) num.erase(num.size() - 1, 1);
-
         while(num.size() > 0 && num[0] == '0') num.erase(0, 1);
         if(num.size() == 0) return "0";
         return num;
     }
 };
-
 
 // Time complexity of the given function is O(nk), where n is the length of the input string "num".
 // The while loop for removing digits at the end takes O(k) time and the for loop takes O(nk) 
@@ -118,3 +102,5 @@ public:
 
 
 
+
+// A 4
