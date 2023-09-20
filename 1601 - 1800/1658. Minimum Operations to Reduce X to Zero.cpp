2b0b2c2752f94a72a7d1272b93e66e1824ce3b20
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-20 10:07:47 +0530

    Update and rename 1658. Minimum Operations to Reduce X to Zero ARRAY HASH_TABLE BINARY_SEARCH SLIDING_WINDOW.cpp to 1658. Minimum Operations to Reduce X to Zero.cpp

--- 1601 - 1800/1658. Minimum Operations to Reduce X to Zero.cpp
+++ 1601 - 1800/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -1,32 +1,25 @@
-//https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+// A 1
 
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int x) {
-        int ans = INT_MIN;
-        
+    int minOperations(vector<int>& nums, int x) {        
         int target = -x;
-        for(auto i : nums)
-            target += i;
-        
-        if(target < 0)
-            return -1;
+        for(auto i : nums) target += i;
+        if(target < 0) return -1;
         
         int j = 0;
         // int sum = accumulate(nums.begin(),nums.end(),0); 
         int sum = 0;
-        
+        int ans = INT_MIN;
         for(int i=0; i<nums.size(); i++){
             sum += nums[i];
             while(sum > target){
                 sum -= nums[j];
                 j++;
             }
-        
-            if(sum == target)
-                ans = max(ans, i-j+1);
-        }
-         
+            if(sum == target) ans = max(ans, i-j+1);
+        }         
         return ans==INT_MIN ? -1 : nums.size() - ans;
     }
 };
+
