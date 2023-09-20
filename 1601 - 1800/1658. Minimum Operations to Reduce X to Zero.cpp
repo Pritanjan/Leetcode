57373511018be71f38043beed7a8e44bbd990c1f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-20 11:47:28 +0530

    Update 1658. Minimum Operations to Reduce X to Zero.cpp

--- 1601 - 1800/1658. Minimum Operations to Reduce X to Zero.cpp
+++ 1601 - 1800/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -52,4 +52,81 @@ public:
 
 
 
-// 
+// A 3
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int sum = 0;
+        for(int n : nums) {
+            sum += n;
+        }
+        x = sum - x;
+        int n = nums.size();
+        
+        if(x == 0) return n;
+        for(int i=1; i<n; i++) {
+            nums[i] += nums[i-1];
+        }
+
+        int res = numeric_limits<int>::max();
+        unordered_map<int, int> map;
+        map[0] = -1;
+
+        for(int i=0; i<n; i++) {
+            int need = nums[i] - x;
+            if(map.count(need)) {
+                int size = i - map[need];
+                res = min(res, n - size);
+            }
+            map.emplace(nums[i], i);
+        }
+        return res == numeric_limits<int>::max() ? -1 : res;
+    }
+};
+
+
+
+
+
+
+// A 4
+
+class Solution {
+public:
+    int INF = 1000000;
+    int binarySearch(int R, vector<int>& v,int target) {
+        // return operations to find the value
+        int L = 0, mid;
+        while(L <= R) {
+            mid = L + (R - L) / 2;
+            if(v[mid] == target) return mid + 1;
+            else if(v[mid] < target) L = mid + 1;
+            else R = mid-1;
+        }
+        return INF;
+    }
+
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        vector<int> v(n, 0);
+        v[0] = nums[0];
+        for(int i=1; i<n; i++) v[i] = v[i-1] + nums[i];
+
+        if(v[n-1] < x)  return -1;
+        if(v[n-1] == x) return n;
+        int op = n;
+        
+        for(int i=n; i>=0; i--) {
+            if(i != n) x += - nums[i];
+            if(x == 0) op = min(op, n-i);
+            if(x < 0) break;
+            int Lop = binarySearch(i-1, v, x);
+
+            op = min(op, n - i + Lop);
+        }
+        return op == n ? -1 : op;
+    }
+};
+
+
