984c7a2f426752d3e6df17628a3ea657fafdfb13
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-21 02:53:34 +0530

    Update 198. House Robber.cpp

--- 0101 - 0200/198. House Robber.cpp
+++ 0101 - 0200/198. House Robber.cpp
@@ -3,10 +3,8 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
         int prev1 = 0, prev2 = 0;
-        int temp = 0;
-        
+        int temp = 0;        
         for(int i=0; i<nums.size(); i++) {
             temp = max(prev1, prev2 + nums[i]);
             prev2 = prev1 ;
@@ -16,8 +14,36 @@ public:
     }
 };
 
-// Time Complexity : O(N)
+// Time Complexity  : O(N)
 // Space Complexity : O(1)
+
+
+// OR
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int h1 = 0, h2 = 0, h3 = 0;
+        int temp = 0;        
+        for(int i=0; i<nums.size(); i++) {
+            temp = nums[i] + max(h2,h3) ;
+            h3 = h2;
+            h2 = h1;
+            h1 = temp;
+        }
+        return max(h1,h2);
+    }
+};
+ 
+// 1. Use 3 variables to store the amount of money the robber have if robbed in previous three houses;
+//    for the current house the amount will be the either :
+// 2. if robbed amount in 2 last house is more than 3rd last then robbed money for current house will be
+//    the sum of money in current house and 2 last house.
+// 3. else the robbed amount in current house would be the sum of money in current and 3rd last house.
+
+// T.C. --> O(N)
+// S.C. --> O(1)
 
 
 
@@ -29,24 +55,36 @@ public:
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int h1 = 0, h2 = 0, h3 = 0;
-        int temp = 0;
-        
-        for(int i=0; i<nums.size(); i++) {
-            temp = nums[i] + max(h2,h3) ;
-            h3 = h2;
-            h2 = h1;
-            h1 = temp;
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        // Create an array to store the maximum amount of money robbed up to the current house
+        vector<int> dp(n, 0);
+
+        // Initialize the first two elements of dp
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        // Fill in the dp array
+        for(int i=2; i<n; ++i) {
+            // Decide whether to rob the current house or not
+            // If we rob the current house (i), we add its value to the maximum from two houses ago (i - 2)
+            // If we skip the current house, we take the maximum from the previous house (i - 1)
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
         }
-        return max(h1,h2);
+
+        // The last element of dp contains the maximum amount that can be robbed
+        return dp[n-1];
     }
 };
 
 
-//C++ solution (time taken : 0ms, time complexity O(n), Space Complexity O(1) )
-//
-//Used 3 variables to store the amount of money the robber have if robbed in previous three houses;
-//for the current house the amount will be the either :
-//
-//if robbed amount in 2 last house is more than 3rd last then robbed money for current house will be the sum of money in current house and 2 last house.
-//else the robbed amount in current house would be the sum of money in current and 3rd last house.
+
+
+
+
+// A 3 
+
+
+// T.C. --> O(N)
