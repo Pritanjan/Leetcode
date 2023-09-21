20b63b0a410cab6b5bc8395e0dc179ddbfce0e91
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-21 11:49:43 +0530

    Update 198. House Robber.cpp

--- 0101 - 0200/198. House Robber.cpp
+++ 0101 - 0200/198. House Robber.cpp
@@ -84,7 +84,7 @@ public:
 
 
 
-// A 3 
+// A 3 - Recursive Approach with Memoization (Top-Down DP)
 
 class Solution {
 public:
@@ -104,9 +104,44 @@ private:
     }
 };
 
+// T.C. --> O(N)
 
 
 
 
 
-// A 4
+
+// A 4 - Greedy Approach
+// Use a greedy strategy by considering whether to rob each house based on the amount of money
+// it contains.
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        int evSum = 0;
+        int odSum = 0;
+        for(int i=0; i<n; ++i) {
+            if(i % 2 == 0) {
+                evSum += nums[i];
+                evSum = max(evSum, odSum);
+            } 
+            else {
+                odSum += nums[i];
+                odSum = max(evSum, odSum);
+            }
+        }
+        return max(evSum, odSum);
+    }
+};
+
+
+
+
+
+
+// A 5
+
