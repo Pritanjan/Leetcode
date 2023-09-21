f5b9ac6e52f9266326bd44546bf1fb13254b8019
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-21 15:50:33 +0530

    Update 930. Binary Subarrays With Sum Solved.cpp

--- 0901 - 1000/930. Binary Subarrays With Sum Solved.cpp
+++ 0901 - 1000/930. Binary Subarrays With Sum Solved.cpp
@@ -1,3 +1,24 @@
+// A 0 - Brute Forc - TLE
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int cnt = 0;
+        int n = nums.size();
+        for(int start=0; start<n; start++) {
+            int sum = 0;
+            for(int end=start; end<n; end++) {
+                sum += nums[end];
+                if(sum == goal) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+
+
 // A 1 - USing Hash Map
 
 class Solution {
