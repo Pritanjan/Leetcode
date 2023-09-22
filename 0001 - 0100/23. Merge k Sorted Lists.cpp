94fca70418f9c912d122e9be56cb4bb920f60306
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-22 15:34:54 +0530

    Update 23. Merge k Sorted Lists.cpp

--- 0001 - 0100/23. Merge k Sorted Lists.cpp
+++ 0001 - 0100/23. Merge k Sorted Lists.cpp
@@ -148,6 +148,39 @@ public:
 };
 
 
+// OR
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(l1 -> val < l2 -> val) {
+            l1 -> next = mergeTwoLists(l1 -> next, l2);
+            return l1;
+        } 
+        else {
+            l2 -> next = mergeTwoLists(l1, l2 -> next);
+            return l2;
+        }
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        if(n == 0) return nullptr;
+        if(n == 1) return lists[0];
+
+        int mid = n / 2;
+        vector<ListNode*> L(lists.begin(), lists.begin() + mid);
+        vector<ListNode*> R(lists.begin() + mid, lists.end());
+
+        ListNode* mergedL = mergeKLists(L);
+        ListNode* mergedR = mergeKLists(R);
+
+        return mergeTwoLists(mergedL, mergedR);
+    }
+};
 
 
 
