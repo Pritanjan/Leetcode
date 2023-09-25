b90770ac7ca9d0d5417fde2724d0ab3c60ca8295
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-25 18:56:13 +0530

    Update 101. Symmetric Tree.cpp

--- 0101 - 0200/101. Symmetric Tree.cpp
+++ 0101 - 0200/101. Symmetric Tree.cpp
@@ -1,7 +1,5 @@
-// A 1 - Recursive Approach with Two Pointers:
-
-// We can use two pointers to traverse the tree simultaneously—one pointing to the left subtree and 
-// the other to the right subtree. 
+// A 1 - Recursive Approach with 2 Pointers
+// Use 2 pointers to traverse tree one pointing to the left subtree and the other to the right subtree. 
 // Compare the values of nodes pointed by these pointers to check for symmetry.
 
 class Solution {
@@ -40,18 +38,17 @@ public:
 
 
 // A 2 - Using a Single Queue with Level Separation
-
-// We start by checking if the root node is NULL. If it is, we return true. 
-// Otherwise, we create a queue and add the left and right child nodes of the root to it.
-// We then iterate over the queue until it is empty. 
-// In each iteration, we pop the first two nodes from the queue and check if they are both NULL.
-// If they are, we skip the current iteration.
-// If one of them is NULL and the other is not, we return false. 
-// If neither of them is NULL, we check if their values are equal. 
-// If they are not, we return false. Otherwise, 
-// we add the left child node of the left node, the right child node of the right node, 
-// the right child node of the left node, and the left child node of the right node
-// to the queue (in that order). We continue this process until the queue is empty. 
+// Start by checking if the root node is NULL. If it is return true. 
+// Otherwise, create a queue and add the left and right child nodes of the root to it.
+// Then iterate over the queue until it is empty. 
+// In each iteration, pop the first two nodes from the queue and check if they are both NULL.
+// If they are, skip the current iteration.
+// If one of them is NULL and the other is not, return false. 
+// If neither of them is NULL, check if their values are equal. 
+// If they are not, we return false. 
+// Add the left child node of the left node, the right child node of the right node, the right child
+// node of the left node, and the left child node of the right node to the queue (in that order). 
+// continue this process until the queue is empty. 
 // If we have not returned false at any point, we return tr
 
 class Solution {
@@ -63,10 +60,8 @@ public:
         q.push(root -> right);
     
         while(!q.empty()) {
-            TreeNode* L = q.front();
-            q.pop();
-            TreeNode* R = q.front();
-            q.pop();
+            TreeNode* L = q.front();  q.pop();
+            TreeNode* R = q.front();  q.pop();
 
             if(L == NULL && R == NULL) continue;
             if(L == NULL || R == NULL) return false;
@@ -85,8 +80,33 @@ public:
 // This is because we need to visit every node in the tree once.
 
 // S.C. --> O(n), where n is the number of nodes in the tree. 
-// This is because in the worst case, the queue can contain all the nodes in the tree, which is n. 
+// Because in worst case, the queue can contain all the nodes in the tree, which is n. 
 // Additionally, we are not using recursion, so we do not need to worry about the call stack space.
+
+
+// OR
+
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        queue<pair<TreeNode*, TreeNode*>> que;
+        que.push({root -> left, root -> right});
+
+        while(!que.empty()) {
+            pair<TreeNode*, TreeNode*> nodes = que.front(); que.pop();
+            TreeNode* L = nodes.first;
+            TreeNode* R = nodes.second;
+
+            if(!L && !R) continue;
+            if(!L || !R || L -> val != R -> val) return false;
+            que.push({L -> left,  R -> right});
+            que.push({L -> right, R -> left});
+        }
+        return true;
+    }
+};
 
 
 
@@ -94,8 +114,7 @@ public:
 
 
 // A 3 - Using a Stack:
-
-// Use two stacks for simultaneous in-order traversals of the left and right subtrees.
+// Use 2 stacks for simultaneous in-order traversals of the left and right subtrees.
 // Compare corresponding nodes from both stacks in each step.
 
 class Solution {
@@ -116,10 +135,8 @@ public:
             }
             if(Lstk.size() != Rstk.size()) return false;
             if((!Lstk.empty() && !Rstk.empty()) && (Lstk.top() -> val != Rstk.top() -> val)) return false;
-            L = Lstk.top() -> right;
-            Lstk.pop();
-            R = Rstk.top() -> left;
-            Rstk.pop();
+            L = Lstk.top() -> right;  Lstk.pop();
+            R = Rstk.top() -> left;   Rstk.pop();
         }
         return true;
     }
@@ -131,7 +148,7 @@ public:
 
 
 // A 4 - Construct Mirrored Trees and Compare:
-// We can create two new trees by mirroring the left and right subtrees of the original tree.
+// Create two new trees by mirroring the left and right subtrees of the original tree.
 // Then, compare the nodes of the two mirrored trees to check for symmetry.
 
 class Solution {
@@ -144,7 +161,6 @@ public:
 
     TreeNode* mirrorTree(TreeNode* root) {
         if(!root) return nullptr;
-
         TreeNode* mirrored = new TreeNode(root -> val);
         mirrored -> left  = mirrorTree(root -> right);
         mirrored -> right = mirrorTree(root -> left);
@@ -168,4 +184,4 @@ public:
 // A 5
 
 
-// 
+
