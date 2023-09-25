d515302d2e1d1568eeb52a575c23be69929444c0
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-25 16:24:52 +0530

    Update 199. Binary Tree Right Side View.cpp

--- 0101 - 0200/199. Binary Tree Right Side View.cpp
+++ 0101 - 0200/199. Binary Tree Right Side View.cpp
@@ -1,21 +1,17 @@
 // Right View of Binary Tree
-
-// APPROACH 1 DFS 
+// A 1 DFS 
 
 class Solution {
 public:
     void RT(TreeNode* root, int level, int& mxLevel, vector<int>& res){
         if(root == nullptr) return ;
-    
         if(level > mxLevel) {
             mxLevel = level;
             res.push_back(root -> val);
-        }
-    
+        }    
         RT(root->right, level + 1, mxLevel, res);
         RT(root->left, level + 1, mxLevel, res);
     }
-
     vector<int> rightSideView(TreeNode* root) {
         vector<int> res;
         int mxLevel = -1;
@@ -36,8 +32,7 @@ public:
         
         helper(node -> right, ans, n+1);
         helper(node -> left,  ans, n+1);
-    }
-    
+    }    
     vector<int> rightSideView(TreeNode* node) {
         vector<int> ans;
         helper(node, ans, 0);
@@ -45,29 +40,28 @@ public:
     }
 };
 
-
-
 // T.C. O(N)
 // S.C. O(h)
 
 
-// APPROACH 2 BFS 
+
+
+
+
+// A 2 BFS 
 
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         if(root == NULL) return {};
-
         vector<int> res;
         queue<TreeNode*> que;
         que.push(root);
-
+        
         while(!que.empty()){
             int n = que.size();
-
             while(n--){
-                TreeNode* temp = que.front();
-                que.pop();
+                TreeNode* temp = que.front();  que.pop();
 
                 if(n == 0) res.push_back(temp -> val);
                 if(temp -> left != NULL) que.push(temp -> left);
@@ -78,9 +72,56 @@ public:
     }
 };
 
-
 // T.C. O(N)
 // S.C. O(N)
+
+
+
+
+
+
+// A 3 - DFS with stack
+
+class Solution {
+public:    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if(!root) return res;
+
+        stack<pair<TreeNode*, int>> stk;  // Stack to perform DFS along with depth information
+        stk.push({root, 0});  // Push the root node along with depth 0
+
+        while(!stk.empty()) {
+            TreeNode* node = stk.top().first;
+            int depth = stk.top().second;
+            stk.pop();
+
+            if(depth == res.size()) {
+                // If the result vector size is the same as the current depth,
+                // it means we are visiting the rightmost node at this depth.
+                res.push_back(node -> val);
+            }
+
+            // Perform DFS on the left and right subtrees, pushing them onto the stack
+            // along with their respective depths.
+            if(node -> left)  stk.push({node -> left,  depth + 1});
+            if(node -> right) stk.push({node -> right, depth + 1});
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
