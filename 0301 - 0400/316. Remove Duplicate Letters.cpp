b1f9e11d3a120b7fa281ac1413d6b1077de9303b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-26 09:17:41 +0530

    Update 316. Remove Duplicate Letters.cpp

--- 0301 - 0400/316. Remove Duplicate Letters.cpp
+++ 0301 - 0400/316. Remove Duplicate Letters.cpp
@@ -1,4 +1,4 @@
-// APPROACH 1
+// A 1
 
 class Solution {
 public:
@@ -12,69 +12,70 @@ public:
 
         // Iterate over the string and remove duplicate characters
         for(const char c : s) {
-            --count[c];
-            
-            if (used[c]) continue;
-
+            --count[c];            
+            if(used[c]) continue;
             while (!ans.empty() && ans.back() > c && count[ans.back()] > 0) {
                 used[ans.back()] = false;
                 ans.pop_back();
             }
-            
             used[c] = true;
             ans.push_back(c);
         }
-
         return ans;
     }
 };
 
-
 // T.C. --> O(N)
 // S.C. --> O(N)
 
-// The given code is a C++ program that implements a function called "removeDuplicateLetters" which takes a string as input and returns a string with duplicate letters removed. The code uses two vectors, "count" and "used", to keep track of the frequency of characters in the string and whether a character has already been used, respectively. The function then iterates over the string and removes duplicate characters while maintaining the original order of the characters in the string.
-    
-// The function initializes an empty string called "ans" and two vectors "count" and "used", each of size 128. It then counts the frequency of each character in the input string using a for-each loop, and updates the "count" vector accordingly.
+ // It uses two vectors, "count" and "used", to keep track of the frequency of characters in the string
+// and whether a character has already been used, respectively. 
+// The function iterates over the string and removes duplicate characters while maintaining the original
+// order of the characters in the string. 
+// It initializes an empty string called "ans" and two vectors "count" and "used", each of size 128. 
+// It then counts the frequency of each character in the input string using a for-each loop, 
+// and updates the "count" vector accordingly.
 
-// The function then iterates over each character in the input string and does the following for each character:
-
+// It iterates over each character in the input string and does the following for each character:
 // -> Decrement the frequency of the current character in the "count" vector.
 // -> If the current character has already been used, continue to the next character.
-// -> While the "ans" string is not empty and the last character in the "ans" string is greater than the current character, and the frequency of the last character in the "count" vector is greater than 0, remove the last character from the "ans" string and mark it as unused in the "used" vector.
+// -> While the "ans" string is not empty and the last character in the "ans" string is greater than
+//    the current character, and the frequency of the last character in the "count" vector is greater
+//    than 0, remove the last character from the "ans" string and mark it as unused in the "used" vector.
 // -> Mark the current character as used in the "used" vector and append it to the "ans" string.
 // Finally, the function returns the resulting string with duplicate characters removed.
 
 
 
 
-// APPROACH 2 SAME AS 1 (USING STACK)
+
+
+// A 2 SAME AS 1 (USING STACK)
 
 class Solution {
 public:
-    string smallestSubsequence(string s) {
+    string removeDuplicateLetters(string s) {
         stack<char> st;
-        vector<int> count(26, 0);
-        vector<bool> visited(26, false);
-        for(char c : s) count[c - 'a']++;
+        vector<int> cnt(26, 0);
+        vector<bool> vis(26, false);
+        for(char c : s) cnt[c - 'a']++;
     
         for(char c : s) {
-            count[c - 'a']--;
-            if(visited[c - 'a']) continue;
-        
-            while(!st.empty() && c < st.top() && count[st.top() - 'a'] > 0) {
-                visited[st.top() - 'a'] = false;
+            cnt[c - 'a']--;
+            if(vis[c - 'a']) continue ;        
+            while(!st.empty() && c < st.top() && cnt[st.top() - 'a'] > 0) {
+                vis[st.top() - 'a'] = false;
                 st.pop();
             }
             st.push(c);
-            visited[c - 'a'] = true;
+            vis[c - 'a'] = true;
         }
-        string result = "";
+        string res = "";
         while(!st.empty()) {
-            result = st.top() + result;
+            res = st.top() + res;
             st.pop();
         }
-        return result;
+        return res;
     }
 };
 
@@ -83,11 +84,9 @@ public:
 
 
 
-// APPROACH 2
-
-// Here we are using two integer arrays to keep track of the count and 
-// maximum index of each character in the input string, and
-// a nested loop to find the lexicographically smallest character at each iteration.
+// A 3
+// Here we are using 2 integer arrays to keep track of the count & max index of each character
+// in the string, and nested loop to find the lexicographically smallest character at each iteration.
 
 class Solution {
 public:
@@ -106,19 +105,15 @@ public:
             int idx = -1;
             for(int i = start; i < s.size(); i++) {
                 if(count[s[i] - 'a'] == 0) continue;
-
                 if(s[i] - 'a' < min) {
                     min = s[i] - 'a';
                     idx = i;
                 }
-                if (count[s[i] - 'a'] == 1 || maxs[s[i] - 'a'] <= i) {
-                    break;
-                }
+                if(count[s[i] - 'a'] == 1 || maxs[s[i] - 'a'] <= i) break;
             }
-            if (idx == -1) {
-                break;
-            }
-            for (int i = start; i < idx; i++) {
+            
+            if(idx == -1) break;
+            for(int i = start; i < idx; i++) {
                 count[s[i] - 'a'] = max(0, count[s[i] - 'a'] - 1);
             }
             count[min] = 0;
@@ -128,9 +123,6 @@ public:
         return result;
     }
 }; 
-
-
-
 
 // T.C. --> O(N)
 // S.C. --> O(1)
