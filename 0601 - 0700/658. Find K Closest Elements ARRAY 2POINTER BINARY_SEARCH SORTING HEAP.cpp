19416c484778f3b0d5f78c0ce8bf26b182005139
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-27 09:41:08 +0530

    Update 658. Find K Closest Elements ARRAY 2POINTER BINARY_SEARCH SORTING HEAP.cpp

--- 0601 - 0700/658. Find K Closest Elements ARRAY 2POINTER BINARY_SEARCH SORTING HEAP.cpp
+++ 0601 - 0700/658. Find K Closest Elements ARRAY 2POINTER BINARY_SEARCH SORTING HEAP.cpp
@@ -1,18 +1,18 @@
-//https://leetcode.com/problems/find-k-closest-elements/
-
-//We can initialize two pointers L=0 and R=n-1. Now our window size is n and contains excess elements. We will keep reducing the [L, R] window size till it becomes exactly equal to k. We can do this based on the condition - x - arr[L] <= arr[R] - x. If it is true, then it means arr[R] is farther away from x than arr[L] and thus we will eliminate arr[R]. Otherwise, we will eliminate arr[L].
+// A 1 - 2 Pointer
+// initialize two pointers L = 0 and R = n-1. Now our window size is n and contains excess elements. 
+// We will keep reducing the [L, R] window size till it becomes exactly equal to k. 
+// We can do this based on the condition - x - arr[L] <= arr[R] - x. 
+// If it is true, then it means arr[R] is farther away from x than arr[L] and
+// thus we will eliminate arr[R]. Otherwise, we will eliminate arr[L].
 
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& nums, int k, int x) {
         int L = 0;
-        int R = nums.size() - 1;
-            
+        int R = nums.size() - 1;         
         while(R - L >= k) {
-            if(abs(nums[L] - x) <= abs(nums[R] - x))
-                R--;
-            else
-                L++;
+            if(abs(nums[L] - x) <= abs(nums[R] - x))  R--;
+            else  L++;
         }
         return vector<int> (nums.begin() + L, nums.begin() + R + 1);        
     }
@@ -24,6 +24,31 @@ public:
 
 
 
+
+
+// A 2 - Binary Search
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& nums, int k, int x) {
+        int L = 0;
+        int R = nums.size() - k;            
+        while(R > L) {
+            int mid = L + (R - L)/2;
+            if(x - nums[mid] > nums[mid + k] - x) L = mid + 1;
+            else R = mid;
+        }
+        return vector<int> (nums.begin() + L, nums.begin() + L + k);        
+    }
+};
+
+
+
+
+
+
+// A 3
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
@@ -33,16 +58,13 @@ public:
         }
         
         sort(arr.begin(), arr.end(),[](int a, int s){
-            if(abs(a) == abs(s))
-                return a < s;
-            else
-                return abs(a) < abs(s);
+            if(abs(a) == abs(s))  return a < s;
+            else  return abs(a) < abs(s);
         });
         
         for(int i=0; i<k; i++){
             ans.push_back(arr[i]+x);
-        }
-            
+        }            
         sort(ans.begin(), ans.end());
         return ans;
     }
@@ -53,7 +75,7 @@ public:
 
 
 
-
+//  A 4 
 
 
 
