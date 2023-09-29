f3baebd498baec09df28582e6897d3ee3d8f276b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-29 21:22:08 +0530

    Update 229. Majority Element II.cpp

--- 0201 - 0300/229. Majority Element II.cpp
+++ 0201 - 0300/229. Majority Element II.cpp
@@ -1,19 +1,52 @@
 // Similar question LC 169
-// APPROACH 1 Brute Force
+// A 1 - Using Hashing and Sliding Window
+// 1. This approach uses a hash map to keep track of the count of each element in the array.
+// 2. As we iterate through the array, we increment the count of each element in the hash map.
+// 3. If the count of an element becomes greater than the threshold, we add that element to the result
+//    vector and set its count to a negative value to mark it as processed.
+// T.C. --> O(n) 
+// S.C. --> O(k), where k is the number of distinct elements in the array.
 
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        vector<int> v;
-        unordered_map<int, int> um;
-        // map<int, int> um;  //BOTH WORKS
-        for(auto i : nums) um[i]++;
-        
-        for(auto it : um) {
-            if(it.second > n/3) v.push_back(it.first);
-        }            
-        return v;
+        int threshold = n / 3;
+        unordered_map<int, int> cnts;
+        vector<int> res;
+        for(auto num : nums) {
+            cnts[num]++;
+        }
+
+        for(auto &it : cnts) {
+            if(it.second > threshold) {
+                res.push_back(it.first);
+            }
+        }
+        return res;
+    }
+};
+
+
+// OR
+
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int threshold = n / 3;
+        unordered_map<int, int> cnts;
+        vector<int> res;
+
+        for(int num : nums) {
+            cnts[num]++;
+            if(cnts[num] > threshold) {
+                res.push_back(num);
+                cnts[num] = -threshold - 1; // To avoid adding duplicates
+            }
+        }
+        return res;
     }
 };
 
@@ -22,14 +55,18 @@ public:
 
 
 
-// APPROACH 2 Boyer-Moore Voting Algorithm
-
- // use the Boyer-Moore Voting Algorithm to identify potential candidates.
-// We maintain two candidates (candidate1 and candidate2) and two counters (count1 and count2) to keep track of their counts.
-// We iterate through the input array. If the current number is one of the candidates, we increase its count; otherwise, if a counter is zero, we update the candidate and reset the count.
-// If neither of the above cases applies, we decrement both counters since we've encountered two distinct numbers that aren't candidates.
-// After finding potential candidates, we iterate through the array again to count their occurrences using count1 and count2.
-// Finally, we check if the counts of the potential candidates are greater than the threshold, and if so, we add them to the result vector.
+// A 2 - Boyer-Moore Voting Algorithm
+// 1. Maintain 2 candidates (candidate1 and candidate2) & 2 counters (count1 and count2) to keep track 
+//    of their counts.
+// 2. We iterate through the input array. If the current number is one of the candidates, 
+//    we increase its count; otherwise, if a counter is zero, 
+//    we update the candidate and reset the count.
+// 3. If neither of the above cases applies, decrement both counters since we've encountered 
+//    2 distinct numbers that aren't candidates.
+// 4. After finding potential candidates, we iterate through the array again to count their occurrences
+//    using count1 and count2.
+// 5. Finally, check if the counts of the potential candidates are greater than the threshold, 
+//    and if so, we add them to the result vector.
 
 class Solution {
 public:
@@ -58,7 +95,6 @@ public:
         // Counting occurrences of the potential candidates
         cnt1 = 0;
         cnt2 = 0;
-
         for(int num : nums) {
             if(num == candidate1) cnt1++;
             else if(num == candidate2) cnt2++;
@@ -77,46 +113,14 @@ public:
 
 
 
-// APPROACH 3  Using Hashing and Sliding Window
-
-Explanation:
-
-// This approach uses a hash map to keep track of the count of each element in the array.
-// As we iterate through the array, we increment the count of each element in the hash map.
-// If the count of an element becomes greater than the threshold, we add that element to the result vector and set its count to a negative value to mark it as processed.
-// This approach has a time complexity of O(n) and a space complexity of O(k), where k is the number of distinct elements in the array.
-    
-class Solution {
-public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int threshold = n / 3;
-        unordered_map<int, int> cnts;
-        vector<int> res;
-
-        for(int num : nums) {
-            cnts[num]++;
-            if(cnts[num] > threshold) {
-                res.push_back(num);
-                cnts[num] = -threshold - 1; // To avoid adding duplicates
-            }
-        }
-        return res;
-    }
-};
-
-
-
-
-
-
-// APPROACH 4  Using Sorting and Linear Scan
-
-// This approach sorts the input array in ascending order.
-// Then, it performs a linear scan through the sorted array and counts consecutive occurrences of each element.
-// When the count exceeds the threshold, the element is added to the result vector.
-// This approach also handles the last element separately outside the loop.
-// This approach has a time complexity of O(n log n) due to the sorting step and a space complexity of O(1).
+// A 3 - Using Sorting and Linear Scan
+// 1. This approach sorts the input array in ascending order.
+// 2. Then, performs a linear scan through the sorted array and counts consecutive occurrences
+//    of each element.
+// 3. When the count exceeds the threshold, the element is added to the result vector.
+// 4. This approach also handles the last element separately outside the loop.
+// T.C. --> O(n log n), due to the sorting step
+// S.C. --> O(1)
 
 class Solution {
 public:
