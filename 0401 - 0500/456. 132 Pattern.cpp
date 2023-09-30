a8256bcbbf3dfb5386c24ce84398fc1766f3d3a8
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-30 06:43:41 +0530

    Update and rename 456. 132 Pattern ARRAY STACK  BINARY_SEARCH.cpp to 456. 132 Pattern.cpp

--- 0401 - 0500/456. 132 Pattern.cpp
+++ 0401 - 0500/456. 132 Pattern.cpp
@@ -6,8 +6,7 @@ public:
         stack<int> st;
         int sec = -2147483648;
         for(int i=nums.size()-1; i>=0; i--){
-            if(nums[i] < sec)
-                return true;
+            if(nums[i] < sec) return true;
             while(st.size() > 0 && nums[i] > st.top()){
                 sec = st.top();
                 st.pop();
@@ -19,30 +18,27 @@ public:
 };
 
 // T.C. O(n)
-// the biggest num will always stay in the stack and second one will stay on my second int ,if I can find nums[i] smaller than second than I can return true because I am searching from right to left :)
+// the biggest num will always stay in the stack and second one will stay on my second int,
+// if I can find nums[i] smaller than second than I can return true because
+// I am searching from right to left :)
 
 
+// OR
 
-
-
-
-// A 2
 
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         int n = nums.size();
-        
         int mini[n];
         mini[0] = nums[0];
-        for(int i=1; i<n; i++)
-            mini[i] = min(nums[i],mini[i-1]);
-        
+        for(int i=1; i<n; i++) {
+            mini[i] = min(nums[i], mini[i-1]);
+        }          
         stack<int> st;
         for(int j=n-1; j>=0; j--){
             while(!st.empty() && st.top() < nums[j]){
-                if(st.top() > mini[j])
-                    return true;
+                if(st.top() > mini[j]) return true;
                 st.pop();
             }
             st.push(nums[j]);
@@ -50,3 +46,4 @@ public:
         return false;
     }
 };
+
