1675e09647e5fcae189152e4c29a2257fd15a246
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-09-30 06:31:17 +0530

    Update 456. 132 Pattern ARRAY STACK  BINARY_SEARCH.cpp

--- 0401 - 0500/456. 132 Pattern ARRAY STACK  BINARY_SEARCH.cpp
+++ 0401 - 0500/456. 132 Pattern ARRAY STACK  BINARY_SEARCH.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/132-pattern/
+// A 1
 
 class Solution {
 public:
@@ -21,3 +21,32 @@ public:
 // T.C. O(n)
 // the biggest num will always stay in the stack and second one will stay on my second int ,if I can find nums[i] smaller than second than I can return true because I am searching from right to left :)
 
+
+
+
+
+
+// A 2
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        
+        int mini[n];
+        mini[0] = nums[0];
+        for(int i=1; i<n; i++)
+            mini[i] = min(nums[i],mini[i-1]);
+        
+        stack<int> st;
+        for(int j=n-1; j>=0; j--){
+            while(!st.empty() && st.top() < nums[j]){
+                if(st.top() > mini[j])
+                    return true;
+                st.pop();
+            }
+            st.push(nums[j]);
+        }
+        return false;
+    }
+};
