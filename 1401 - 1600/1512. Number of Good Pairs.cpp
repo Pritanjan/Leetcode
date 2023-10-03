a3a7a944841f654a176a607193e04d813250aec7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-03 09:26:20 +0530

    Update 1512. Number of Good Pairs.cpp

--- 1401 - 1600/1512. Number of Good Pairs.cpp
+++ 1401 - 1600/1512. Number of Good Pairs.cpp
@@ -42,4 +42,100 @@ public:
 
 
 
+// A 3 - Using an Array
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int MAX_NUM = 101; // Assuming maximum value of nums[i] is 100
+        vector<int> freq(MAX_NUM, 0);
+        int cnt = 0;
+        for(int num : nums) {
+            cnt += freq[num];
+            freq[num]++;
+        }
+        return cnt;
+    }
+};
+
+
+
+
+
+
+// A 4 - Using Combination Formula
+// Same as A 2
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int num : nums) {
+            freq[num]++;
+        }
+
+        int cnt = 0;
+        for(auto& pair : freq) {
+            int n = pair.second;
+            cnt += (n * (n - 1)) / 2;
+        }
+        return cnt;
+    }
+};
+
+
+
+
+
+
+
+// A 5 - Using Sorting
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int cnt = 0;
+        int currCnt = 1;
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] == nums[i-1]) currCnt++;
+            else {
+                cnt += (currCnt * (currCnt - 1)) / 2;
+                currCnt = 1;
+            }
+        }
+        cnt += (currCnt * (currCnt - 1)) / 2;
+        return cnt;
+    }
+};
+
+
+
+
+
+
+// A 6  - Using 2 Pointer
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int L = 0, R = 1, cnt = 0;
+        while(R < nums.size()) {
+            if(nums[L] == nums[R]) {
+                cnt += R - L;
+                R++;
+            } 
+            else {
+                L++;
+                if(L == R) R++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+
+
 
