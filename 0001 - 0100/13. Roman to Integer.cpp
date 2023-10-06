677013103a5dc0d8aeb3066e656b1ebd7c48ee85
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-06 13:51:47 +0530

    Update 13. Roman to Integer.cpp

--- 0001 - 0100/13. Roman to Integer.cpp
+++ 0001 - 0100/13. Roman to Integer.cpp
@@ -83,36 +83,14 @@ public:
     int findVal(char ch){
         int ans = 0;
         switch(ch){
-            case 'I' :
-                ans = 1;
-                break;
-                
-            case 'V' :
-                ans = 5;
-                break;
-                
-            case 'X' :
-                ans = 10;
-                break;
-            
-            case 'L' :
-                ans = 50;
-                break;
-                
-            case 'C' :
-                ans = 100;
-                break;
-            
-            case 'D' :
-                ans = 500;
-                break;
-                
-            case 'M' :
-                ans = 1000;
-                break;
-            
-            default :
-                break;
+            case 'I' : ans = 1; break;
+            case 'V' : ans = 5; break;
+            case 'X' : ans = 10; break;
+            case 'L' : ans = 50; break;
+            case 'C' : ans = 100; break;
+            case 'D' : ans = 500; break;
+            case 'M' : ans = 1000; break;
+            default  : break;
         }
         return ans;
     }
