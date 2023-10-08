65b1b3b4d99153fb7e8da8f0305a68e0acd1480e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-08 12:52:43 +0530

    Update 2895. Minimum Processing Time.cpp

--- 2801 - 3000/2895. Minimum Processing Time.cpp
+++ 2801 - 3000/2895. Minimum Processing Time.cpp
@@ -18,3 +18,34 @@ public:
     }
 };
 
+
+// OR
+
+
+class Solution {
+public:
+    int minProcessingTime(vector<int>& pt, vector<int>& t) {
+        sort(pt.begin(), pt.end());
+        sort(t.begin(), t.end(), [](const int a, const int b) { return a > b; });
+        int l = 0, ans = 0;
+        for(int st : pt) {
+            int curr = 0, cnt = 0;
+            while(l < t.size() && cnt < 4) {
+                curr = max(curr, st + t[l]);
+                l++;
+                cnt++;
+            }
+            ans = max(ans, curr);
+        }        
+        return ans;
+    }
+};
+
+
+
+
+
+
+// A 2
+
+
