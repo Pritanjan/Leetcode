0677f3af73d4b68f49bc2078f42e7d3d173ccaad
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-09 21:54:26 +0530

    Update 994. Rotting Oranges.cpp

--- 0901 - 1000/994. Rotting Oranges.cpp
+++ 0901 - 1000/994. Rotting Oranges.cpp
@@ -7,7 +7,9 @@ public:
         int m = grid[0].size();
 
         queue<pair<pair<int, int>, int>> que;
-        int vis[n][m];        
+        int vis[n][m];  
+
+        int cntFresh = 0;      
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == 2) {
@@ -17,12 +19,15 @@ public:
                 else {
                     vis[i][j] = 0;
                 }
+
+                if(grid[i][j] == 1) cntFresh++;
             }
         }
-
+        
         int time = 0;
         int dx[] = {0, 0, 1, -1};
         int dy[] = {1, -1, 0, 0};
+        int cnt =0;
         while(!que.empty()) {
             int row = que.front().first.first;
             int col = que.front().first.second;
@@ -36,19 +41,22 @@ public:
                 if(nRow >= 0 and nRow < n and nCol >= 0 and nCol < m and vis[nRow][nCol] == 0 and grid[nRow][nCol] == 1) {
                     que.push({{nRow, nCol}, time + 1});
                     vis[nRow][nCol] = 2;
+                    cnt++;
                 }
             }
         }
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(vis[i][j] != 2 and grid[i][j] == 1) {
-                    return -1;
-                }
-            }
-        }
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<m; j++) {
+        //         if(vis[i][j] != 2 and grid[i][j] == 1) {
+        //             return -1;
+        //         }
+        //     }
+        // }
+        if(cnt != cntFresh) return -1;
         return time;
     }
 };
+
 
 
 
