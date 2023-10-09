5bfd9abf8cc4ccd9cc5d804c4d58ec4ab2e7101d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-09 21:24:52 +0530

    Create  Code  994. Rotting Oranges.cpp

---
+++ 0901 - 1000/Code  994. Rotting Oranges.cpp
@@ -0,0 +1,58 @@
+// A 1 BFS
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<pair<int, int>, int>> que;
+        int vis[n][m];        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(grid[i][j] == 2) {
+                    que.push({{i, j}, 0});
+                    vis[i][j] = 2;
+                }
+                else {
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int time = 0;
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
+        while(!que.empty()) {
+            int row = que.front().first.first;
+            int col = que.front().first.second;
+            int tme = que.front().second;
+            time = max(tme, time);
+            que.pop();
+
+            for(int i=0; i<4; i++) {
+                int nRow = row + dx[i];
+                int nCol = col + dy[i];
+                if(nRow >= 0 and nRow < n and nCol >= 0 and nCol < m and vis[nRow][nCol] == 0 and grid[nRow][nCol] == 1) {
+                    que.push({{nRow, nCol}, time + 1});
+                    vis[nRow][nCol] = 2;
+                }
+            }
+        }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(vis[i][j] != 2 and grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return time;
+    }
+};
+
+
+
+
+
+
+// A 2
