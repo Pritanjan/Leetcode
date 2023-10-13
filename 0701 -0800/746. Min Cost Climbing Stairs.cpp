1f35959937b6e0fb710426de129ec91eeba36f85
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-13 14:03:55 +0530

    Update 746. Min Cost Climbing Stairs.cpp

--- 0701 -0800/746. Min Cost Climbing Stairs.cpp
+++ 0701 -0800/746. Min Cost Climbing Stairs.cpp
@@ -37,7 +37,8 @@ public:
 
 
 
-// A 1 
+// A 1 - DP  -  (Bottom-Up)
+// in-place 
 
 class Solution {
 public:
@@ -50,6 +51,23 @@ public:
         return min(cost[n-1], cost[n-2]);
     }
 };
+
+
+// OR
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n, 0);
+        
+        for(int i=2; i<n; i++) {
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }        
+        return min(dp[n-1] + cost[n-1], dp[n-2] + cost[n-2]);
+    }
+};
+
 
 
 
