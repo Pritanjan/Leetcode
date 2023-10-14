ed3fd4c4486fb0bfd3370349631c7b6716aefb6c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-14 09:24:41 +0530

    Update 1305. All Elements in Two Binary Search Trees.cpp

--- 1201 - 1400/1305. All Elements in Two Binary Search Trees.cpp
+++ 1201 - 1400/1305. All Elements in Two Binary Search Trees.cpp
@@ -37,30 +37,31 @@ public:
 
 class Solution {
 public:
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {    
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         vector<int> ans;
-        stack<TreeNode *> st1, st2;        
-        while(root1 || root2 || !st1.empty() || !st2.empty()) {
+        stack<TreeNode *> stk1, stk2;
+        
+        while(root1 || root2 || !stk1.empty() || !stk2.empty()) {
             while(root1 != NULL){
-                st1.push(root1);
+                stk1.push(root1);
                 root1 = root1 -> left;
-            }            
-             while(root2 != NULL){
-                st2.push(root2);
-                root2= root2 -> left;
             }
             
-            if(st2.empty() || (!st1.empty() && st1.top()->val <= st2.top()->val)){
-                root1 = st1.top();
-                st1.pop();
-                ans.push_back(root1->val);
-                root1 = root1->right;
+            while(root2 != NULL){
+                stk2.push(root2);
+                root2= root2 -> left;
+            }            
+            if(stk2.empty() || (!stk1.empty() && stk1.top() -> val <= stk2.top() -> val)){
+                root1 = stk1.top();
+                stk1.pop();
+                ans.push_back(root1 -> val);
+                root1 = root1 -> right;
             }
             else{
-                root2 = st2.top();
-                st2.pop();
-                ans.push_back(root2->val);
-                root2 = root2->right;
+                root2 = stk2.top();
+                stk2.pop();
+                ans.push_back(root2 -> val);
+                root2 = root2 -> right;
             }
         }
         return ans;
