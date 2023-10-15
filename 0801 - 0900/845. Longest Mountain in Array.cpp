b4443201ea188fa4951476a66269584baf98d27b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-15 19:59:40 +0530

    Update 845. Longest Mountain in Array.cpp

--- 0801 - 0900/845. Longest Mountain in Array.cpp
+++ 0801 - 0900/845. Longest Mountain in Array.cpp
@@ -1,40 +1,37 @@
-// https://www.codingninjas.com/codestudio/problems/longest-mountain-subarray_893069?leftPanelTab=0
+// A 1
 
 class Solution {
 public:
     int longestMountain(vector<int>& arr) {
         int n = arr.size();
         if(n < 3) return 0;
-        int ans = 0;
-        
+        int ans = 0;        
         for(int i=1; i<n-1; ){
-        if(arr[i] > arr[i+1] and arr[i-1] < arr[i]) {
-            int cnt = 0;
-            int j = i;
-            while(j>0 and arr[j] > arr[j-1]){
-                cnt++;
-                j--;
+            if(arr[i] > arr[i+1] and arr[i-1] < arr[i]) {
+                int cnt = 0;
+                int j = i;
+                while(j > 0 and arr[j] > arr[j-1]){
+                    cnt++;
+                    j--;
+                }
+                while(i < n-1 and arr[i] > arr[i+1]){
+                    cnt++;
+                    i++;
+                }
+                ans = max(ans, cnt);
             }
-            while(i<n-1 and arr[i] > arr[i+1]){
-                cnt++;
-                i++;
-            }
-            ans = max(ans, cnt);
+            else i++;
         }
-        else
-            i++;
-        }
-        
-            
-    if(ans > 0)
-        return ans+1;
-        
-    return ans;
-    
+        if(ans > 0) return ans+1;
+        return ans;
     }
 };
 
 
 
+
+
+
+// A 2
 
 
