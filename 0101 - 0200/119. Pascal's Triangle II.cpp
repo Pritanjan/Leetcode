437bc3b4fa231fee68e01bfb6aa1dd374ab3428f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-16 11:53:50 +0530

    Update 119. Pascal's Triangle II.cpp

--- 0101 - 0200/119. Pascal's Triangle II.cpp
+++ 0101 - 0200/119. Pascal's Triangle II.cpp
@@ -96,4 +96,28 @@ public:
 
 
 
+// A 3 - Recursion
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex == 0) return {1};
+        vector<int> prevRow = getRow(rowIndex - 1);
+        vector<int> row(rowIndex + 1, 1);
+        for(int i=1; i<rowIndex; i++) {
+            row[i] = prevRow[i-1] + prevRow[i];
+        }
+        return row;
+    }
+};
+
+
+
+
+
+
+// A 4 
+
+
+
 
