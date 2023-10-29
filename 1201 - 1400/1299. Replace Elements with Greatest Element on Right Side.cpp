33980d5bc0e8d72ee30b7c29d62ca291d511b4f8
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-29 19:50:59 +0530

    Update 1299. Replace Elements with Greatest Element on Right Side.cpp

--- 1201 - 1400/1299. Replace Elements with Greatest Element on Right Side.cpp
+++ 1201 - 1400/1299. Replace Elements with Greatest Element on Right Side.cpp
@@ -1,3 +1,26 @@
+// A 0 - Brute Force - TLE
+
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n = arr.size();
+        for(int i=0; i<n-1; i++) {
+            int mx = -1;
+            for(int j=i+1; j<n; j++) {
+                mx = max(mx, arr[j]);
+            }
+            arr[i] = mx;
+        }
+        arr[n-1] = -1;
+        return arr;
+    }
+};
+
+
+
+
+
+
 // A 1
 
 class Solution {
