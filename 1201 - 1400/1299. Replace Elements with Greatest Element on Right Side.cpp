7ba5873e07d0899732916386bca53cfea4203878
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-29 19:14:32 +0530

    Update 1299. Replace Elements with Greatest Element on Right Side.cpp

--- 1201 - 1400/1299. Replace Elements with Greatest Element on Right Side.cpp
+++ 1201 - 1400/1299. Replace Elements with Greatest Element on Right Side.cpp
@@ -45,6 +45,12 @@ public:
 
 // A 3
 
+// The question is just a variation of problem " Leaders in an array ". 
+// We just have to keep track of maximum element on right side of any element. 
+// Traverse the loop from last element. 
+// We know there is no element on right side of last element so put -1.  
+// We just need to identify the variation ( easy peasy ☺️)
+
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
