b49c792d4635c48cf566f41c29a83d6de1a41011
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-10-30 09:58:26 +0530

    Update 1356. Sort Integers by The Number of 1 Bits ARRAY BIT_MANIPULATION SORTING.cpp

--- 1201 - 1400/1356. Sort Integers by The Number of 1 Bits ARRAY BIT_MANIPULATION SORTING.cpp
+++ 1201 - 1400/1356. Sort Integers by The Number of 1 Bits ARRAY BIT_MANIPULATION SORTING.cpp
@@ -1,24 +1,26 @@
-//https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+// A 1
 
-// APPROACH 1
 class Solution {
 public:
     static bool compare(int a, int b){
         bitset<32> c0(a);
         bitset<32> c1(b);
-        if(c0.count() != c1.count())
-            return c0.count() < c1.count();
-        else
-            return a < b;
-    }
-    
+        if(c0.count() != c1.count()) return c0.count() < c1.count();
+        else return a < b;
+    }    
     vector<int> sortByBits(vector<int>& arr) {
         sort(arr.begin(), arr.end(), compare);
         return arr;
     }
 };
 
-// APPROACH 2
+
+
+
+
+
+// A 2
+
 class Solution {
 public: 
     vector<int> sortByBits(vector<int>& arr) {
@@ -31,7 +33,13 @@ public:
     }
 };
 
-// APPROACH 3
+
+
+
+
+
+// A 3
+
 class Solution {
 public:
     static bool compare(const int& a, const int& b){
@@ -39,11 +47,16 @@ public:
         int c1 = __builtin_popcount(b); // Count the number of zero
         // cout<<a<<" "<<b;
         return c0 == c1 ? a<b : c0<c1 ;
-    }
-    
-    
+    }    
     vector<int> sortByBits(vector<int>& arr) {
         sort(arr.begin(), arr.end(), compare);
         return arr;
     }
 };
+
+
+
+
+
+// A 4
+
