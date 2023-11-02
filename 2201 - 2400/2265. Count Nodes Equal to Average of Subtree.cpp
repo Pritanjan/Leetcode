016ba46690103204783d3c66b82e23cc69f48e91
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-02 17:49:30 +0530

    Update 2265. Count Nodes Equal to Average of Subtree.cpp

--- 2201 - 2400/2265. Count Nodes Equal to Average of Subtree.cpp
+++ 2201 - 2400/2265. Count Nodes Equal to Average of Subtree.cpp
@@ -31,5 +31,50 @@ public:
 
 // A 2
 
+class Solution {
+public:
+    int res = 0;
+    int averageOfSubtree(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        queue<TreeNode*> que;
+        que.push(root);
+
+        while(!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+
+            vector<int> v = dfs(node);
+            int sum = v[0];
+            int tmp = v[1];
+            int avg = sum / tmp;
+
+            if(node -> val == avg) res++;
+            if(node -> left) que.push(node -> left);
+            if(node -> right) que.push(node -> right);
+        }
+        return res; 
+    }
+
+    vector<int> dfs(TreeNode* root) {
+        if(root == nullptr) return {0, 0};
+        
+        vector<int> L = dfs(root -> left);
+        vector<int> R = dfs(root -> right);
+
+        int sum = L[0] + R[0] + root -> val;
+        int nodes = L[1] + R[1] + 1;
+
+        return {sum, nodes};
+    }
+};
+
+
+
+
+
+
+// A 3
+
 
 
