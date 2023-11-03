b080dcc948ae6d8fa34fab95787ed2199371d948
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-03 06:21:58 +0530

    Update 1441. Build an Array With Stack Operations.cpp

--- 1401 - 1600/1441. Build an Array With Stack Operations.cpp
+++ 1401 - 1600/1441. Build an Array With Stack Operations.cpp
@@ -4,6 +4,27 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> res;
+        int curr = 0; 
+        for(int i=1; i<=n and curr < target.size(); i++) {
+            res.push_back("Push");
+            if(i != target[curr]) res.push_back("Pop");
+            else curr++;
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 2
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> res;
         int curr = 1; 
         for(int i=0; i<target.size(); i++) {
             while(curr < target[i]) {
@@ -25,6 +46,8 @@ public:
 
 
 
-// A 2 
+// A 3
+
+
 
 
