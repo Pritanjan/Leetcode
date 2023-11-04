eadc25071755877c04522307974946f2fabc6fb0
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-04 09:50:35 +0530

    Create 1503. Last Moment Before All Ants Fall Out of a Plank.cpp

---
+++ 1401 - 1600/1503. Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -0,0 +1,14 @@
+// A 1
+
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& L, vector<int>& R) {
+        int mxL = 0;
+        int mxR = n;
+
+        if(!L.empty()) mxL = *max_element(L.begin(), L.end());
+        if(!R.empty()) mxR = *min_element(R.begin(), R.end());
+
+        return max(mxL, n - mxR);        
+    }
+};
