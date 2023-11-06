ac78f52c65295a68a81b4d79b48fb87e5645dbe3
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-06 11:59:10 +0530

    Create 1845. Seat Reservation Manager.cpp

---
+++ 1801 - 2000/1845. Seat Reservation Manager.cpp
@@ -0,0 +1,22 @@
+// A 1 - Using PQ
+
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    SeatManager(int n) {
+        for(int i=1; i<=n; i++) {
+            pq.push(i);
+        }
+    }
+    
+    int reserve() {
+        int tmp = pq.top();
+        pq.pop();
+        return tmp;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+    }
+};
+
