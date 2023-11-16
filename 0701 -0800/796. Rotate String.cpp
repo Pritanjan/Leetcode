6dcbe51eef47bf5a760fc6b41b092a349dcd7ba6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-16 17:55:42 +0530

    Update 796. Rotate String.cpp

--- 0701 -0800/796. Rotate String.cpp
+++ 0701 -0800/796. Rotate String.cpp
@@ -20,3 +20,11 @@ public:
 
 // A 2
 
+class Solution {
+public:
+    bool rotateString(string A, string B) {
+        return A.length() == B.length() && (A + A).find(B) != string::npos;
+    }
+};
+
+
