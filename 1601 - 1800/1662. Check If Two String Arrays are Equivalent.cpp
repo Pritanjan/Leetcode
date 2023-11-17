d3190fb677802abe6498ddd8c41a759fcaf60cf7
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-17 13:41:31 +0530

    Update 1662. Check If Two String Arrays are Equivalent.cpp

--- 1601 - 1800/1662. Check If Two String Arrays are Equivalent.cpp
+++ 1601 - 1800/1662. Check If Two String Arrays are Equivalent.cpp
@@ -1,3 +1,5 @@
+// A 1 
+
 class Solution {
 public:
     string Concatenate(vector<string> &v){
@@ -6,14 +8,40 @@ public:
             s += i;
         }
         return s;
-    }
-    
+    }    
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         string s1 = Concatenate(word1);
-        string s2 = Concatenate(word2);
-        
+        string s2 = Concatenate(word2);        
         return !s1.compare(s2) ? true : false;
     }
 };
 
+
+
+
+
+
+// A 2
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1;
+        for(string s : word1) {
+            s1 += s;
+        }
+        string s2;
+        for(string s : word2) {
+            s2 += s;
+        }
+        return s1 == s2;
+    }
+};
+
+
+
+
+
+
+// A 3 
 
