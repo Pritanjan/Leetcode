eb9bed083976c483862497e53d2a15edb3c5b5b2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-23 19:00:31 +0530

    Update 112. Path Sum.cpp

--- 0101 - 0200/112. Path Sum.cpp
+++ 0101 - 0200/112. Path Sum.cpp
@@ -8,6 +8,7 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root == NULL) return false;
         if(root->left == NULL && root->right == NULL && targetSum  - root->val == 0) return true;
+        // if(root->left == NULL && root->right == NULL) return targetSum == root->val;
         else return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val) ;
     }
 };
