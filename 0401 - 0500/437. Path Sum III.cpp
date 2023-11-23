45c552c5386957adac0337277d86f8f54a5ab556
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-11-23 18:42:28 +0530

    Update and rename 437. Path Sum III to 437. Path Sum III.cpp

--- 0401 - 0500/437. Path Sum III.cpp
+++ 0401 - 0500/437. Path Sum III.cpp
@@ -1,26 +1,20 @@
+// A 1
+
 class Solution{
 public:
     void f(TreeNode* root, int k, int &cnt, vector<int> &path){
         if(root == NULL) return ;
-        
+
         path.push_back(root -> val);
+
+        f(root -> left, k, cnt, path);     // left
+        f(root -> right, k, cnt, path);    // right
         
-        // left
-        f(root -> left, k, cnt, path);
-        
-        // right
-        f(root -> right, k, cnt, path);
-        
-        // check for k sum
-        long long  sum = 0;
-        
+        long long  sum = 0;            // check for k sum
         for(int i=path.size()-1; i>=0; i--){
             sum += path[i];
-    
             if(sum == k) cnt++;
         }
-
-
         path.pop_back();
     }
     
@@ -28,7 +22,6 @@ public:
         vector<int> path;
         int cnt = 0;
         f(root, k, cnt, path);
-        
         return cnt;
     }
 };
