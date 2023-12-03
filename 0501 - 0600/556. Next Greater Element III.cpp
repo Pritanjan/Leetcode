53dfe5497bd0286025027e13a1333fcfc8efd20b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-03 19:34:04 +0530

    Update 556. Next Greater Element III.cpp

--- 0501 - 0600/556. Next Greater Element III.cpp
+++ 0501 - 0600/556. Next Greater Element III.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/next-greater-element-iii/
+// A 1
 
 class Solution {
 public:
@@ -9,6 +9,43 @@ public:
         return (ans <= n or ans > INT_MAX) ? -1 : ans;
     }
 };
+
+
+// OR
+// Without using STL
+
+#define ll long long
+class Solution {
+public:
+    void nextPermutation(string &nums){
+        ll int i;
+        for(i=nums.size()-2; i>=0; i--){
+            if(nums[i] < nums[i+1]) break;
+        }
+
+        ll int j=nums.size()-1;
+        if(i >= 0) {
+            while(nums[j] <= nums[i]) j--;
+            swap(nums[j],nums[i]);
+        }
+        reverse(nums.begin()+i+1,nums.end());
+    }
+
+    int nextGreaterElement(int n) {
+        auto num = to_string(n);
+        nextPermutation(num);
+        auto ans = stoll(num);
+        return (ans <= n or ans > INT_MAX) ? -1 : ans;
+    }    
+};
+
+
+
+
+
+
+
+
 
 
 
