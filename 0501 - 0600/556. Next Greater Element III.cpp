bb168c93f64fc95de4c064768e8702c8da341a80
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-05 22:08:05 +0530

    Update 556. Next Greater Element III.cpp

--- 0501 - 0600/556. Next Greater Element III.cpp
+++ 0501 - 0600/556. Next Greater Element III.cpp
@@ -1,11 +1,11 @@
-// A 1
+// A 1 - Brute Force
 
 class Solution {
 public:
     int nextGreaterElement(int n) {
         auto num = to_string(n);
         next_permutation(num.begin(), num.end());
-        auto ans=stoll(num);
+        auto ans = stoll(num);
         return (ans <= n or ans > INT_MAX) ? -1 : ans;
     }
 };
@@ -43,45 +43,59 @@ public:
 
 
 
+// A 1 - use set
+
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+        set<int> s_set(s.begin(), s.end());
+        while(next_permutation(s.begin(), s.end())) {
+            long long res = stoll(s);
+            if (res > INT_MAX || res <= n) continue;
+            return res;
+        }        
+        return -1;
+    }       
+};
+
+
+
+
+// A 2 - using stack
+
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+        stack<int> st;
+        
+        int i = s.size() - 2;
+        while(i >= 0 && s[i] >= s[i + 1]) {
+            st.push(s[i] - '0');
+            i--;
+        }
+        
+        if(i < 0) return -1;
+        
+        int j = s.size() - 1;
+        while(!st.empty() && st.top() <= s[i] - '0') {
+            st.pop();
+        }
+        
+        swap(s[i], s[j]);
+        reverse(s.begin() + i + 1, s.end());
+        
+        long long res = stoll(s);
+        return (res > INT_MAX) ? -1 : res;
+    }     
+};
 
 
 
 
 
-
-// https://www.codingninjas.com/codestudio/problems/bob-homework_2656187?topList=amazon-sde-challenge
-
-#include<bits/stdc++.h>
-
-void nextPermutation(string &nums){
-    int i;
-    for(i=nums.size()-2;i>=0;i--){
-        if(nums[i]<nums[i+1])
-            break;
-    }
-       int j=nums.size()-1;
-       if(i>=0){
-       while(nums[j]<=nums[i])j--;
-       
-         swap(nums[j],nums[i]);
-       }
-    
-    reverse(nums.begin()+i+1,nums.end());
-}
-long long int bobsHomework(int n)
-{
-    // Write your code here. 
-    string num=to_string(n);
-    nextPermutation(num);
-    size_t ans=stoll(num);
-    return (ans<=n)?-1:ans;
-}
+// A 3 
 
 
 
-
-
-
-
-
- 
