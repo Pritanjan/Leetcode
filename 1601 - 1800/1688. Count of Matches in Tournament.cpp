e508cd550f0ad8366f382c928b087ee05d26990d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-05 19:48:32 +0530

    Update 1688. Count of Matches in Tournament.cpp

--- 1601 - 1800/1688. Count of Matches in Tournament.cpp
+++ 1601 - 1800/1688. Count of Matches in Tournament.cpp
@@ -31,3 +31,72 @@ public:
 };
 
 
+
+
+
+// A 3  -  Iterative Solution
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans = 0;
+        while(n > 1) {
+            ans += n / 2;
+            n = (n + 1) / 2; // Advance to the next round
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+// A 4 - Recursive solution
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if(n == 1) return 0;
+        return n / 2 + numberOfMatches((n + 1) / 2);
+    }
+};
+
+
+// OR
+
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if(n == 1) return 0;
+        else if(n % 2 == 0) return n / 2 + numberOfMatches(n / 2);
+        else return (n - 1) / 2 + numberOfMatches((n + 1) / 2);
+    }
+};
+
+
+
+
+
+
+// A 5 Bit Manipulation
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans = 0;
+        while(n > 1) {
+            ans += n >> 2;
+            n = (n + 1) >> 2; // Advance to the next round
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+// A 6 
+
