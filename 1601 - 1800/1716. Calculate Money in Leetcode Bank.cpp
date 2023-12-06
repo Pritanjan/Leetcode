05622ab62f3a35f031099765b44f7878ceece676
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-06 23:52:43 +0530

    Update 1716. Calculate Money in Leetcode Bank.cpp

--- 1601 - 1800/1716. Calculate Money in Leetcode Bank.cpp
+++ 1601 - 1800/1716. Calculate Money in Leetcode Bank.cpp
@@ -22,5 +22,39 @@ public:
 
 
 
-// A 2 
+// A 2  - Brute Force
+
+int totalMoney(int n) {
+    int total = 0;
+    int base = 1;
+    for (int i = 0; i < n; ++i) {
+        total += base + (i % 7);
+        if (i % 7 == 6) base += 1;
+    }
+    return total;
+}
+
+
+
+
+
+// A 3
+
+class Solution {
+public:
+    int totalMoney(int n) {
+        int week = n / 7;
+        int rem  = n % 7;
+        int res  = 28 * week + 7 * week * (week - 1) / 2;
+        res += (week + 1 + week + rem) * rem / 2;
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 4 
 
