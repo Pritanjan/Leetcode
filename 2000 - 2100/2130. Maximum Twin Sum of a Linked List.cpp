bc22a46528e45d211f59c66a6c3d9e193d43fb23
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-08 09:43:16 +0530

    Update 2130. Maximum Twin Sum of a Linked List.cpp

--- 2000 - 2100/2130. Maximum Twin Sum of a Linked List.cpp
+++ 2000 - 2100/2130. Maximum Twin Sum of a Linked List.cpp
@@ -1,13 +1,5 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// A 1
+
 class Solution {
 public:
     int pairSum(ListNode* head) {
@@ -29,46 +21,46 @@ public:
 };
 
 
-// APPROCH 2
+
+
+
+
+// A 2
 
 class Solution {
- public:
-  int pairSum(ListNode* head) {
-    int ans = 0;
-    ListNode* slow = head;
-    ListNode* fast = head;
+public:
+    int pairSum(ListNode* head) {
+        int ans = 0;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-    // Let slow point to the start of the second half
-    while (fast && fast->next) {
-      slow = slow->next;
-      fast = fast->next->next;
+        // Let slow point to the start of the second half
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }   
+
+        // Tail points to the end with reversed second half
+        ListNode* tail = reverse(slow);
+        while (tail) {
+            ans = max(ans, head->val + tail->val);
+            head = head->next;
+            tail = tail->next;
+        }
+        return ans;
     }
-
-    // Tail points to the end with reversed second half
-    ListNode* tail = reverseList(slow);
-
-    while (tail) {
-      ans = max(ans, head->val + tail->val);
-      head = head->next;
-      tail = tail->next;
+private:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            auto next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
-
-    return ans;
-  }
-
- private:
-  ListNode* reverseList(ListNode* head) {
-    ListNode* prev = nullptr;
-    while (head) {
-      auto next = head->next;
-      head->next = prev;
-      prev = head;
-      head = next;
-    }
-    return prev;
-  }
 };
-
 
 // T.C. --> O(N)
 // S.C. --> O(1)
