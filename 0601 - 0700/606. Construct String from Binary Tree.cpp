c68f63bbd21af4e5f527e0b473958498e021b0e5
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-08 08:56:27 +0530

    Update 606. Construct String from Binary Tree.cpp

--- 0601 - 0700/606. Construct String from Binary Tree.cpp
+++ 0601 - 0700/606. Construct String from Binary Tree.cpp
@@ -1,5 +1,18 @@
-// A 1
+// A 1 - Inorder Traversal
 
+// In this approach, we use inorder traversal to construct the string. 
+// This approach may produce different strings for trees with the same structure but different node values.
+    
+// Initialize an empty string to store the result.
+// Perform a preorder traversal of the binary tree.
+// When visiting a node, append its value to the result string.
+// If the current node has a left child or right child (or both), append an opening parenthesis '(' to the result string.
+// Recursively process the left child and append its subtree representation to the result string.
+// If the current node has a right child (even if the left child is null), append an empty pair
+//    of parenthesis '()' to the result string to maintain the one-to-one mapping relationship.
+// Recursively process the right child and append its subtree representation to the result string.
+// If the current node has at least one child, append a closing parenthesis ')' to the result string.
+    
 class Solution {
 public:
     string tree2str(TreeNode* root) {
