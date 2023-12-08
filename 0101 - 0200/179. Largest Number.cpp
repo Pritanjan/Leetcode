2d796f5d9e8d63989cced3ad791c62dbe260af6a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-08 21:53:19 +0530

    Update 179. Largest Number.cpp

--- 0101 - 0200/179. Largest Number.cpp
+++ 0101 - 0200/179. Largest Number.cpp
@@ -5,8 +5,7 @@ class Solution {
 public:
     static bool compare (string &a, string &b) {
         return a+b > b+a;
-    }
-    
+    }    
     string largestNumber(vector<int>& nums) {
         int n = nums.size();
         vector<string> v(n);
@@ -17,7 +16,6 @@ public:
         }
 
         // sort(v.begin(), v.end(), compare);
-        
         // Sort using a lambda function
         sort(v.begin(), v.end(), [](const string &a, const string &b) {
             return ((a+b) > (b+a));
@@ -29,7 +27,6 @@ public:
 
         // Concatenate sorted strings to form the largest number using accumulate
         string ans = accumulate(v.begin(), v.end(), string(""));
-    
         return ans[0] == '0' ? "0" : ans;
     }
 };
@@ -42,8 +39,7 @@ class Solution {
 public:
     static bool compare (int a,  int b) {
         return to_string(a) + to_string(b) > to_string(b) + to_string(a);
-    }
-    
+    }    
     string largestNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end(), compare);
         if(nums[0] == 0) return "0";
@@ -57,9 +53,53 @@ public:
 };
 
 
+// OR
+
+
+class Solution {
+public:    
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](int a, int b) {
+            return to_string(a) + to_string(b) > to_string(b) + to_string(a);
+        });
+        if(nums[0] == 0) return "0";
+        stringstream res;
+        for(auto it = nums.begin(); it != nums.end(); ++it) {
+            res << *it;
+        }
+        return res.str();
+    }
+};
+
 
 
 
 
 
 // A 2
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> vec;
+        for(int num : nums)
+            vec.push_back(to_string(num));
+
+        sort(vec.begin(), vec.end(), [](const string &s1, const string &s2) {
+            return s1 + s2 > s2 + s1;
+        });
+
+        string ans;
+        for(auto &num : vec)
+            ans += num;
+
+        // remove leading zeros
+        while(ans.front() == '0' && ans.length() > 1)
+            ans.erase(0, 1);
+
+        return ans;
+    }
+};
+
+
+
