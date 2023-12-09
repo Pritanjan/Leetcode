9140ef95ebff84b2af3cd1e9f7f81d9fe68e2b41
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-09 17:14:33 +0530

    Update 88. Merge Sorted Array.cpp

--- 0001 - 0100/88. Merge Sorted Array.cpp
+++ 0001 - 0100/88. Merge Sorted Array.cpp
@@ -61,25 +61,21 @@ public:
         int k = m + n - 1; // Index of last element in merged array
 
         while(i >= 0 && j >= 0) {
-            if(nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
-            } 
-            else {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
+            if(nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
         }
 
         // If there are remaining elements in nums2, copy them to nums1
         while(j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
+            nums1[k--] = nums2[j--];
         }
+        // Now, arr1 contains the merged and sorted elements
+        // No need to sort separately
     }
 };
+
+
+
 
 
 
