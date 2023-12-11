ade09b66285c2a348d12b7900d194a16817f3e34
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-11 10:11:57 +0530

    Update 33. Search in Rotated Sorted Array.cpp

--- 0001 - 0100/33. Search in Rotated Sorted Array.cpp
+++ 0001 - 0100/33. Search in Rotated Sorted Array.cpp
@@ -274,3 +274,37 @@ public:
 
 
 // A 8
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int L = 0;
+        int R = n-1;
+        int ans = -1;
+
+        while(L <= R) {
+            int mid = L + (R - L) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] >= nums[L]) {
+                if(target > nums[mid]) L = mid+1;
+                else {
+                    if(target >= nums[L]) R = mid-1;
+                    else L = mid+1;
+                }
+            }
+            else {
+                if(target < nums[mid]) R = mid-1;
+                else {
+                    if(target <= nums[R]) L = mid+1;
+                    else R = mid-1;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
