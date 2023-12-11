adbccaa50fa52cc6d0203293ebaa9f0a19fd874a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-11 09:44:13 +0530

    Update 1287. Element Appearing More Than 25% In Sorted Array.cpp

--- 1201 - 1400/1287. Element Appearing More Than 25% In Sorted Array.cpp
+++ 1201 - 1400/1287. Element Appearing More Than 25% In Sorted Array.cpp
@@ -2,6 +2,29 @@
 
 class Solution {
 public:
+    int findSpecialInteger(vector<int>& arr) {
+        unordered_map<int, int> counts;
+        for (int num : arr) {
+            counts[num]++;
+        }
+        
+        int target = arr.size() / 4;
+        for (auto& [key, value] : counts) {
+            if (value > target) {
+                return key;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+
+// OR
+
+
+class Solution {
+public:
     int findSpecialInteger(vector<int>& nums) {
         int n = nums.size();
         map<int, int> cnts;
@@ -14,5 +37,64 @@ public:
         return 0;
     }
 };
+
+
+// T.C. --> O(N)
+// S.C. --> O(N)
+
+
+
+
+
+// A 2  - Check the Element N/4 Ahead
+
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int size = arr.size() / 4;
+        for (int i = 0; i < arr.size() - size; i++) {
+            if (arr[i] == arr[i + size]) {
+                return arr[i];
+            }
+        }
+        
+        return -1;
+    }
+};
+
+// T.C. --> O(N)
+// S.C. --> O(1)
+
+
+
+
+
+
+// A 3 - Binary Search Leetcode
+
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> candidates = {arr[n / 4], arr[n / 2], arr[3 * n / 4]};
+        int target = n / 4;
+        
+        for (int candidate : candidates) {
+            int left = lower_bound(arr.begin(), arr.end(), candidate) - arr.begin();
+            int right = upper_bound(arr.begin(), arr.end(), candidate) - arr.begin() - 1;
+            if (right - left + 1 > target) {
+                return candidate;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+
+// T.C. --> O(log N)
+// S.C. --> O(1)
+
+
 
 
