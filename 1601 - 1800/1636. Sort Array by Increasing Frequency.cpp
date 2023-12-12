fd8c1a7ec788223e955b4502bf1fba62ef05eb1c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-12 18:14:05 +0530

    Update 1636. Sort Array by Increasing Frequency.cpp

--- 1601 - 1800/1636. Sort Array by Increasing Frequency.cpp
+++ 1601 - 1800/1636. Sort Array by Increasing Frequency.cpp
@@ -36,10 +36,51 @@ public:
 };
 
 
+// OR 
+
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int> ump;
+        for(auto it : nums) ump[it]++;
+        
+        sort(nums.begin(), nums.end(),[&](int a, int b){
+            return ump[a] != ump[b] ? ump[a] < ump[b] : a > b;
+        });       
+        return nums;
+    }
+};
 
 
 
 
-// A 2 
+
+
+// A 2
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int,int> mp;
+        for(auto i : nums) mp[i]++;
+        
+        // vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        priority_queue<pair<int, int>> pq; 
+        for(auto e : mp) pq.push({-e.second, e.first});
+        
+        vector<int> ans;
+        while(!pq.empty()) {
+            int freq = pq.top().first;
+            for(int i=0; i<abs(freq); i++) {
+                 ans.push_back(pq.top().second);
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+
 
 
