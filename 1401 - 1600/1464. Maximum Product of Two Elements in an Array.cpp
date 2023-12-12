c486da7cf8361179a1dcc4471f7913313e5b6636
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-12 07:54:12 +0530

    Update and rename 1464. Maximum Product of Two Elements in an Array ARRAY SORTING HEAP.cpp to 1464. Maximum Product of Two Elements in an Array.cpp

--- 1401 - 1600/1464. Maximum Product of Two Elements in an Array.cpp
+++ 1401 - 1600/1464. Maximum Product of Two Elements in an Array.cpp
@@ -1,15 +1,9 @@
-// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+// A 1
 
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        
-//		 int n = nums.size();
-//       nth_element(nums.begin(), nums.begin()+n-2, nums.end());
-//       return (nums[n-1] - 1) * (nums[n-2] - 1);
-		
-		
-		int nums1 = 0, nums2 = 0;
+        int nums1 = 0, nums2 = 0;
         for(auto num : nums){
             auto temp = max( min(nums1,nums2), num );
             
@@ -19,3 +13,21 @@ public:
         return (nums1 - 1) * (nums2 - 1);
     }
 };
+
+
+
+
+
+
+// A 2 
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        nth_element(nums.begin(), nums.begin()+n-2, nums.end());
+        return (nums[n-1] - 1) * (nums[n-2] - 1);
+    }
+};
+
+
