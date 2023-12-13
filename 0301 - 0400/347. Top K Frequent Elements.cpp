28c3a4a29e7c07b18151488628b79547fab24fac
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-13 17:57:47 +0530

    Update 347. Top K Frequent Elements.cpp

--- 0301 - 0400/347. Top K Frequent Elements.cpp
+++ 0301 - 0400/347. Top K Frequent Elements.cpp
@@ -173,3 +173,33 @@ public:
     }
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First element to occur k times
+// https://www.geeksforgeeks.org/problems/first-element-to-occur-k-times5150/1
+
+class Solution{
+public:
+    int firstElementKTime(int arr[], int n, int k) {
+        unordered_map<int,int> m;
+        for(int i=0; i<n; i++){
+            m[arr[i]]++;
+            if(m[arr[i]] >= k) return arr[i];
+        }
+        return -1;
+    }
+};
+
+
