5d702cba8729856aaf52e367b2a91a01319380bd
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-20 23:28:39 +0530

    Update 2706. Buy Two Chocolates.cpp

--- 2601 - 2800/2706. Buy Two Chocolates.cpp
+++ 2601 - 2800/2706. Buy Two Chocolates.cpp
@@ -1,3 +1,29 @@
+// A 0 
+
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int n = prices.size();
+        int minCost = INT_MAX;
+                
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                int cost = prices[i] + prices[j];
+                if(cost < minCost) {
+                    minCost = cost;
+                }
+            }
+        }
+        if(minCost <= money) return money - minCost;
+        else return money;
+    }
+};
+
+
+
+
+
+
 // A 1 
 
 class Solution {
