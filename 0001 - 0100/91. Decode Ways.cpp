2f0c2a38303c0d31b27727f28481ab1096159ae2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-25 16:15:21 +0530

    Update 91. Decode Ways.cpp

--- 0001 - 0100/91. Decode Ways.cpp
+++ 0001 - 0100/91. Decode Ways.cpp
@@ -82,3 +82,33 @@ public:
 
 // A 4 
 
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        dp[0][1] = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            if (s[i - 1] != '0') {
+                dp[i][1] += dp[i - 1][1];
+            }
+
+            if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
+                dp[i][1] += dp[i - 2][1];
+            }
+
+            dp[i][0] = dp[i - 1][1];
+        }
+
+        return dp[n][1];
+    }
+};
+
+
+
+
+
+
+
