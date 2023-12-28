2475188f7444aa9bf55db5b37bb7f09c38fd61ad
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-28 08:46:24 +0530

    Create 443. String Compression.cpp

---
+++ 0401 - 0500/443. String Compression.cpp
@@ -0,0 +1,44 @@
+// A 1
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i =0;       // for taversing the entire charracter
+        int Index = 0;  // storing the count of a valid charracter
+        int n = chars.size();   // size  of charracter
+        
+        while(i < n){
+            int j = i + 1;      // to compare with the next charracter
+            while(j < n and chars[i] == chars[j]){      // now traverse untill we have the same the same charracter & make increment in j. 
+                j++;
+            }
+            
+            // Now, either the vector has been traverse completely 
+            // or new character has been encountered.
+            
+            // storing the oldchar
+            chars[Index++] = chars[i];
+            
+            // count of a charracter will be j - i as j has been incrmented untill the charracter is same
+            int cnt = j - i;
+            
+            if(cnt > 1){
+                // Now converitng count into string and stroing in ans
+                string c = to_string(cnt);
+                for(char ch : c){
+                    chars[Index++] = ch;
+                }
+            }
+            // Now going to the new charracter as by i = j we have ignore all the first similar charracter
+            i = j;
+        }
+        return Index;
+    }
+};
+
+
+
+
+
+
+// A 2 
