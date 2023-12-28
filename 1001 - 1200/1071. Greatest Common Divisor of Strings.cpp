5494d21738719ce8eec97a736487ff29dd9fa4f1
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-28 21:22:20 +0530

    Update 1071. Greatest Common Divisor of Strings.cpp

--- 1001 - 1200/1071. Greatest Common Divisor of Strings.cpp
+++ 1001 - 1200/1071. Greatest Common Divisor of Strings.cpp
@@ -1,17 +1,4 @@
-class Solution {
-public:
-    string gcdOfStrings(string str1, string str2) {
-        if (str1.length() < str2.length())
-            swap(str1, str2);
-        if (str1 == str2)
-            return str1;
-        if (str1.substr(0, str2.length()) != str2)
-            return "";
-        return gcdOfStrings(str1.substr(str2.length()), str2);
-    }
-};
-
-
+// A 1
 
 // 1. If the length of str1 is less than the length of str2, the two strings are swapped.
 // 2. If str1 is equal to str2, the function returns str1 as the GCD of the two equal strings.
@@ -22,21 +9,22 @@ public:
 //    function with str1.substr(str2.length()) and str2 as inputs.
 
 
-
-
-
-
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if (str1 + str2 != str2 + str1) return "";
-        return str1.substr(0, __gcd(str1.length(), str2.length()));
+        if(str1.length() < str2.length())  swap(str1, str2);
+        if(str1 == str2) return str1;
+        if(str1.substr(0, str2.length()) != str2)  return "";
+        return gcdOfStrings(str1.substr(str2.length()), str2);
     }
 };
 
-// This implementation uses a loop to find the greatest common divisor (GCD) of two strings. 
-// The function takes two strings str1 and str2 as input, and returns the largest string x 
-// that divides both str1 and str2.
+
+
+
+
+
+// A 2 
 
 // The __gcd function is used to find the greatest common divisor of the lengths of str1 and str2. 
 // If the concatenation of str1 and str2 is not equal to the concatenation of str2 and str1, 
@@ -46,5 +34,17 @@ public:
 // length equal to the greatest common divisor of the lengths of str1 and str2. This substring 
 // is guaranteed to be the largest string that divides both str1 and str2.
 
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        return str1.substr(0, __gcd(str1.length(), str2.length()));
+    }
+};
 
 
+
+
+
+
+// A 3 
