8e15085c6eb16956e520c9b1a6fa5a3062929b2f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-29 11:40:56 +0530

    Update and rename 1431. Kids With the Greatest Number of Candies.CPP to 1431. Kids With the Greatest Number of Candies.cpp

--- 1401 - 1600/1431. Kids With the Greatest Number of Candies.cpp
+++ 1401 - 1600/1431. Kids With the Greatest Number of Candies.cpp
@@ -1,23 +1,44 @@
-// APPROACH 1
+// A 0
 
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = candies.size();
+        vector<bool> res(n, true);
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(i != j and candies[j] - candies[i] > extraCandies) {
+                    res[i] = false;
+                    break;
+                }                
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 1
 
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
       int max = 0;
       vector<bool> ans;
-      
       // finding the max
       for(int i=0; i<candies.size(); i++) {
          if(candies[i] > max)
             max = candies[i];
       }
-      
       for(int i=0; i<candies.size(); i++) {
           if( (candies[i] + extraCandies) >= max)  ans.push_back(true);
           else  ans.push_back(false);
       }
-      
       return ans;
     }
 };
@@ -27,10 +48,7 @@ public:
 
 
 
-
-
 // APPROACH 2 [ STL ]
-
 
 class Solution {
 public:
