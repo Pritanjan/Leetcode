281aae963a39168a25d2642f8971bccbe984aa2a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-30 19:18:34 +0530

    Update 345. Reverse Vowels of a String.cpp

--- 0301 - 0400/345. Reverse Vowels of a String.cpp
+++ 0301 - 0400/345. Reverse Vowels of a String.cpp
@@ -1,22 +1,17 @@
-// APPROACH 1
+// A 1
 
 class Solution {
 public:
     string reverseVowels(string s) {
         int i = 0;
-        int j = s.size() - 1 ;
-        
+        int j = s.size() - 1 ;  
         while(i < j){
             bool a = false;
             bool b = false;    // TO CHECK WE ARE POINTING TO VOWEL OR NOT
             
-            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' || s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' )
-                a = true ;
-            
-            if(s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U' || s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u' )
-                b = true;
-            
-            if(a && b){ 	   // IF BOTH POINTING TO VOWELS JUST SWAP THEM
+            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' || s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' )  a = true ;            
+            if(s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O' || s[j]=='U' || s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o' || s[j]=='u' )  b = true;
+            if(a && b) { 	   // IF BOTH POINTING TO VOWELS JUST SWAP THEM
                 swap(s[j], s[i]);
                 i++;
                 j--; 
@@ -33,7 +28,7 @@ public:
 
 
 
-// APPROACH 2
+// A 2
 
 class Solution {
 public:
@@ -46,15 +41,10 @@ public:
         
         int R = vowel.size()-1;
         int L = 0;
-        
         while(L < R ){
-            // if(s[])
-            swap(s[vowel[L]] , s[vowel[R]]);
-                L++;
-                R--;
+            swap(s[vowel[L++]] , s[vowel[R--]]);
         }
-        return s;
-        
+        return s;        
     }
 };
 
@@ -63,7 +53,7 @@ public:
 
 
 
-// APPROACH 3
+// A 3
 
 class Solution {
 public:
@@ -74,13 +64,9 @@ public:
     string reverseVowels(string s) {
         int i = 0;
         int j = s.size() - 1;
-        
-        while(i < j){
-            while(i < s.size() and !isVowel(s[i]))
-                i++;
-            while(j >= 0 and !isVowel(s[j]))
-                j--;
-            
+        while(i < j) {
+            while(i < s.size() and !isVowel(s[i])) i++;
+            while(j >= 0 and !isVowel(s[j])) j--;
             if(i < j){
                 swap(s[i], s[j]);
                 i++;
@@ -99,11 +85,7 @@ public:
 
 
 
-
-
-
-
-// APPROACH 4
+// A 4
 
 class Solution {
 public:
@@ -111,23 +93,17 @@ public:
         vector<char> ss(s.begin(), s.end());
         vector<char> aeiou = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
         int n = s.size(), i = 0, j = n - 1;
-
-        while (i < j) {
-            if (find(aeiou.begin(), aeiou.end(), ss[i]) == aeiou.end()) {
+        while(i < j) {
+            if(find(aeiou.begin(), aeiou.end(), ss[i]) == aeiou.end()) {
                 i++;
                 continue;
             }
-            if (find(aeiou.begin(), aeiou.end(), ss[j]) == aeiou.end()) {
+            if(find(aeiou.begin(), aeiou.end(), ss[j]) == aeiou.end()) {
                 j--;
                 continue;
             }
-            if (i < j) {
-                swap(ss[i], ss[j]);
-            }
-            i++;
-            j--;
-        }
-        
+            if(i < j) swap(ss[i++], ss[j--]);
+        }        
         string d = "";
         for (auto c : ss) {
             d += c;
@@ -138,3 +114,28 @@ public:
 
 
 
+
+
+// A 5 - Using 2 Pointer & Hash Map
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j) {
+            while(i < j && vowels.find(s[i]) == vowels.end()) i++;
+            while(i < j && vowels.find(s[j]) == vowels.end()) j--;
+            swap(s[i++], s[j--]);
+        }
+        return s;        
+    }
+};
+
+
+
+
+
+
+// A 6 
