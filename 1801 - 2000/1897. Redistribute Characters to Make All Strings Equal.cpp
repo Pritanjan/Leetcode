df58ce12f65566e8f4d59f542fc465a2e0e10de9
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-30 13:26:26 +0530

    Update 1897. Redistribute Characters to Make All Strings Equal.cpp

--- 1801 - 2000/1897. Redistribute Characters to Make All Strings Equal.cpp
+++ 1801 - 2000/1897. Redistribute Characters to Make All Strings Equal.cpp
@@ -1,4 +1,4 @@
-// A 1 
+// A 1  - Count Character Frequencies
 
 class Solution {
 public:
@@ -15,8 +15,48 @@ public:
             if(val % n != 0) {
                 return false;
             }
-        }        
+        }
+        return true;
+
+        // for(auto& entry : counts) {
+        //     if(entry.second % words.size() != 0) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+    }
+};
+
+// T.C. --> O(n.k)
+// S.C. --> O(1)
+
+
+
+
+
+
+// A 2 - Count With Array
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        vector<int> counts(26, 0);
+        for (string word : words) {
+            for (char c : word) {
+                counts[c - 'a']++;
+            }
+        }
+        
+        int n = words.size();
+        for (int val : counts) {
+            if (val % n != 0) {
+                return false;
+            }
+        }
+        
         return true;
     }
 };
 
+// T.C. --> O(n.k)
+// S.C. --> O(1)
