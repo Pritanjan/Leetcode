c5d602e918bcfaa6dcdd103dba7059b324cadeaf
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-31 16:21:25 +0530

    Update 334. Increasing Triplet Subsequence ARRAY GREEDY.cpp

--- 0301 - 0400/334. Increasing Triplet Subsequence ARRAY GREEDY.cpp
+++ 0301 - 0400/334. Increasing Triplet Subsequence ARRAY GREEDY.cpp
@@ -1,18 +1,17 @@
-//https://leetcode.com/problems/increasing-triplet-subsequence/
+// A 0 - Brute Force - TLE
 
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int c1 = INT_MAX;
-        int c2 = INT_MAX;
-        
-        for(auto i : nums){
-            if(i <= c1)
-                c1 = i ;        // c1 is min seen so far (it's a candidate for 1st element)
-            else if(i <= c2)	// here when i > c1, i.e. i might be either c2 or c3
-                c2 = i;			// i is better than the current c2, store it
-            else 				// here when we have/had c1 < c2 already and i > c2
-                return true;	// the increasing subsequence of 3 elements exists
+        int n = nums.size();
+        for(int i=0; i<n-2; ++i) {
+            for(int j=i+1; j<n-1; ++j) {
+                for(int k=j+1; k<n; ++k) {
+                    if(nums[i] < nums[j] && nums[j] < nums[k]) {
+                        return true;
+                    }
+                }
+            }
         }
         return false;
     }
@@ -21,4 +20,30 @@ public:
 
 
 
+
+// A 1
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int c1 = INT_MAX;
+        int c2 = INT_MAX;        
+        for(auto i : nums){
+            // c1 is min seen so far
+            if(i <= c1) c1 = i ;
+            // here when i > c1, i.e. i might be either c2 or c3
+            else if(i <= c2) c2 = i;   // i is better than the current c2, store it
+            // here when we have/had c1 < c2 already and i > c2
+            else return true;	       // the increasing subsequence of 3 elements exists
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+// A 2 
 
