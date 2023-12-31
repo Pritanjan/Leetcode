fe25304d144eec5df43ddb016a8f4c1d55db298b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-31 10:57:41 +0530

    Update 238. Product of Array Except Self.cpp

--- 0201 - 0300/238. Product of Array Except Self.cpp
+++ 0201 - 0300/238. Product of Array Except Self.cpp
@@ -28,3 +28,35 @@ public:
 
 // A 2 
 
+#define ll long long int 
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        ll pdt = 1;
+        int zeroCnt = 0; // Count the number of zeros in the array
+        for(auto i : nums) {
+            pdt *= (i == 0 ? 1 : i); // If i is zero, don't multiply, just keep track of the zero count
+            if(i == 0) zeroCnt++;
+        }
+
+        vector<int> res;
+        for(int i : nums) {
+            if(i == 0) {
+                // If i is zero, the result is product of all elements except the current zero
+                res.push_back(zeroCount == 1 ? pdt : 0);
+            } 
+            else {
+                // If i is not zero, the result is total product divided by i
+                res.push_back(zeroCnt == 0 ? pdt / i : 0);
+            }
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 3 
