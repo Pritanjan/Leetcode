08b5788f3070b4bf63af13743bfc4e27426009d6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-31 21:58:54 +0530

    Update 443. String Compression.cpp

--- 0401 - 0500/443. String Compression.cpp
+++ 0401 - 0500/443. String Compression.cpp
@@ -3,25 +3,21 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i =0;       // for taversing the entire charracter
-        int Index = 0;  // storing the count of a valid charracter
+        int i = 0;              // for taversing the entire charracter
+        int Index = 0;          // storing the count of a valid charracter
         int n = chars.size();   // size  of charracter
-        
-        while(i < n){
-            int j = i + 1;      // to compare with the next charracter
-            while(j < n and chars[i] == chars[j]){      // now traverse untill we have the same the same charracter & make increment in j. 
-                j++;
-            }
-            
+        while(i < n) {
+            int j = i+1;      // to compare with the next charracter
+            // now traverse untill we have the same the same charracter & make increment in j. 
+            while(j < n and chars[i] == chars[j]) j++;
+                        
             // Now, either the vector has been traverse completely 
             // or new character has been encountered.
-            
             // storing the oldchar
             chars[Index++] = chars[i];
             
             // count of a charracter will be j - i as j has been incrmented untill the charracter is same
-            int cnt = j - i;
-            
+            int cnt = j - i;            
             if(cnt > 1){
                 // Now converitng count into string and stroing in ans
                 string c = to_string(cnt);
@@ -37,8 +33,39 @@ public:
 };
 
 
+// OR
+
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {    
+        int index = 0;
+        int i = 0;
+        while (i < chars.size()) {
+            char c = chars[i];
+            int count = 0;
+            while (i < chars.size() && chars[i] == c) {
+                i++;
+                count++;
+            }
+            chars[index] = c;
+            index++;
+            if (count > 1) {
+                string s = to_string(count);
+                for (int j = 0; j < s.size(); j++) {
+                    chars[index] = s[j];
+                    index++;
+                }
+            }
+        }
+        return index;
+    }
+};
+
+
 
 
 
 
 // A 2 
+
