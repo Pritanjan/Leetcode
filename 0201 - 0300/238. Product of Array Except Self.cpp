298892c3a9558218febc30a4181ef3d51b61282c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2023-12-31 10:46:51 +0530

    Create 238. Product of Array Except Self.cpp

---
+++ 0201 - 0300/238. Product of Array Except Self.cpp
@@ -0,0 +1,30 @@
+// A 1 
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        int prefix = 1, suffix = 1;
+        vector<int> prod(n);
+        
+        for(int i = 0; i<n; i++){
+            prod[i] = prefix;
+            prefix*=nums[i];
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            prod[i]*=suffix;
+            suffix*=nums[i];
+        }
+        
+        return prod;
+    }
+};
+
+
+
+
+
+
+// A 2 
+
