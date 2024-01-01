8f55fae4389c1e5fac4136cbfe5eef6a83aa8344
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-01-01 20:40:08 +0530

    Update 1679. Max Number of K-Sum Pairs.cpp

--- 1601 - 1800/1679. Max Number of K-Sum Pairs.cpp
+++ 1601 - 1800/1679. Max Number of K-Sum Pairs.cpp
@@ -1,6 +1,30 @@
-//https://leetcode.com/problems/max-number-of-k-sum-pairs/
+// A 0 - Brute Force - TLE
 
-// Approach 1: Brute force approach Time complexty is o(logn) space complexty is O(1)
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int count = 0;
+        for(int i=0; i<nums.size(); ++i) {
+            for(int j=i+1; j<nums.size(); ++j) {
+                if(nums[i] + nums[j] == k) {
+                    ++count;
+                    nums.erase(nums.begin() + j);
+                    nums.erase(nums.begin() + i);
+                    --i; // adjust index after removal
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+
+
+
+
+
+// A 1 - Brute force approach Time complexty is o(logn) space complexty is O(1)
 
 class Solution {
 public:
@@ -9,41 +33,37 @@ public:
         int L = 0;
         int R = nums.size() - 1;
         int count = 0;
-        
         while(L < R){
             if(nums[L] + nums[R] == k){
                 count++;
                 L++;
                 R--;
             }
-            if(nums[L] + nums[R] > k){
-                R--;
-            }
-            if(nums[L] + nums[R] < k){
-                L++;
-            }
-                
+            if(nums[L] + nums[R] > k) R--;
+            if(nums[L] + nums[R] < k) L++;                
         }
         return count;
     }  
 };
 
 
-// Approach 2:Optimize approach Time complexty is O(n) space complexty is O(n) because we are using map
+
+
+
+
+// A 2 - Optimize approach Time complexty is O(n) space complexty is O(n) because we are using map
 
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         unordered_map<int,int> um;
-        int count = 0;
-        
+        int count = 0;        
         for(int i=0; i<nums.size(); i++){
             if(um[k - nums[i]] > 0){
                 um[k - nums[i]]--;    // as we have used one pair so we are deleting it
                 count++;              // if element found in hashmap increase count 
             }
-            else
-                um[nums[i]]++;        // if element is not present in map then add the current element
+            else um[nums[i]]++;       // if element is not present in map then add the current element
         }
         return count;
     }
