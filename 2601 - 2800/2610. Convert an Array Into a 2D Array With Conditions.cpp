7e47e21fe71906fa974bbd55979de4f92b042ff1
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-01-02 12:49:16 +0530

    Update 2610. Convert an Array Into a 2D Array With Conditions.cpp

--- 2601 - 2800/2610. Convert an Array Into a 2D Array With Conditions.cpp
+++ 2601 - 2800/2610. Convert an Array Into a 2D Array With Conditions.cpp
@@ -108,3 +108,27 @@ public:
     }
 };
 
+
+
+
+
+
+// A 3  - count frequency
+
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> freq(nums.size() + 1);
+        
+        vector<vector<int>> ans;
+        for(auto c : nums) {
+            if(freq[c] >= ans.size()) {
+                ans.push_back({});
+            }            
+            // Store the integer in the list corresponding to its current frequency.
+            ans[freq[c]].push_back(c);
+            freq[c]++;
+        }
+        return ans;
+    }
+};
