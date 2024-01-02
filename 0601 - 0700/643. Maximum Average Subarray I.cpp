f047a9ee6959ebb8bba58450661dfa1e90cf2599
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-01-02 13:15:26 +0530

    Update 643. Maximum Average Subarray I.cpp

--- 0601 - 0700/643. Maximum Average Subarray I.cpp
+++ 0601 - 0700/643. Maximum Average Subarray I.cpp
@@ -46,5 +46,79 @@ public:
 
 
 
-// A 2 
+// A 2  - Prefix Sum
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> prefixSum(n+1, 0);
+        for(int i=1; i<=n; ++i) {
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
+        }
+
+        double maxAvg = INT_MIN;
+        for(int i=k; i<=n; ++i) {
+            maxAvg = max(maxAvg, static_cast<double>(prefixSum[i] - prefixSum[i-k]) / k);
+        }
+        return maxAvg;
+    }
+};
+
+
+
+
+
+
+// A 3 - Binary Search
+
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& nums, int k) {
+//         double left = *min_element(nums.begin(), nums.end());
+//         double right = *max_element(nums.begin(), nums.end());
+
+//         while(R - L > 1e-5) {
+//             double mid = L + (R - L) / 2;
+//             if(check(nums, k, mid)) L = mid;
+//             else R = mid;
+//         }
+//         return L;
+//     }
+
+//     bool check(vector<int>& nums, int k, double mid) {
+//         int n = nums.size();
+//         double sum = 0, prevSum = 0;
+
+//         for(int i=0; i<k; ++i) {
+//             sum += nums[i] - mid;
+//         }
+
+//         if (sum >= 0) {
+//             return true;
+//         }
+
+//         for(int i=k; i<n; ++i) {
+//             sum += nums[i] - mid;
+//             prevSum += nums[i-k] - mid;
+
+//             if(prevSum < 0) {
+//                 sum -= prevSum;
+//                 prevSum = 0;
+//             }
+
+//             if (sum >= 0) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
+
+
+// A 4 
 
