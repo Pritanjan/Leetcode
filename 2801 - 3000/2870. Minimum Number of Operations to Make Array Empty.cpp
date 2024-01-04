d9dbcd239719736b8d565df4cd0177a02ca4ed4f
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-01-04 07:58:07 +0530

    Update 2870. Minimum Number of Operations to Make Array Empty.cpp

--- 2801 - 3000/2870. Minimum Number of Operations to Make Array Empty.cpp
+++ 2801 - 3000/2870. Minimum Number of Operations to Make Array Empty.cpp
@@ -25,3 +25,26 @@ public:
 
 // A 2
 
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(auto i : nums) mp[i]++;
+
+        int cnt = 0;
+        for(auto it : mp) {
+            int tmp = it.second;
+            if(tmp == 1) return -1;
+            if(tmp % 3 == 0) cnt += tmp/3;
+            else cnt += (tmp/3) + 1;
+        }        
+        return cnt;
+    }
+};
+
+
+
+
+
+
+// A 3
