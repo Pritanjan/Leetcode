bb820c208fc5bff697f4c9d556a33f1a99e10090
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-01-06 07:11:14 +0530

    Create 2352. Equal Row and Column Pairs.cpp

---
+++ 2201 - 2400/2352. Equal Row and Column Pairs.cpp
@@ -0,0 +1,27 @@
+// A 1 
+
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                int k = 0;
+                for(; k<n; k++) {
+                    if(grid[i][k] != grid[k][j]) break;
+                }
+                if(k == n) cnt++;  // row[i] = col[j]
+            }
+        }
+        return cnt;
+    }
+};
+
+
+
+
+
+
+// A 2 
+
