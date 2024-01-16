76deb29c670dde382f92b477c53f0209c62ef158
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-01-16 18:35:39 +0530

    Update 1207. Unique Number of Occurrences.cpp

--- 1201 - 1400/1207. Unique Number of Occurrences.cpp
+++ 1201 - 1400/1207. Unique Number of Occurrences.cpp
@@ -53,5 +53,49 @@ public:
 
 
 
-// A 3
+// A 3 - STL
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_set<int> occ;
+
+        transform(arr.begin(), arr.end(), inserter(occ, occ.begin()),
+                    [&arr](int val) { return count(arr.begin(), arr.end(), val); });
+                    
+        return occ.size() == unordered_set<int>(arr.begin(), arr.end()).size();
+    }
+};
+
+
+
+
+
+
+// A 4 
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        const int maxSize = 2001;
+        array<int, maxSize>  freq{};
+        
+        for(auto num : arr) {
+            freq[num + 1000]++;
+        }
+
+        unordered_set<int> occ;
+        for(auto f : freq) {
+            if(f > 0 and !occ.insert(f).second) return false;
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+// A 5 
 
