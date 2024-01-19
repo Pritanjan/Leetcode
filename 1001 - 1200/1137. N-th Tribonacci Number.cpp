a56ab176708a757bb7f3a8991c7889c7fd381669
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-01-19 22:34:01 +0530

    Update 1137. N-th Tribonacci Number.cpp

--- 1001 - 1200/1137. N-th Tribonacci Number.cpp
+++ 1001 - 1200/1137. N-th Tribonacci Number.cpp
@@ -6,7 +6,6 @@ public:
     int tribonacci(int n) {
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-
         return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
     }
 };
@@ -66,27 +65,25 @@ public:
 // A 4 - Space Optimized
 // Time Complexity: O(N)
 // Space Complexity: O(1)
+
 class Solution {
 public:
     int tribonacci(int n) {
-        int a = 0, b = 1, c = 1, d = 0;
-
-        if(n == 0) return a;
+        if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-
-        for(int i=3;i<=n;i++) {
+        int a = 0, b = 1, c = 1, d = 0;
+        for(int i=3; i<=n; i++) {
             d = a + b + c;
             a = b;
             b = c;
             c = d;
         }
-
         return c;
     }
 };
  
 
-
+ 
 
 
 
@@ -99,20 +96,18 @@ public:
         if(n==1 || n==2) return 1;
         
         int t0 = 0;
-		int t1 = 1;
-		int t2 = 1;
-		int t3 = t0 + t1 + t2 ;
+	int t1 = 1;
+	int t2 = 1;
+	int t3 = t0 + t1 + t2 ;
         while(n-- >2){
             t3 = t0 + t1 + t2 ,
-        	t0 = t1,
- 			t1 = t2,
- 			t2 = t3;
-		}        
+	    t0 = t1,
+ 	    t1 = t2,
+ 	    t2 = t3;
+	}        
         return t2;
     }
 };
-
-
 
 // t7 = t4 + t5 + t6
 
