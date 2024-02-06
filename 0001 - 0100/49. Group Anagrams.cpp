fc73423ba4c0415cdb514475a9321d6a780017d4
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-02-06 21:07:53 +0530

    Update 49. Group Anagrams.cpp

--- 0001 - 0100/49. Group Anagrams.cpp
+++ 0001 - 0100/49. Group Anagrams.cpp
@@ -1,4 +1,5 @@
-//https://leetcode.com/problems/group-anagrams/
+// A 1 
+// Sorting each string and using a hash table to group anagrams.
 
 class Solution {
 public:
@@ -17,3 +18,43 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+// A 2 
+// Using a character count array to represent each string and grouping based on counts.
+
+class Solution {
+public:
+    string cntChars(const string& str) {
+        string cnts(26, 0);
+        for(char ch : str) {
+            cnts[ch - 'a']++;
+        }
+        return cnts;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> ump;
+        for(string& str : strs) {
+            ump[cntChars(str)].push_back(str);
+        }
+
+        vector<vector<string>> res;
+        for(auto& it : ump) {
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+// A 3
+
