03ee969160183ff4d3c2a770883c59ddded5e986
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-02-23 08:23:18 +0530

    Create 787. Cheapest Flights Within K Stops.cpp

---
+++ 0701 -0800/787. Cheapest Flights Within K Stops.cpp
@@ -0,0 +1,35 @@
+// A 1 
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dist(n, numeric_limits<int>::max());
+        dist[src] = 0;
+
+        // Relax edges k + 1 times
+        for (int i = 0; i <= k; ++i) {
+            vector<int> temp = dist;
+            for (const auto& flight : flights) {
+                int u = flight[0];
+                int v = flight[1];
+                int w = flight[2];
+                if (dist[u] != numeric_limits<int>::max() &&
+                    dist[u] + w < temp[v]) {
+                    temp[v] = dist[u] + w;
+                }
+            }
+            dist = temp;
+        }
+
+        // Return the distance to destination if reachable, otherwise return -1
+        return dist[dst] != numeric_limits<int>::max() ? dist[dst] : -1;
+    }
+};
+
+
+
+
+
+
+// A 2 
+
