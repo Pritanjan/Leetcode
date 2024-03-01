ac1eab8813f0aa3ff42aab7f78f5259d1d4f48c8
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-03-01 09:32:25 +0530

    Update 2864. Maximum Odd Binary Number.cpp

--- 2801 - 3000/2864. Maximum Odd Binary Number.cpp
+++ 2801 - 3000/2864. Maximum Odd Binary Number.cpp
@@ -57,3 +57,18 @@ public:
 
 // T.C. --> O(N log N)
 
+
+
+
+
+
+// A 3 
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int ones = count(s.begin(), s.end(), '1');
+        return string(ones - 1, '1') + string(s.size() - ones, '0') + '1';
+    }
+};
+
