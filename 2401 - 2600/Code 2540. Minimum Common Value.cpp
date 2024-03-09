799d5b90e35f6c82bbc0efd1a265dd26dc148cf6
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-03-09 09:14:22 +0530

    Create  Code 2540. Minimum Common Value.cpp

---
+++ 2401 - 2600/Code 2540. Minimum Common Value.cpp
@@ -0,0 +1,27 @@
+// A 1
+
+class Solution {
+public:
+    int getCommon(vector<int>& a, vector<int>& b) {
+        int i = 0, j = 0;
+        while (i < a.size() && j < b.size()) {
+            if (a[i] == b[j]) {
+                return a[i];
+            } else if (a[i] < b[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+
+// A 2
+
+
