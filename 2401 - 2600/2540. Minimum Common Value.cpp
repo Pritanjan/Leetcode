dff72253fba3978577dc7724d1c859f084045185
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-03-09 11:29:56 +0530

    Update 2540. Minimum Common Value.cpp

--- 2401 - 2600/2540. Minimum Common Value.cpp
+++ 2401 - 2600/2540. Minimum Common Value.cpp
@@ -22,6 +22,32 @@ public:
 
 
 
-// A 2
+// A 2 - Using Hash Set
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        // Add the elements from nums1 to set1
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+
+        // Search for each element of nums2 in set1
+        // Return the first common element found
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                return num;
+            }
+        }
+
+        // Return -1 if there are no common elements
+        return -1;
+    }
+};
+
+
+
+
+
+
+// A 3 
 
 
