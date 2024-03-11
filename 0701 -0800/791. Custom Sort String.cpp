8cf9f7fa14b88046d816b8298a5b6fccb136937e
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-03-11 10:03:22 +0530

    Create 791. Custom Sort String.cpp

---
+++ 0701 -0800/791. Custom Sort String.cpp
@@ -0,0 +1,19 @@
+// A 1 - Using Sorting 
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        sort(s.begin(), s.end(), [&](char a, char b) {
+            return order.find(a) < order.find(b);
+        });
+        return s;
+    }
+};
+
+
+
+
+
+
+// A 2
+
