fbfa809ce93effa62d355c195dfc3d60e8c3e202
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-03-11 12:43:33 +0530

    Update 791. Custom Sort String.cpp

--- 0701 -0800/791. Custom Sort String.cpp
+++ 0701 -0800/791. Custom Sort String.cpp
@@ -17,4 +17,50 @@ public:
 
 // A 2
 
+class Solution {
+public:
+    static string str;
+    static bool compare(char char1, char char2) {
+        return (str.find(char1) < str.find(char2));
+    }
 
+    string customSortString(string order, string s) {
+        str = order;
+        sort(s.begin(), s.end(), compare);
+        return s;
+    }
+};
+string Solution::str;
+
+
+
+
+
+
+// A 3 
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> d(26);
+        for (int i = 0; i < order.size(); i++) {
+            d[order[i] - 'a'] = i;
+        }
+
+        int n = s.length();
+        vector<char> b(n);
+        for (int i = 0; i < n; i++) {
+            b[i] = s[i];
+        }
+
+        sort(b.begin(), b.end(), [&](auto a, auto b) {
+            return d[a - 'a'] < d[b - 'a'];
+        });
+
+        string o = "";
+        for (char c : b) {
+            o.push_back(c);
+        }
+        return o;
+    }
+};
