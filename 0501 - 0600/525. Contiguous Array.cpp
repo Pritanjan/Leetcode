d4edbb64f4298388258832132876e3898f3b082a
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-03-16 19:49:11 +0530

    Update 525. Contiguous Array.cpp

--- 0501 - 0600/525. Contiguous Array.cpp
+++ 0501 - 0600/525. Contiguous Array.cpp
@@ -21,6 +21,22 @@ public:
 
 
 
-// A 2 
+// A 2 - Prefis Sum
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int maxLength = 0, count = 0;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        for (int i = 0; i < nums.size(); ++i) {
+            count += (nums[i] == 0) ? -1 : 1;
+            if (mp.find(count) != mp.end()) maxLength = max(maxLength, i - mp[count]);
+            else mp[count] = i;
+        }
+        return maxLength;
+    }
+};
+
 
 
