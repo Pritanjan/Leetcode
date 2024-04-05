4b15f9e8c82293dce20693505e66d216f01eb290
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-04-05 07:29:40 +0530

    Update 1544. Make The String Great.cpp

--- 1401 - 1600/1544. Make The String Great.cpp
+++ 1401 - 1600/1544. Make The String Great.cpp
@@ -1,11 +1,10 @@
+// A 1
+
 class Solution {
 public:
     string makeGood(string s) {
         int n = s.size();
-        
-        if(s.empty())
-            return s;
-        
+        if(s.empty()) return s;        
         for(int i=0; i<n-1; i++){
             if(abs(s[i] - s[i+1]) == 32){
                 return makeGood(s.substr(0, i) + s.substr(i+2));                
@@ -18,3 +17,27 @@ public:
 
 
 
+
+// A 2 
+
+class Solution {
+public:
+    string makeGood(string s) {
+        int n = s.size();
+        for (int i = 0; i < n - 1; ) {
+            if (abs(int(s[i]) - int(s[i + 1])) == 32) {
+                // Characters are of different cases and same letter
+                s.erase(i, 2); // Remove both characters
+                if (i > 0) {
+                    // Move back one step to check if the previous character and the new character can react
+                    i--;
+                }
+                n -= 2; // Decrease the size of the string accordingly
+            } else {
+                // Characters do not react, move to the next pair
+                i++;
+            }
+        }
+        return s;
+    }
+};
