a8033686655ce18d650a4ccc8625b47de91d268c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-04-05 08:49:49 +0530

    Update 1544. Make The String Great.cpp

--- 1401 - 1600/1544. Make The String Great.cpp
+++ 1401 - 1600/1544. Make The String Great.cpp
@@ -41,3 +41,25 @@ public:
         return s;
     }
 };
+
+
+
+
+
+
+// A 3 
+
+class Solution {
+public:
+    string makeGood(string s) {
+        string res;
+        for (char c : s) {
+            if (!res.empty() && (toupper(c) == toupper(res.back())) && (c - res.back() != 0)) res.pop_back();
+            else res.push_back(c);
+        }        
+        return res;
+    }
+};
+
+
+
