042b7cb925d0f152c2cb7f5e1df2cb17b1cfe449
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-04-14 10:12:23 +0530

    Update 404. Sum of Left Leaves TREE DFS BFS BT.cpp

--- 0401 - 0500/404. Sum of Left Leaves TREE DFS BFS BT.cpp
+++ 0401 - 0500/404. Sum of Left Leaves TREE DFS BFS BT.cpp
@@ -1,19 +1,64 @@
-//https://leetcode.com/problems/sum-of-left-leaves/
+// A 1  - Recursive Approach (DFS)
 
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) return 0;
         int sum = 0;
-        if(root == NULL)
-            return NULL;
-        
-        if(root -> left && !root->left->left && !root->left->right)
-            sum += root -> left -> val;
-        if(root -> left )
-            sum += sumOfLeftLeaves(root -> left);
-        if(root -> right)
-            sum += sumOfLeftLeaves(root -> right);
-        
+        if (root->left && !root->left->left && !root->left->right) sum += root->left->val;
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
         return sum;
     }
 };
+
+
+
+
+
+
+// A 2 - Iterative Approach (DFS using Stack):
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) return 0;
+        stack<TreeNode*> stk;
+        int sum = 0;
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode* node = stk.top();
+            stk.pop();
+            if (node->left) {
+                if (!node->left->left && !node->left->right) 
+                    sum += node->left->val;
+                stk.push(node->left);
+            }
+            if (node->right)
+                stk.push(node->right);
+        }
+        return sum;
+    }
+};
+
+
+
+
+
+
+// A 3 = Recursion
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        return sumOfLeftLeavesHelper(root, false);
+    }
+
+    int sumOfLeftLeavesHelper(TreeNode* root, bool isLeft) {
+        if (!root) return 0;
+        if (!root->left && !root->right && isLeft) return root->val;
+        return sumOfLeftLeavesHelper(root->left, true) + sumOfLeftLeavesHelper(root->right, false);
+    }
+};
+
+
