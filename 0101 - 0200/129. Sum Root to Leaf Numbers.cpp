0df1428c4c8574d4d211b2e417d0e56c4dd77a58
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-04-15 16:22:11 +0530

    Update 129. Sum Root to Leaf Numbers.cpp

--- 0101 - 0200/129. Sum Root to Leaf Numbers.cpp
+++ 0101 - 0200/129. Sum Root to Leaf Numbers.cpp
@@ -1,3 +1,5 @@
+// A 1 
+
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
@@ -7,15 +9,12 @@ public:
     }
     
     void sumNumbersHelper(TreeNode* node, int currentSum, int& totalSum) {
-        if(node == NULL) return;
-        
+        if(node == NULL) return ;        
         currentSum = currentSum * 10 + node->val;
-        
         if(node->left == NULL && node->right == NULL) {
             totalSum += currentSum;
             return;
-        }
-        
+        }        
         sumNumbersHelper(node->left, currentSum, totalSum);
         sumNumbersHelper(node->right, currentSum, totalSum);
     }
@@ -24,20 +23,21 @@ public:
 
 
 
+
+
+// A 2 
+
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
         int totalSum = 0;
-        
         if (root == NULL) return totalSum;
-        
         stack<pair<TreeNode*, int>> stk;
-        stk.push({root, root->val});
+        stk.push({root, root->val});        
         
         while(!stk.empty()) {
             auto [node, currentSum] = stk.top();
-            stk.pop();
-            
+            stk.pop();    
             if(node->left == NULL && node->right == NULL) totalSum += currentSum;
             if(node->left != NULL) stk.push({node->left, currentSum * 10 + node->left->val});
             if(node->right != NULL) stk.push({node->right, currentSum * 10 + node->right->val});
@@ -51,36 +51,35 @@ public:
 
 
 
-
-
+// A 3 
 
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
         if(!root) return 0;
-
         int sum = 0;
         queue<pair<TreeNode*, int>> q;
         q.push({root, root -> val});
-
+        
         while(!q.empty()) {
             int size = q.size();
             while(size--) {
                 auto node = q.front();
                 q.pop();
-
                 if(!node.first -> left && !node.first -> right) sum += node.second;
-
                 if(node.first -> left) q.push({node.first -> left, node.second * 10 + node.first -> left -> val});
                 if(node.first -> right) q.push({node.first -> right, node.second * 10 + node.first -> right -> val});
             }
         } 
-
         return sum;
     }
 };
 
 
+
+
+
+// A 4 
 
 class Solution {
 public:
@@ -111,33 +110,32 @@ public:
 
 
 
+
+
+// A 5 
+
 class Solution {
 public:
-    void func(TreeNode *root,string &temp,vector<string> &ans)
-    {
-        if(!root->left && !root->right)
-        {
+    void func(TreeNode *root,string &temp,vector<string> &ans) {
+        if(!root->left && !root->right) {
             temp+=to_string(root->val);
             ans.push_back(temp);
             temp.pop_back();
             return;
-        }
+        }        
         temp+=to_string(root->val);
-        if(root->left)
-            func(root->left,temp,ans);
-        if(root->right)
-            func(root->right,temp,ans);
+        if(root->left)  func(root->left,temp,ans);
+        if(root->right) func(root->right,temp,ans);
         temp.pop_back();
     }
+
     int sumNumbers(TreeNode* root) {
-        if(!root)
-            return 0;
+        if(!root) return 0;
         vector<string> ans;
         string temp="";
         func(root,temp,ans);
         int finalSum=0;
-        for(auto it:ans)
-        {
+        for(auto it:ans) {
             finalSum+=stoi(it);
         }
         return finalSum;
@@ -145,39 +143,43 @@ public:
 };
 
 
+
+
+
+
+// A 6 
+
 class Solution {
 public:
-    void solve(TreeNode * root , vector<string>&ans , string temp)
-    {
-        if(root == NULL)
-        return;
+    void solve(TreeNode * root , vector<string>&ans , string temp)     {
+        if(root == NULL) return ;
         temp = temp + to_string(root->val);
-        if(root->left == NULL and root->right == NULL)
-        {
+        if(root->left == NULL and root->right == NULL) {
             ans.push_back(temp);
         }
         solve(root->left , ans , temp);
         solve(root->right , ans , temp);
-
     }
-    int sumNumbers(TreeNode* root) 
-    {
-        vector<string>ans;
+
+    int sumNumbers(TreeNode* root) {
+        vector<string> ans;
         string temp = "";
         solve(root , ans , temp );
         long long int res = 0;
-        for(int i = 0 ;i<ans.size();i++)
-        {
+        for(int i = 0 ;i<ans.size();i++) {
             cout<<ans[i]<<"  ";
             res = res  + stoi(ans[i]);
         }
-
-        return res;
-        
-        
+        return res;        
     }
 };
 
+
+
+
+
+
+// A 7 
 
 class Solution {
 public:
@@ -191,6 +193,7 @@ public:
         }
         return sum;
     }
+
     void collect(TreeNode* root, string s) {
         s += to_string(root->val);
         if(root->right != NULL && root->left != NULL) {
@@ -211,32 +214,36 @@ public:
 };
 
 
+
+
+
+
+// A 8 
+
 class Solution {
 public:
     vector<string> nums;
-    void recurAdd(string init, TreeNode* root){
-        if(root==nullptr){
-            return;
-        }
+    void recurAdd(string init, TreeNode* root) {
+        if (root == nullptr) return;
         string num = init + to_string(root->val);
-        if(root->left == nullptr && root->right == nullptr){
+        if (root->left == nullptr && root->right == nullptr) {
             nums.push_back(num);
-        }else if(root->left == nullptr){
-            recurAdd(num,root->right);
-        }else if(root->right == nullptr){
-            recurAdd(num,root->left);
-        }else{
+        } else if (root->left == nullptr) {
+            recurAdd(num, root->right);
+        } else if (root->right == nullptr) {
             recurAdd(num, root->left);
-            recurAdd(num,root->right);
+        } else {
+            recurAdd(num, root->left);
+            recurAdd(num, root->right);
         }
     }
+
     int sumNumbers(TreeNode* root) {
         int sum = 0;
-        recurAdd("",root);
-        for(auto i: nums){
-            sum+=stoi(i);
+        recurAdd("", root);
+        for (auto i : nums) {
+            sum += stoi(i);
         }
         return sum;
     }
 };
-
