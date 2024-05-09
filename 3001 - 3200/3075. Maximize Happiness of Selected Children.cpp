67bbaf4603599a9b62d26487c01f87722b1c0f37
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-05-09 09:42:18 +0530

    Update 3075. Maximize Happiness of Selected Children.cpp

--- 3001 - 3200/3075. Maximize Happiness of Selected Children.cpp
+++ 3001 - 3200/3075. Maximize Happiness of Selected Children.cpp
@@ -1,4 +1,4 @@
-// A 1
+// A 1 - Greedy Sortingg
 
 class Solution {
 public:
@@ -12,5 +12,38 @@ public:
             if(cnt >= k) break;
         }        
         return res;
+    }
+};
+
+
+
+
+
+// A2 - Max heap
+
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        // Max heap by default
+        priority_queue<int> pq; 
+
+        // Push all the happiness values into the priority queue
+        for(const auto& h: happiness)
+            pq.push(h); 
+
+        long long totalHappinessSum = 0;
+        int turns = 0;
+
+        for(int i = 0; i < k; i++) {
+            // Add the current highest value to the total happiness sum
+            totalHappinessSum += max(pq.top() - turns, 0);
+
+            // Remove the highest value after using it
+            pq.pop(); 
+
+            // Increment turns for the next iteration
+            turns++; 
+        }
+        return totalHappinessSum;
     }
 };
