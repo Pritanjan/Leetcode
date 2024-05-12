8806f47a7648911332c4126defe0d36222201c0d
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-05-12 18:30:42 +0530

    Create 2373. Largest Local Values in a Matrix.cpp

---
+++ 2201 - 2400/2373. Largest Local Values in a Matrix.cpp
@@ -0,0 +1,24 @@
+// A 1
+
+class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> res(n - 2, vector<int>(n - 2, 0));
+
+        for (int i = 1; i < n - 1; ++i) {
+            for (int j = 1; j < n - 1; ++j) {
+                int maxVal = grid[i - 1][j - 1];
+                for (int x = i - 1; x <= i + 1; ++x) {
+                    for (int y = j - 1; y <= j + 1; ++y) {
+                        maxVal = max(maxVal, grid[x][y]);
+                    }
+                }
+                res[i - 1][j - 1] = maxVal;
+            }
+        }
+        return res;
+    }
+};
+
+
