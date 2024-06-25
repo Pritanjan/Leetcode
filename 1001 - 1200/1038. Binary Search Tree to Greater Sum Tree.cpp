f47d703b773cfacd1f63a4055f991fb032aa993b
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-06-25 10:18:44 +0530

    Update 1038. Binary Search Tree to Greater Sum Tree.cpp

--- 1001 - 1200/1038. Binary Search Tree to Greater Sum Tree.cpp
+++ 1001 - 1200/1038. Binary Search Tree to Greater Sum Tree.cpp
@@ -15,3 +15,39 @@ public:
     }
 };
 
+
+
+
+
+
+// A 2: Iterative Reverse In-order Traversal
+
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+
+        while (!st.empty() or node != nullptr) {
+            while (node != nullptr) {
+                st.push(node);
+                node = node->right;
+            }
+
+            // Store the top value of stack in node and pop it.
+            node = st.top();
+            st.pop();
+
+            // Update value of node.
+            sum += node->val;
+            node->val = sum;
+
+            // Move to the left child of node.
+            node = node->left;
+        }
+        return root;
+    }
+};
+
+
