a621f602f855647a69677dbdba0813a4c17143f2
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-06-25 09:56:20 +0530

    Create 1038. Binary Search Tree to Greater Sum Tree.cpp

---
+++ 1001 - 1200/1038. Binary Search Tree to Greater Sum Tree.cpp
@@ -0,0 +1,17 @@
+// A 1
+
+class Solution {
+public:
+    int sum = 0;
+    TreeNode* bstToGst(TreeNode* root) {
+        if(root == NULL) return NULL;
+        
+        bstToGst(root -> right);
+        sum += root->val;
+        root -> val = sum;
+        
+        bstToGst(root->left);
+        return root;
+    }
+};
+
