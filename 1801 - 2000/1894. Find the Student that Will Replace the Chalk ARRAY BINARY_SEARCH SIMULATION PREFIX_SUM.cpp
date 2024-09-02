57e5bb26cf69894f85f8975812ef6a3dd4115ad5
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-09-02 09:33:20 +0530

    Update 1894. Find the Student that Will Replace the Chalk ARRAY BINARY_SEARCH SIMULATION PREFIX_SUM.cpp

--- 1801 - 2000/1894. Find the Student that Will Replace the Chalk ARRAY BINARY_SEARCH SIMULATION PREFIX_SUM.cpp
+++ 1801 - 2000/1894. Find the Student that Will Replace the Chalk ARRAY BINARY_SEARCH SIMULATION PREFIX_SUM.cpp
@@ -1,29 +1,18 @@
-//https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/
+// A 1 
 
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        
-        long sum = 0;
-        for(auto &i : chalk)
+        long long sum = 0;
+        for(int i : chalk) {
             sum += i;
-        
-        k %= sum;
-
-        for(int i=0; i<chalk.size(); i++)
-        {
-            if((k -= chalk[i]) < 0)
-                return i;
-            
-            // k = k - chalk[i];
         }
-        
-        return 0;
-        
-        
+
+        k %= sum;
+        for(int i=0; i<chalk.size(); i++) {
+            if(k < chalk[i]) return i;
+            k-= chalk[i];
+        }
+        return -1;
     }
 };
-
-
-    
-    
