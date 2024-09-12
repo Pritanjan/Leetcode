e6a0f1e77a08bfaf5d81928368bbbbf7436eb374
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-09-12 14:34:38 +0530

    Create 1684. Count the Number of Consistent Strings.cpp

---
+++ 1601 - 1800/1684. Count the Number of Consistent Strings.cpp
@@ -0,0 +1,29 @@
+// A 1
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int cnt = 0;
+        for(string & word :words){
+            bool flag = true;
+            for(int i=0; i<word.length(); i++) {
+                char currChar = word.at(i);
+                bool isFind = false;
+
+                for(int j=0; j<allowed.size(); j++) {
+                    if(allowed[j] == currChar) {
+                        isFind = true;
+                        break;
+                    }
+                }
+
+                if(!isFind) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) cnt++;
+        }
+        return cnt;
+    }
+};
