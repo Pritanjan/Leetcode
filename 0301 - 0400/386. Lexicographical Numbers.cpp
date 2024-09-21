392626c224aa98ff8ef87783a9e7dfb58c25f26c
Pritanjan <75140076+Pritanjan@users.noreply.github.com>
2024-09-21 12:21:15 +0530

    Update 386. Lexicographical Numbers.cpp

--- 0301 - 0400/386. Lexicographical Numbers.cpp
+++ 0301 - 0400/386. Lexicographical Numbers.cpp
@@ -43,3 +43,23 @@ var lexicalOrder = function(n) {
 
 
 // A 3 
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        int curr = 1;
+        for (int i = 0; i < n; i++) {
+            res.push_back(curr);
+            if (curr * 10 <= n)
+                curr *= 10;
+            else {
+                while (curr % 10 == 9 || curr + 1 > n) {
+                    curr = floor(curr / 10);
+                }
+                curr++;
+            }
+        }
+        return res;
+    }
+};
